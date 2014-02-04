f783a51116080624032022be898eca18e57e424d
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-02-04 17:00:07 +0100

    Fix FSF address

--- src/ws/mock-io-stream.c
+++ src/ws/mock-io-stream.c
@@ -16,8 +16,8 @@
  *
  * You should have received a copy of the GNU Lesser General
  * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  *
  * Author: David Zeuthen <davidz@redhat.com>
  * Author: Simon McVittie <simon.mcvittie@collabora.co.uk>

19e697806ead205dbc23c6bebb4955be59885706
Stef Walter <stefw@redhat.com>
2014-04-02 07:59:49 +0200

    daemon: Disable unreliable test of CPU usage
    This is hard to predict currently, and leads to sporadic failures
    when building in mock or elsewhere. Needs work, but lets disable
    for now.
    
    Closes #397
    
    Reviewed-by: Patrick Uiterwijk <puiterwijk@redhat.com>
    * Added todo comment

--- src/daemon/test-cgroupmonitor.c
+++ src/daemon/test-cgroupmonitor.c
@@ -325,7 +325,7 @@ test_get_samples (TestCase *tc,
   g_variant_get_child (values, 3, "d", &value);
   g_assert_cmpfloat (value, ==, -1.0);
   g_variant_get_child (values, 4, "d", &value);
-  /* Cannot reliably predict the CPU value */
+  /* TODO: Cannot reliably predict the CPU value */
   g_variant_unref (values);
 
   g_variant_unref (child);
@@ -366,7 +366,7 @@ test_new_samples (TestCase *tc,
   g_variant_get_child (values, 3, "d", &value);
   g_assert_cmpfloat (value, ==, -1.0);
   g_variant_get_child (values, 4, "d", &value);
-  g_assert_cmpfloat (value, >, 1.0);
+  /* Cannot reliably predict the CPU value */
   g_variant_unref (values);
 
   g_variant_unref (sample);

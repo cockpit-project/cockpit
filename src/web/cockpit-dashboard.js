9fbd061ba41ebc8c1be1e598a1e7ec4a5835ad2b
Marius Vollmer <mvollmer@redhat.com>
2014-04-15 11:55:46 +0300

    web: Show address in dashboard when hostname is not available
    Reviewed-by: Stef Walter <stefw@redhat.com>

--- src/web/cockpit-dashboard.js
+++ src/web/cockpit-dashboard.js
@@ -178,7 +178,7 @@ PageDashboard.prototype = {
                 var manager = client.lookup("/com/redhat/Cockpit/Manager",
                                             "com.redhat.Cockpit.Manager");
                 if (manager) {
-                    $(info_divs[0]).text(manager.PrettyHostname || manager.Hostname);
+                    $(info_divs[0]).text(manager.PrettyHostname || manager.Hostname || client.target);
                     $(info_divs[1]).text(manager.System || "--");
                     $(info_divs[2]).text(manager.OperatingSystem || "--");
                     manager.call('GetAvatarDataURL', function (error, result) {

818163d30cba9dcf1b731cdeb3ea5e24b91dff69
Marius Vollmer <mvollmer@redhat.com>
2014-05-09 11:10:41 +0300

    web: Directly talk to NetworkManager.
    Fixes #453

--- src/web/cockpit-networking.js
+++ src/web/cockpit-networking.js
@@ -20,237 +20,237 @@
 PageNetworking.prototype = {
     _init: function () {
         this.id = "networking";
-        this._interfaceProxies = {};
-    },
-
-    getInterfaceProxy: function(ifname) {
-        return this._interfaceProxies[ifname];
     },
 
     getTitle: function() {
         return C_("page-title", "Networking");
     },
 
-    _set_subtract: function (a, b) {
-        var result = {};
-        for (var k in a) {
-            if (!b[k])
-                result[k] = true;
-        }
-        return result;
-    },
-
-    _resync_network_interface_list: function () {
-        var listContainer = $("#networking_content").get(0);
-
-        var newInterfaceProxies = this.client.getInterfacesFrom("/com/redhat/Cockpit/Network/",
-                                                                "com.redhat.Cockpit.Network.Netinterface");
-        var newInterfaceProxyIndexes = {};
-
-        var ifname, i, proxy, node, parent;
-
-        for (i = 0; i < newInterfaceProxies.length; i++) {
-            proxy = newInterfaceProxies[i];
-            ifname = proxy['Name'];
-            newInterfaceProxyIndexes[ifname] = true;
-        }
-        var currentInterfaceProxyIndexes = {};
-        for (ifname in this._interfaceProxies) {
-            currentInterfaceProxyIndexes[ifname] = true;
-        }
-
-        var deletedProxyIndexes = this._set_subtract(currentInterfaceProxyIndexes, newInterfaceProxyIndexes);
-
-        for (ifname in deletedProxyIndexes) {
-            proxy = this._interfaceProxies[ifname];
-            delete this._interfaceProxies[ifname];
-            $(proxy).off("notify");
-            node = $("#networking-interface-" + ifname).get(0);
-            parent = node.parentNode;
-            parent.removeChild(node);
-        }
-
-        var addedProxyIndexes = this._set_subtract(newInterfaceProxyIndexes, currentInterfaceProxyIndexes);
-
-        var match, insertBefore, interfaces, top, header, ifaceContent;
-
-        for (ifname in addedProxyIndexes) {
-            match = $.grep(newInterfaceProxies, function (v) {
-                return v['Name'] == ifname;
-            });
-            proxy = match[0];
-            if (!proxy)
-                throw Error("no matching proxy for " + ifname);
-            this._interfaceProxies[ifname] = proxy;
-            $(proxy).on("notify", $.proxy(this._on_iface_notify, this));
-
-            insertBefore = null;
-            interfaces = $(".networking-interface").each(function (i, node) {
-                if (insertBefore === null) {
-                    var cur_ifname = node._ifname;
-                    if (cur_ifname > ifname) {
-                        insertBefore = node;
-                    }
-                }
-            });
-
-            top = document.createElement("li");
-            top.setAttribute("id", "networking-interface-" + ifname);
-            top.classList.add("networking-interface");
-            top.classList.add("list-group-item");
-            top._ifname = ifname; // expando
-
-            header = document.createElement("div");
-            header.classList.add("cockpit-network-header");
-            header.appendChild(document.createTextNode(proxy['Name']));
-            top.appendChild(header);
-            ifaceContent = document.createElement("div");
-            ifaceContent.setAttribute("id", "networking_interface_content_" + ifname);
-            top.appendChild(ifaceContent);
-
-            ifaceContent.appendChild(document.createTextNode("Loading..."));
-
-            if (insertBefore)
-                listContainer.insertBefore(top, insertBefore);
-            else
-                listContainer.appendChild(top);
-        }
-
-        this._resync_network_interface_list_content();
-    },
-
-    _ip4AddressToHTML: function (address) {
-        var addrlen = 4;
-        var p = document.createElement("span");
-        var i;
-        var text;
-        var addressString;
-
-        for (i = 0; i < addrlen; i++) {
-            addressString = address[i].toString(10);
-            text = document.createTextNode(addressString);
-            p.appendChild(text);
-            if (i < addrlen - 1)
-                    p.appendChild(document.createTextNode('.'));
-        }
-        p.appendChild(document.createTextNode('/'));
-        text = document.createTextNode(address[4]);
-        p.appendChild(text);
-        return p;
-
-    },
-
-    _ip6AddressToHTML: function (address) {
-        var addrlen = 16;
-        var p = document.createElement("span");
-        var i;
-        var text;
-        var firstByte, secondByte;
-
-        for (i = 0; i < addrlen; i += 2) {
-            firstByte = address[i].toString(16);
-            if (firstByte.length == 1)
-                firstByte = "0" + firstByte;
-            p.appendChild(document.createTextNode(firstByte));
-            secondByte = address[i+1].toString(16);
-            if (secondByte.length == 1)
-                secondByte = "0" + secondByte;
-            p.appendChild(document.createTextNode(secondByte));
-            if (i < addrlen - 2)
-                    p.appendChild(document.createTextNode(':'));
-        }
-        p.appendChild(document.createTextNode('/'));
-        text = document.createTextNode(address[address.length - 1]);
-        p.appendChild(text);
-        return p;
-    },
-
-    _resync_network_interface_list_content: function () {
-        var i, j, ifname, proxy, content, ip4Addresses, ip6Addresses;
-        var table, tr, td;
-
-        for (ifname in this._interfaceProxies) {
-            proxy = this._interfaceProxies[ifname];
-            content = $("#networking_interface_content_" + ifname).get(0);
-
-            $(content).empty();
-
-            ip4Addresses = proxy['IP4Addresses'];
-            ip6Addresses = proxy['IP6Addresses'];
-
-            table = document.createElement('table');
-            table.classList.add("cockpit-info-table");
-            content.appendChild(table);
-
-            tr = document.createElement('tr');
-            table.appendChild(tr);
-            td = document.createElement('td');
-            tr.appendChild(td);
-            td.appendChild(document.createTextNode("Hardware Address"));
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(proxy['HwAddress']));
-            tr.appendChild(td);
-
-            tr = document.createElement('tr');
-            table.appendChild(tr);
-            td = document.createElement('td');
-            tr.appendChild(td);
-            td.appendChild(document.createTextNode("IP Addresses"));
-
-            for (j = 0; j < ip4Addresses.length; j++) {
-                td = document.createElement('td');
-                tr.appendChild(td);
-                td.appendChild(this._ip4AddressToHTML(ip4Addresses[j]));
-
-                tr = document.createElement('tr');
-                table.appendChild(tr);
-                td = document.createElement('td');
-                tr.appendChild(td);
-            }
-
-            for (j = 0; j < ip6Addresses.length; j++) {
-                td = document.createElement('td');
-                tr.appendChild(td);
-                td.appendChild(this._ip6AddressToHTML(ip6Addresses[j]));
-
-                tr = document.createElement('tr');
-                table.appendChild(tr);
-                td = document.createElement('td');
-                tr.appendChild(td);
-            }
-
-            td = document.createElement('td');
-            tr.appendChild(td);
-        }
-    },
-
-    _on_iface_notify: function (event) {
-        var iface = event.target;
-        this._resync_network_interface_list_content();
-    },
-
-    _resync_if_netinterface_changed: function (event, obj) {
-        if (obj.objectPath.indexOf('/com/redhat/Cockpit/Network/') === 0)
-            this._resync_network_interface_list();
-    },
-
     enter: function () {
         this.address = cockpit_get_page_param('machine', 'server') || "localhost";
-        this.client = $cockpit.dbus(this.address);
+        this.client = $cockpit.dbus(this.address,
+                                    { 'service': "org.freedesktop.NetworkManager",
+                                      'object-paths': [ "/org/freedesktop/NetworkManager" ]
+                                    });
 
-        $(this.client).on("objectAdded.networking", $.proxy(this._resync_if_netinterface_changed, this));
-        $(this.client).on("objectRemoved.networking", $.proxy(this._resync_if_netinterface_changed, this));
-        this._resync_network_interface_list();
+        this.manager = this.client.get("/org/freedesktop/NetworkManager",
+                                       "org.freedesktop.NetworkManager");
+
+        $(this.client).on("objectAdded.networking", $.proxy(this, "trigger_update_devices"));
+        $(this.client).on("objectRemoved.networking", $.proxy(this, "trigger_update_devices"));
+        $(this.client).on("interfaceAdded.networking", $.proxy(this, "trigger_update_devices"));
+        $(this.client).on("interfaceRemoved.networking", $.proxy(this, "trigger_update_devices"));
+        $(this.client).on("propertiesChanged.networking", $.proxy(this, "trigger_update_devices"));
+        $(this.client).on("signalEmitted.networking", $.proxy(this, "handle_signal"));
+        this.update_devices();
     },
 
     show: function() {
     },
 
     leave: function() {
-        $(this.client).off(".networking");
+        $(this.manager).off(".networking");
+        this.client.off(".networking");
         this.client.release();
         this.client = null;
+    },
+
+    handle_signal: function(event, iface, signal, args) {
+        if (signal == "PropertiesChanged") {
+            /* HACK
+             *
+             * NetworkManager interfaces have their own
+             * PropertiesChanged signals, so we catch them here and
+             * tell the interfaces to update their values.
+             *
+             * Unfortunatly, o.f.NM.Device doesn't have a
+             * PropertiesChanged signal.  Instead, the specialized
+             * interfaces like o.f.NM.Device.Wired do double duty:
+             * Their PropertiesChanged signals contain change
+             * notifications for both themselves and the o.f.NM.Device
+             * properties.
+             *
+             * In order to make this work, we put all properties of
+             * o.f.NM.Device.* interfaces into the o.f.NM.Device
+             * interface, and access them there.
+             *
+             * https://bugzilla.gnome.org/show_bug.cgi?id=729826
+             */
+
+            if (iface._iface_name.startsWith("org.freedesktop.NetworkManager.Device."))
+                iface = iface._client.get(iface.getObject().objectPath,
+                                          "org.freedesktop.NetworkManager.Device");
+
+            if (iface)
+                iface.propertiesChanged(args[0]);
+        } else if (signal == "Updated") {
+            /* HACK
+             *
+             * Some versions of NetworkManager don't always send
+             * PropertyChanged notifications about the
+             * o.f.NM.Device.Ip4Config property.
+             *
+             * https://bugzilla.gnome.org/show_bug.cgi?id=729828
+             */
+            this.refresh_device_props();
+        }
+    },
+
+    refresh_device_props: function() {
+        var self = this;
+        var devices = self.manager.Devices || [];
+        devices.forEach(function (objpath) {
+            var props = self.client.get(objpath, "org.freedesktop.DBus.Properties");
+            var device = self.client.get(objpath, "org.freedesktop.NetworkManager.Device");
+            props.call('GetAll', "org.freedesktop.NetworkManager.Device",
+                       function (error, result) {
+                           if (!error) {
+                               device.propertiesChanged(result);
+                           }
+                       });
+        });
+    },
+
+    trigger_update_devices: function() {
+        if (!this.udpate_devices_pending) {
+            this.udpate_devices_pending = true;
+            setTimeout($.proxy(this, "update_devices"), 0);
+        }
+    },
+
+    update_devices: function() {
+        var self = this;
+
+        self.udpate_devices_pending = false;
+
+        var i;
+        var devices = this.manager.Devices || [];
+        var addresses;
+        var device, ip4config, ip6config;
+        var tbody;
+
+        function toDec(n) {
+            return n.toString(10);
+        }
+
+        function toHex(n) {
+            var x = n.toString(16);
+            while (x.length < 2)
+                x = '0' + x;
+            return x;
+        }
+
+        function net32_to_bytes(num) {
+            var bytes = [], i;
+            if (self.client.byteorder == "be") {
+                for (i = 3; i >= 0; i--) {
+                    bytes[i] = num & 0xFF;
+                    num = num >>> 8;
+                }
+            } else {
+                for (i = 0; i < 4; i++) {
+                    bytes[i] = num & 0xFF;
+                    num = num >>> 8;
+                }
+            }
+            return bytes;
+        }
+
+        function render_ip4_address(addr) {
+            var num = addr[0];
+            var bytes = net32_to_bytes(addr[0]);
+            var prefix = addr[1];
+            return bytes.map(toDec).join('.') + '/' + toDec(addr[1]);
+        }
+
+        function render_ip6_address(addr) {
+            var bytes = addr[0];
+            var prefix = addr[1];
+            return bytes.map(toHex).join(':') + '/' + toDec(addr[1]);
+        }
+
+        function device_state_to_text(state) {
+            switch (state) {
+                // NM_DEVICE_STATE_UNKNOWN
+            case 0: return "?";
+                // NM_DEVICE_STATE_UNMANAGED
+            case 10: return "";
+                // NM_DEVICE_STATE_UNAVAILABLE
+            case 20: return _("Not available");
+                // NM_DEVICE_STATE_DISCONNECTED
+            case 30: return _("Disconnected");
+                // NM_DEVICE_STATE_PREPARE
+            case 40: return _("Preparing");
+                // NM_DEVICE_STATE_CONFIG
+            case 50: return _("Configuring");
+                // NM_DEVICE_STATE_NEED_AUTH
+            case 60: return _("Authenticating");
+                // NM_DEVICE_STATE_IP_CONFIG
+            case 70: return _("Configuring IP");
+                // NM_DEVICE_STATE_IP_CHECK
+            case 80: return _("Checking IP");
+                // NM_DEVICE_STATE_SECONDARIES
+            case 90: return _("Waiting");
+                // NM_DEVICE_STATE_ACTIVATED
+            case 100: return _("Active");
+                // NM_DEVICE_STATE_DEACTIVATING
+            case 110: return _("Deactivating");
+                // NM_DEVICE_STATE_FAILED
+            case 120: return _("Failed");
+            default: return "";
+            }
+        }
+
+        function merge_props(iface, other_iface, props) {
+            var other = self.client.lookup(devices[i], other_iface);
+            if (other) {
+                props.forEach(function (p) {
+                    if (!iface[p])
+                        iface[p] = other[p];
+                });
+            }
+        }
+
+        tbody = $('#networking-interfaces tbody');
+        tbody.empty();
+        for (i = 0; i < devices.length; i++) {
+            device = this.client.lookup(devices[i], "org.freedesktop.NetworkManager.Device");
+
+            if (!device)
+                continue;
+
+            // Skip loopback
+            if (device.DeviceType == 14)
+                continue;
+
+            merge_props(device, "org.freedesktop.NetworkManager.Device.Wired",
+                        [ "HwAddress" ]);
+
+            addresses = [ ];
+
+            ip4config = this.client.lookup(device.Ip4Config, "org.freedesktop.NetworkManager.IP4Config");
+            if (ip4config && ip4config.Addresses) {
+                ip4config.Addresses.forEach(function (a) {
+                    addresses.push(render_ip4_address(a));
+                });
+            }
+
+            ip6config = this.client.lookup(device.Ip6Config, "org.freedesktop.NetworkManager.IP6Config");
+            if (ip6config && ip6config.Addresses) {
+                ip6config.Addresses.forEach(function (a) {
+                    addresses.push(render_ip6_address(a));
+                });
+            }
+
+            tbody.append(
+                $('<tr>').append(
+                    $('<td>').text(device.Interface),
+                    $('<td>').text(addresses.join(", ")),
+                    $('<td>').text(device.HwAddress),
+                    $('<td>').text(device_state_to_text(device.State))));
+        }
     }
+
 };
 
 function PageNetworking() {

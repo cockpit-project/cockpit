4f97fe1e0fb0eab89a1daff4d2c3d3f881031b5b
Stef Walter <stefw@redhat.com>
2014-06-17 15:48:35 +0200

    websocket: Make sure headers parse without space
    Just an additional little tweak to the test case.
    
    Closes #718
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- src/websocket/test-websocket.c
+++ src/websocket/test-websocket.c
@@ -274,6 +274,7 @@ test_parse_headers (void)
       "Header1: value3\r\n"
       "Header2:  field\r\n"
       "Head3:  Another \r\n"
+      "Host:http://cockpit-project.org\r\n"
       "\r\n"
       "BODY  ",
   };
@@ -289,6 +290,7 @@ test_parse_headers (void)
       g_assert_cmpstr (g_hash_table_lookup (headers, "header1"), ==, "value3");
       g_assert_cmpstr (g_hash_table_lookup (headers, "Header2"), ==, "field");
       g_assert_cmpstr (g_hash_table_lookup (headers, "hEAD3"), ==, "Another");
+      g_assert_cmpstr (g_hash_table_lookup (headers, "Host"), ==, "http://cockpit-project.org");
       g_assert (g_hash_table_lookup (headers, "Something else") == NULL);
       g_hash_table_unref (headers);
     }

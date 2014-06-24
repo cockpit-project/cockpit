12793410bc3b424d2f7788763db28fbefbd508c9
Marius Vollmer <mvollmer@redhat.com>
2014-06-24 09:05:47 +0300

    shell: Add cockpit.util.uuid()
    Reviewed-by: Stef Walter <stefw@redhat.com>

--- modules/shell/cockpit-util.js
+++ modules/shell/cockpit-util.js
@@ -610,4 +610,17 @@ function make_resource_cache() {
     return { get: get };
 }
 
+/* - uuid = cockpit.util.uuid()
+ *
+ * Create a new random UUID.
+ */
+
+cockpit.util.uuid = uuid;
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
 })(cockpit);

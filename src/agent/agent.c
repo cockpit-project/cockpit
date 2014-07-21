7f68e5d2a3c05a84381c87467cfa34543a55233f
Stef Walter <stefw@redhat.com>
2014-07-21 12:04:08 +0200

    agent: Make session dbus-daemon print address to custom fd
    It seems like other output may be getting mixed in with
    stdout, leading to 'Address is empty' errors.
    
    Closes #901

--- src/agent/agent.c
+++ src/agent/agent.c
@@ -24,6 +24,7 @@
 
 #include "common/cockpitlog.h"
 #include "common/cockpitpipetransport.h"
+#include "common/cockpitunixfd.h"
 
 #include <sys/prctl.h>
 #include <errno.h>
@@ -130,9 +131,10 @@ on_closed_set_flag (CockpitTransport *transport,
 }
 
 static void
-setup_dbus_daemon (gpointer unused)
+setup_dbus_daemon (gpointer addrfd)
 {
   prctl (PR_SET_PDEATHSIG, SIGTERM);
+  cockpit_unix_fd_close_all (3, GPOINTER_TO_INT (addrfd));
 }
 
 static GPid
@@ -140,36 +142,52 @@ start_dbus_daemon (void)
 {
   GError *error = NULL;
   const gchar *env;
-  GString *address;
-  gint std_output;
+  GString *address = NULL;
   gchar *line;
   gsize len;
   gssize ret;
   GPid pid = 0;
+  gchar *print_address = NULL;
+  int addrfd[2] = { -1, -1 };
+  GSpawnFlags flags;
 
   gchar *dbus_argv[] = {
       "dbus-daemon",
-      "--print-address",
+      "--print-address=X",
       "--session",
-      "--nofork",
-      "--nopidfile",
       NULL
   };
 
   /* Automatically start a DBus session if necessary */
   env = g_getenv ("DBUS_SESSION_BUS_ADDRESS");
   if (env != NULL && env[0] != '\0')
-    return 0;
+    goto out;
+
+  if (pipe (addrfd))
+    {
+      g_warning ("pipe failed to allocate fds: %m");
+      goto out;
+    }
+
+  print_address = g_strdup_printf ("--print-address=%d", addrfd[1]);
+  dbus_argv[1] = print_address;
 
   /* The DBus daemon produces useless messages on stderr mixed in */
-  g_spawn_async_with_pipes (NULL, dbus_argv, NULL,
-                            G_SPAWN_SEARCH_PATH | G_SPAWN_STDERR_TO_DEV_NULL,
-                            setup_dbus_daemon, NULL, &pid, NULL, &std_output, NULL, &error);
+  flags = G_SPAWN_LEAVE_DESCRIPTORS_OPEN | G_SPAWN_SEARCH_PATH |
+          G_SPAWN_STDERR_TO_DEV_NULL | G_SPAWN_STDOUT_TO_DEV_NULL;
+
+  g_spawn_async_with_pipes (NULL, dbus_argv, NULL, flags,
+                            setup_dbus_daemon, GINT_TO_POINTER (addrfd[1]),
+                            &pid, NULL, NULL, NULL, &error);
+
+  close (addrfd[1]);
+
   if (error != NULL)
     {
       g_warning ("couldn't start DBus session bus: %s", error->message);
       g_error_free (error);
-      return 0;
+      pid = 0;
+      goto out;
     }
 
   address = g_string_new ("");
@@ -177,14 +195,14 @@ start_dbus_daemon (void)
     {
       len = address->len;
       g_string_set_size (address, len + 256);
-      ret = read (std_output, address->str + len, 256);
+      ret = read (addrfd[0], address->str + len, 256);
       if (ret < 0)
         {
           g_string_set_size (address, len);
           if (errno != EAGAIN && errno != EINTR)
             {
               g_warning ("couldn't read address from dbus-daemon: %s", g_strerror (errno));
-              break;
+              goto out;
             }
         }
       else if (ret == 0)
@@ -198,16 +216,24 @@ start_dbus_daemon (void)
           line = strchr (address->str, '\n');
           if (line != NULL)
             {
+              g_printerr ("dbus-daemon output line: %s\n", address->str);
               *line = '\0';
               break;
             }
         }
     }
 
-  close (std_output);
-  g_setenv ("DBUS_SESSION_BUS_ADDRESS", address->str, TRUE);
-  g_string_free (address, TRUE);
+  if (address->str[0] == '\0')
+    g_warning ("dbus-daemon didn't send us a dbus address");
+  else
+    g_setenv ("DBUS_SESSION_BUS_ADDRESS", address->str, TRUE);
 
+out:
+  if (addrfd[0] >= 0)
+    close (addrfd[0]);
+  if (address)
+    g_string_free (address, TRUE);
+  g_free (print_address);
   return pid;
 }
 

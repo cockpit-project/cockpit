47ca77aa8f194087e7128324b4d447ee182645f1
Yin Zhang <yizhang@redhat.com>
2014-08-14 16:48:59 -0400

    shell: Adjust code format
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- modules/shell/cockpit-accounts.js
+++ modules/shell/cockpit-accounts.js
@@ -935,11 +935,11 @@ PageAccountSetPassword.prototype = {
             else
                 hide_error();
         } else if (behavior == "input") {
-              if ($('#account-set-password-pw2').val() !== "" &&
-                  !$('#account-set-password-pw1').val().startsWith($('#account-set-password-pw2').val()))
-                  highlight_error();
-              else
-                  hide_error();
+            if ($('#account-set-password-pw2').val() !== "" &&
+                !$('#account-set-password-pw1').val().startsWith($('#account-set-password-pw2').val()))
+                highlight_error();
+            else
+                hide_error();
 
             this.error_timeout = setTimeout(check_password_match, 2000);
             this.setTimeout = null;
@@ -958,8 +958,8 @@ PageAccountSetPassword.prototype = {
                                                  });
         } else if (PageAccountSetPassword.user_name) {
             cockpit.spawn([ "passwd", "--stdin", PageAccountSetPassword.user_name ]).
-                write($('#account-set-password-pw1').val()).
-                fail(cockpit.show_unexpected_error);
+                          write($('#account-set-password-pw1').val()).
+                          fail(cockpit.show_unexpected_error);
         }
     }
 };

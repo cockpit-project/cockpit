397829cef4254a8e35e4e3e093c94e043f44794f
Stef Walter <stefw@redhat.com>
2014-09-12 12:29:51 +0200

    test: Label the systemd SIGKILL related hack with a bug
    https://bugzilla.redhat.com/show_bug.cgi?id=1141137
    
    Closes #1165
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- test/testlib.py
+++ test/testlib.py
@@ -378,6 +378,7 @@ class MachineCase(unittest.TestCase):
         self.allow_journal_messages("Error receiving data: Connection reset by peer",
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Underlying GIOStream returned 0 bytes on an async read \\(g-io-error-quark, 0\\). Exiting.",
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Error sending message: Broken pipe \\(g-io-error-quark, 44\\). Exiting.",
+                                    # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1141137
                                     "localhost: agent program failed: Child process killed by signal 9")
 
     def check_journal_messages(self, machine=None):

e56d7d73e2ff1c132f763772b8fc89a183f3af4f
Stef Walter <stefw@redhat.com>
2014-10-20 16:18:33 +0200

    test: Add extra restart allowed journal message
    The "request timed out, closing" happens intermittently during tests.
    
    Closes #1376
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- test/testlib.py
+++ test/testlib.py
@@ -388,7 +388,8 @@ class MachineCase(unittest.TestCase):
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Underlying GIOStream returned 0 bytes on an async read \\(g-io-error-quark, 0\\). Exiting.",
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Error sending message: Broken pipe \\(g-io-error-quark, 44\\). Exiting.",
                                     # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1141137
-                                    "localhost: bridge program failed: Child process killed by signal 9")
+                                    "localhost: bridge program failed: Child process killed by signal 9",
+                                    "request timed out, closing")
 
     def check_journal_messages(self, machine=None):
         """Check for unexpected journal entries."""

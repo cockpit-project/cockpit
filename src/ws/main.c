cb75cf0f1b7c1ac44e528760d5a73468ce5d6f93
Stef Walter <stefw@redhat.com>
2014-10-22 13:44:12 +0200

    ws: Remove the cockpit-ws --uninstalled option
    While you can still run cockpit-ws from the build tree it will
    spawn cockpit-session and cockpit-bridge from the $prefix.
    
    This was already broken ... this commit just makes it official.
    It was broken for two reasons:
    
     * cockpit-session usually runs as privileged, and cockpit-ws does
       not. It is a security hole to have cockpit-ws tell cockpit-session
       which cockpit-bridge to launch.
    
     * cockpit-bridge loads packages from directories based on the prefix
       and/or $XDG_DATA_DIRS, so --uninstalled didn't work here.
    
    If someone wants to invest time in making Cockpit runnable from the
    build tree, and can do so in a secure and clean way, then I wouldn't
    be against it.

--- src/ws/main.c
+++ src/ws/main.c
@@ -41,14 +41,10 @@
 
 static gint      opt_port         = 9090;
 static gboolean  opt_no_tls       = FALSE;
-static gboolean  opt_uninstalled = FALSE;
 
 static GOptionEntry cmd_entries[] = {
   {"port", 'p', 0, G_OPTION_ARG_INT, &opt_port, "Local port to bind to (9090 if unset)", NULL},
   {"no-tls", 0, 0, G_OPTION_ARG_NONE, &opt_no_tls, "Don't use TLS", NULL},
-#ifdef WITH_DEBUG
-  {"uninstalled", 0, 0, G_OPTION_ARG_NONE, &opt_uninstalled, "Run from cockpit-ws from build directory", NULL},
-#endif
   {NULL}
 };
 
@@ -106,16 +102,7 @@ main (int argc,
       g_info ("Using certificate: %s", cert_path);
     }
 
-  if (opt_uninstalled)
-    {
-      roots = cockpit_web_server_resolve_roots (SRCDIR "/src/static", SRCDIR "/lib", NULL);
-      cockpit_ws_bridge_program = BUILDDIR "/cockpit-bridge";
-      cockpit_ws_session_program = BUILDDIR "/cockpit-session";
-    }
-  else
-    {
-      roots = cockpit_web_server_resolve_roots (DATADIR "/cockpit/static", NULL);
-    }
+  roots = cockpit_web_server_resolve_roots (DATADIR "/cockpit/static", NULL);
 
   data.auth = cockpit_auth_new ();
   data.static_roots = (const gchar **)roots;

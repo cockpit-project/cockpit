2279f589db5b7a1010ae5c5427733562ef89d2c8
Marius Vollmer <mvollmer@redhat.com>
2014-10-24 15:49:19 +0300

    shell: Test hash encoding/decoding
    Reviewed-by: Stef Walter <stefw@redhat.com>

--- pkg/shell/cockpit-main.js
+++ pkg/shell/cockpit-main.js
@@ -359,93 +359,13 @@ cockpit.go_rel_cmd = function go_rel_cmd(path, options)
     return "cockpit.go_rel(" + JSON.stringify(path) + "," + JSON.stringify(options) + ");";
 };
 
-/* Page navigation parameters
-
-   The parameters of the current page are stored in a dict with these
-   fields:
-
-   - host (string)
-
-   The address of the default host to connect to.
-
-   - path (array of strings)
-
-   The path to the page, such as [ "storage", "block", "vda" ].
-
-   - options (dict of strings to strings)
-
-   The display options of the page, such as { collapse: "all" }.
-
-   'host' and 'path' are constant for the life-time of a page, but
-   'options' might change.  A page will be notified about this in some
-   to-be-specified way.  (Legacy pages will get a leave/enter
-   sequence.)  A page can also change the options.
-
-   Guarantees: 'host' is always a non-empty string.  'path' has always
-   at least length 1.  'options' is always a dict.
-*/
-
-function encode_page_hash(params) {
-    var full_path = params.path;
-    var host = params.host;
-    if (host !== false) {
-        if (!host || host == "localhost")
-            host = "local";
-        full_path = [ host ].concat(full_path);
-    }
-
-    var res = "/" + full_path.map(encodeURIComponent).join("/");
-    var query = [];
-    for (var opt in params.options) {
-        if (params.options.hasOwnProperty(opt))
-            query.push(encodeURIComponent(opt) + "=" + encodeURIComponent(params.options[opt]));
-    }
-    if (query.length > 0)
-        res += "?" + query.join("&");
-    return "#" + res;
-}
-
-function decode_page_hash(hash) {
-    var params = { host: "localhost", path: [ "dashboard" ], options: { } };
-
-    if (hash[0] == '#')
-        hash = hash.substr(1);
-
-    var query = hash.split('?');
-
-    var path = query[0].split('/').map(decodeURIComponent);
-    if (path[0] === "")
-        path.shift();
-    if (path[path.length-1] === "")
-        path.length--;
-
-    if (path.length > 0) {
-        params.host = path.shift();
-        if (params.host === "" || params.host == "local")
-            params.host = "localhost";
-    }
-
-    if (path.length > 0)
-        params.path = path;
-
-    if (query.length > 1) {
-        var opts = query[1].split("&");
-        for (var i = 0; i < opts.length; i++) {
-            var parts = opts[i].split('=');
-            params.options[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
-        }
-    }
-
-    return params;
-}
-
 function show_hash() {
-    current_hash = encode_page_hash(current_params);
+    current_hash = cockpit.hash.encode_page_hash(current_params);
     set_window_location_hash(current_hash);
 }
 
 function go_hash(hash) {
-    go_params(decode_page_hash(hash));
+    go_params(cockpit.hash.decode_page_hash(hash));
 }
 
 cockpit.page_from_id = function page_from_id(id) {
@@ -698,7 +618,7 @@ function PageExternal(id, url, title) {
             frame = $(document.createElement("iframe"));
             frame.addClass("container-frame").
                 attr("name", id + "-" + server).
-                hide().attr("src", url + encode_page_hash(params));
+                hide().attr("src", url + cockpit.hash.encode_page_hash(params));
             self.body.append(frame);
             self.frames[server] = frame;
         }

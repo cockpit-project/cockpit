2096750f9365766c95072e70171b74816c1dc413
Stef Walter <stefw@redhat.com>
2014-11-03 20:50:28 +0100

    base: Implement base64 encoding of binary data
    This is a surprising omission from the ArrayBuffer and typed array
    work. Very odd.
    
    Implementation is a polyfill from MDN, and fixed up.
    
    https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
    
    Our implementation works with binary data in Array, Uint8Array or
    Strings.

--- pkg/base/cockpit.js
+++ pkg/base/cockpit.js
@@ -65,10 +65,87 @@ var init_callback = null;
 var default_host = null;
 var filters = [ ];
 
+var have_array_buffer = !!window.ArrayBuffer;
+
 var origin = window.location.origin;
 if (!origin) {
     origin = window.location.protocol + "//" + window.location.hostname +
         (window.location.port ? ':' + window.location.port: '');
+}
+
+function array_from_raw_string(str, constructor) {
+    var length = str.length;
+    var data = new (constructor || Array)(length);
+    for (var i = 0; i < length; i++)
+        data[i] = str.charCodeAt(i) & 0xFF;
+    return data;
+}
+
+function array_to_raw_string(data) {
+    var length = data.length, str = "";
+    for (var i = 0; i < length; i++)
+        str += String.fromCharCode(data[i]);
+    return str;
+}
+
+/*
+ * These are the polyfills from Mozilla. It's pretty nasty that
+ * these weren't in the typed array standardization.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+ */
+
+function uint6_to_b64 (x) {
+    return x < 26 ? x + 65 : x < 52 ? x + 71 : x < 62 ? x - 4 : x === 62 ? 43 : x === 63 ? 47 : 65;
+}
+
+function base64_encode(data) {
+    if (typeof data === "string")
+        return window.btoa(data);
+    /* For when the caller has chosen to use ArrayBuffer */
+    if (have_array_buffer && data instanceof window.ArrayBuffer)
+        data = new window.Uint8Array(data);
+    var length = data.length, mod3 = 2, str = "";
+    for (var uint24 = 0, i = 0; i < length; i++) {
+        mod3 = i % 3;
+        uint24 |= data[i] << (16 >>> mod3 & 24);
+        if (mod3 === 2 || length - i === 1) {
+            str += String.fromCharCode(uint6_to_b64(uint24 >>> 18 & 63),
+                                       uint6_to_b64(uint24 >>> 12 & 63),
+                                       uint6_to_b64(uint24 >>> 6 & 63),
+                                       uint6_to_b64(uint24 & 63));
+            uint24 = 0;
+        }
+    }
+
+    return str.substr(0, str.length - 2 + mod3) + (mod3 === 2 ? '' : mod3 === 1 ? '=' : '==');
+}
+
+function b64_to_uint6 (x) {
+    return x > 64 && x < 91 ? x - 65 : x > 96 && x < 123 ?
+        x - 71 : x > 47 && x < 58 ? x + 4 : x === 43 ? 62 : x === 47 ? 63 : 0;
+}
+
+function base64_decode(str, constructor) {
+    if (constructor === String)
+        return window.atob(str);
+    var ilen = str.length;
+    for (var eq = 0; eq < 3; eq++) {
+        if (str[ilen - (eq + 1)] != '=')
+            break;
+    }
+    var olen = (ilen * 3 + 1 >> 2) - eq;
+    var data = new (constructor || Array)(olen);
+    for (var mod3, mod4, uint24 = 0, oi = 0, ii = 0; ii < ilen; ii++) {
+        mod4 = ii & 3;
+        uint24 |= b64_to_uint6(str.charCodeAt(ii)) << 18 - 6 * mod4;
+        if (mod4 === 3 || ilen - ii === 1) {
+            for (mod3 = 0; mod3 < 3 && oi < olen; mod3++, oi++)
+                data[oi] = uint24 >>> (16 >>> mod3 & 24) & 255;
+            uint24 = 0;
+        }
+    }
+    return data;
 }
 
 window.addEventListener('beforeunload', function() {
@@ -631,6 +708,9 @@ function basic_scope(cockpit) {
     cockpit.channel = function channel(options) {
         return new Channel(options);
     };
+
+    cockpit.base64_encode = base64_encode;
+    cockpit.base64_decode = base64_decode;
 
     cockpit.logout = function logout(reload) {
         if (reload !== false)

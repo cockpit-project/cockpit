cbd116e0840fdb8546c14a047d791657e32f4fae
Stef Walter <stefw@redhat.com>
2014-11-11 11:51:30 +0100

    playground: Include a translated string in the playground
    Closes #1466
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

---
+++ pkg/playground/po.de.js
@@ -0,0 +1,6 @@
+define({
+    "": { "language": "de", "plural-forms": "nplurals=2; plural=(n != 1);" },
+    "Translation": [ null, "Übersetzung" ],
+    "Networking": [ null, "Vernetzung" ],
+    "reverse\u0004Translation": [ null, "gnuztesrebÜ" ],
+});

98d00bfbd35d4979d671f54fe4ff509ec824a8f9
Stef Walter <stefw@redhat.com>
2015-02-26 07:30:19 +0100

    bridge: Split out PCP stuff into cockpit-pcp process

---
+++ src/bridge/cockpitpcp.c
@@ -0,0 +1,251 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "cockpitchannel.h"
+#include "cockpitpcpmetrics.h"
+
+#include "common/cockpitjson.h"
+#include "common/cockpitlog.h"
+#include "common/cockpitpipetransport.h"
+#include "common/cockpitunixfd.h"
+
+#include <sys/prctl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+
+#include <glib-unix.h>
+
+
+/* This program is run on each managed server, with the credentials
+   of the user that is logged into the Server Console.
+*/
+
+static GHashTable *channels;
+static gboolean init_received;
+
+static void
+on_channel_closed (CockpitChannel *channel,
+                   const gchar *problem,
+                   gpointer user_data)
+{
+  g_hash_table_remove (channels, cockpit_channel_get_id (channel));
+}
+
+static void
+process_init (CockpitTransport *transport,
+              JsonObject *options)
+{
+  gint64 version;
+
+  if (!cockpit_json_get_int (options, "version", -1, &version))
+    version = -1;
+
+  if (version == 0)
+    {
+      g_debug ("received init message");
+      init_received = TRUE;
+    }
+  else
+    {
+      g_message ("unsupported version of cockpit protocol: %" G_GINT64_FORMAT, version);
+      cockpit_transport_close (transport, "protocol-error");
+    }
+}
+
+static void
+process_open (CockpitTransport *transport,
+              const gchar *channel_id,
+              JsonObject *options)
+{
+  CockpitChannel *channel;
+  const gchar *payload;
+  GType channel_type;
+
+  if (!channel_id)
+    {
+      g_warning ("Caller tried to open channel with invalid id");
+      cockpit_transport_close (transport, "protocol-error");
+    }
+  else if (g_hash_table_lookup (channels, channel_id))
+    {
+      g_warning ("Caller tried to reuse a channel that's already in use");
+      cockpit_transport_close (transport, "protocol-error");
+    }
+  else
+    {
+      if (!cockpit_json_get_string (options, "payload", NULL, &payload))
+        payload = NULL;
+
+      if (g_strcmp0 (payload, "metrics1") == 0)
+        channel_type = COCKPIT_TYPE_PCP_METRICS;
+      else
+        channel_type = COCKPIT_TYPE_CHANNEL;
+
+      channel = g_object_new (channel_type,
+                              "transport", transport,
+                              "id", channel_id,
+                              "options", options,
+                              NULL);
+
+      g_hash_table_insert (channels, g_strdup (channel_id), channel);
+      g_signal_connect (channel, "closed", G_CALLBACK (on_channel_closed), NULL);
+    }
+}
+
+static gboolean
+on_transport_control (CockpitTransport *transport,
+                      const char *command,
+                      const gchar *channel_id,
+                      JsonObject *options,
+                      GBytes *message,
+                      gpointer user_data)
+{
+  if (g_str_equal (command, "init"))
+    {
+      process_init (transport, options);
+      return TRUE;
+    }
+
+  if (!init_received)
+    {
+      g_warning ("caller did not send 'init' message first");
+      cockpit_transport_close (transport, "protocol-error");
+      return TRUE;
+    }
+
+  if (g_str_equal (command, "open"))
+    {
+      process_open (transport, channel_id, options);
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+static void
+on_closed_set_flag (CockpitTransport *transport,
+                    const gchar *problem,
+                    gpointer user_data)
+{
+  gboolean *flag = user_data;
+  *flag = TRUE;
+}
+
+static void
+send_init_command (CockpitTransport *transport)
+{
+  const gchar *response = "{ \"command\": \"init\", \"version\": 0 }";
+  GBytes *bytes = g_bytes_new_static (response, strlen (response));
+  cockpit_transport_send (transport, NULL, bytes);
+  g_bytes_unref (bytes);
+}
+
+static gboolean
+on_signal_done (gpointer data)
+{
+  gboolean *closed = data;
+  *closed = TRUE;
+  return TRUE;
+}
+
+int
+main (int argc,
+      char **argv)
+{
+  CockpitTransport *transport;
+  gboolean terminated = FALSE;
+  gboolean closed = FALSE;
+  GOptionContext *context;
+  GError *error = NULL;
+  guint sig_term;
+  int outfd;
+
+  static GOptionEntry entries[] = {
+    { NULL }
+  };
+
+  signal (SIGPIPE, SIG_IGN);
+
+  g_setenv ("GSETTINGS_BACKEND", "memory", TRUE);
+  g_setenv ("GIO_USE_PROXY_RESOLVER", "dummy", TRUE);
+  g_setenv ("GIO_USE_VFS", "local", TRUE);
+
+  context = g_option_context_new (NULL);
+  g_option_context_add_main_entries (context, entries, NULL);
+  g_option_context_set_description (context, "cockpit-pcp is run automatically inside of a Cockpit session.\n");
+
+  g_option_context_parse (context, &argc, &argv, &error);
+  g_option_context_free (context);
+
+  if (error)
+    {
+      g_printerr ("cockpit-pcp: %s\n", error->message);
+      g_error_free (error);
+      return 1;
+    }
+
+  if (isatty (1))
+    {
+      g_printerr ("cockpit-pcp: only run from cockpit-bridge\n");
+      return 2;
+    }
+
+  cockpit_set_journal_logging (!isatty (2));
+
+  /*
+   * This process talks on stdin/stdout. However lots of stuff wants to write
+   * to stdout, such as g_debug, and uses fd 1 to do that. Reroute fd 1 so that
+   * it goes to stderr, and use another fd for stdout.
+   */
+
+  outfd = dup (1);
+  if (outfd < 0 || dup2 (2, 1) < 1)
+    {
+      g_warning ("bridge couldn't redirect stdout to stderr");
+      outfd = 1;
+    }
+
+  sig_term = g_unix_signal_add (SIGTERM, on_signal_done, &terminated);
+
+  g_type_init ();
+
+  transport = cockpit_pipe_transport_new_fds ("stdio", 0, outfd);
+
+  g_signal_connect (transport, "control", G_CALLBACK (on_transport_control), NULL);
+  g_signal_connect (transport, "closed", G_CALLBACK (on_closed_set_flag), &closed);
+  send_init_command (transport);
+
+  /* Owns the channels */
+  channels = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_object_unref);
+
+  while (!closed && !terminated)
+    g_main_context_iteration (NULL, TRUE);
+
+  g_object_unref (transport);
+  g_hash_table_destroy (channels);
+
+  g_source_remove (sig_term);
+
+  return 0;
+}

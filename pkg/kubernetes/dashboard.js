e59d3c3a144457a66452f260ad026343a65ceacd
Stef Walter <stefw@redhat.com>
2015-03-02 20:54:10 +0100

    kubernetes: Bring in the rough design of the dashboard
    The basis for the angular application.
    
    We don't correctly yet fill in all the data, but this is a start.
    Watch for changes and rerender when things happen.
    
    Closes #1895
    Fixes #1687
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

---
+++ pkg/kubernetes/dashboard.js
@@ -0,0 +1,25 @@
+define([
+    "kubernetes/angular",
+    "kubernetes/app"
+], function(angular) {
+    'use strict';
+
+    return angular.module('kubernetes.dashboard', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'dashboard.html',
+                controller: 'DashboardCtrl'
+            });
+        }])
+        .controller('DashboardCtrl', [
+                '$scope',
+                'kubernetesServices',
+                'kubernetesNodes',
+                function($scope, services, nodes) {
+            $scope.services = services;
+            $scope.nodes = nodes;
+            $([services, nodes]).on("changed", function() {
+                $scope.$digest();
+            });
+        }]);
+});

61b834f87237ae1eed2a343d3e2f1e4fe0104ee1
Marius Vollmer <mvollmer@redhat.com>
2015-03-17 09:38:32 +0200

    test-avocado: Disable /bin/less in cockpit guest.
    Otherwise 'avocado run' might use it and hang while waiting for user
    input.
    
    See https://github.com/avocado-framework/avocado/issues/486

--- test-avocado/lib/guest-cockpit.sh
+++ test-avocado/lib/guest-cockpit.sh
@@ -65,3 +65,6 @@ fi
 # Audit events to the journal
 rm -f '/etc/systemd/system/multi-user.target.wants/auditd.service'
 rm -rf /var/log/audit/
+
+# HACK - https://github.com/avocado-framework/avocado/issues/486
+mv -f /bin/less /bin/less.disabled

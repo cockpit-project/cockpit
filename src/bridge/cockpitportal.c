11d978b2724c8db051d0315886b4b7838f40d546
Stef Walter <stefw@redhat.com>
2015-03-17 15:03:39 +0100

    bridge: Allow skipping init message when interactive bridge
    This makes life much easier for debugging.
    
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- src/bridge/cockpitportal.c
+++ src/bridge/cockpitportal.c
@@ -25,6 +25,7 @@
 #include "common/cockpitpipetransport.h"
 
 #include <sys/wait.h>
+#include <string.h>
 
 /**
  * CockpitPortal:
@@ -206,6 +207,7 @@ static void
 open_portal (CockpitPortal *self)
 {
   CockpitPipe *pipe;
+  const gchar *data;
 
   if (self->other)
     return;
@@ -221,8 +223,13 @@ open_portal (CockpitPortal *self)
   self->other_closed_sig = g_signal_connect (self->other, "closed", G_CALLBACK (on_other_closed), self);
   self->channels = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
 
-  if (self->last_init)
-    cockpit_transport_send (self->other, NULL, self->last_init);
+  if (!self->last_init)
+    {
+      data = "{\"command\":\"init\",\"version\":0}";
+      self->last_init = g_bytes_new_static (data, strlen (data));
+    }
+
+  cockpit_transport_send (self->other, NULL, self->last_init);
 }
 
 static gboolean

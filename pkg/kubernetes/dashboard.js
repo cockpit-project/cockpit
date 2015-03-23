71f8b8c68dd20904571fdb713f5100411cd837a0
Subin M <subin@apache.org>
2015-03-23 18:45:14 +0530

    WIP: deploy app initial code

--- pkg/kubernetes/dashboard.js
+++ pkg/kubernetes/dashboard.js
@@ -22,5 +22,51 @@ define([
             $([services, nodes]).on("changed", function() {
                 $scope.$digest();
             });
-        }]);
+            $scope.showModal = false;
+            $scope.toggleModal = function(){
+                $scope.showModal = !$scope.showModal;
+            };
+        }])
+        .directive('deployApp', function () {
+            return {
+                template: 
+                '<div class="modal fade">' +
+                  '<div class="modal-dialog">' +
+                    '<div class="modal-content">' +
+                      '<div class="modal-header">' +
+                        '<h4 class="modal-title" translatable="yes">{{ title }}</h4>' +
+                      '</div>' +
+                      '<div class="modal-body" ng-transclude></div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>',
+                restrict: 'E',
+                transclude: true,
+                replace:true,
+                scope:true,
+                link: function postLink(scope, element, attrs) {
+                    scope.title = attrs.title;
+
+                    scope.$watch(attrs.visible, function(value){
+                        if(value == true)
+                            $(element).modal('show');
+                        else
+                            $(element).modal('hide');
+                    });
+
+                    $(element).on('shown.bs.modal', function(){
+                        scope.$apply(function(){
+                            scope.$parent[attrs.visible] = true;
+                        });
+                    });
+
+                    $(element).on('hidden.bs.modal', function(){
+                        scope.$apply(function(){
+                            scope.$parent[attrs.visible] = false;
+                        });
+                    });
+                }
+            };
+      });
+
 });

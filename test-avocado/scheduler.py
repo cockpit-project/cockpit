4f7969e731c616a8e75cbeb0ca44efe6dc647a0d
Jan Scotka <jscotka@redhat.com>
2015-05-22 11:24:47 +0200

    test: Adapt to changes in avocado framework

--- test-avocado/scheduler.py
+++ test-avocado/scheduler.py
@@ -11,6 +11,9 @@ test_func("abc")
 guest_favour("abc","lib/guest-basic-redhat.sh",)
 upload("abc","../tools/cockpit.spec","/var/tmp/cockpit.spec")
 guest_favour("abc","lib/guest-cockpit-redhat.sh", "/var/tmp/cockpit.spec")
+
+upload("abc","..","/root/cockpit")
+avocado_run("abc",'compiletest-mock.sh')
 avocado_run("abc",'checklogin-basic.py')
 
 

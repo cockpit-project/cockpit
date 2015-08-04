ccaee9d211156a0cf1c766fdf7f4d1e6b2591ad4
Stef Walter <stefw@redhat.com>
2015-08-04 17:21:00 +0200

    shell: Fix test failures when ssh-keygen uses MD5
    Reviewed-by: Dominik Perpeet <dperpeet@redhat.com>

--- pkg/shell/test-list-public-keys.sh
+++ pkg/shell/test-list-public-keys.sh
@@ -5,10 +5,15 @@ cd $(dirname $0)
 
 echo "1..3"
 
+function normalize_fp()
+{
+	sed -e 's/\([0-9]\+ \)[a-z0-9:]\+ \+/\1FINGERPRINT /'
+}
+
 function test_compare()
 {
     input="mock/ssh/$1-input"
-    if /bin/sh ./ssh-list-public-keys.sh < $input | diff -U3 mock/ssh/$1-output - >&2; then
+    if /bin/sh ./ssh-list-public-keys.sh < $input | normalize_fp | diff -U3 mock/ssh/$1-output - >&2; then
         echo "ok $1 $input"
     else
         echo "not ok $1 $input"

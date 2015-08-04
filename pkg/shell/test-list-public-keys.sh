bfbf24b3ecd0f0a43750d96f370b9496932cad1e
Stef Walter <stefw@redhat.com>
2015-08-04 17:35:59 +0200

    shell: Fix for ssh-keygen output on OpenSSH 6.6
    Reviewed-by: Dominik Perpeet <dperpeet@redhat.com>

--- pkg/shell/test-list-public-keys.sh
+++ pkg/shell/test-list-public-keys.sh
@@ -5,21 +5,22 @@ cd $(dirname $0)
 
 echo "1..3"
 
-function normalize_fp()
+function normalize()
 {
-	sed -e 's/\([0-9]\+ \)[a-z0-9:]\+ \+/\1FINGERPRINT /'
+	sed -e 's/\([0-9]\+ \)[A-Za-z0-9:]\+ \+/\1FINGERPRINT /' \
+        -e 's/authorized_keys is not a public key file.//'
 }
 
 function test_compare()
 {
     input="mock/ssh/$1-input"
-    if /bin/sh ./ssh-list-public-keys.sh < $input | normalize_fp | diff -U3 mock/ssh/$1-output - >&2; then
+    if /bin/sh ./ssh-list-public-keys.sh < $input | normalize | diff -U3 mock/ssh/$1-output - >&2; then
         echo "ok $1 $input"
     else
         echo "not ok $1 $input"
     fi
 }
 
-for n in 1 2 3; do
+for n in 1 2 3 4; do
     test_compare $n
 done

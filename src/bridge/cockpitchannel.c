46c099d425e59a6cdc95aa750f1dab87258e36c4
Stef Walter <stefw@redhat.com>
2015-08-11 16:48:24 +0200

    common: Refactor code to force utf8 into its own file
    And add a test for it.
    
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- src/bridge/cockpitchannel.c
+++ src/bridge/cockpitchannel.c
@@ -23,6 +23,7 @@
 
 #include "common/cockpitjson.h"
 #include "common/cockpitloopback.h"
+#include "common/cockpitunicode.h"
 
 #include <json-glib/json-glib.h>
 
@@ -759,38 +760,6 @@ cockpit_channel_ready (CockpitChannel *self)
   g_object_unref (self);
 }
 
-static GBytes *
-check_utf8_and_force_if_necessary (GBytes *input)
-{
-  const gchar *data;
-  const gchar *end;
-  gsize length;
-  GString *string;
-
-  data = g_bytes_get_data (input, &length);
-  if (g_utf8_validate (data, length, &end))
-    return g_bytes_ref (input);
-
-  string = g_string_sized_new (length + 16);
-  do
-    {
-      /* Valid part of the string */
-      g_string_append_len (string, data, end - data);
-
-      /* Replacement character */
-      g_string_append (string, "\xef\xbf\xbd");
-
-      length -= (end - data) + 1;
-      data = end + 1;
-    }
-  while (!g_utf8_validate (data, length, &end));
-
-  if (length)
-    g_string_append_len (string, data, length);
-
-  return g_string_free_to_bytes (string);
-}
-
 /**
  * cockpit_channel_send:
  * @self: a pipe
@@ -813,7 +782,7 @@ cockpit_channel_send (CockpitChannel *self,
   if (!trust_is_utf8)
     {
       if (!self->priv->binary_ok)
-        payload = validated = check_utf8_and_force_if_necessary (payload);
+        payload = validated = cockpit_unicode_force_utf8 (payload);
     }
 
   if (self->priv->base64_encoding)

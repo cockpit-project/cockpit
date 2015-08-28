abe53bf3f0dd68c16dada7b414283f724878d97f
Stef Walter <stefw@redhat.com>
2015-08-28 08:04:44 +0200

    bridge: CockpitDBusCache batch_flush() causes use after free
    This is because other callers will cause batch_unref() on already
    freed batch memory.

--- src/bridge/cockpitdbuscache.c
+++ src/bridge/cockpitdbuscache.c
@@ -147,6 +147,7 @@ typedef struct {
 typedef struct {
   gint refs;
   guint number;
+  gboolean orphan;
 #if DEBUG_BATCHES
   GSList *debug;
 #endif
@@ -208,6 +209,15 @@ batch_dump (BatchData *batch)
 #endif /* DEBUG_BATCHES */
 
 static void
+batch_free (BatchData *batch)
+{
+#if DEBUG_BATCHES
+  g_slist_foreach (batch->debug, (GFunc)g_free, NULL);
+#endif
+  g_slice_free (BatchData, batch);
+}
+
+static void
 batch_progress (CockpitDBusCache *self)
 {
   BatchData *batch;
@@ -236,10 +246,7 @@ batch_progress (CockpitDBusCache *self)
           g_hash_table_unref (update);
         }
 
-#if DEBUG_BATCHES
-      g_slist_foreach (batch->debug, (GFunc)g_free, NULL);
-#endif
-      g_slice_free (BatchData, batch);
+      batch_free (batch);
       barrier_progress (self);
     }
 }
@@ -254,7 +261,10 @@ batch_flush (CockpitDBusCache *self)
       batch = g_queue_pop_head (self->batches);
       if (!batch)
         return;
-      g_slice_free (BatchData, batch);
+      if (batch->refs == 0)
+        batch_free (batch);
+      else
+        batch->orphan = TRUE;
     }
 }
 
@@ -304,7 +314,10 @@ _batch_unref (CockpitDBusCache *self,
                                                                  batch->refs, function, line));
 #endif
 
-  batch_progress (self);
+  if (batch->refs == 0 && batch->orphan)
+    batch_free (batch);
+  else
+    batch_progress (self);
 }
 
 #define batch_unref(self, batch) \

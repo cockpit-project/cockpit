fe7b831828b6fe1d93eafcff0bad5ada33ccd53f
Stef Walter <stefw@redhat.com>
2015-10-20 11:43:49 +0200

    common: Backport PEM certificate chain loading from GLib 2.44
    This functionality is not available in GLib 2.42 and earlier, but
    TLS servers need to be able to serve intermediate certificates
    from the .cert file.
    
    Upstream bug: https://bugzilla.gnome.org/show_bug.cgi?id=729739
    
    Fixes #2980

--- src/common/cockpitcertificate.c
+++ src/common/cockpitcertificate.c
@@ -24,8 +24,10 @@
 #include "cockpitmemory.h"
 
 #include <glib/gstdio.h>
+#include <glib/gi18n.h>
 
 #include <errno.h>
+#include <string.h>
 
 static gchar *
 generate_subject (void)
@@ -280,13 +282,325 @@ cockpit_certificate_locate (gboolean create_if_necessary,
   return cert_path;
 }
 
+/*
+ * When running on GLib earlier than 2.44 we have to do our own
+ * certificate chain loading. This can be removed once we only
+ * support GLib 2.44 and later.
+ *
+ * GIO - GLib Input, Output and Certificateing Library
+ *
+ * Copyright (C) 2010 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define PEM_CERTIFICATE_HEADER     "-----BEGIN CERTIFICATE-----"
+#define PEM_CERTIFICATE_FOOTER     "-----END CERTIFICATE-----"
+#define PEM_PKCS1_PRIVKEY_HEADER   "-----BEGIN RSA PRIVATE KEY-----"
+#define PEM_PKCS1_PRIVKEY_FOOTER   "-----END RSA PRIVATE KEY-----"
+#define PEM_PKCS8_PRIVKEY_HEADER   "-----BEGIN PRIVATE KEY-----"
+#define PEM_PKCS8_PRIVKEY_FOOTER   "-----END PRIVATE KEY-----"
+#define PEM_PKCS8_ENCRYPTED_HEADER "-----BEGIN ENCRYPTED PRIVATE KEY-----"
+#define PEM_PKCS8_ENCRYPTED_FOOTER "-----END ENCRYPTED PRIVATE KEY-----"
+
+static gchar *
+parse_private_key (const gchar *data,
+                   gsize data_len,
+                   gboolean required,
+                   GError **error)
+{
+  const gchar *start, *end, *footer;
+
+  start = g_strstr_len (data, data_len, PEM_PKCS1_PRIVKEY_HEADER);
+  if (start)
+    footer = PEM_PKCS1_PRIVKEY_FOOTER;
+  else
+    {
+      start = g_strstr_len (data, data_len, PEM_PKCS8_PRIVKEY_HEADER);
+      if (start)
+        footer = PEM_PKCS8_PRIVKEY_FOOTER;
+      else
+        {
+          start = g_strstr_len (data, data_len, PEM_PKCS8_ENCRYPTED_HEADER);
+          if (start)
+            {
+              g_set_error_literal (error, G_TLS_ERROR, G_TLS_ERROR_BAD_CERTIFICATE,
+                                   _("Cannot decrypt PEM-encoded private key"));
+            }
+          else if (required)
+            {
+              g_set_error_literal (error, G_TLS_ERROR, G_TLS_ERROR_BAD_CERTIFICATE,
+                                   _("No PEM-encoded private key found"));
+            }
+          return NULL;
+        }
+    }
+
+  end = g_strstr_len (start, data_len - (data - start), footer);
+  if (!end)
+    {
+      g_set_error_literal (error, G_TLS_ERROR, G_TLS_ERROR_BAD_CERTIFICATE,
+                           _("Could not parse PEM-encoded private key"));
+      return NULL;
+    }
+  end += strlen (footer);
+  while (*end == '\r' || *end == '\n')
+    end++;
+
+  return g_strndup (start, end - start);
+}
+
+static gchar *
+parse_next_pem_certificate (const gchar **data,
+                            const gchar *data_end,
+                            gboolean required,
+                            GError **error)
+{
+  const gchar *start, *end;
+
+  start = g_strstr_len (*data, data_end - *data, PEM_CERTIFICATE_HEADER);
+  if (!start)
+    {
+      if (required)
+        {
+          g_set_error_literal (error, G_TLS_ERROR, G_TLS_ERROR_BAD_CERTIFICATE,
+                               _("No PEM-encoded certificate found"));
+        }
+      return NULL;
+    }
+
+  end = g_strstr_len (start, data_end - start, PEM_CERTIFICATE_FOOTER);
+  if (!end)
+    {
+      g_set_error_literal (error, G_TLS_ERROR, G_TLS_ERROR_BAD_CERTIFICATE,
+                           _("Could not parse PEM-encoded certificate"));
+      return NULL;
+    }
+  end += strlen (PEM_CERTIFICATE_FOOTER);
+  while (*end == '\r' || *end == '\n')
+    end++;
+
+  *data = end;
+
+  return g_strndup (start, end - start);
+}
+
+static GSList *
+parse_and_create_certificate_list (const gchar *data,
+                                   gsize data_len,
+                                   GError **error)
+{
+  GSList *first_pem_list = NULL, *pem_list = NULL;
+  gchar *first_pem;
+  const gchar *p, *end;
+
+  p = data;
+  end = p + data_len;
+
+  /* Make sure we can load, at least, one certificate. */
+  first_pem = parse_next_pem_certificate (&p, end, TRUE, error);
+  if (!first_pem)
+    return NULL;
+
+  /* Create a list with a single element. If we load more certificates
+   * below, we will concatenate the two lists at the end. */
+  first_pem_list = g_slist_prepend (first_pem_list, first_pem);
+
+  /* If we read one certificate successfully, let's see if we can read
+   * some more. If not, we will simply return a list with the first one.
+   */
+  while (p && *p)
+    {
+      gchar *cert_pem;
+      GError *error = NULL;
+
+      cert_pem = parse_next_pem_certificate (&p, end, FALSE, &error);
+      if (error)
+        {
+          g_slist_free_full (pem_list, g_free);
+          g_error_free (error);
+          return first_pem_list;
+        }
+      else if (!cert_pem)
+        {
+          break;
+        }
+
+      pem_list = g_slist_prepend (pem_list, cert_pem);
+    }
+
+  pem_list = g_slist_concat (pem_list, first_pem_list);
+
+  return pem_list;
+}
+
+static GTlsCertificate *
+tls_certificate_new_internal (const gchar *certificate_pem,
+                              const gchar *private_key_pem,
+                              GTlsCertificate *issuer,
+                              GError **error)
+{
+  GObject *cert;
+  GTlsBackend *backend;
+
+  backend = g_tls_backend_get_default ();
+
+  cert = g_initable_new (g_tls_backend_get_certificate_type (backend),
+                         NULL, error,
+                         "certificate-pem", certificate_pem,
+                         "private-key-pem", private_key_pem,
+                         "issuer", issuer,
+                         NULL);
+
+  return G_TLS_CERTIFICATE (cert);
+}
+
+static GTlsCertificate *
+create_certificate_chain_from_list (GSList *pem_list,
+                                    const gchar  *key_pem)
+{
+  GTlsCertificate *cert = NULL, *issuer = NULL, *root = NULL;
+  GTlsCertificateFlags flags;
+  GSList *pem;
+
+  pem = pem_list;
+  while (pem)
+    {
+      const gchar *key = NULL;
+
+      /* Private key belongs only to the first certificate. */
+      if (!pem->next)
+        key = key_pem;
+
+      /* We assume that the whole file is a certificate chain, so we use
+       * each certificate as the issuer of the next one (list is in
+       * reverse order).
+       */
+      issuer = cert;
+      cert = tls_certificate_new_internal (pem->data, key, issuer, NULL);
+      if (issuer)
+        g_object_unref (issuer);
+
+      if (!cert)
+        return NULL;
+
+      /* root will point to the last certificate in the file. */
+      if (!root)
+        root = cert;
+
+      pem = g_slist_next (pem);
+    }
+
+  /* Verify that the certificates form a chain. (We don't care at this
+   * point if there are other problems with it.)
+   */
+  flags = g_tls_certificate_verify (cert, NULL, root);
+  if (flags & G_TLS_CERTIFICATE_UNKNOWN_CA)
+    {
+      /* It wasn't a chain, it's just a bunch of unrelated certs. */
+      g_clear_object (&cert);
+    }
+
+  return cert;
+}
+
+static GTlsCertificate *
+parse_and_create_certificate (const gchar *data,
+                              gsize data_len,
+                              const gchar *key_pem,
+                              GError **error)
+
+{
+  GSList *pem_list;
+  GTlsCertificate *cert;
+
+  pem_list = parse_and_create_certificate_list (data, data_len, error);
+  if (!pem_list)
+    return NULL;
+
+  /* We don't pass the error here because, if it fails, we still want to
+   * load and return the first certificate.
+   */
+  cert = create_certificate_chain_from_list (pem_list, key_pem);
+  if (!cert)
+    {
+      GSList *last = NULL;
+
+      /* Get the first certificate (which is the last one as the list is
+       * in reverse order).
+       */
+      last = g_slist_last (pem_list);
+
+      cert = tls_certificate_new_internal (last->data, key_pem, NULL, error);
+    }
+
+  g_slist_free_full (pem_list, g_free);
+
+  return cert;
+}
+
+static GTlsCertificate *
+tls_certificate_new_from_pem  (const gchar *data,
+                               gssize length,
+                               GError **error)
+{
+  GError *child_error = NULL;
+  gchar *key_pem;
+  GTlsCertificate *cert;
+
+  g_return_val_if_fail (data != NULL, NULL);
+  g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+  if (length == -1)
+    length = strlen (data);
+
+  key_pem = parse_private_key (data, length, FALSE, &child_error);
+  if (child_error != NULL)
+    {
+      g_propagate_error (error, child_error);
+      return NULL;
+    }
+
+  cert = parse_and_create_certificate (data, length, key_pem, error);
+  g_free (key_pem);
+
+  return cert;
+}
+
+static GTlsCertificate *
+tls_certificate_new_from_file (const gchar *file,
+                               GError **error)
+{
+  GTlsCertificate *cert;
+  gchar *contents;
+  gsize length;
+
+  if (!g_file_get_contents (file, &contents, &length, error))
+    return NULL;
+
+  cert = tls_certificate_new_from_pem (contents, length, error);
+  g_free (contents);
+  return cert;
+}
+
 GTlsCertificate *
 cockpit_certificate_load (const gchar *cert_path,
                           GError **error)
 {
   GTlsCertificate *cert;
 
-  cert = g_tls_certificate_new_from_file (cert_path, error);
+  cert = tls_certificate_new_from_file (cert_path, error);
   if (cert == NULL)
     g_prefix_error (error, "%s: ", cert_path);
   return cert;

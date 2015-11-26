cockpit-project/cockpit

commit 8beee96e9576c398c331843961d861f2abc69ce8

ws: Treat unparseable output as 'no-cockpit' in SSH transport
So the SSH transport may be talking to a process that's not
cockpit-bridge. How does that happen? ssh always executes
commands inside of a shell ... and bash prints its
'cockpit-bridge: not found' message on stdout (!)

So we degrade gracefully in this case, and start to treat output as
error output, and handle this as a 'no-cockpit' case.

Reviewed-by: Peter <petervo@redhat.com>

src/ws/cockpitsshtransport.c
@@ -785,9 +785,14 @@ struct _CockpitSshTransport {
   /* Input */
   GByteArray *buffer;
   gboolean drain_buffer;
+  gboolean received_frame;
   gboolean received_eof;
   gboolean received_close;
   gboolean received_exit;
+
+  /* Error Data */
+  GString *errbuf;
+  gboolean not_cockpit;
 };
 
 struct _CockpitSshTransportClass {
@@ -855,15 +860,18 @@ on_channel_data (ssh_session session,
 
   if (is_stderr)
     {
-      g_debug ("%s: received %d stderr bytes", self->logname, (int)len);
-      g_printerr ("%.*s", (int)len, (const char *)data);
+      g_debug ("%s: received %d error bytes", self->logname, (int)len);
+      g_string_append_len (self->errbuf, data, len);
     }
   else
     {
       g_debug ("%s: received %d bytes", self->logname, (int)len);
-      g_byte_array_append (self->buffer, data, len);
-      self->drain_buffer = TRUE;
+      if (self->not_cockpit)
+        g_string_append_len (self->errbuf, data, len);
+      else
+        g_byte_array_append (self->buffer, data, len);
     }
+  self->drain_buffer = TRUE;
   return len;
 }
 
@@ -955,6 +963,10 @@ on_channel_exit_status (ssh_session session,
       g_debug ("%s: received exit status %d", self->logname, exit_status);
       problem = "no-cockpit";        /* cockpit-bridge not installed */
     }
+  else if (self->not_cockpit)
+    {
+      problem = "no-cockpit";
+    }
   else if (exit_status)
     {
       g_warning ("%s: bridge exited with %d status", self->logname, exit_status);
@@ -989,6 +1001,7 @@ cockpit_ssh_transport_init (CockpitSshTransport *self)
   g_return_if_fail (self->data->session != NULL);
 
   self->buffer = g_byte_array_new ();
+  self->errbuf = g_string_sized_new (64);
   self->queue = g_queue_new ();
 
   memcpy (&self->channel_cbs, &channel_cbs, sizeof (channel_cbs));
@@ -1035,7 +1048,12 @@ close_immediately (CockpitSshTransport *self,
       cockpit_ssh_agent_close (self->agent);
 
   if (problem == NULL)
-    problem = self->problem;
+    {
+      if (self->not_cockpit)
+        problem = "no-cockpit";
+      else
+        problem = self->problem;
+    }
 
   g_object_ref (self);
 
@@ -1066,7 +1084,42 @@ close_immediately (CockpitSshTransport *self,
 }
 
 static void
-drain_buffer (CockpitSshTransport *self)
+drain_error_buffer (CockpitSshTransport *self)
+{
+  gchar *data = self->errbuf->str;
+  gsize len = self->errbuf->len;
+  gchar *pos = data;
+
+  for (;;)
+    {
+      pos = memchr (data, '\n', len);
+      if (pos == NULL)
+        break;
+
+      pos[0] = '\0';
+      pos += 1;
+
+      g_printerr ("%s", data);
+
+      len -= (pos - data);
+      data = pos;
+    }
+
+  if (self->received_eof)
+    {
+      if (len > 0)
+          g_printerr ("%s", data);
+      g_string_erase (self->errbuf, 0, -1);
+    }
+  else
+    {
+      /* Drain the remainder */
+      g_string_erase (self->errbuf, 0, pos - self->errbuf->str);
+    }
+}
+
+static void
+drain_output_buffer (CockpitSshTransport *self)
 {
   GBytes *message;
   GBytes *payload;
@@ -1094,10 +1147,27 @@ drain_buffer (CockpitSshTransport *self)
           break;
         }
 
+      /*
+       * So we may be talking to a process that's not cockpit-bridge. How does
+       * that happen? ssh always executes commands inside of a shell ... and
+       * bash prints its 'cockpit-bridge: not found' message on stdout (!)
+       *
+       * So we degrade gracefully in this case, and start to treat output as
+       * error output.
+       */
       if (data[i] != '\n')
         {
-          g_warning ("%s: incorrect protocol: received invalid length prefix", self->logname);
-          close_immediately (self, "protocol-error");
+          if (self->received_frame)
+            {
+              g_message ("%s: incorrect protocol: received invalid length prefix", self->logname);
+              close_immediately (self, "protocol-error");
+            }
+          else
+            {
+              g_string_append_len (self->errbuf, (gchar *)self->buffer->data, self->buffer->len);
+              cockpit_pipe_skip (self->buffer, self->buffer->len);
+              self->not_cockpit = TRUE;
+            }
           break;
         }
 
@@ -1109,13 +1179,29 @@ drain_buffer (CockpitSshTransport *self)
         }
 
       message = cockpit_pipe_consume (self->buffer, i + 1, size, 0);
-      payload = cockpit_transport_parse_frame (message, &channel);
+      if (self->received_frame)
+        payload = cockpit_transport_parse_frame (message, &channel);
+      else
+        payload = cockpit_transport_maybe_frame (message, &channel);
       if (payload)
         {
           g_debug ("%s: received a %d byte payload", self->logname, (int)g_bytes_get_size (payload));
+          self->received_frame = TRUE;
           cockpit_transport_emit_recv ((CockpitTransport *)self, channel, payload);
           g_bytes_unref (payload);
           g_free (channel);
+        }
+      else if (self->received_frame)
+        {
+          close_immediately (self, "protocol-error");
+          break;
+        }
+      else
+        {
+          g_string_append_len (self->errbuf, (gchar *)self->buffer->data, self->buffer->len);
+          cockpit_pipe_skip (self->buffer, self->buffer->len);
+          self->not_cockpit = TRUE;
+          break;
         }
       g_bytes_unref (message);
     }
@@ -1380,7 +1466,8 @@ cockpit_ssh_source_dispatch (GSource *source,
   if (self->drain_buffer)
     {
       self->drain_buffer = 0;
-      drain_buffer (self);
+      drain_output_buffer (self);
+      drain_error_buffer (self);
     }
 
   if (cond & (G_IO_HUP | G_IO_ERR))
@@ -1440,7 +1527,8 @@ cockpit_ssh_source_dispatch (GSource *source,
   if (self->drain_buffer)
     {
       self->drain_buffer = 0;
-      drain_buffer (self);
+      drain_output_buffer (self);
+      drain_error_buffer (self);
     }
 
   if (cond & G_IO_OUT)
@@ -1611,6 +1699,7 @@ cockpit_ssh_transport_finalize (GObject *object)
 
   g_queue_free_full (self->queue, (GDestroyNotify)g_bytes_unref);
   g_byte_array_free (self->buffer, TRUE);
+  g_string_free (self->errbuf, TRUE);
 
   g_assert (self->io == NULL);
 

src/ws/cockpitchannelresponse.c
@@ -184,6 +184,7 @@ cockpit_channel_response_close (CockpitChannelResponse *chesp,
           cockpit_web_response_error (chesp->response, 404, NULL, NULL);
         }
       else if (g_str_equal (problem, "no-host") ||
+               g_str_equal (problem, "no-cockpit") ||
                g_str_equal (problem, "no-forwarding") ||
                g_str_equal (problem, "unknown-hostkey") ||
                g_str_equal (problem, "authentication-failed"))

src/ws/test-sshtransport.c
@@ -746,14 +746,19 @@ test_expect_empty_key (TestCase *tc,
   g_free (problem);
 }
 
-
+/* The output from this will go to stderr */
 static const TestFixture fixture_bad_command = {
-  .ssh_command = "/nonexistant 2> /dev/null",
+  .ssh_command = "/nonexistant",
+};
+
+/* Yes this makes a difference with bash, output goes to stdout */
+static const TestFixture fixture_command_not_found = {
+  .ssh_command = "nonexistant-command",
 };
 
 static void
-test_bad_command (TestCase *tc,
-                  gconstpointer data)
+test_no_cockpit (TestCase *tc,
+                 gconstpointer data)
 {
   gchar *problem = NULL;
 
@@ -930,7 +935,9 @@ main (int argc,
 #endif
 #endif
   g_test_add ("/ssh-transport/bad-command", TestCase, &fixture_bad_command,
-              setup_transport, test_bad_command, teardown);
+              setup_transport, test_no_cockpit, teardown);
+  g_test_add ("/ssh-transport/command-not-found", TestCase, &fixture_command_not_found,
+              setup_transport, test_no_cockpit, teardown);
   g_test_add ("/ssh-transport/close-while-connecting", TestCase, &fixture_cat,
               setup_transport, test_close_while_connecting, teardown);
   g_test_add_func ("/ssh-transport/cannot-connect", test_cannot_connect);

test/check-multi-machine
@@ -429,7 +429,6 @@ class TestMultiMachine(MachineCase):
         self.allow_journal_messages('.*: couldn\'t connect: Connection refused',
                                     '.* failed to retrieve resource: hostkey-changed',
                                     '.* host key for server has changed to: .*',
-                                    '.*: incorrect protocol: received invalid length prefix',
                                     '.*: received truncated .*')
 
 if __name__ == '__main__':

commit 036d78b189240c57ad5f7b8272f18f78a9bf2e09

ws: Drain errbuf correctly when no lines are found
This is a regression introduced in 8beee96e9576c398c331843961d861f2abc69ce8
Fails on certain architectures with a precondition fail in
g_string_erase()

Closes #3243

src/ws/cockpitsshtransport.c
@@ -1110,11 +1110,12 @@ drain_error_buffer (CockpitSshTransport *self)
       if (len > 0)
           g_printerr ("%s", data);
       g_string_erase (self->errbuf, 0, -1);
+      pos = NULL;
     }
   else
     {
-      /* Drain the remainder */
-      g_string_erase (self->errbuf, 0, pos - self->errbuf->str);
+      /* Drain the stuff that was consumed */
+      g_string_erase (self->errbuf, 0, self->errbuf->len - len);
     }
 }
 
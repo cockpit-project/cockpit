c42abe48c97f2e065a319e9f7ac84f5e36ca58da
Stef Walter <stefw@redhat.com>
2015-11-30 21:10:56 +0100

    kubernetes: Remove use of jQuery scoped in the kubernetes code
    This isn't necessary

--- pkg/base1/jquery.js
+++ pkg/base1/jquery.js
@@ -15852,34 +15852,6 @@ API.txt for details.
 
 }(jQuery));
 
-(function(jq) {
-    "use strict";
-    jq.scoped = function scoped(/* ... */) {
-        var scope;
-
-        function init(/* ... */) {
-            return jq.fn.init.apply(this, arguments);
-        }
-
-        function jQueryScoped(selector, context) {
-            return new init(selector, context || scope);
-        }
-
-        jq.extend(jQueryScoped, jq);
-
-        var prototypes = [ { }, jq.fn ];
-        prototypes.push.apply(prototypes, Array.prototype.slice.call(arguments, 1));
-        jQueryScoped.fn = jQueryScoped.prototype = jq.extend.apply(jq, prototypes);
-        jQueryScoped.fn.constructor = jQueryScoped.prototype.constructor = jQueryScoped;
-        init.prototype = jQueryScoped.fn;
-
-        scope = jQueryScoped(arguments[0]);
-        scope.constructor = jQueryScoped;
-
-        return jQueryScoped;
-    };
-}(jQuery));
-
 /*
 A simple jQuery function that can add listeners on attribute change.
 http://meetselva.github.io/attrchange/

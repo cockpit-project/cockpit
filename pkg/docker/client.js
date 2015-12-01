f78a08da5fd32341f22331424ff956fbff6fe0e7
Dominik Perpeet <dperpeet@redhat.com>
2015-12-01 09:22:44 +0100

    docker: replace 'me' with 'self' in js
    Using 'me' is inconsistent with the rest of cockpit.
    
    Fixes #517
    Closes #3259
    Reviewed-by: Marius Vollmer <marius.vollmer@redhat.com>

--- pkg/docker/client.js
+++ pkg/docker/client.js
@@ -30,7 +30,7 @@ define([
      */
 
     function DockerClient() {
-        var me = this;
+        var self = this;
         var events;
         var watch;
         var http;
@@ -43,7 +43,7 @@ define([
             if (!later) {
                 later = window.setTimeout(function() {
                     later = null;
-                    $(me).trigger("event");
+                    $(self).trigger("event");
                 }, 300);
             }
         }
@@ -117,13 +117,13 @@ define([
         }
 
         function remove_container(id) {
-            var container = me.containers[id];
+            var container = self.containers[id];
             if (container) {
                 var name = container_to_name(container);
                 if (name && containers_by_name[name] == id)
                     delete containers_by_name[name];
-                delete me.containers[id];
-                $(me).trigger("container", [id, undefined]);
+                delete self.containers[id];
+                $(self).trigger("container", [id, undefined]);
             }
         }
 
@@ -152,7 +152,7 @@ define([
                             done(function(data) {
                                 var container = JSON.parse(data);
                                 populate_container(id, container);
-                                me.containers[id] = container;
+                                self.containers[id] = container;
                                 usage_samples[id] = [ usage_grid.add(usage_metrics_channel,
                                                                      [ "cgroup.memory.usage", container.CGroup ]),
                                                       usage_grid.add(usage_metrics_channel,
@@ -162,12 +162,12 @@ define([
                                                       usage_grid.add(usage_metrics_channel,
                                                                      [ "cgroup.cpu.shares", container.CGroup ])
                                                     ];
-                                $(me).trigger("container", [id, container]);
+                                $(self).trigger("container", [id, container]);
                             });
                     });
 
                     var removed = [];
-                    $.each(me.containers, function(id) {
+                    $.each(self.containers, function(id) {
                         if (!seen[id])
                             removed.push(id);
                     });
@@ -180,13 +180,13 @@ define([
                     if (connected.state() == "pending")
                         connected.reject(ex);
                     got_failure = true;
-                    $(me).trigger("failure", [ex]);
+                    $(self).trigger("failure", [ex]);
                 });
         }
 
         function handle_usage_samples() {
             for (var id in usage_samples) {
-                var container = me.containers[id];
+                var container = self.containers[id];
                 if (!container)
                     continue;
                 var samples = usage_samples[id];
@@ -207,7 +207,7 @@ define([
                     container.MemoryLimit = limit;
                     container.CpuUsage = cpu;
                     container.CpuPriority = priority;
-                    $(me).trigger("container", [id, container]);
+                    $(self).trigger("container", [id, container]);
                 }
             }
         }
@@ -227,9 +227,9 @@ define([
         }
 
         function remove_image(id) {
-            if (me.images[id]) {
-                delete me.images[id];
-                $(me).trigger("image", [id, undefined]);
+            if (self.images[id]) {
+                delete self.images[id];
+                $(self).trigger("image", [id, undefined]);
             }
         }
 
@@ -256,13 +256,13 @@ define([
                             done(function(data) {
                                 var image = JSON.parse(data);
                                 populate_image(id, image);
-                                me.images[id] = image;
-                                $(me).trigger("image", [id, image]);
+                                self.images[id] = image;
+                                $(self).trigger("image", [id, image]);
                             });
                     });
 
                     var removed = [];
-                    $.each(me.images, function(id) {
+                    $.each(self.images, function(id) {
                         if (!seen[id])
                             removed.push(id);
                     });
@@ -275,11 +275,11 @@ define([
                     if (connected.state() == "pending")
                         connected.reject(ex);
                     got_failure = true;
-                    $(me).trigger("failure", [ex]);
+                    $(self).trigger("failure", [ex]);
                 });
         }
 
-        $(me).on("event", function() {
+        $(self).on("event", function() {
             fetch_containers();
             fetch_images();
         });
@@ -305,11 +305,11 @@ define([
                         console.warn("monitor for docker directory failed: " + options.problem);
                 });
 
-                $(me).triggerHandler("event");
+                $(self).triggerHandler("event");
             }).fail(function(err) {
                 if (err != "not-found")
                     console.warn("monitor for docker directory failed: " + err);
-                $(me).triggerHandler("event");
+                $(self).triggerHandler("event");
             });
 
             usage_metrics_channel = cockpit.metrics(1000,
@@ -365,25 +365,25 @@ define([
         }
 
         function trigger_id(id) {
-            if (id in me.containers)
-                $(me).trigger("container", [id, me.containers[id]]);
-            else if (id in me.images)
-                $(me).trigger("image", [id, me.images[id]]);
+            if (id in self.containers)
+                $(self).trigger("container", [id, self.containers[id]]);
+            else if (id in self.images)
+                $(self).trigger("image", [id, self.images[id]]);
         }
 
         function waiting(id, yes) {
-            if (id in me.waiting) {
-                me.waiting[id]++;
+            if (id in self.waiting) {
+                self.waiting[id]++;
             } else {
-                me.waiting[id] = 1;
+                self.waiting[id] = 1;
                 trigger_id(id);
             }
         }
 
         function not_waiting(id) {
-            me.waiting[id]--;
-            if (me.waiting[id] === 0) {
-                delete me.waiting[id];
+            self.waiting[id]--;
+            if (self.waiting[id] === 0) {
+                delete self.waiting[id];
                 trigger_id(id);
             }
         }

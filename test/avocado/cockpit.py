a78587bd6d57c4624d909de19992ea9c694fada0
Jan Scotka <jscotka@redhat.com>
2015-12-07 14:48:25 +0100

    test: Cleanup avocado tests

--- test/avocado/cockpit.py
+++ test/avocado/cockpit.py
@@ -17,25 +17,28 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
-import os, re, subprocess, shutil, imp
+import os
+import re
+import subprocess
+import shutil
+import imp
 
-from avocado import Test
 from avocado.utils import process
 from testlib import Browser
 
-__all__ = [ 'Test' ]
 
-class Test(Test):
-    def __init__(self, **args):
-        test.Test.__init__(self, **args)
-        self.cleanup_funcs = [ ]
-        self.environment = self.params
+class Cockpit():
+
+    def __init__(self,):
+        self.cleanup_funcs = []
+        self.setUp()
 
     def atcleanup(self, func):
         self.cleanup_funcs.append(func)
 
-    def run_shell_command(self, cmd, cleanup_cmd):
-        self.atcleanup(lambda: process.run(cleanup_cmd, shell=True))
+    def run_shell_command(self, cmd, cleanup_cmd=None):
+        if cleanup_cmd:
+            self.atcleanup(lambda: process.run(cleanup_cmd, shell=True))
         process.run(cmd, shell=True)
 
     def replace_file(self, file, content):
@@ -44,29 +47,27 @@ class Test(Test):
             os.remove(file + ".cockpitsave")
         shutil.copyfile(file, file + ".cockpitsave")
         self.atcleanup(restore)
-        with open(file, 'w') as f: f.write(content)
+        with open(file, 'w') as f:
+            f.write(content)
 
     def setUp(self):
-        state = self.get_state()
-        self.label = re.sub('.py$', '', os.path.basename(state['name']))
+        #        state = self.get_state()
+        self.label = ("avocado")
         self.browser = Browser("localhost", self.label)
         self.journal_start = re.sub('.*cursor: ', '',
                                     subprocess.check_output("journalctl --show-cursor -n0 -o cat || true", shell=True))
 
-    def test(self):
-        #process.run("systemctl start cockpit.socket", shell=True)
-        #process.run("sleep 5", shell=True)
-        self.phase()
-        #self.check_journal_messages()
-
     def tearDown(self):
-        state = self.get_state()
+        pass
+        # self.check_journal_messages()
+        for foo in self.cleanup_funcs:
+            foo()
 
         # Make a final screenshot and save the journal for the test run.
         #
         # TODO: Do this only when the test has failed.
         #
-        #self.browser.snapshot("DONE")
+        # self.browser.snapshot("DONE")
         #process.run("journalctl >'%s.journal' -c'%s'" % (self.label, self.journal_start), shell=True)
 
         # If this is a remote run, copy all screenshots and journals
@@ -79,7 +80,7 @@ class Test(Test):
         #
         # TODO: Do this only once after running all tests.
         #
-        #if not state['job_logdir'].startswith("/tmp/"):
+        # if not state['job_logdir'].startswith("/tmp/"):
         #    process.run("cp -v *.png *.journal '%s'" %  state['job_logdir'],
         #                shell=True, ignore_status=True)
 
@@ -102,7 +103,7 @@ class Test(Test):
         # Will go away with glib 2.43.2
         ".*: couldn't write web output: Error sending data: Connection reset by peer",
 
-        ## Bugs
+        # Bugs
 
         # https://bugs.freedesktop.org/show_bug.cgi?id=70540
         ".*ActUserManager: user .* has no username.*",
@@ -136,7 +137,8 @@ class Test(Test):
         self.allow_journal_messages("Error receiving data: Connection reset by peer",
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Underlying GIOStream returned 0 bytes on an async read \\(g-io-error-quark, 0\\). Exiting.",
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Error sending message: Broken pipe \\(g-io-error-quark, 44\\). Exiting.",
-                                    # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1141137
+                                    # HACK:
+                                    # https://bugzilla.redhat.com/show_bug.cgi?id=1141137
                                     "localhost: bridge program failed: Child process killed by signal 9")
 
     def journal_messages(self, syslog_ids, log_level):
@@ -147,35 +149,38 @@ class Test(Test):
         # a dying process, so we filter by the untrusted but reliable
         # SYSLOG_IDENTIFIER instead
 
-        matches = " ".join(map(lambda id: "SYSLOG_IDENTIFIER=" + id, syslog_ids))
+        matches = " ".join(
+            map(lambda id: "SYSLOG_IDENTIFIER=" + id, syslog_ids))
 
         # Some versions of journalctl terminate unsuccessfully when
         # the output is empty.  We work around this by ignoring the
         # exit status and including error messages from journalctl
         # itself in the returned messages.
 
-        cmd = "journalctl 2>&1 -c'%s' -o cat -p %d %s" % (self.journal_start, log_level, matches)
+        cmd = "journalctl 2>&1 -c'%s' -o cat -p %d %s" % (
+            self.journal_start, log_level, matches)
         out = process.run(cmd, shell=True, ignore_status=True)
         messages = out.stdout.splitlines()
         if len(messages) == 1 and "Cannot assign requested address" in messages[0]:
             # No messages
-            return [ ]
+            return []
         else:
             return messages
 
     def audit_messages(self, type_pref):
-        cmd = "journalctl -c'%s' -o cat SYSLOG_IDENTIFIER=kernel 2>&1 | grep 'type=%s.*audit' || true" % (self.journal_start, type_pref)
+        cmd = "journalctl -c'%s' -o cat SYSLOG_IDENTIFIER=kernel 2>&1 | grep 'type=%s.*audit' || true" % (
+            self.journal_start, type_pref)
         out = process.run(cmd, shell=True)
         messages = out.stdout.splitlines()
         if len(messages) == 1 and "Cannot assign requested address" in messages[0]:
-            messages = [ ]
+            messages = []
         return messages
 
     def check_journal_messages(self):
         """Check for unexpected journal entries."""
-        syslog_ids = [ "cockpit-wrapper", "cockpit-ws", "cockpit-session" ]
+        syslog_ids = ["cockpit-wrapper", "cockpit-ws", "cockpit-session"]
         messages = self.journal_messages(syslog_ids, 5)
-        messages += self.audit_messages("14") # 14xx is selinux
+        messages += self.audit_messages("14")  # 14xx is selinux
         all_found = True
         for m in messages:
             found = False
@@ -185,6 +190,7 @@ class Test(Test):
                     found = True
                     break
             if not found:
-                self.log.info("Unexpected journal message '%s'" % m)
+                print "Unexpected journal message '%s'" % m
                 all_found = False
-        self.assertTrue(all_found, msg="There were unexpected journal messages")
+        self.assertTrue(
+            all_found, msg="There were unexpected journal messages")

0fd6a4d28fe2b78da1e4ebaa81ed07995c0afcdc
Jan Scotka <jscotka@redhat.com>
2015-12-14 14:59:30 +0100

    test: Fix avocado tests
    For selenium tests, wait for the browser nodes to become available
    on the selenium grid hub console page.
    
    Make avocado tests more verbose to help debugging.

--- test/guest/selenium_start.sh
+++ test/guest/selenium_start.sh
@@ -18,11 +18,35 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA.
 
-set -ex
+set -e
+TICKS=120
+
+function wait_curl(){
+    LINK=$1
+    GREP_CMD=$2
+    FOUND=""
+    FULLLINK="http://localhost:4444$LINK"
+    for foo in `seq $TICKS`; do
+        if curl -s --connect-timeout 1 $FULLLINK | grep "$GREP_CMD" >/dev/null; then
+            echo "$FULLLINK ('$GREP_CMD' available on page)" >&2
+            FOUND="yes"
+            break
+        else
+            sleep 0.5
+        fi
+    done
+    if [ -z "$FOUND" ]; then
+        echo "ERROR: $FULLLINK ('$GREP_CMD' not available)" >&2
+        return 1
+    fi
+}
 
 # Make sure docker is up and running
 systemctl start docker
 
-docker run -d -p 4444:4444 --name selenium-hub selenium/hub:2.48.2
-docker run -d --link selenium-hub:hub selenium/node-chrome:2.48.2
-docker run -d --link selenium-hub:hub selenium/node-firefox:2.48.2
+docker run  -d -p 4444:4444 --name selenium-hub selenium/hub:2.48.2 2> >(grep -v 'Usage of loopback devices' >&2)
+wait_curl /grid/console "Grid Console"
+docker run -d --link selenium-hub:hub selenium/node-chrome:2.48.2 2> >(grep -v 'Usage of loopback devices' >&2)
+wait_curl /grid/console "googlechrome"
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.48.2 2> >(grep -v 'Usage of loopback devices' >&2)
+wait_curl /grid/console "firefox"

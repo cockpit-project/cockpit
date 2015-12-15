2e2e983593c6e15017b7cb99f496863191f4e20c
Jan Scotka <jscotka@redhat.com>
2015-12-15 13:51:13 +0100

    test: added out to stderr of command output and retun 1 in case not found

--- test/guest/selenium_start.sh
+++ test/guest/selenium_start.sh
@@ -24,15 +24,21 @@ TICKS=120
 function wait_curl(){
     LINK=$1
     GREP_CMD=$2
+    FOUND=""
     FULLLINK="http://localhost:4444$LINK"
     for foo in `seq $TICKS`; do
         if curl -s --connect-timeout 1 $FULLLINK | grep "$GREP_CMD" >/dev/null; then
-            echo "$FULLLINK ('$GREP_CMD' available on page)"
+            echo "$FULLLINK ('$GREP_CMD' available on page)" >&2
+            FOUND="yes"
             break
         else
             sleep 0.5
         fi
     done
+    if [ -z "$FOUND" ]; then
+        echo "ERROR: $FULLLINK ('$GREP_CMD' not available)" >&2
+        return 1
+    fi
 }
 
 # Make sure docker is up and running

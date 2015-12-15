7380690cd2d0a30d99eef4486323e6a00d4f4235
Stef Walter <stefw@redhat.com>
2015-12-15 17:42:09 +0100

    tuned: Fix up the initial prototype
    Closes #3386
    Closes #3279

--- pkg/tuned/tuned.js
+++ pkg/tuned/tuned.js
@@ -17,16 +17,14 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* global jQuery   */
-
-define([
+require([
     "jquery",
-    "base1/bootstrap-select",
     "base1/cockpit",
+    "system/service",
     "translated!base1/po",
-    "base1/mustache",
-    "base1/patterns"
-], function($, cockpit, po, Mustache) {
+    "base1/patterns",
+    "base1/bootstrap-select",
+], function($, cockpit, service, po) {
     "use strict";
 
     cockpit.locale(po);
@@ -38,39 +36,37 @@ define([
             console.debug.apply(console, arguments);
     }
 
-    /*
-     * gather and display information regarding tuned profiles
-     *
-     * update_info: if true, trigger an immediate update of the information
-     */
-    function tuned(update_info) {
-        var service = cockpit.dbus('com.redhat.tuned');
-        var profile;
+    var tuned = cockpit.dbus('com.redhat.tuned');
 
-        /* we trigger status update via dbus
-         * if we don't get a timely reply, tuned is probably not running
-         */
-        var update_timeout;
+    /* The current profile */
+    var profile = null;
+    var failure = null;
 
-        /* The promise for the last operation */
-        var last_promise;
+    function update() {
+        $("body").show();
+        $("div.curtains").toggle(!profile);
+        $("div.panel").toggle(!!profile);
+        $("div.curtains .spinner").toggle(!failure);
+        $("div.curtains i").toggle(!!failure);
+        $("#tuned-profile-button").text(profile || "");
 
-        /* Tuned doesn't provide GetAll(), so we can't introspect */
-
-        //TODO: implement fail() and other bits to make this nicer
-        function wrapper(method, args, cb) {
-            if (args === null)
-                args = []
-            var call = service.call(
-                '/Tuned',
-                'com.redhat.tuned.control',
-                method,
-                args).
-                done(function(result) { cb(result);});
+        if (!failure) {
+            $("div.curtains h1").text(_("Connecting to tuned..."));
+            $("div.curtains p").text("");
+            return;
         }
 
-        function get_profiles() {
-            wrapper('profiles', [], function(result) {
+        var notfound = failure.problem === "not-found";
+        $("div.curtains h1").text(_("Failed to connect to tuned"));
+        $("div.curtains p").text(notfound ? "" : cockpit.message(failure));
+        $("div.curtains button").toggle(notfound);
+    }
+
+    /* Tuned doesn't provide GetAll(), so we can't introspect */
+
+    function get_profiles() {
+        tuned.call('/Tuned', 'com.redhat.tuned.control', 'profiles', [])
+            .done(function(result) {
                 var profiles = result.toString().split(',');
                 $('#change-tuned').empty();
                 for (var i = 0; i < profiles.length; i++) {
@@ -81,93 +77,78 @@ define([
                     }));
                 }
                 $('.selectpicker').selectpicker('refresh');
+            })
+            .fail(function(ex) {
+                $("#tuned-change-profile").dialog("failure", ex);
+                console.warn(ex);
             });
-        }
-
-        function get_profile() {
-            var call = service.call(
-                '/Tuned',
-                'com.redhat.tuned.control',
-                'active_profile',
-                []).
-                    done(function(result) {
-                        update_timeout = null,
-                        $('#tuned-profile-button').text(result);
-                        profile = result[0];
-                        $('#tuned-system-status').show();
-                    });
-            update_timeout = window.setTimeout(status_update_failed, 10000);
-        }
-
-        function status_update_failed() {
-            if (update_timeout !== null) {
-                $('#tuned-not-found').show();
-            }
-            update_timeout = null;
-        }
-
-        function remove_notifications() {
-            $('div.container-fluid.alert').remove();
-        }
-
-        $('#tuned-start').on('click', function() {
-
-            var proc = cockpit.spawn(["systemctl", "restart", "tuned.service"]).
-                    done(function() {});
-        });
-
-        $('#tuned-profile-button').on('click', function () {
-            get_profiles();
-        });
-
-        $('#tuned-recommend-button').on('click', function () {
-            wrapper('recommend_profile', [], function(result) {
-                $('#recommended-profile').text(result);
-            });
-            $('#recommend').show();
-        });
-
-        $('#tuned-change-button').on('click', function() {
-            wrapper('switch_profile', [$('#change-tuned').val()], function cb(result) {
-                console.warn(result[0]);
-                if (!result[0][0]) {
-                    $('#tuned-change-profile').dialog('failure', new Error("Problem applying tuned profile"));
-                    //TODO: log errors
-                }
-                $('#tuned-change-profile').modal('hide');
-            });
-        });
-
-        if (update_info) {
-            get_profile();
-        }
-
-        return {
-            'get_profiles': get_profiles,
-        };
     }
 
-    tuned.register = function() {
-        $('#subscriptions-register-dialog').modal('show');
-    };
+    function get_profile() {
+        tuned.call('/Tuned', 'com.redhat.tuned.control', 'active_profile', [])
+            .done(function(result) {
+                profile = result[0];
+                update();
+            })
+            .fail(function(ex) {
+                failure = ex;
+                update();
+            });
+    }
 
-    tuned.init = function(retrieve_current) {
-        if (retrieve_current === undefined)
-            retrieve_current = true;
+    $('#tuned-start').on('click', function() {
+        failure = null;
+        update();
+        var proxy = service.proxy("tuned");
+        proxy.start()
+            .done(function() {
+                get_profile();
+            })
+            .fail(function(ex) {
+                failure = ex;
+                update();
+            })
+            .always(function() {
+                proxy.close();
+                proxy = null;
+            });
+    });
 
-        var profile_selector = $('#subscription-register-url');
-        profile_selector.on('change', function() {
-            if (url_selector.val() === 'Default') {
-                custom_url.hide();
-            } else {
-                custom_url.show();
-                custom_url.focus();
-                custom_url.select();
-            }
-        });
-        profile_selector.selectpicker('refresh');
-        tuned.profile = tuned(retrieve_current);
-    };
+    $('#tuned-profile-button').on('click', function () {
+        get_profiles();
+    });
 
-    return tuned;
+    $('#tuned-recommend-button').on('click', function () {
+        tuned.call('/Tuned', 'com.redhat.tuned.control', 'recommend_profile', [])
+            .done(function(result) {
+                $('#recommend').show();
+                $('#recommended-profile').text(result);
+            })
+            .fail(function(ex) {
+                $("#tuned-change-profile").dialog("failure", ex);
+            });
+    });
+
+    $('#tuned-change-button').on('click', function() {
+        var args = [$('#change-tuned').val()];
+        var dlg = $("#tuned-change-profile");
+        var promise = tuned.call('/Tuned', 'com.redhat.tuned.control', 'switch_profile', args)
+            .done(function(result) {
+                console.log(result[0]);
+                if (!result[0][0])
+                    $('#tuned-change-profile').dialog('failure', new Error("Problem applying tuned profile"));
+                else
+                    dlg.modal('hide');
+            })
+            .fail(function(ex) {
+                dlg.dialog("failure", ex);
+            })
+            .always(function() {
+                get_profile();
+            });
+        dlg.dialog("wait", promise);
+    });
+
+    get_profile();
+    window.setTimeout(update, 2000);
 });

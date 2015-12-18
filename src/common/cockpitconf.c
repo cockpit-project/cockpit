349dc0dfc9a4da7aee68242c96b9681dbebfd322
Stef Walter <stefw@redhat.com>
2015-12-18 09:29:00 +0100

    common: Make the config file case insensitive
    This makes it far more usable. Less pain for admins.
    
    Closes #3356
    Reviewed-by: Peter <petervo@redhat.com>

--- src/common/cockpitconf.c
+++ src/common/cockpitconf.c
@@ -21,6 +21,8 @@
 
 #include "cockpitconf.h"
 
+#include "cockpithash.h"
+
 static GHashTable *cockpit_conf = NULL;
 static GHashTable *cached_strvs = NULL;
 const gchar *cockpit_config_file = PACKAGE_SYSCONF_DIR "/cockpit/cockpit.conf";
@@ -51,7 +53,7 @@ load_key_file (const gchar *file_path,
       section = g_hash_table_lookup (cockpit_conf, groups[i]);
       if (section == NULL)
         {
-          section = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
+          section = g_hash_table_new_full (cockpit_str_case_hash, cockpit_str_case_equal, g_free, g_free);
           g_hash_table_insert (cockpit_conf, g_strdup (groups[i]), section);
         }
 
@@ -77,9 +79,9 @@ cockpit_conf_init (void)
 {
   GError *error = NULL;
 
-  cockpit_conf = g_hash_table_new_full (g_str_hash, g_str_equal, g_free,
+  cockpit_conf = g_hash_table_new_full (cockpit_str_case_hash, cockpit_str_case_equal, g_free,
                                         (GDestroyNotify)g_hash_table_unref);
-  cached_strvs = g_hash_table_new_full (g_str_hash, g_str_equal, g_free,
+  cached_strvs = g_hash_table_new_full (cockpit_str_case_hash, cockpit_str_case_equal, g_free,
                                         (GDestroyNotify)g_strfreev);
 
   if (cockpit_config_file)

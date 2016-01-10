c8d6d88efdbba5c3d99fd003075accbf1d8f42fc
Jan Scotka <jscotka@redhat.com>
2016-01-10 11:36:58 +0100

    changed all tests to using new style of checking, removed checking of styles

--- test/avocado/selenium-login.py
+++ test/avocado/selenium-login.py
@@ -34,6 +34,7 @@ import selenium.webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import os
 import sys
 import re
@@ -73,8 +74,8 @@ class BasicTestSuite(Test):
 
         self.driver.set_window_size(1400, 1200)
         self.driver.set_page_load_timeout(90)
-        self.driver.implicitly_wait(3)
-        self.default_try = 10
+        self.driver.implicitly_wait(1)
+        self.default_try = 40
         self.default_sleep = 1
         self.driver.get('http://%s:9090' % guest_machine)
 
@@ -83,20 +84,23 @@ class BasicTestSuite(Test):
         self.driver.close()
         self.driver.quit()
 
-    def wait(self, method, text, overridetry, fatal, cond):
+    def wait(self, method, text, baseelement, overridetry, fatal, cond):
+        if not baseelement:
+            baseelement = self.driver
         returned = None
+        excpt=""
         cond = cond if cond else visible
         # time.sleep(self.default_sleep)
         internaltry = overridetry if overridetry else self.default_try
         for foo in range(0, internaltry):
             try:
                 #returned = method(text)
-                returned = cond((method, text))
+                returned = WebDriverWait(baseelement, self.default_sleep).until(cond((method, text)))
                 if returned:
                     break
             except:
                 print "REP>", foo
-                time.sleep(self.default_sleep)
+                #time.sleep(self.default_sleep)
                 pass
         if returned is None:
             if fatal:
@@ -106,54 +110,26 @@ class BasicTestSuite(Test):
                 print screenshot_file
                 raise Exception('ERR: Unable to locate name: %s' %
                                 str(text), screenshot_file)
+                #returned=WebDriverWait(baseelement, self.default_sleep).until(cond((method, text)))
             else:
                 return None
         return returned
 
     def wait_id(self, el, baseelement=None, overridetry=None, fatal=True, cond=None):
-        if not baseelement:
-            baseelement = self.driver
-        return self.wait(By.ID, el, overridetry=overridetry, fatal=fatal, cond=cond)
+        return self.wait(By.ID, text=el, baseelement = baseelement, overridetry=overridetry, fatal=fatal, cond=cond)
 
     def wait_link(self, el, baseelement=None, overridetry=None, fatal=True, cond=None):
-        if not baseelement:
-            baseelement = self.driver
-        return self.wait(By.PARTIAL_LINK_TEXT, el, overridetry=overridetry, fatal=fatal, cond=cond)
+        return self.wait(By.PARTIAL_LINK_TEXT, baseelement = baseelement, text=el, overridetry=overridetry, fatal=fatal, cond=cond)
 
     def wait_xpath(self, el, baseelement=None, overridetry=None, fatal=True, cond=None):
-        if not baseelement:
-            baseelement = self.driver
-        return self.wait(By.XPATH, el, overridetry=overridetry, fatal=fatal, cond=cond)
+        return self.wait(By.XPATH, text=el, baseelement = baseelement, overridetry=overridetry, fatal=fatal, cond=cond)
 
-    def wait_iframe(self, el, baseelement=None, overridetry=None, fatal=True, cond=None):
-        if not baseelement:
-            baseelement = self.driver
-        out = None
-        self.wait_xpath("//iframe", baseelement,
-                        overridetry=overridetry, fatal=fatal)
-        iframes = self.wait(baseelement.find_elements_by_xpath,
-                            "//iframe", overridetry=overridetry, fatal=fatal, cond=cond)
-        if len(iframes) == 0:
-            raise Exception('There is no iframe, but SHOULD be')
-        elif len(iframes) == 1:
-            out = [x for x in iframes][0]
-        else:
-            for frame in iframes:
-                if el in str(frame.get_attribute("name")):
-                    out = frame
-            for frame in iframes:
-                if "shell" in str(frame.get_attribute("name")):
-                    out = frame
-        print out
-        return out
+    def wait_frame(self, el, baseelement=None, overridetry=None, fatal=True, cond=None):
+        text="//iframe[contains(@name,'%s')]" % el
+        return self.wait(By.XPATH, text=text, baseelement = baseelement, overridetry=overridetry, fatal=fatal, cond=frame)
 
     def mainframe(self):
         self.driver.switch_to_default_content()
-
-    def selectframe(self, framename, baseelement=None):
-        if not baseelement:
-            baseelement = self.driver
-        baseelement.switch_to_frame(self.wait_iframe(framename))
 
     def login(self, tmpuser=user, tmppasswd=passwd):
         elem = self.wait_id('login-user-input')
@@ -178,7 +154,6 @@ class BasicTestSuite(Test):
 
     def test20Login(self):
         elem = self.login()
-        self.wait_iframe("system")
         elem = self.wait_id("content-user-name")
         self.assertEqual(elem.text, user)
 
@@ -192,15 +167,13 @@ class BasicTestSuite(Test):
         self.assertTrue("Wrong" in elem.text)
 
         elem = self.login()
-        self.wait_iframe("system")
         elem = self.wait_id("content-user-name")
         self.assertEqual(elem.text, user)
 
     def test30ChangeTabServices(self):
         self.login()
-        self.wait_iframe("system")
         self.wait_link('Services').click()
-        self.selectframe("services")
+        self.wait_frame("services")
 
         elem = self.wait_xpath("//*[contains(text(), '%s')]" % "Socket")
         elem.click()
@@ -219,50 +192,37 @@ class BasicTestSuite(Test):
 
     def test40ContainerTab(self):
         self.login()
-        self.wait_iframe("system")
         self.wait_link('Containers').click()
-        self.selectframe("docker")
-        time.sleep(3)
-        elem = self.wait_id('curtain')
-        if "display: block;" in str(elem.get_attribute("style")):
-            elem = self.wait_xpath("//*[@data-action='docker-start']")
-            elem.click()
-        elem = self.wait_xpath(
-            "//*[@id='containers' and @style='display: block;']")
+        self.wait_frame("docker")
+        if self.wait_xpath("//*[@data-action='docker-start']", fatal=False, overridetry=5, cond=clickable):
+            self.wait_xpath("//*[@data-action='docker-start']").click()
+        elem = self.wait_id('containers')
         self.wait_id('containers-storage')
         self.wait_id('containers-images-search').click()
-        elem = self.wait_xpath(
-            "//*[@id='containers-search-image-dialog' and @class='modal in']")
-        baseelem = elem
-        elem = self.wait_id('containers-search-image-search', baseelem)
+        elem = self.wait_id('containers-search-image-dialog')
+        elem = self.wait_id('containers-search-image-search')
         elem.clear()
         elem.send_keys("fedora")
-        elem = self.wait_xpath(
-            "//*[@id='containers-search-image-results' and @style='display: block;']")
+        elem = self.wait_id('containers-search-image-results')
         elem = self.wait_xpath(
             "//*[contains(text(), '%s')]" % "Official Docker")
         elem = self.wait_xpath(
-            "//div[@id='containers-search-image-dialog']//button[contains(text(), '%s')]" % "Cancel")
+            "//div[@id='containers-search-image-dialog']//button[contains(text(), '%s')]" % "Cancel",cond=clickable)
         elem.click()
-        elem = self.wait_xpath(
-            "//*[@id='containers-search-image-dialog' and @style='display: none;']")
+        elem = self.wait_id('containers-search-image-dialog',cond=invisible)
 
-        self.wait_id('containers-images-search').click()
-        elem = self.wait_xpath(
-            "//*[@id='containers-search-image-dialog' and @class='modal in']")
-        baseelem = elem
-        elem = self.wait_id('containers-search-image-search', baseelem)
+        self.wait_id('containers-images-search',cond=clickable).click()
+        elem = self.wait_id('containers-search-image-dialog')
+        elem = self.wait_id('containers-search-image-search')
         elem.clear()
         elem.send_keys("cockpit")
+        elem = self.wait_id('containers-search-image-results')
         elem = self.wait_xpath(
-            "//*[@id='containers-search-image-results' and @style='display: block;']")
-        elem = self.wait_xpath(
-            "//*[contains(text(), '%s')]" % "Cockpit Web Ser")
+            "//*[contains(text(), '%s')]" % "Cockpit Web Ser",cond=clickable)
         elem.click()
-        elem = self.wait_id('containers-search-download', baseelem)
+        elem = self.wait_id('containers-search-download', cond=clickable)
         elem.click()
-        elem = self.wait_xpath(
-            "//*[@id='containers-search-image-dialog' and @style='display: none;']")
+        elem = self.wait_id('containers-search-image-dialog', cond=invisible)
         elem = self.wait_xpath(
             "//*[@class='container-col-tags' and contains(text(), 'cockpit/ws')]")
 
@@ -270,23 +230,21 @@ class BasicTestSuite(Test):
 
     def test50ChangeTabLogs(self):
         self.login()
-        self.wait_iframe("system")
         self.wait_link('Logs').click()
-        self.selectframe("logs")
-        elem = self.wait_xpath("//button[contains(text(), 'Errors')]")
+        self.wait_frame("logs")
+        elem = self.wait_xpath("//button[contains(text(), 'Errors')]", cond=clickable)
         elem.click()
-        elem = self.wait_xpath("//button[contains(text(), 'Warnings')]")
+        elem = self.wait_xpath("//button[contains(text(), 'Warnings')]", cond=clickable)
         elem.click()
-        elem = self.wait_xpath("//button[contains(text(), 'Notices')]")
+        elem = self.wait_xpath("//button[contains(text(), 'Notices')]", cond=clickable)
         elem.click()
-        checkt = "ahoj notice"
+        checkt = "ahojnotice"
         out = process.run("systemd-cat -p notice echo '%s'" %
                           checkt, shell=True)
         elem = self.wait_xpath(
-            "//*[@class='cockpit-log-message' and contains(text(), '%s')]" % checkt)
+            "//span[@class='cockpit-log-message' and contains(text(), '%s')]" % checkt)
         elem.click()
-        elem = self.wait_xpath(
-            "//*[@id='journal-entry' and @style='display: block;']")
+        elem = self.wait_id('journal-entry')
         self.mainframe()
 
     def test60ChangeTabStorage(self):
@@ -299,63 +257,50 @@ class BasicTestSuite(Test):
         other_discname = other_disc.adddisc("d1")
         other_shortname = os.path.basename(other_discname)
         self.login()
-        self.wait_iframe("system")
         self.wait_link('Storage').click()
-        self.selectframe("storage")
-        elem = self.wait_id("drives")
-        elem = self.wait_xpath("//*[@data-goto-block='%s']" % other_shortname)
+        self.wait_frame("storage")
+        self.wait_id("drives")
+        elem = self.wait_xpath("//*[@data-goto-block='%s']" % other_shortname, cond=clickable)
         elem.click()
-        elem = self.wait_xpath(
-            "//*[@id='storage-detail' and @style='display: block;']")
-        basel = elem
-        self.wait_xpath("//*[contains(text(), '%s')]" % "Capacity")
-        self.wait_xpath("//*[contains(text(), '%s')]" % "1000 MB")
+        self.wait_id('storage-detail')
+        self.wait_xpath("//td[contains(text(), '%s')]" % other_discname)
+        self.wait_xpath("//td[contains(text(), '%s')]" % "Capacity")
+        self.wait_xpath("//td[contains(text(), '%s')]" % "1000 MB")
 
-        self.wait_link('Storage', basel).click()
+        self.wait_link('Storage').click()
         elem = self.wait_xpath("//*[@data-goto-block='%s']" % other_shortname)
 
         self.mainframe()
 
     def test70ChangeTabNetworking(self):
         self.login()
-        self.wait_iframe("system")
         out = process.run(
             "ip r |grep default | head -1 | cut -d ' ' -f 5", shell=True)
         self.wait_link('Network').click()
-        self.selectframe("network")
+        self.wait_frame("network")
 
         self.wait_id("networking-interfaces")
         self.wait_id("networking-tx-graph")
 
-        elem = self.wait_xpath("//*[contains(text(), '%s')]" % out.stdout[:-1])
+        elem = self.wait_xpath("//tr[@data-interface='%s']" % out.stdout[:-1],cond=clickable)
+        elem.click()
+        self.wait_xpath("//td[contains(text(), '%s')]" % "Carrier")
         self.mainframe()
 
     def test80ChangeTabTools(self):
         self.login()
-        self.wait_iframe("system")
-        elem = self.wait_link('Tools')
-        self.assertEqual(elem.get_attribute('class'), "collapsed")
-        elem.click()
-        time.sleep(self.default_sleep)
-        elem = self.wait_link('Tools')
-        self.assertEqual(elem.get_attribute('class'), "")
-        elem.click()
-        time.sleep(self.default_sleep)
-        elem = self.wait_link('Tools')
-        self.assertEqual(elem.get_attribute('class'), "collapsed")
-        elem.click()
-        time.sleep(self.default_sleep)
+        self.wait_id("tools-panel",cond=invisible)
+        self.wait_link('Tools').click()
+        self.wait_id("tools-panel")
         self.wait_link('Accounts').click()
-        self.selectframe("users")
+        self.wait_frame("users")
         elem = self.wait_xpath(
-            "//*[@class='cockpit-account-user-name' and contains(text(), '%s')]" % user)
+            "//*[@class='cockpit-account-user-name' and contains(text(), '%s')]" % user, cond=clickable)
         elem.click()
-        elem = self.wait_xpath(
-            "//*[@id='account' and @style='display: block;']")
+        elem = self.wait_id('account')
         self.wait_xpath("//*[contains(text(), '%s')]" % "Full Name")
         self.wait_link('Accounts', elem).click()
-        time.sleep(self.default_sleep)
-        self.wait_xpath('//*[@id="accounts-create"]').click()
+        self.wait_xpath('//*[@id="accounts-create"]', cond=clickable).click()
         elem = self.wait_id('accounts-create-real-name')
         elem.clear()
         elem.send_keys('testxx')
@@ -366,32 +311,37 @@ class BasicTestSuite(Test):
         elem.clear()
         elem.send_keys(passwd)
         time.sleep(self.default_sleep)
-        self.wait_id('accounts-create-create').click()
+        self.wait_id('accounts-create-create', cond=clickable).click()
+        time.sleep(self.default_sleep)
         elem = self.wait_xpath(
-            "//*[@class='cockpit-account-user-name' and contains(text(), '%s')]" % 'testxx')
+            "//*[@class='cockpit-account-user-name' and contains(text(), '%s')]" % 'testxx', cond=clickable)
         elem.click()
-        self.wait_id('account-delete').click()
-        elem = self.wait_xpath(
-            "//*[@id='account-confirm-delete-dialog' and @style='display: block;']")
-        self.wait_id('account-confirm-delete-apply').click()
-        time.sleep(2)
+        self.wait_id('account-delete', cond=clickable).click()
+        self.wait_id('account-confirm-delete-dialog')
+        self.wait_id('account-confirm-delete-apply', cond=clickable).click()
+        time.sleep(self.default_sleep)
         self.mainframe()
 
         self.wait_link('Terminal').click()
-        self.selectframe("terminal")
-        self.wait_xpath("//div[@id='terminal']")
+        self.wait_frame("terminal")
+        self.wait_id('terminal')
         elem = self.wait_xpath("//*[@class='terminal']")
         terminal = elem
         terminal.send_keys("touch /tmp/testabc\n")
         terminal.send_keys("touch /tmp/testabd\n")
+        time.sleep(self.default_sleep)
         terminal.send_keys("ls /tmp/test*\n")
         elem = self.wait_xpath(
-            "//*[contains(text(), '%s') and contains(text(), '%s')]" % ('/tmp/testabc', '/tmp/testabd'))
+            "//div[contains(text(), '%s') and contains(text(), '%s')]" % ('/tmp/testabc', '/tmp/testabd'))
         self.assertTrue("/tmp/testabc" in elem.text)
         process.run("ls /tmp/testabc", shell=True)
         time.sleep(self.default_sleep)
-        terminal.send_keys("rm -v /tmp/testabc /tmp/testabd\n")
+        terminal.send_keys("rm /tmp/testabc /tmp/testabd\n")
         time.sleep(self.default_sleep)
+        terminal.send_keys("ls /tmp/test*\n")
+        elem = self.wait_xpath(
+            "//div[contains(text(), '%s') and contains(text(), '%s')]" % ('cannot', 'access'))
+        self.assertTrue("No such file" in elem.text)
         process.run("ls /tmp/testabc |wc -l |grep 0", shell=True)
         process.run("ls /tmp/testabd |wc -l |grep 0", shell=True)
         self.mainframe()

3d4a427611e169923a2f3a22d6bee94a2a718453
Marius Vollmer <mvollmer@redhat.com>
2016-01-27 10:43:46 +0200

    WIP

--- test/testimagetask.py
+++ test/testimagetask.py
@@ -29,9 +29,12 @@ class GithubImageTask(object):
     def check_publishing(self, github):
         return True
 
-    def stop_publishing(self, ret):
+    def stop_publishing(self, ret, branch):
         if ret == 0:
             message = "Image creation done"
+            if branch:
+                self.sink.status['github']['pull'] = { 'head': branch,
+                                                       'base': "master" }
         else:
             message = "Image creation failed"
         self.sink.status['github']['message'] = message
@@ -39,16 +42,50 @@ class GithubImageTask(object):
 
     def run(self, opts, github):
 
+        if not github.token:
+            print "Need a github token to run image creation tasks"
+            return
+
         if opts.publish:
             self.start_publishing(opts.publish, github)
+
+        user = github.get("/user")['login']
 
         msg = "Creating image {0} on {1}...\n".format(self.image, testinfra.HOSTNAME)
         sys.stderr.write(msg)
 
-        cmd = [ "ln", "-sf", "test-test-test", "images/" + self.image ]
-
-        proc = subprocess.Popen(cmd)
+        proc = subprocess.Popen([ "ln", "-sf", "test-test-test", "images/" + self.image ])
         ret = testinfra.wait_testing(proc, lambda: self.check_publishing(github))
 
+        # Github wants the OAuth token as the username and git will
+        # happily echo that back out.  So we censor all output.
+
+        def run_censored(cmd):
+
+            def censored(text):
+                return text.replace(github.token, "CENSORED")
+
+            try:
+                output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+                print censored(output)
+                return True
+            except subprocess.CalledProcessError as e:
+                print censored(e.output)
+                print "Failed:", censored(' '.join(e.cmd))
+                return False
+
+        # Push the new image link to origin, but don't change any local refs
+
+        branch = "refresh-" + self.image
+        url = "https://{0}@github.com/{1}/cockpit.git".format(github.token, user)
+
+        if ret == 0:
+            if (run_censored([ "git", "checkout", "--detach" ]) and
+                run_censored([ "git", "commit", "-a", "-m", "Refreshed {0} image".format(self.image) ]) and
+                run_censored([ "git", "push", url, "+HEAD:refs/heads/" + branch ])):
+                pass
+            else:
+                ret = 1
+
         if self.sink:
-            self.stop_publishing(ret)
+            self.stop_publishing(ret, user + ":" + branch)

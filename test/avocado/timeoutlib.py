1ee917ea960dde5b8e47a0c42f9871627a256f06
Jan Scotka <jscotka@redhat.com>
2016-02-19 14:05:43 +0100

    added license and author to library

--- test/avocado/timeoutlib.py
+++ test/avocado/timeoutlib.py
@@ -1,5 +1,23 @@
-#!/usr/bin/python
-# copied from: https://gist.github.com/happz/d50897af8a2e90cce8c7
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Cockpit.
+#
+# Copyright (C) 2016 Red Hat, Inc.
+#
+# Cockpit is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Cockpit is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+# Author: Miloš Prchlík (https://gist.github.com/happz/d50897af8a2e90cce8c7)
 
 import signal
 import time

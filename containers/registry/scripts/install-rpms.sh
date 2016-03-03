ef1ae985afb9ab73a55a799282e00a3d3be72b09
petervo <petervo@redhat.com>
2016-03-03 10:01:18 -0800

    containers: Get the rpm OS from the container
    When we pull in rpms get the os portion of
    the release url and filename from the container
    itself.

--- containers/registry/scripts/install-rpms.sh
+++ containers/registry/scripts/install-rpms.sh
@@ -37,11 +37,15 @@ if [ -z "$INSTALLER" ]; then
     INSTALLER="dnf"
 fi
 
+if [ -z "$OS" ]; then
+    OS=$(rpm -q --qf "%{release}" json-glib | sed -n -e 's/^[0-9]*\.\(\S\+\).*/\1/p')
+fi
+
 for package in $@
 do
     rpm=$(/usr/bin/find /container/rpms -name "$package*.rpm" || true)
     if [ -z "$rpm" ]; then
-        rpm="$COCKPIT_RPM_URL/$VERSION/$RELEASE/$arch/$package$VERSION-$RELEASE.$arch.rpm"
+        rpm="$COCKPIT_RPM_URL/$VERSION/$RELEASE.$OS/$arch/$package$VERSION-$RELEASE.$OS.$arch.rpm"
     fi
 
     echo "$rpm"

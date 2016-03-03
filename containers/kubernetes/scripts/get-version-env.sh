fe2e37f1e4cc1f09b13bc0ea3a750d4b8f4f99b9
petervo <petervo@redhat.com>
2016-03-03 10:01:18 -0800

    containers: Get the rpm OS from the container
    When we pull in rpms get the os portion of
    the release url and filename from the container
    itself.
    
    Closes #3914
    Reviewed-by: Stef Walter <stefw@redhat.com>

--- containers/kubernetes/scripts/get-version-env.sh
+++ containers/kubernetes/scripts/get-version-env.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-OS='f23-updates-testing'
+v=$(rpm -q --qf "%{version}" -f /etc/fedora-release)
+OS="f$v-updates-testing"
 
-echo $(curl -s https://bodhi.fedoraproject.org/latest_builds?package=cockpit | python3 -c "import json, sys; obj=json.load(sys.stdin); parts=obj['$OS'].strip('cockpit-').split('-', 1); print('export VERSION={0} RELEASE={1}'.format(parts[0], parts[1]))")
+echo $(curl -s https://bodhi.fedoraproject.org/latest_builds?package=cockpit | python3 -c "import json, sys; obj=json.load(sys.stdin); parts=obj['$OS'].strip('cockpit-').split('-', 1); rparts=parts[1].split('.', 1); print('export VERSION={0} RELEASE={1} OS={2}'.format(parts[0], rparts[0], rparts[1]))")

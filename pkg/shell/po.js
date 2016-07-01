906d1e13ec556fa51334246432c0d1a1185cdcb1
Stef Walter <stefw@redhat.com>
2016-07-01 09:19:20 -0700

    tools: Use a UMD format for po2json files
    This allows the PO files to be used by non-AMD modules. A global
    po object is exported in this case.

--- pkg/shell/po.js
+++ pkg/shell/po.js
@@ -1,1 +1,7 @@
-define({"": { "language": "en" }});
+(function (root, data) {
+    if (typeof define === 'function' && define.amd) {
+        define(data);
+    } else {
+        root.po = data;
+    }
+}(this, {"": { "language": "en" }}));

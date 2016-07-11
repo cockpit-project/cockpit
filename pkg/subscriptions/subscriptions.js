69be4574966e88c137e84a4a8c4bbcae55225254
Dominik Perpeet <dperpeet@redhat.com>
2016-07-11 21:56:46 +0200

    subscriptions: Add activation key support
    Also add them to the integration test.

--- pkg/subscriptions/subscriptions.js
+++ pkg/subscriptions/subscriptions.js
@@ -39,28 +39,33 @@ require([
     var register_url = "default";
 
     function register_system() {
-        var url = null;
         var ex, errors = [];
+        var subscription_details = {};
+
+        subscription_details.url = null;
         if (register_url != "default")
-          url = $('#subscription-register-url-custom').val().trim();
-        var username = $('#subscription-register-username').val();
-        if (username === '') {
-            ex = new Error(_("Login cannot be empty"));
-            ex.target = "#subscription-register-username";
+            subscription_details.url = $('#subscription-register-url-custom').val().trim();
+
+        subscription_details.activation_key = $('#subscription-register-key').val();
+
+        subscription_details.username = $('#subscription-register-username').val();
+        subscription_details.password = $('#subscription-register-password').val();
+        if ((subscription_details.username === '' || subscription_details.password === '') &&
+            subscription_details.activation_key === '') {
+            ex = new Error(_("Username/Password or activation key required"));
+            if (subscription_details.password === '')
+                ex.target = "#subscription-register-password";
+            else
+                ex.target = "#subscription-register-username";
             errors.push(ex);
         }
-        var password = $('#subscription-register-password').val();
-        if (password === '') {
-            ex = new Error(_("Password cannot be empty"));
-            ex.target = "#subscription-register-password";
-            errors.push(ex);
-        }
-        var org = $('#subscription-register-org').val();
+
+        subscription_details.org = $('#subscription-register-org').val();
 
         $("#subscriptions-register-dialog").dialog("failure", errors);
 
         if (!errors.length)
-            subscriptions.manager.register_system(url, username, password, org);
+            subscriptions.manager.register_system(subscription_details);
     }
 
     function register_dialog_initialize() {
@@ -383,20 +388,25 @@ require([
             return Mustache.render("{{text}}", { 'text': message });
         }
 
-        function perform_register(url, username, password, org) {
+        function perform_register(subscription_details) {
             var deferred = $.Deferred();
 
-            var args = ['subscription-manager', 'register', '--auto-attach'];
-            if (url !== null)
-                args.push('--serverurl', url);
-            /* if username or password are empty, subscription-manager will prompt for them
-             * so we always need to pass them
-             */
-            args.push('--username', username, '--password', password);
+            var args = ['subscription-manager', 'register'];
+            if (subscription_details.url !== null)
+                args.push('--serverurl', subscription_details.url);
+
+            // activation keys can't be used with auto-attach
+            if (subscription_details.activation_key)
+                args.push('--activationkey', subscription_details.activation_key);
+            else
+                args.push('--auto-attach');
+
+            if (subscription_details.username || subscription_details.password)
+                args.push('--username', subscription_details.username, '--password', subscription_details.password);
 
             // only pass org info if user provided it
-            if (org)
-                args.push('--org', org);
+            if (subscription_details.org)
+                args.push('--org', subscription_details.org);
 
             deferred.notify(_("Registering system..."));
 
@@ -477,7 +487,7 @@ require([
             return promise;
         }
 
-        function register_system(url, username, password, org) {
+        function register_system(subscription_details) {
             if (action_in_progress()) {
                 console.log(_('Unable to register at this time because a call to subscription manager ' +
                   'is already in progress. Please try again.'));
@@ -488,7 +498,7 @@ require([
             $('#subscriptions-register-password-note-details').hide();
             $('#subscriptions-register-org-note-details').hide();
 
-            var promise = perform_register(url, username, password, org);
+            var promise = perform_register(subscription_details);
             $("#subscriptions-register-dialog").dialog("wait", promise);
 
             promise

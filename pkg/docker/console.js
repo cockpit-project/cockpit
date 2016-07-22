102957fadecbad016394f7e8653594da1f76cb27
Stef Walter <stefw@redhat.com>
2016-07-22 14:02:10 +0200

    docker: Fix up regressions in the console html file
    As documented here:
    
    http://cockpit-project.org/guide/latest/api-console-html.html
    
    Closes #4777
    Reviewed-by: Peter <petervo@redhat.com>

---
+++ pkg/docker/console.js
@@ -0,0 +1,37 @@
+require([
+    "jquery",
+    "base1/cockpit",
+    "docker/docker"
+], function($, cockpit, docker) {
+
+    var box;
+    function update() {
+        if (box && box.close)
+            box.close();
+
+        var path = cockpit.location.path;
+        if (path.length === 0) {
+            box = $("<pre>").text("usage: console.html#/container_id");
+        } else if (path.length !== 1) {
+            console.warn("not a container id: " + path);
+            cockpit.location = '';
+        } else {
+            box = docker.console(path[0]);
+            $("title").text(path[0]);
+
+            /*
+             * TODO: Once we get more code into the docker module this
+             * should reflect the actual docker container state and
+             * become typable when the container is running.
+             */
+            box.typeable(true);
+            box.connect();
+        }
+
+        $("#container").empty().append(box);
+        $("body").show();
+    }
+
+    $(cockpit).on("locationchanged", update);
+    $(update);
+});

91af03a7dc8273c4bc5a43b0d8e3dd24a8ec582e
Stef Walter <stefw@redhat.com>
2016-11-09 20:36:53 +0100

    base1: Print warnings when using deprecated Cockpit javascript
    We put deprecated warnings both in the javascript file and in
    print them in the browser console when the deprecated files
    are used.
    
    Secondly we make bundle.js just be a link to require.js. Users of
    bundle.js were using AMD ... and this will continue to work,
    albeit in a deprecated slower fashion.
    
    Remove require.js from the documentation. It's deprecated, not
    documented.

---
+++ src/base1/deprecated.js
@@ -0,0 +1,4 @@
+/* @preserve: WARNING: This javascript file in the base1 package is deprecated */
+console.warn("Deprecated: " +
+             ((new Error).fileName || "This javascript file").split("/").reverse()[0] +
+             " in the Cockpit base1 package is deprecated");

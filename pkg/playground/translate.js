37a40d5efdd70b7022ab6fe5ff44a0aa37ea9082
Stef Walter <stefw@redhat.com>
2016-11-15 09:14:20 +0100

    playground: Add test page for trying out translations
    And make tests start to use it.

---
+++ pkg/playground/translate.js
@@ -0,0 +1,44 @@
+var $ = require("jquery");
+var cockpit = require("cockpit");
+var mustache = require("mustache");
+
+var _ = cockpit.gettext;
+var C_ = cockpit.gettext;
+
+cockpit.translate();
+
+var text = _("Empty");
+$("#underscore-empty").text(text);
+
+text = _("verb", "Empty");
+$("#underscore-context-empty").text(text);
+
+text = C_("verb", "Empty");
+$("#cunderscore-context-empty").text(text);
+
+text = cockpit.gettext("Control");
+$("#gettext-control").text(text);
+
+text = cockpit.gettext("key", "Control");
+$("#gettext-context-control").text(text);
+
+text = cockpit.ngettext("$0 disk is missing", "$0 disks are missing", 1);
+$("#ngettext-disks-1").text(text);
+
+text = cockpit.ngettext("$0 disk is missing", "$0 disks are missing", 2);
+$("#ngettext-disks-2").text(text);
+
+text = cockpit.ngettext("disk-non-rotational", "$0 disk is missing", "$0 disks are missing", 1);
+$("#ngettext-context-disks-1").text(text);
+
+text = cockpit.ngettext("disk-non-rotational", "$0 disk is missing", "$0 disks are missing", 2);
+$("#ngettext-context-disks-2").text(text);
+
+var template = $("#mustache-input").text();
+var output = $.parseHTML(mustache.render(template));
+cockpit.translate(output);
+$("#mustache-output").empty().append(output);
+
+cockpit.transport.wait(function() {
+    $("body").show();
+});

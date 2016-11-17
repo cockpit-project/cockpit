94cd53db6da7117aa51d157133462bf4a5a34ed9
Stef Walter <stefw@redhat.com>
2016-11-17 11:21:35 +0100

    po: Make our po files and extraction compatible with angular-gettext
    Support extracting the getString() and getPlural() style of markup.
    Also in our po files support loading into angular-gettext.
    
    Closes #5420

--- po/po.js
+++ po/po.js
@@ -1,9 +1,60 @@
 (function (root, data) {
+    var loaded, module;
+
+    /* Load into AMD if desired */
     if (typeof define === 'function' && define.amd) {
         define(data);
-    } else if (typeof cockpit === 'object') {
-        cockpit.locale(data);
-    } else {
-        root.po = data;
+        loaded = true;
     }
-}(this, {"": { "language": "en" }}));
+
+    /* Load into Cockpit locale */
+    if (typeof cockpit === 'object') {
+        cockpit.locale(data)
+        loaded = true;
+    }
+
+    function transformAngular(data) {
+        var key, context, parts, value, result = { };
+        for (key in data) {
+            if (key === "")
+                continue;
+            parts = key.split("\u0004");
+            value = data[key];
+            if (parts[1]) {
+                context = parts[0];
+                key = parts[1];
+            } else {
+                context = "$$noContext";
+                key = parts[0];
+            }
+            if (value[0] === null)
+                value = value[1];
+            else
+                value = value.slice(1);
+            if (!(key in result))
+                result[key] = { };
+            result[key][context] = value;
+        }
+        return result;
+    }
+
+    /* Load into angular here */
+    if (typeof angular === 'object') {
+        try {
+            module = angular.module(["gettext"]);
+        } catch(ex) { console.log(ex); /* Either no angular or angular-gettext */ };
+        if (module) {
+            loaded = true;
+            module.run(['gettextCatalog', function(gettextCatalog) {
+                var lang = data[""]["language"];
+                gettextCatalog.setStrings(lang, transformAngular(data));
+                gettextCatalog.setCurrentLanguage(lang);
+            }]);
+        }
+    }
+
+    if (!loaded)
+        root.po = data;
+
+/* The syntax of this line is important  by po2json */
+}(this, {"":{"language":"en"}}));

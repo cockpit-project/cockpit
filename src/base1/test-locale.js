b0f261f93c025972757c8815666216b64304677f
Stef Walter <stefw@redhat.com>
2016-11-17 07:49:37 +0100

    po: Extract HTML translations properly
    This now supports most angular-gettext extractions. We support
    extracting title translations, as well as prefering the newer
    form HTML translate attributes.
    
    This implements support in cockpit.translate().
    
    The playground translate page has been updated to clearly denote
    what forms of translate syntax are supported and where.
    
    The tests have been updated both unit tests and integration test.
    
    Closes #5418

--- src/base1/test-locale.js
+++ src/base1/test-locale.js
@@ -93,55 +93,99 @@ QUnit.test("translate document", function() {
 
     $("#translations")
         .empty()
-        .append("<span translatable='yes' id='translatable-html'>Control</span>");
+        .append("<span translate id='translatable-html'>Control</span>");
 
     cockpit.translate();
-    assert.equal($("#translatable-html").text(), "Ontrolcay", "translatable element");
-    assert.strictEqual($("#translatable-html").attr("translatable"), undefined, "translatable element attribute removed");
+    assert.equal($("#translatable-html").text(), "Ontrolcay", "translate element");
+    assert.strictEqual($("#translatable-html").attr("translate"), undefined, "translate element attribute removed");
 });
 
 QUnit.test("translate elements", function() {
     cockpit.locale(null);
     cockpit.locale(pig_latin);
 
-    var div1 = $("<div><span translatable='yes' id='translatable-html'>Control</span>" +
-                 "<span translatable='yes' context='key' id='translatable-context-html'>Control</span></div>");
+    var div1 = $("<div><span translate id='translatable-html'>Control</span>" +
+                 "<span translate translate-context='key' id='translatable-context-html'>Control</span></div>");
 
-    var div2 = $("<div translatable='yes'>User</div>");
+    var div2 = $("<div translate>User</div>");
 
-    var div3 = $("<div><span><i translatable='yes'>Waiting</i></span></div>");
+    var div3 = $("<div><span><i translate>Waiting</i></span></div>");
 
     $("#translations")
         .empty()
         .append(div1, div2, div3);
 
     cockpit.translate(div1[0], div2[0], div3[0]);
-    assert.equal($("#translatable-html").text(), "Ontrolcay", "translatable element");
-    assert.strictEqual($("#translatable-html").attr("translatable"), undefined, "translatable element removed");
-    assert.equal($("#translatable-context-html").text(), "OntrolCAY", "translatable context");
-    assert.strictEqual($("#translatable-context-html").attr("translatable"), undefined, "translatable context attribute removed");
+    assert.equal($("#translatable-html").text(), "Ontrolcay", "translate element");
+    assert.strictEqual($("#translatable-html").attr("translate"), undefined, "translate element removed");
+    assert.equal($("#translatable-context-html").text(), "OntrolCAY", "translate context");
+    assert.strictEqual($("#translatable-context-html").attr("translate"), undefined, "translate context attribute removed");
 });
 
 QUnit.test("translate array", function() {
     cockpit.locale(null);
     cockpit.locale(pig_latin);
 
-    var div1 = $("<div><span translatable='yes' id='translatable-html'>Control</span>" +
-                 "<span translatable='yes' context='key' id='translatable-context-html'>Control</span></div>");
+    var div1 = $("<div><span translate id='translatable-html'>Control</span>" +
+                 "<span translate translate-context='key' id='translatable-context-html'>Control</span></div>");
 
-    var div2 = $("<div translatable='yes'>User</div>");
+    var div2 = $("<div translate>User</div>");
 
-    var div3 = $("<div><span><i translatable='yes'>Waiting</i></span></div>");
+    var div3 = $("<div><span><i translate>Waiting</i></span></div>");
 
     $("#translations")
         .empty()
         .append(div1, div2, div3);
 
     cockpit.translate($("#translations div"));
-    assert.equal($("#translatable-html").text(), "Ontrolcay", "translatable element");
-    assert.strictEqual($("#translatable-html").attr("translatable"), undefined, "translatable element no attribute");
-    assert.equal($("#translatable-context-html").text(), "OntrolCAY", "translatable context");
-    assert.strictEqual($("#translatable-context-html").attr("translatable"), undefined, "translatable context attribute removed");
+    assert.equal($("#translatable-html").text(), "Ontrolcay", "translate element");
+    assert.strictEqual($("#translatable-html").attr("translate"), undefined, "translate element no attribute");
+    assert.equal($("#translatable-context-html").text(), "OntrolCAY", "translate context");
+    assert.strictEqual($("#translatable-context-html").attr("translate"), undefined, "translate context attribute removed");
+});
+
+QUnit.test("translate glade", function() {
+    cockpit.locale(null);
+    cockpit.locale(pig_latin);
+
+    var div = $("<div><span translatable='yes' id='translatable-glade'>Control</span>" +
+                "<span translatable='yes' context='key' id='translatable-glade-context'>Control</span></div>");
+
+    $("#translations").empty().append(div);
+
+    cockpit.translate(div);
+    assert.equal($("#translatable-glade").text(), "Ontrolcay", "translatable element");
+    assert.strictEqual($("#translatable-glade").attr("translatable"), undefined, "translatable element removed");
+    assert.equal($("#translatable-glade-context").text(), "OntrolCAY", "translatable context");
+    assert.strictEqual($("#translatable-glade-context").attr("translatable"), undefined, "translatable context attribute removed");
+});
+
+QUnit.test("translate attributes", function() {
+    cockpit.locale(null);
+    cockpit.locale(pig_latin);
+
+    var div = $("<div><span translate='title' title='Control' id='translatable-attribute'>Waiting</span>" +
+                "<div><span translate='title' translate-context='key' title='Control'" +
+                    "id='translatable-attribute-context'>Waiting</span>" +
+                "<span translate='yes title' title='User' id='translatable-attribute-both'>Waiting</span></div>" +
+                "<span translate='  yes title ' title='User'" +
+                    "id='translatable-attribute-syntax'>Waiting</span></div>");
+
+    $("#translations").empty().append(div);
+
+    cockpit.translate(div);
+    assert.equal($("#translatable-attribute").attr("title"), "Ontrolcay", "translate attribute");
+    assert.equal($("#translatable-attribute").text(), "Waiting", "translate attribute doesn't affect text");
+    assert.strictEqual($("#translatable-attribute").attr("translate"), undefined, "translate removed");
+    assert.equal($("#translatable-attribute-context").attr("title"), "OntrolCAY", "translatable element");
+    assert.equal($("#translatable-attribute-context").text(), "Waiting", "translate context doesn't affect text");
+    assert.strictEqual($("#translatable-attribute-context").attr("translate"), undefined, "translate removed");
+    assert.equal($("#translatable-attribute-both").attr("title"), "Useray", "translate attribute both");
+    assert.equal($("#translatable-attribute-both").text(), "Aitingway", "translate text both");
+    assert.strictEqual($("#translatable-attribute-both").attr("translate"), undefined, "translate removed");
+    assert.equal($("#translatable-attribute-syntax").attr("title"), "Useray", "translate syntax both");
+    assert.equal($("#translatable-attribute-syntax").text(), "Aitingway", "translate syntax both");
+    assert.strictEqual($("#translatable-attribute-syntax").attr("translate"), undefined, "translate removed");
 });
 
 $(function() {

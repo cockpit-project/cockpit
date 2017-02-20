7aa4f6a192ebc97e18e434ba57c2d32350d140b3
Stef Walter <stefw@redhat.com>
2017-02-20 11:15:48 +0200

    bridge: Split out MockEchoChannel into its own file
    We want the MockEchoChannel stuff in its own file so it can be used
    by multiple tests. Note that the test-channel.c tests still need their
    own copy, since it modified behavior somewhat.
    
    Reviewed-by: Peter <petervo@redhat.com>

---
+++ src/bridge/mock-channel.h
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MOCK_CHANNEL_H
+#define MOCK_CHANNEL_H
+
+#include "cockpitchannel.h"
+
+#define MOCK_TYPE_ECHO_CHANNEL         (mock_echo_channel_get_type ())
+#define MOCK_ECHO_CHANNEL(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), MOCK_TYPE_ECHO_CHANNEL, MockEchoChannel))
+#define MOCK_IS_ECHO_CHANNEL(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), MOCK_TYPE_ECHO_CHANNEL))
+
+typedef struct {
+  CockpitChannel parent;
+  gboolean close_called;
+} MockEchoChannel;
+
+GType                mock_echo_channel_get_type      (void);
+
+#endif /* MOCK_CHANNEL_H */
--- src/bridge/test-portal.c
+++ src/bridge/test-portal.c
@@ -21,6 +21,7 @@
 
 #include "cockpitchannel.h"
 #include "cockpitportal.h"
+#include "mock-channel.h"
 #include "mock-transport.h"
 
 #include "common/cockpitjson.h"
@@ -31,56 +32,6 @@
 #include <gio/gio.h>
 
 #include <string.h>
-
-static GType mock_echo_channel_get_type (void) G_GNUC_CONST;
-
-typedef struct {
-    CockpitChannel parent;
-    gboolean close_called;
-} MockEchoChannel;
-
-typedef CockpitChannelClass MockEchoChannelClass;
-
-G_DEFINE_TYPE (MockEchoChannel, mock_echo_channel, COCKPIT_TYPE_CHANNEL);
-
-static void
-mock_echo_channel_recv (CockpitChannel *channel,
-                        GBytes *message)
-{
-  cockpit_channel_send (channel, message, FALSE);
-}
-
-static void
-mock_echo_channel_close (CockpitChannel *channel,
-                         const gchar *problem)
-{
-  MockEchoChannel *self = (MockEchoChannel *)channel;
-  self->close_called = TRUE;
-  COCKPIT_CHANNEL_CLASS (mock_echo_channel_parent_class)->close (channel, problem);
-}
-
-static void
-mock_echo_channel_init (MockEchoChannel *self)
-{
-
-}
-
-static void
-mock_echo_channel_constructed (GObject *obj)
-{
-  G_OBJECT_CLASS (mock_echo_channel_parent_class)->constructed (obj);
-  cockpit_channel_ready (COCKPIT_CHANNEL (obj), NULL);
-}
-
-static void
-mock_echo_channel_class_init (MockEchoChannelClass *klass)
-{
-  CockpitChannelClass *channel_class = COCKPIT_CHANNEL_CLASS (klass);
-  GObjectClass *object_class = G_OBJECT_CLASS (klass);
-  object_class->constructed = mock_echo_channel_constructed;
-  channel_class->recv = mock_echo_channel_recv;
-  channel_class->close = mock_echo_channel_close;
-}
 
 static CockpitChannel *
 mock_echo_channel_open (CockpitTransport *transport,

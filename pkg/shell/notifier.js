0897a7acec7e47ad8afac53f5591f786c40f434d
Stef Walter <stefw@redhat.com>
2017-02-27 13:03:04 +0100

    shell: Make prompting from sudo reauthorize correctly
    This makes sudo work for the purposes of reauthorizing the user.
    
    Closes #5930

--- pkg/shell/notifier.js
+++ pkg/shell/notifier.js
@@ -28,13 +28,47 @@ function TtyChannel(opts) {
         password: opts.password,
     };
 
+    var buffer = "";
+
     var channel = cockpit.channel(cockpit.extend({
         payload: "tty",
     }, options));
 
     channel.addEventListener("message", function(ev, data) {
-        console.log(data);
+        buffer += data;
+
+        /*
+         * When sudo or polkit ask for a prompt on the tty it looks like this:
+         * @authorize@user:self@authorize@
+         */
+        var i, parts = buffer.split("@authorize@");
+        for (i = 0; i < parts.length; i += 2) {
+            if (i + 2 < parts.length) {
+                if (authorize.apply(null, parts[i + 1].split(":"))) {
+                    parts[i + 1] = "";
+                    while (parts[i + 2].charAt(0) == ' ')
+                        parts[i + 2] = parts[i + 2].substr(1);
+                }
+            }
+        }
+        buffer = parts.join("");
+
+        /* Now print out the remaining lines */
+        var lines = buffer.split("\n");
+        buffer = lines.pop();
+        for (i = 0; i < lines.length; i++)
+            console.log(lines[i]);
     });
+
+    function authorize(user, who) {
+        if (user == who) {
+            channel.control({ "command": "authorize", "credential": "inject" });
+            channel.send("\n");
+            return true;
+        }
+        console.warn("Not reauthorizing due to user mismatch: ", user, who);
+        return false;
+    }
 }
 
 module.exports = {

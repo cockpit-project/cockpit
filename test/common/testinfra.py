9af984949ed2c618d54c064cbdd657459ea541e7
Stef Walter <stefw@redhat.com>
2017-03-03 09:00:03 +0100

    test: Fedora 26 has branched start testing there
    Now that Fedora 26 has branched, we won't be releasing into the
    main Fedora 25 channels any longer.
    
    We don't yet remove support for Fedora 24 because other pull
    requests are coming that migrate Avocado and Selenium stuff
    later.
    
    Also remove a HACK that's supposedly fixed on Fedora 26.

--- test/common/testinfra.py
+++ test/common/testinfra.py
@@ -66,7 +66,7 @@ DEFAULT_VERIFY = {
     'verify/fedora-24': [ ],
     'verify/fedora-25': [ 'master', 'pulls' ],
     'verify/fedora-i386': [ 'master', 'pulls' ],
-    'verify/fedora-26': [ ],
+    'verify/fedora-26': [ 'master', 'pulls' ],
     'verify/fedora-atomic': [ 'master', 'pulls' ],
     'verify/fedora-testing': [ ],
     'verify/rhel-7': [ 'master', 'pulls' ],
@@ -94,7 +94,12 @@ DEFAULT_IMAGE_REFRESH = {
     'fedora-25': {
         'triggers': [
             "verify/fedora-25",
-            "verify/fedora-atomic",  # builds in fedora-25
+            "verify/fedora-atomic", # builds in fedora-25
+        ]
+    },
+    'fedora-26': {
+        'triggers': [
+            "verify/fedora-26",
         ]
     },
     'fedora-atomic': {

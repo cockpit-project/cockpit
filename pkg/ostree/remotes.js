b94e2ae295db95347cae860382ff3112f6d7dd28
petervo <petervo@redhat.com>
2017-05-01 07:40:50 -0700

    ostree: Handle no remotes cases
    Fixes #6277

--- pkg/ostree/remotes.js
+++ pkg/ostree/remotes.js
@@ -40,7 +40,11 @@ angular.module('ostree.remotes', [
             return cockpit.spawn(["ostree", "remote", "list"],
                                  { "superuser" : "try", "err" : "message"}).
                 then(function(data) {
-                    var d = data.trim().split(/\r\n|\r|\n/);
+                    var d = [];
+                    angular.forEach(data.trim().split(/\r\n|\r|\n/), function (v, k) {
+                        if (v)
+                            d.push(v);
+                    });
                     return d.sort();
                 });
         }
@@ -55,7 +59,7 @@ angular.module('ostree.remotes', [
                             var parts = v.split(":");
                             if (parts.length > 1)
                                 d.push(parts.slice(1).join(":"));
-                            else
+                            else if (v)
                                 d.push(v);
                         });
                         return d.sort();

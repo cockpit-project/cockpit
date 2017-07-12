3384b65ca9af204e2f26c724a459999eab31c653
Martin Pitt <martinpitt@users.noreply.github.com>
2017-07-12 09:50:58 +0200

    test: Add another expected restart journal message
    Observed in `check-connection TestConnection.testBasic`:
    
        > transport closed: terminated
        Unexpected journal message 'peer did not close io when expected'
    
    Closes #7206

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -663,6 +663,7 @@ class MachineCase(unittest.TestCase):
                                     ".*Broken pipe.*",
                                     "g_dbus_connection_real_closed: Remote peer vanished with error: Underlying GIOStream returned 0 bytes on an async read \\(g-io-error-quark, 0\\). Exiting.",
                                     "connection unexpectedly closed by peer",
+                                    "peer did not close io when expected",
                                     # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1141137
                                     "localhost: bridge program failed: Child process killed by signal 9",
                                     "request timed out, closing",

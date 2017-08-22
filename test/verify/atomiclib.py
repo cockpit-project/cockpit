14f1448b02c4f6e605a173ded9be2b8c77f44a3a
petervo <petervo@redhat.com>
2017-08-22 10:46:58 -0700

    test: Enable all the multi machine tests on atomic

---
+++ test/verify/atomiclib.py
@@ -0,0 +1,10 @@
+def overlay_dashboard(m):
+    # If we can do package overlays on Atomic host, then try that here
+    # This tests the case that cockpit-dashboard is installable via
+    # package overlays on Atomic Host
+    # The libssh RPM was placed by the atomic setup scripts
+
+    if m.image in [ "fedora-atomic", "rhel-atomic", "continuous-atomic" ]:
+        m.execute("rm -rf /etc/yum.repos.d/* && rpm-ostree install /root/rpms/libssh-*.rpm /var/tmp/build-results/cockpit-dashboard-*.rpm")
+        m.spawn("sleep 2 && systemctl reboot", "reboot")
+        m.wait_reboot()

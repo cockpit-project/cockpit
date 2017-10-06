bebaa6b5965b95265765fa6b8b204e80563c2edc
Marius Vollmer <mvollmer@redhat.com>
2017-10-06 15:15:39 +0300

    storage: Fix crypto option editing with no stored passphrase

--- pkg/storaged/crypto-tab.jsx
+++ pkg/storaged/crypto-tab.jsx
@@ -54,6 +54,14 @@ var CryptoTab = React.createClass({
                 function (items) {
                     old_config = utils.array_find(items, function (c) { return c[0] == "crypttab"; });
                     new_config = [ "crypttab", old_config? $.extend({ }, old_config[1]) : { } ];
+
+                    // UDisks insists on always having a "passphrase-contents" field when
+                    // adding a crypttab entry, but doesn't include one itself when returning
+                    // an entry without a stored passphrase.
+                    //
+                    if (!new_config[1]['passphrase-contents'])
+                        new_config[1]['passphrase-contents'] = { t: 'ay', v: utils.encode_filename("") };
+
                     modify (new_config[1], commit);
                 });
         }

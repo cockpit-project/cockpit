dc5bda55c18055fc53c9c93919a970657377959a
Martin Pitt <martin@piware.de>
2017-10-12 12:38:31 +0200

    welder: Drop boostrap import
    It causes modal.js, dropdown.js, and others to get added to welder.js,
    but these are already being pulled in through cockpit's base/jquery.js.
    This caused duplicated event handlers, leading to non-functioning
    dialogs and buttons.

--- pkg/welder/main.jsx
+++ pkg/welder/main.jsx
@@ -5,7 +5,6 @@ import React from 'react';  // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 import FastClick from 'fastclick';
 import { Provider } from 'react-redux';
-import 'bootstrap';
 import 'bootstrap-select';
 
 import store from './core/store';

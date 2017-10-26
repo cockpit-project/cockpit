b015bbf8c11fe3b0a7bd6d89d4869600676ca246
Bohdan Iakymets <biakymet@redhat.com>
2017-10-26 15:15:40 +0200

    Added InfoRecord element, for nice table grouping detail information

---
+++ pkg/machines/components/infoRecord.jsx
@@ -0,0 +1,47 @@
+/*jshint esversion: 6 */
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, { PropTypes } from "react";
+
+React;
+
+const InfoRecord = ({id, descr, value}) => {
+    return (<tr>
+        <td className='top'>
+            <label className='control-label'>
+                {descr}
+            </label>
+        </td>
+        <td id={id}>
+            {value}
+        </td>
+    </tr>);
+};
+
+InfoRecord.propTypes = {
+    id: PropTypes.string,
+    descr: PropTypes.string.isRequired,
+    value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.element
+    ]).isRequired,
+}
+
+export default InfoRecord;

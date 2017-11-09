0532859481258ecb4e67c46ff83863e2e316145f
Martin Pitt <martin@piware.de>
2017-11-09 14:27:33 +0100

    Move integration tests from PhantomJS to Chrome Debug Protocol
    Introduce cdp-driver.js to provide a line-based CLI interface between
    the CDP and our Python test suite.
    `node_modules/.bin/chrome-remote-interface inspect` is also a command
    line interface, but it crashes if stdout is not a tty, so we need our
    own driver program anyway. This also handles frame tracking, which
    unfortunately is very involved with the current stable Chome Debug
    Protocol 1.2 at least.
    
    CDP allows a test to attach to an already existing browser, to
    interactively follow (and influence) what a test is doing. Describe this
    in test/README.
    
    Remove the checkpoint concept from waiting for a condition. Every
    browser except for PhantomJS runs the web page's code without having to
    get prodded for progress.
    
    A significant behaviour change is that tests now don't ignore invalid
    SSL certificates by default any more (as with the previous
    `phantom-command`), but this needs to be explicitly enabled on a
    per-test basis through `Browser.ignore_ssl_certificate_errors()`. This
    is only needed for the container tests, and brings the other tests
    closer to real browser behaviour.
    
    Keep phantomjs for the unit tests for now, as tools/tap-phantom needs to
    be ported. This can and should happen separately from the integration
    tests.
    
    Closes #8069

---
+++ test/common/cdp-driver.js
@@ -0,0 +1,238 @@
+#!/usr/bin/env node
+
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* cdp-driver -- A command-line JSON input/output wrapper around
+ * chrome-remote-interface (Chrome Debug Protocol).
+ * See https://chromedevtools.github.io/devtools-protocol/
+ * This needs support for protocol version 1.3.
+ *
+ * Set $TEST_CDP_DEBUG environment variable to enable additional
+ * frame/execution context debugging.
+ */
+
+const CDP = require('chrome-remote-interface');
+
+var enable_debug = false;
+
+function debug(msg) {
+    if (enable_debug)
+        process.stderr.write("CDP: " + msg + "\n");
+}
+
+/**
+ * Format response to the client
+ */
+
+function fatal() {
+    console.error.apply(console.error, arguments);
+    process.exit(1);
+}
+
+function fail(err) {
+    process.stdout.write(JSON.stringify({"error": err}) + '\n');
+}
+
+function success(result) {
+    process.stdout.write(JSON.stringify({"result": result === undefined ? null : result}) + '\n');
+}
+
+/**
+ * Record console.*() calls and Log messages so that we can forward them to
+ * stderr and dump them on test failure
+ */
+var messages = [];
+
+function setupLogging(client) {
+    client.Runtime.enable();
+
+    client.Runtime.consoleAPICalled(info => {
+        let msg = info.type + ": " + info.args.map(v => (v.value || "").toString()).join(" ");
+        messages.push(msg);
+        process.stderr.write("> " + msg + "\n")
+    });
+
+    client.Log.enable();
+    client.Log.entryAdded(entry => process.stderr.write("CDP: " + JSON.stringify(entry["entry"]) + "\n"));
+}
+
+/**
+ * Frame tracking
+ *
+ * For tests to be able to select the current frame (by its name) and make
+ * subsequent queries apply to that, we need to track frame name → frameId →
+ * executionContextId. Frame and context IDs can even change through page
+ * operations (e. g. in systemd/logs.js when reporting a crash is complete),
+ * so we also need a helper function to explicitly wait for a particular frame
+ * to load. This is very laborious, see this issue for discussing improvements:
+ * https://github.com/ChromeDevTools/devtools-protocol/issues/72
+ */
+var frameIdToContextId = {};
+var frameNameToFrameId = {};
+
+// set these to wait for a frame to be loaded
+var frameWaitName = null;
+var frameWaitPromiseResolve = null;
+
+function setupFrameTracking(client) {
+    client.Page.enable();
+
+    // map frame names to frame IDs; root frame has no name, no need to track that
+    client.Page.frameNavigated(info => {
+        debug("frameNavigated " + JSON.stringify(info));
+        if (info.frame.name)
+            frameNameToFrameId[info.frame.name] = info.frame.id;
+
+        // were we waiting for this frame to be loaded?
+        if (frameWaitPromiseResolve && frameWaitName === info.frame.name) {
+            frameWaitPromiseResolve();
+            frameWaitPromiseResolve = null;
+        }
+    });
+
+    // track execution contexts so that we can map between context and frame IDs
+    client.Runtime.executionContextCreated(info => {
+        debug("executionContextCreated " + JSON.stringify(info));
+        frameIdToContextId[info.context.auxData.frameId] = info.context.id;
+    });
+
+    client.Runtime.executionContextDestroyed(info => {
+        debug("executionContextDestroyed " + info.executionContextId);
+        for (let frameId in frameIdToContextId) {
+            if (frameIdToContextId[frameId] == info.executionContextId) {
+                delete frameIdToContextId[frameId];
+                break;
+            }
+        }
+    });
+}
+
+
+// helper functions for testlib.py which are too unwieldy to be poked in from Python
+function getFrameExecId(frame) {
+    var frameId = frameNameToFrameId[frame];
+    if (!frameId)
+        throw Error(`Frame ${frame} is unknown`);
+    var execId = frameIdToContextId[frameId];
+    if (!execId)
+        throw Error(`Frame ${frame} (${frameId}) has no executionContextId`);
+    return execId;
+}
+
+function expectLoadFrame(name, timeout) {
+    return new Promise((resolve, reject) => {
+        let tm = setTimeout( () => reject("timed out waiting for frame load"), timeout );
+
+        // we can only have one Page.frameNavigated() handler, so let our handler above resolve this promise
+        frameWaitName = name;
+        new Promise((fwpResolve, fwpReject) => { frameWaitPromiseResolve = fwpResolve })
+            .then(() => {
+                // For the frame to be fully valid for queries, it also needs the corresponding
+                // executionContextCreated() signal. This might happen before or after frameNavigated(), so wait in case
+                // it happens afterwards.
+               function pollExecId() {
+                    if (frameIdToContextId[frameNameToFrameId[name]]) {
+                        clearTimeout(tm);
+                        resolve();
+                    } else {
+                        setTimeout(pollExecId, 100);
+                    }
+                }
+                pollExecId();
+            });
+    });
+}
+
+/**
+ * SSL handling
+ */
+
+// secure by default; tests can override to "continue"
+// https://chromedevtools.github.io/devtools-protocol/1-3/Security/#type-CertificateErrorAction
+var ssl_bad_certificate_action = "cancel";
+
+function setupSSLCertHandling(client) {
+    client.Security.enable();
+
+    client.Security.setOverrideCertificateErrors({override: true});
+    client.Security.certificateError(info => {
+        process.stderr.write(`CDP: Security.certificateError ${JSON.stringify(info)}; action: ${ssl_bad_certificate_action}\n`);
+        client.Security.handleCertificateError({ eventId: info.eventId, action: ssl_bad_certificate_action });
+    });
+}
+
+/**
+ * Main input/process loop
+ *
+ * Read one line with a JS expression, eval() it, and respond with the result:
+ *    success <JSON formatted return value>
+ *    fail <JSON formatted error>
+ * EOF shuts down the client.
+ */
+process.stdin.setEncoding('utf8');
+
+if (process.env["TEST_CDP_DEBUG"])
+    enable_debug = true;
+
+options = { };
+if (process.argv.length >= 3) {
+    options.port = parseInt(process.argv[2]);
+    if (!options.port) {
+        process.stderr.write("Usage: cdp-driver.js [port]\n");
+        process.exit(1);
+    }
+}
+
+CDP.New(options)
+    .then(target => {
+        target.port = options.port;
+        CDP({target: target})
+            .then(client => {
+                setupLogging(client);
+                setupFrameTracking(client);
+                setupSSLCertHandling(client);
+
+                let input_buf = '';
+                process.stdin
+                    .on('data', chunk => {
+                        input_buf += chunk;
+                        while (true) {
+                            let i = input_buf.indexOf('\n');
+                            if (i < 0)
+                                break;
+
+                            // run the command
+                            eval(input_buf.slice(0, i))
+                                .then(success)
+                                .catch(fail);
+
+                            input_buf = input_buf.slice(i+1);
+                        }
+
+                    })
+                   .on('end', () => {
+                       CDP.Close(target)
+                           .then(() => process.exit(0))
+                           .catch(fatal);
+                   });
+            })
+            .catch(fatal);
+    })
+    .catch(fatal);

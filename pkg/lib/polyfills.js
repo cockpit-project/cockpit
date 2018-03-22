8af483945b7e73a6c0ff8fcf32de24b1243eefee
Marius Vollmer <mvollmer@redhat.com>
2018-03-22 15:14:07 +0200

    lib: Add endsWith polyfill

--- pkg/lib/polyfills.js
+++ pkg/lib/polyfills.js
@@ -33,6 +33,16 @@ if (!String.prototype.startsWith) {
     };
 }
 
+// for IE 11
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
+
 if (!Array.prototype.findIndex) {
     Array.prototype.findIndex = function (predicate) {
         if (this === null) {

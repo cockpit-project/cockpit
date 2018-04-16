c72b9f691e0b6dea84ea905b3c71e0d77ee80435
Katerina Koukiou <kkoukiou@redhat.com>
2018-04-16 14:09:24 +0200

    Implement Rename method for Domain Interface
    Signed-off-by: Katerina Koukiou <kkoukiou@redhat.com>
    Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

--- src/domain.c
+++ src/domain.c
@@ -1257,6 +1257,31 @@ virtDBusDomainReboot(GVariant *inArgs,
 }
 
 static void
+virtDBusDomainRename(GVariant *inArgs,
+                     GUnixFDList *inFDs G_GNUC_UNUSED,
+                     const gchar *objectPath,
+                     gpointer userData,
+                     GVariant **outArgs G_GNUC_UNUSED,
+                     GUnixFDList **outFDs G_GNUC_UNUSED,
+                     GError **error)
+
+{
+    virtDBusConnect *connect = userData;
+    g_autoptr(virDomain) domain = NULL;
+    const gchar *name;
+    guint flags;
+
+    g_variant_get(inArgs, "(&su)", &name, &flags);
+
+    domain = virtDBusDomainGetVirDomain(connect, objectPath, error);
+    if (!domain)
+        return;
+
+    if (virDomainRename(domain, name, flags) < 0)
+        virtDBusUtilSetLastVirtError(error);
+}
+
+static void
 virtDBusDomainReset(GVariant *inArgs,
                     GUnixFDList *inFDs G_GNUC_UNUSED,
                     const gchar *objectPath,
@@ -1500,6 +1525,7 @@ static virtDBusGDBusMethodTable virtDBusDomainMethodTable[] = {
     { "MigrateSetMaxSpeed", virtDBusDomainMigrateSetMaxSpeed },
     { "MigrateStartPostCopy", virtDBusDomainMigrateStartPostCopy },
     { "Reboot", virtDBusDomainReboot },
+    { "Rename", virtDBusDomainRename },
     { "Reset", virtDBusDomainReset },
     { "Resume", virtDBusDomainResume },
     { "SendKey", virtDBusDomainSendKey },

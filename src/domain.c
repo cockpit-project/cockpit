ef206b5cfefe90a71cf5a9422864a29caa7d696b
Katerina Koukiou <kkoukiou@redhat.com>
2018-04-24 11:37:06 +0200

    Implement GetInterfaceParameters method for Domain Interface
    Signed-off-by: Katerina Koukiou <kkoukiou@redhat.com>
    Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

--- src/domain.c
+++ src/domain.c
@@ -1179,6 +1179,46 @@ virtDBusDomainGetHostname(GVariant *inArgs,
 }
 
 static void
+virtDBusDomainGetInterfaceParameters(GVariant *inArgs,
+                                     GUnixFDList *inFDs G_GNUC_UNUSED,
+                                     const gchar *objectPath,
+                                     gpointer userData,
+                                     GVariant **outArgs,
+                                     GUnixFDList **outFDs G_GNUC_UNUSED,
+                                     GError **error)
+{
+    virtDBusConnect *connect = userData;
+    g_autoptr(virDomain) domain = NULL;
+    g_auto(virtDBusUtilTypedParams) params = { 0 };
+    const gchar *device;
+    guint flags;
+    gint ret;
+    GVariant *grecords;
+
+    g_variant_get(inArgs, "(&su)", &device, &flags);
+
+    domain = virtDBusDomainGetVirDomain(connect, objectPath, error);
+    if (!domain)
+        return;
+
+    ret = virDomainGetInterfaceParameters(domain, device, NULL,
+                                          &params.nparams, flags);
+    if (ret < 0)
+        return virtDBusUtilSetLastVirtError(error);
+    if (params.nparams != 0) {
+        params.params = g_new0(virTypedParameter, params.nparams);
+        if (virDomainGetInterfaceParameters(domain, device, params.params,
+                                            &params.nparams, flags) < 0) {
+            return virtDBusUtilSetLastVirtError(error);
+        }
+    }
+
+    grecords = virtDBusUtilTypedParamsToGVariant(params.params, params.nparams);
+
+    *outArgs = g_variant_new_tuple(&grecords, 1);
+}
+
+static void
 virtDBusDomainGetJobInfo(GVariant *inArgs G_GNUC_UNUSED,
                          GUnixFDList *inFDs G_GNUC_UNUSED,
                          const gchar *objectPath,
@@ -2260,6 +2300,7 @@ static virtDBusGDBusMethodTable virtDBusDomainMethodTable[] = {
     { "GetFSInfo", virtDBusDomainGetFSInfo },
     { "GetGuestVcpus", virtDBusDomainGetGuestVcpus },
     { "GetHostname", virtDBusDomainGetHostname },
+    { "GetInterfaceParameters", virtDBusDomainGetInterfaceParameters },
     { "GetJobInfo", virtDBusDomainGetJobInfo },
     { "GetMemoryParameters", virtDBusDomainGetMemoryParameters },
     { "GetSchedulerParameters", virtDBusDomainGetSchedulerParameters },

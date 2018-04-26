430bbc12149ecb99fdf312d7e0c0da7210c0b9fe
Katerina Koukiou <kkoukiou@redhat.com>
2018-04-26 19:35:39 +0200

    Implement PinEmulator method for Domain Interface
    Signed-off-by: Katerina Koukiou <kkoukiou@redhat.com>
    Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

--- src/domain.c
+++ src/domain.c
@@ -2103,6 +2103,45 @@ virtDBusDomainMigrateStartPostCopy(GVariant *inArgs,
 }
 
 static void
+virtDBusDomainPinEmulator(GVariant *inArgs,
+                          GUnixFDList *inFDs G_GNUC_UNUSED,
+                          const gchar *objectPath,
+                          gpointer userData,
+                          GVariant **outArgs G_GNUC_UNUSED,
+                          GUnixFDList **outFDs G_GNUC_UNUSED,
+                          GError **error)
+
+{
+    virtDBusConnect *connect = userData;
+    g_autoptr(virDomain) domain = NULL;
+    g_autoptr(GVariantIter) iter = NULL;
+    guint flags;
+    guint cpus;
+    guint cpumaplen;
+    g_autofree guchar *cpumap = NULL;
+    gboolean usable;
+    guint cnt = 0;
+
+    g_variant_get(inArgs, "(abu)", &iter, &flags);
+
+    domain = virtDBusDomainGetVirDomain(connect, objectPath, error);
+    if (!domain)
+        return;
+
+    cpus = g_variant_iter_n_children(iter);
+    cpumaplen = VIR_CPU_MAPLEN(cpus);
+    cpumap = g_new0(guchar, cpumaplen);
+    while (g_variant_iter_loop(iter, "b", &usable)) {
+        if (usable)
+            VIR_USE_CPU(cpumap, cnt);
+        cnt++;
+    }
+
+    if (virDomainPinEmulator(domain, cpumap, cpumaplen, flags) < 0)
+        virtDBusUtilSetLastVirtError(error);
+}
+
+static void
 virtDBusDomainReboot(GVariant *inArgs,
                      GUnixFDList *inFDs G_GNUC_UNUSED,
                      const gchar *objectPath,
@@ -2833,6 +2872,7 @@ static virtDBusGDBusMethodTable virtDBusDomainMethodTable[] = {
     { "MigrateSetMaxDowntime", virtDBusDomainMigrateSetMaxDowntime },
     { "MigrateSetMaxSpeed", virtDBusDomainMigrateSetMaxSpeed },
     { "MigrateStartPostCopy", virtDBusDomainMigrateStartPostCopy },
+    { "PinEmulator", virtDBusDomainPinEmulator },
     { "Reboot", virtDBusDomainReboot },
     { "Rename", virtDBusDomainRename },
     { "Reset", virtDBusDomainReset },

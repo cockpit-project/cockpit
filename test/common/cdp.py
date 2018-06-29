e6599a065544767c80bcc0cb586bb8115ecb1adf
Martin Pitt <martin@piware.de>
2018-06-29 12:17:41 +0200

    test: Disable pushState() throttling
    Chromium 67 finally fixes window.history.pushState() to not
    [silently fail](https://bugs.chromium.org/p/chromium/issues/detail?id=794923)
    any more. Disable the throttling and drop the sleep hack from commit
    9db1e74a85a.
    
    This should also fix some flakes in other tests, e. g.
    `TestDashboardEditLimits` was also running into this.
    
    Closes #9540

--- test/common/cdp.py
+++ test/common/cdp.py
@@ -183,8 +183,8 @@ class CDP:
             self._browser = subprocess.Popen(
                 argv + ["--disable-gpu", "--no-sandbox", "--disable-setuid-sandbox",
                     "--disable-namespace-sandbox", "--disable-seccomp-filter-sandbox",
-                    "--disable-sandbox-denial-logging", "--window-size=1280x1200",
-                    "--remote-debugging-port=%i" % cdp_port, "about:blank"],
+                    "--disable-sandbox-denial-logging", "--disable-pushstate-throttle",
+                    "--window-size=1280x1200", "--remote-debugging-port=%i" % cdp_port, "about:blank"],
                 env=environ, close_fds=True)
             if self.verbose:
                 sys.stderr.write("Started %s (pid %i) on port %i\n" % (exe, self._browser.pid, cdp_port))

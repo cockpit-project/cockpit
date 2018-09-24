f54b8a5343f2a31abc25273b5dfb5fa2257612a1
Martin Pitt <martin@piware.de>
2018-09-24 08:42:43 +0200

    storage: Make luksmeta-monitor-hack.py bilingual
    So that we can use it with Python 2 on RHEL 7.

--- pkg/storaged/luksmeta-monitor-hack.py
+++ pkg/storaged/luksmeta-monitor-hack.py
@@ -13,7 +13,7 @@ def b64_decode(data):
     # The data we get doesn't seem to have any padding, but the base64
     # module requires it.  So we add it back.  Can't anyone agree on
     # anything?  Not even base64?
-    return base64.urlsafe_b64decode(data + '=' * ((4 - len(data) % 4) % 4))
+    return base64.urlsafe_b64decode((data + '=' * ((4 - len(data) % 4) % 4)).encode('ascii', 'ignore'))
 
 def get_clevis_config_from_protected_header(protected_header):
     header = b64_decode(protected_header).decode("utf-8")
@@ -45,13 +45,14 @@ def info(dev):
     version = 1
     max_slots = 8
 
-    result = subprocess.run([ "cryptsetup", "luksDump", dev ], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    if result.returncode != 0:
+    try:
+        result = subprocess.check_output([ "cryptsetup", "luksDump", dev ], stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
         return { "version": version, "slots": [ ], "max_slots": max_slots }
 
     in_luks2_slot_section = False
     in_luks2_token_section = False
-    for line in result.stdout.splitlines():
+    for line in result.splitlines():
         if not (line.startswith(b" ") or line.startswith(b"\t")):
             in_luks2_slot_section = False
             in_luks2_token_section = False
@@ -63,36 +64,40 @@ def info(dev):
             in_luks2_token_section = True
 
         if in_luks2_slot_section:
-            match = re.fullmatch(b"  ([0-9]+): luks2", line)
+            match = re.match(b"  ([0-9]+): luks2$", line)
         else:
-            match = re.fullmatch(b"Key Slot ([0-9]+): ENABLED", line)
+            match = re.match(b"Key Slot ([0-9]+): ENABLED$", line)
         if match:
             slot = int(match.group(1))
             entry = { "Index": { "v": slot } }
             if version == 1:
-                luksmeta = subprocess.run([ "luksmeta", "load", "-d", dev, "-s", str(slot),
-                                            "-u", "cb6e8904-81ff-40da-a84a-07ab9ab5715e" ],
-                                          stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-                if (luksmeta.returncode == 0):
+                try:
+                    luksmeta = subprocess.check_output([ "luksmeta", "load", "-d", dev, "-s", str(slot),
+                                                "-u", "cb6e8904-81ff-40da-a84a-07ab9ab5715e" ],
+                                              stderr=subprocess.PIPE)
                     entry["ClevisConfig"] = {
-                        "v": json.dumps(get_clevis_config_from_jwe(luksmeta.stdout.decode("utf-8")))
+                        "v": json.dumps(get_clevis_config_from_jwe(luksmeta.decode("utf-8")))
                     }
+                except subprocess.CalledProcessError:
+                    pass
             if slot not in slots:
                 slots[slot] = entry
 
         if in_luks2_token_section:
-            match = re.fullmatch(b"  ([0-9]+): clevis", line)
+            match = re.match(b"  ([0-9]+): clevis$", line)
             if match:
-                token = subprocess.run([ "cryptsetup", "token", "export", dev, "--token-id", match.group(1) ],
-                                       stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-                if (token.returncode == 0):
-                    token_object = json.loads(token.stdout.decode("utf-8"))
+                try:
+                    token = subprocess.check_output([ "cryptsetup", "token", "export", dev, "--token-id", match.group(1) ],
+                                                    stderr=subprocess.PIPE)
+                    token_object = json.loads(token.decode("utf-8"))
                     if token_object.get("type") == "clevis":
                         config = json.dumps(get_clevis_config_from_protected_header(token_object["jwe"]["protected"]))
                         for slot_str in token_object.get("keyslots", [ ]):
                             slot = int(slot_str)
                             slots[slot] = { "Index": { "v": slot },
                                             "ClevisConfig": { "v": config } }
+                except subprocess.CalledProcessError:
+                    pass
 
     return { "version": version, "slots": list(slots.values()), "max_slots": max_slots }
 

3f04aeb50c8c99990bf90cb85e9cc48514c1ad60
Katerina Koukiou <kkoukiou@redhat.com>
2018-11-23 17:38:00 +0100

    tests: Run isort to automatically fix wrong import order

--- test/verify/machineslib.py
+++ test/verify/machineslib.py
@@ -17,14 +17,14 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
-import parent
-from testlib import *
+import functools
+import os
 import subprocess
+import time
 import unittest
 
-import os
-import functools
-import time
+import parent
+from testlib import *
 
 
 def readFile(name):

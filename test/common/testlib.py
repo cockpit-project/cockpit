7e7920beaf9717063df7d989be18e2b2eab73cab
Martin Pitt <martin@piware.de>
2018-12-04 21:06:48 +0100

    bots, test: Drop Fedora 28 release, image, and tests
    Everything in CI and external projects was moved to Fedora 29, and that
    has been out long enough now to be the established stable release.
    
    Closes #10761

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -875,14 +875,6 @@ class MachineCase(unittest.TestCase):
         if "TEST_AUDIT_NO_SELINUX" not in os.environ:
             messages += machine.audit_messages("14", cursor=cursor) # 14xx is selinux
 
-        # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1557913
-        # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1563143
-        # these fail tons of tests due to the SELinux violations (so naughty override causes too much spamming)
-        if self.image in ['fedora-28']:
-            self.allowed_messages.append('audit: type=1400 audit(.*): avc:  denied  { dac_override }.*')
-            self.allowed_messages.append('audit: type=1400 audit(.*): avc:  denied  { module_request }.*')
-            self.allowed_messages.append('audit: type=1400 audit(.*): avc:  denied  { getattr } for .* comm="which" path="/usr/sbin/setfiles".*')
-
         if self.image in ['fedora-29', 'fedora-testing', 'fedora-i386', 'fedora-atomic']:
             # HACK: https://bugzilla.redhat.com/show_bug.cgi?id=1563143
             self.allowed_messages.append('audit: type=1400 audit(.*): avc:  denied  { getattr } for .* comm="which" path="/usr/sbin/setfiles".*')

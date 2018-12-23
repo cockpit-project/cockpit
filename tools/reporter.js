1c88464e316d81b034aa367214f1b5dd478936b8
Martin Pitt <martin@piware.de>
2018-12-23 21:58:50 +0100

    tools: Drop unused reporter.js

--- tools/reporter.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    reporter: function (errors) {
-        var err;
-        for (var i = 0; i < errors.length; i++) {
-            err = errors[i].error;
-            console.log(errors[i].file + ":" + err.line + ":" + err.character + ": " + err.reason);
-        }
-    }
-};

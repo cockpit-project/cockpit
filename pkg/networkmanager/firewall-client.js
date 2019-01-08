f7bd457fe8c67866dcb5157c61de4a15e0021fe8
Martin Pitt <martinpitt@users.noreply.github.com>
2019-01-08 15:00:42 +0100

    networkmanager: Connect to firewalld as superuser
    This makes the page work when connecting as a user which has sudo, but
    not polkit privileges. (I. e. when they are not in the `wheel` group,
    but have an explicit user sudo rule).
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1664158
    Closes #10926

--- pkg/networkmanager/firewall-client.js
+++ pkg/networkmanager/firewall-client.js
@@ -34,7 +34,7 @@ const firewalld_service = service.proxy('firewalld');
 var firewalld_dbus = null;
 
 function initFirewalldDbus() {
-    firewalld_dbus = cockpit.dbus('org.fedoraproject.FirewallD1');
+    firewalld_dbus = cockpit.dbus('org.fedoraproject.FirewallD1', { superuser: "try" });
 
     firewalld_dbus.addEventListener('owner', (event, owner) => {
         firewall.enabled = !!owner;

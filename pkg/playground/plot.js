9c7e8181f39a91dbdf5e60832e01bc417317056c
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2019-03-19 12:18:48 +0100

    playground: Make sure plot is visible when page loads
    Construct plot after the page loads, otherwise the plot will try to
    attach itself to a non-existent element.
    
    Explicitely set plot height.

--- pkg/playground/plot.js
+++ pkg/playground/plot.js
@@ -3,12 +3,13 @@ import * as plot from "plot.js";
 
 import "plot.css";
 
-var pl = new plot.Plot($('#plot'), 300);
-pl.set_options(plot.plot_simple_template());
-pl.add_metrics_sum_series({ direct: [ "mem.util.used" ]
-}, { });
 $(function () {
+    var pl = new plot.Plot($('#plot'), 300);
+    pl.set_options(plot.plot_simple_template());
+    pl.add_metrics_sum_series({ direct: [ "mem.util.used" ], }, { });
+
     $("body").show();
+    $("#plot").css({ height: "200px" });
     pl.resize();
 
     var plot_controls = plot.setup_plot_controls($('body'), $('#toolbar'));

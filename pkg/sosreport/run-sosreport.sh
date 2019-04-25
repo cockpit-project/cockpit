1a74bafded6a8b759f90cf0bbacac8bb61210a29
Matej Marusak <mmarusak@redhat.com>
2019-04-25 06:19:05 +0200

    sosreport: Use support-tools container instead of rhel-tools
    Quite some time ago `sosreport` has been dropped from `rhel-tools`
    image (as the image was too big) and it is advised to use
    `support-tools` image instead.
    
    Also I drop naughty for issue #7462 - as the underlying issue has been
    fixed a long time ago but this naughty has been catching this missing
    command.
    
    Fixes #7462
    Closes #11666

--- pkg/sosreport/run-sosreport.sh
+++ pkg/sosreport/run-sosreport.sh
@@ -14,7 +14,7 @@ elif present atomic; then
     # https://bugzilla.redhat.com/show_bug.cgi?id=1299794
     # https://bugzilla.redhat.com/show_bug.cgi?id=1277223
 
-    exec atomic run rhel7/rhel-tools -- sosreport --sysroot /host --tmp-dir /host/var/tmp "$@"
+    exec atomic run rhel7/support-tools -- sosreport --sysroot /host --tmp-dir /host/var/tmp "$@"
 else
     echo >&2 "The sosreport utility is not installed.  This should not happen.  Please complain to your distribution vendor."
     exit 1

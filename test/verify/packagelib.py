c2e036234c7bbc76303f3228acba48141b69d7a5
Martin Pitt <martinpitt@users.noreply.github.com>
2019-04-30 14:54:19 +0200

    bots, test: Introduce rhel-8-0-distropkg mode
    We will use this to test the rhel-8-appstream branch as well as z-stream
    updates.
    
    Closes #11695

--- test/verify/packagelib.py
+++ test/verify/packagelib.py
@@ -36,7 +36,7 @@ class PackageCase(MachineCase):
         # expected backend; hardcode this on image names to check the auto-detection
         if self.machine.image.startswith("debian") or self.machine.image.startswith("ubuntu"):
             self.backend = "apt"
-        elif self.machine.image.startswith("fedora") or self.machine.image in ["rhel-8-0", "rhel-8-1"]:
+        elif self.machine.image.startswith("fedora") or self.machine.image in ["rhel-8-0", "rhel-8-0-distropkg", "rhel-8-1"]:
             self.backend = "dnf"
         elif self.machine.image in ["centos-7", "rhel-7-6", "rhel-7-6-distropkg", "rhel-7-7"]:
             self.backend = "yum"

6afb53fea8a91906ad0a7368b61b23a47860d7ba
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2019-05-19 11:39:06 +0200

    bots: fix 'no-test' label
    The labels_of_pull() function was returning an iterable, but in some cases the
    return value was being evaluated more than once.  The first evaluation
    would be fine, but all evaluations after the first would just see an
    empty list (since the values had already been consumed).
    
    Let's just return a list here to avoid the problem.
    
    We can also take the chance to simplify code in a few other places since
    we now have a list.
    
    Closes #11854

--- bots/task/__init__.py
+++ bots/task/__init__.py
@@ -471,7 +471,7 @@ def label(issue, labels=['bot']):
 def labels_of_pull(pull):
     if "labels" not in pull:
         pull["labels"] = api.get("issues/{0}/labels".format(pull["number"]))
-    return map(lambda label: label["name"], pull["labels"])
+    return list(map(lambda label: label["name"], pull["labels"]))
 
 def comment(issue, comment):
     try:

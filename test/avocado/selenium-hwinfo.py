db73acee8a104c29d08cba6cfce22461628c37f6
jscotka <jscotka@redhat.com>
2019-05-24 17:21:15 +0200

    test: Add selenium tests for kdump and hwinfo page
    Closes #11837

---
+++ test/avocado/selenium-hwinfo.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import os
+import sys
+
+machine_test_dir = os.path.dirname(os.path.realpath(__file__))
+if machine_test_dir not in sys.path:
+    sys.path.insert(1, machine_test_dir)
+
+from testlib_avocado.seleniumlib import SeleniumTest, clickable, visible
+
+
+class TestHWinfo(SeleniumTest):
+    """
+    :avocado: enable
+    """
+
+    lscpu = """#!/bin/sh
+    echo 'CPU(s):              8'
+    echo 'On-line CPU(s) list: 0-7'
+    echo 'Thread(s) per core:  2'
+    echo 'Core(s) per socket:  4'
+    echo 'Socket(s):           1'
+    """
+    lscpu_file = '/usr/local/bin/lscpu'
+
+    def setUp(self):
+        super().setUp()
+        self.login()
+        cmd = "cat | sudo tee '%s'" % self.lscpu_file
+        self.machine.execute(command=cmd, input=self.lscpu)
+        self.machine.execute('sudo chmod a+x {}'.format(self.lscpu_file))
+        self.click(self.wait_link('System', cond=clickable))
+        self.wait_frame("localhost/system")
+
+        self.click(self.wait_id("system_information_hardware_text"))
+        self.mainframe()
+        self.wait_frame("localhost/system/hwinfo")
+        self.wait_id("hwinfo", jscheck=True)
+
+    def tearDown(self):
+        self.machine.execute('sudo rm -f {}'.format(self.lscpu_file))
+        super().tearDown()
+
+    def testCPUinfo(self):
+        self.wait_text("BIOS", cond=visible)
+        cpuinfo = self.machine.execute("cat /proc/cpuinfo | grep  'model name' |cut -d ':' -f 2").strip()
+        self.wait_text(cpuinfo, cond=visible)
+
+    def testSMT(self):
+
+        self.click(self.wait_link("Mitigations"))
+        self.wait_id("cpu-mitigations-dialog", jscheck=True)
+        self.click(self.wait_id("nosmt-switch"))
+        self.wait_text("Save and reboot", cond=clickable)
+        self.click(self.wait_text("Cancel", cond=clickable))
+        self.click(self.wait_link("Mitigations", cond=clickable))
+        self.mainframe()
---
+++ test/avocado/selenium-kdump.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+import os
+import sys
+import time
+machine_test_dir = os.path.dirname(os.path.realpath(__file__))
+if machine_test_dir not in sys.path:
+    sys.path.insert(1, machine_test_dir)
+
+from selenium.webdriver.support.select import Select
+from testlib_avocado.seleniumlib import SeleniumTest, clickable, visible
+
+
+class TestKdump(SeleniumTest):
+    """
+    :avocado: enable
+    """
+    share = "/nfsfileshare"
+    exporttab = """
+{}    *(rw,sync,no_root_squash)
+    """.format(share)
+    export_file = '/etc/exports'
+
+
+    def requirements(self):
+        execs = [
+            # start NFS server
+            "sudo yum install -y nfs-utils || true",
+            "sudo mkdir -p {}".format(self.share),
+            "sudo chmod a+rwx {}".format(self.share),
+            "echo '{}' | sudo tee {}".format(self.exporttab, self.export_file),
+            "sudo systemctl start nfs-server rpcbind",
+            "sudo exportfs -r",
+            # TODO: these two lines are workaround, because it is unable otherwise create initrd for nfs
+            "sudo mkdir -p {}/var/crash".format(self.share),
+            "sudo mount localhost:{} /var/crash".format(self.share),
+        ]
+        for exe in execs:
+            self.machine.execute(exe)
+
+    def requirements_cleanup(self):
+        execs = [
+            "sudo umount /var/crash",
+            "sudo systemctl stop nfs-server rpcbind",
+            "echo '' | sudo tee {}".format(self.export_file),
+            "sudo rm -rf {}".format(self.share),
+        ]
+        for exe in execs:
+            self.machine.execute(exe)
+
+    def setUp(self):
+        super().setUp()
+        self.login()
+        self.requirements()
+        self.click(self.wait_link("Kernel Dump"))
+        self.wait_frame("localhost/kdump")
+        self.base_element = self.wait_id("app", jscheck=True)
+
+    def tearDown(self):
+        self.requirements_cleanup()
+        super().tearDown()
+
+    def testKdumpBasePage(self):
+        self.wait_text("Crash dump location", cond=visible)
+
+    def check_type(self, old_text, new_text, selector_item, textbox_dict):
+        self.wait_text("Service is running", cond=visible)
+        self.click(self.wait_text(old_text, cond=visible))
+        s1 = Select(self.wait_id("kdump-settings-location", cond=clickable, jscheck=True))
+        for _ in s1.options:
+            s1.select_by_visible_text(selector_item)
+        for textbox_key in textbox_dict:
+            location = self.wait_id(textbox_key)
+            self.send_keys(location, textbox_dict[textbox_key])
+        self.click(self.wait_text("Apply", cond=visible))
+        self.wait_id("app", jscheck=True)
+        self.wait_text(new_text, cond=visible)
+        self.wait_text("Service is running", cond=visible)
+
+    def testNFS(self):
+        self.check_type("locally in /var/crash",
+                        "Remote over NFS",
+                        "Remote over NFS",
+                        {"kdump-settings-nfs-mount": "localhost:{}".format(self.share)},
+                        )
+        self.check_type("Remote over NFS",
+                        "locally in /var/crash",
+                        "Local Filesystem",
+                        {"kdump-settings-local-directory": "/var/crash"},
+                        )

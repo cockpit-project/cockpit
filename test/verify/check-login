cockpit-project/cockpit

commit a9375441df7777b63a5884b72e920852a16224d1

ws: Replace pam_shells with running bridge through user's shell
There are some shells like session-recording's tlog or rsh which the
user shouldn't be able to enable or disable, and thus they aren't in
/etc/shells. But they are nevertheless a legit login shell.

So using pam_shells in commit a96ec9b8343 was too strong. Drop it, and
instead simulate what /sbin/login or ssh do by running our session
leader cockpit-bridge through the user's configured shell. This will
fail (with exit code 1) for e. g. /sbin/nologin or /bin/false; handle
that in on_pipe_close() for a nicer error feedback than
"internal-error".

However, this makes the cases of cockpit-session failing to run
cockpit-bridge, and cockpit-ws failing to  run cockpit-session itself
(broken install) indistinguishable by just looking at the exit code and
problem (1 and "access-denied") in both cases). To decide which case we
have, check if the authorization protocol already started or not.

https://bugzilla.redhat.com/show_bug.cgi?id=1631905

Closes #11575

src/ws/session.c
@@ -525,12 +525,22 @@ out:
 static int
 session (char **env)
 {
-  char *argv[] = { "cockpit-bridge", NULL };
+  char *argv[] = { NULL /* user's shell */, "-c", "exec cockpit-bridge", NULL };
   gss_key_value_set_desc store;
   struct gss_key_value_element_struct element;
   OM_uint32 major, minor;
   krb5_context k5;
+  struct passwd *pwd;
   int res;
+
+  /* determine the user's shell and run the bridge through it, so that we catch
+   * disabled accounts like /bin/false or /bin/nologin shells */
+  pwd = getpwuid (geteuid ());
+  if (!pwd)
+    errx (EX, "failed to get passwd entry for user: %m");
+  argv[0] = pwd->pw_shell;
+  if (!argv[0])
+    errx (EX, "passwd entry for user has NULL shell");
 
   if (creds != GSS_C_NO_CREDENTIAL)
     {
@@ -556,7 +566,7 @@ session (char **env)
         }
     }
 
-  debug ("executing bridge: %s", argv[0]);
+  debug ("executing cockpit-bridge through user shell %s", pwd->pw_shell);
 
   if (env)
     execvpe (argv[0], argv, env);

src/common/cockpitpipetransport.c
@@ -117,6 +117,8 @@ on_pipe_close (CockpitPipe *pipe,
             problem = "terminated";
           else if (is_cockpit && WIFEXITED (status) && WEXITSTATUS (status) == 127)
             problem = "no-cockpit";      // cockpit-bridge not installed
+          else if (is_cockpit && WIFEXITED (status) && WEXITSTATUS (status) == 1)
+            problem = "access-denied";   // disabled user shells like /bin/false or /sbin/nologin exit 1
           else if (WIFEXITED (status) && WEXITSTATUS (status) == 255)
             problem = "terminated";      // failed or got a signal, etc.
           else if (!g_spawn_check_exit_status (status, &error))

src/ws/cockpitauth.c
@@ -844,7 +844,10 @@ on_transport_closed (CockpitTransport *transport,
       if (cockpit_pipe_get_pid (pipe, NULL))
         status = cockpit_pipe_exit_status (pipe);
       g_debug ("%s: authentication process exited: %d; problem %s", session->name, status, problem);
-      if (problem)
+
+      /* we get "access-denied" both if cockpit-session cannot execute cockpit-bridge (common case)
+       * and if cockpit-session itself is not executable (corner case, messed up install) */
+      if (problem && (!session->authorize || g_strcmp0 (problem, "access-denied") != 0))
         {
           g_set_error (&error, COCKPIT_ERROR, COCKPIT_ERROR_FAILED,
                        g_strcmp0 (problem, "no-cockpit") == 0

tools/cockpit.debian.pam
@@ -3,7 +3,6 @@ auth	   required	pam_sepermit.so
 auth       substack     common-auth
 auth       optional     pam_ssh_add.so
 account    required     pam_nologin.so
-account    required     pam_shells.so
 account    include      common-account
 password   include      common-password
 # pam_selinux.so close should be the first session rule

tools/cockpit.pam
@@ -4,7 +4,6 @@ auth       substack     password-auth
 auth       include      postlogin
 auth       optional     pam_ssh_add.so
 account    required     pam_nologin.so
-account    required     pam_shells.so
 account    include      password-auth
 password   include      password-auth
 # pam_selinux.so close should be the first session rule

commit e1a6ffbbce8eba34b11489d17f82d4e991026869

session: Circumvent user shell stdout noise
Commit a9375441df7 caused Cockpit logins to fail if the user's ~/.bashrc
or ~/.profile writes anything to stdout. That interferes with the
protocol and causes an error like

    incorrect protocol: received invalid length prefix

To avoid this, route the shell's stdout to stderr (so that we can still
see it in the logs) and pass session's stdout to cockpit-bridge through
fd 3. This assumes that the shell supports `>&3` input redirection,
which is the case for any POSIX shell (ash, bash, ksh, dash, zsh) and at
least tcsh.

https://bugzilla.redhat.com/show_bug.cgi?id=1710604

Closes #11978

src/ws/session.c
@@ -25,6 +25,7 @@
 #include <gssapi/gssapi_generic.h>
 #include <gssapi/gssapi_krb5.h>
 #include <krb5/krb5.h>
+#include <fcntl.h>
 
 static char *last_txt_msg = NULL;
 static char *conversation = NULL;
@@ -525,7 +526,7 @@ out:
 static int
 session (char **env)
 {
-  char *argv[] = { NULL /* user's shell */, "-c", "exec cockpit-bridge", NULL };
+  char *argv[] = { NULL /* user's shell */, "-c", "exec cockpit-bridge >&3", NULL };
   gss_key_value_set_desc store;
   struct gss_key_value_element_struct element;
   OM_uint32 major, minor;
@@ -567,6 +568,12 @@ session (char **env)
     }
 
   debug ("executing cockpit-bridge through user shell %s", pwd->pw_shell);
+
+  /* connect our and cockpit-bridge's stdout via fd 3, to avoid stdout output
+   * from ~/.profile and friends to interfere with the protocol; route shell's
+   * stdout to its stderr, so that we can still see it in the logs */
+  if (dup2 (1, 3) < 0 || dup2 (2, 1) < 0)
+    err (1, "could not redirect user shell stdout");
 
   if (env)
     execvpe (argv[0], argv, env);

test/verify/check-login
@@ -132,6 +132,15 @@ account    required     pam_succeed_if.so user ingroup %s""" % m.get_admin_group
             b.open("/system")
             b.wait_not_visible("#option-group")
 
+        if not m.atomic_image:
+            # login with user shell that prints some stdout/err noise
+            # having stdout output in ~/.bashrc confuses docker, so don't run on Atomic
+            m.execute("echo 'echo noise-rc-out; echo noise-rc-err >&2' >> ~admin/.bashrc")
+            m.execute("echo 'echo noise-profile-out; echo noise-profile-err >&2' >> ~admin/.profile")
+            login("admin", "foobar")
+            b.expect_load()
+            b.wait_present("#content")
+
         self.allow_journal_messages("Returning error-response ... with reason .*",
                                     "pam_unix\(cockpit:auth\): authentication failure; .*",
                                     "pam_unix\(cockpit:auth\): check pass; user unknown",
8e7bbfedbdce8cbf04f62c65ac6267929c81bdaa
Katerina Koukiou <kkoukiou@redhat.com>
2019-06-12 11:29:16 +0200

    machines: remove lvm2 from supported disk pool format types
    There is not lvm2 partition table type. (See man parted)
    This list was copied from https://libvirt.org/storage.html which has
    lvm2 listed incorrectly as partition table type.

--- pkg/machines/components/storagePools/createStoragePoolDialog.jsx
+++ pkg/machines/components/storagePools/createStoragePoolDialog.jsx
@@ -197,7 +197,7 @@ const StoragePoolInitiatorRow = ({ onValueChanged, dialogValues }) => {
 const StoragePoolSourceRow = ({ onValueChanged, dialogValues }) => {
     let validationState;
     let placeholder;
-    const diskPoolSourceFormatTypes = ['dos', 'dvh', 'gpt', 'mac', 'bsd', 'pc98', 'sun', 'lvm2'];
+    const diskPoolSourceFormatTypes = ['dos', 'dvh', 'gpt', 'mac', 'bsd', 'pc98', 'sun'];
 
     if (dialogValues.type == 'netfs') {
         validationState = dialogValues.source.dir.length == 0 && dialogValues.validationFailed.source ? 'error' : undefined;

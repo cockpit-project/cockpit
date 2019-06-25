93a5498b32b021df0108c5de043034a112d70bb7
Simon Kobyda <skobyda@redhat.com>
2019-06-25 15:17:54 +0200

    machines: Fix virsh parser of volumes
    Support parsing of all volume types, which are already supported.

--- pkg/machines/libvirt-virsh.js
+++ pkg/machines/libvirt-virsh.js
@@ -290,8 +290,7 @@ LIBVIRT_PROVIDER = {
     GET_STORAGE_VOLUMES({ connectionName, poolName }) {
         logDebug(`${this.name}.GET_STORAGE_VOLUMES(connectionName='${connectionName}', poolName='${poolName}')`);
         const connection = VMS_CONFIG.Virsh.connections[connectionName].params.join(' ');
-        // Caution: output of virsh v1.3.1 (ubuntu-1604) and v3.7.0 differs, the 'grep' unifies it
-        const command = `virsh ${connection} -q pool-refresh ${poolName} && virsh ${connection} -q -r vol-list ${poolName} --details | (grep file || true)`;
+        const command = `virsh ${connection} -q pool-refresh ${poolName} && virsh ${connection} -q -r vol-list ${poolName} --details`;
         let data = '';
         return dispatch => cockpit
                 .script(command, null, { err: "message", environ: ['LC_ALL=en_US.UTF-8'] })
@@ -559,7 +558,7 @@ function parseStorageVolumes(dispatch, connectionName, poolName, volumes) {
                 .filter(volume => !!volume) // non-empty lines
                 .map(volume => {
                     const fields = volume.split(/\s\s+/); // two spaces at least; lowers chance for bug with spaces in the volume name
-                    if (fields.length < 3 || fields[2] !== 'file') {
+                    if (fields.length < 3 || fields[2] === 'dir') {
                         // skip 'dir' type; use just flatten dir-pool structure
                         return null;
                     }

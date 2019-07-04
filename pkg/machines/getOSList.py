68130773cbd1821418de6857a2dd670546b22152
Katerina Koukiou <kkoukiou@redhat.com>
2019-07-04 14:57:16 +0200

    machines: introduce check for VM memory to be above allowed minimum
    Also add a helpblock indicating the suggested memory if an operating
    system was detected but the selected memory is less than recommended.
    
    Ideally we would like to prefill the memory values with the suggested
    ram size from libosinfo, but this should be part of bigger refactoring of
    the 'Create VM' dialog.

--- pkg/machines/getOSList.py
+++ pkg/machines/getOSList.py
@@ -26,6 +26,14 @@ for i in range(oses.get_length()):
     osObj['releaseDate'] = os.get_release_date_string() or ""
     osObj['eolDate'] = os.get_eol_date_string() or ""
     osObj['codename'] = os.get_codename() or ""
+    osObj['recommendedResources'] = {}
+    recommendedResources = os.get_recommended_resources()
+    if recommendedResources.get_length():
+        osObj['recommendedResources']['ram'] = recommendedResources.get_nth(0).get_ram()
+    osObj['minimumResources'] = {}
+    minimumResources = os.get_minimum_resources()
+    if minimumResources.get_length():
+        osObj['minimumResources']['ram'] = minimumResources.get_nth(0).get_ram()
 
     res.append(osObj)
 

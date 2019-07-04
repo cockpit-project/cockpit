68b703f7471f764a2542ae45d2cfb74959949cc6
Matej Marusak <mmarusak@redhat.com>
2019-07-04 09:31:45 +0200

    bots: Remove `Recommends` before parsing `BuildRequires`
    `Recommends:` has never been supported by RHEL/CentOS 7. Until recently these
    were %if'ed out on RHEL 7, but commit dedd6f7 cleaned that up.
    
    Just remove `Recommends:` with sed as they are not not needed for build
    dependencies anyway.
    
    Closes #12225

--- bots/images/scripts/lib/build-deps.sh
+++ bots/images/scripts/lib/build-deps.sh
@@ -5,6 +5,7 @@ set -eu
 # Download cockpit.spec, replace `npm-version` macro and then query all build requires
 curl -s https://raw.githubusercontent.com/cockpit-project/cockpit/master/tools/cockpit.spec |
     sed 's/%{npm-version:.*}/0/' |
+    sed '/Recommends:/d' |
     rpmspec -D "$1" --buildrequires --query /dev/stdin |
     sed 's/.*/"&"/' |
     tr '\n' ' '

67831b9d2f5adacccb4b6bdfe6dacf2f4f0ec013
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2019-07-10 18:46:26 +0200

    containers: don't use xz --extreme for unit-tests
    In the course of 'make distcheck', we end up building two tarballs,
    twice (for a total of 4) and compressing them with 'xz --extreme', which
    is really slow.
    
    We can substantially speed things up using -0.  This is actually even
    faster than gzip, but let's leave the i386 gzip case in place anyway,
    because it tests a different path.
    
    Measured locally on my unloaded laptop, this results in a speedup of a
    bit over 2m30s, which is a ~30% improvement on a unit-tests run (ie:
    `containers/unit-tests/start`).  On Semaphore, under load, the change is
    even larger.
    
    Closes #12289

--- containers/unit-tests/run.sh
+++ containers/unit-tests/run.sh
@@ -55,7 +55,7 @@ python3 -c "import fcntl, os; map(lambda fd: fcntl.fcntl(fd, fcntl.F_SETFL, fcnt
 
 if dpkg-architecture --is amd64; then
     # run distcheck on main arch
-    make distcheck 2>&1
+    make XZ_COMPRESS_FLAGS='-0' distcheck 2>&1
 else
     # on i386, validate that "distclean" does not remove too much
     make dist-gzip

01bbaf438b90c8281e0e76c89a99e42f479fb441
Martin Pitt <mpitt@redhat.com>
2019-07-17 15:50:57 +0200

    bots: Put back golang into rhel/centos-7 images
    We still need these for running builds/tests for the stable rhel-7.x
    branches.
    
    Closes #12351

--- bots/images/scripts/lib/build-deps.sh
+++ bots/images/scripts/lib/build-deps.sh
@@ -9,3 +9,8 @@ curl -s https://raw.githubusercontent.com/cockpit-project/cockpit/master/tools/c
     rpmspec -D "$1" --buildrequires --query /dev/stdin |
     sed 's/.*/"&"/' |
     tr '\n' ' '
+
+# support for backbranches
+if [ "$1" = "rhel 7" ] || [ "$1" = "centos 7" ]; then
+    echo "golang-bin golang-src"
+fi

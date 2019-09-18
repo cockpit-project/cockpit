32e7abc4bd4b50b1dda0ba2e9d4651e01596e421
Martin Pitt <mpitt@redhat.com>
2019-09-18 10:06:16 +0200

    Fix untranslatable ngettext() calls
    Use double quotes so that xgettext recognizes the strings. Extend
    test-static-code check to catch gettext variants in addition to _().
    
    Part of issue #12835

--- pkg/docker/containers-view.jsx
+++ pkg/docker/containers-view.jsx
@@ -433,8 +433,8 @@ class ImageSecurity extends React.Component {
         } else if (info.vulnerabilities.length === 0) {
             text = _("The scan from $time ($type) found no vulnerabilities.");
         } else {
-            text = cockpit.ngettext('The scan from $time ($type) found one vulnerability:',
-                                    'The scan from $time ($type) found $count vulnerabilities:',
+            text = cockpit.ngettext("The scan from $time ($type) found one vulnerability:",
+                                    "The scan from $time ($type) found $count vulnerabilities:",
                                     info.vulnerabilities.length);
 
             rows = info.vulnerabilities.map(function (vulnerability) {
@@ -611,7 +611,7 @@ export class ImageList extends React.Component {
                     <div>
                         <span className="pficon pficon-warning-triangle-o" />
                         &nbsp;
-                        { cockpit.format(cockpit.ngettext('1 Vulnerability', '$0 Vulnerabilities', count), count) }
+                        { cockpit.format(cockpit.ngettext("1 Vulnerability", "$0 Vulnerabilities", count), count) }
                     </div>
                 );
         }

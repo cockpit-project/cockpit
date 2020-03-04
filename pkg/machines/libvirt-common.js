168f819ebd09ddc220097915aecd3cc829c4a8bc
Marius Vollmer <mvollmer@redhat.com>
2020-03-04 12:00:07 +0200

    all: Add { superuser: "try" } for things that might need Polkit
    Cockpit should always use the maximum privileges it has.  Otherwise
    actions might be rejected that are actually allowed.

--- pkg/machines/libvirt-common.js
+++ pkg/machines/libvirt-common.js
@@ -1298,6 +1298,10 @@ export function CREATE_VM({ connectionName, vmName, source, sourceType, os, memo
             setVmInstallInProgress(dispatch, vmName, connectionName);
         }
 
+        const opts = { err: "message", environ: ['LC_ALL=C'] };
+        if (connectionName === 'system')
+            opts.superuser = 'try';
+
         return cockpit.script(createVmScript, [
             connectionName,
             vmName,
@@ -1313,7 +1317,7 @@ export function CREATE_VM({ connectionName, vmName, source, sourceType, os, memo
             userPassword,
             rootPassword,
             profile,
-        ], { err: "message", environ: ['LC_ALL=C'] })
+        ], opts)
                 .done(() => {
                     finishVmCreateInProgress(dispatch, vmName, connectionName);
                     if (startVm) {
@@ -1391,6 +1395,10 @@ export function INSTALL_VM({ name, vcpus, cpu, currentMemory, memory, metadata, 
         // vm should be returned even if script fails
         setVmInstallInProgress(dispatch, name, connectionName);
 
+        const opts = { err: "message", environ: ['LC_ALL=C'] };
+        if (connectionName === 'system')
+            opts.superuser = 'try';
+
         return cockpit.script(installVmScript, [
             connectionName,
             name,
@@ -1404,7 +1412,7 @@ export function INSTALL_VM({ name, vcpus, cpu, currentMemory, memory, metadata, 
             prepareNICParam(interfaces),
             firmware == "efi" ? 'uefi' : '',
             autostart,
-        ], { err: "message", environ: ['LC_ALL=C'] })
+        ], opts)
                 .done(() => clearVmUiState(dispatch, name, connectionName))
                 .fail(ex => {
                     clearVmUiState(dispatch, name, connectionName); // inProgress cleanup
--- pkg/systemd/overview-cards/realmd-operation.js
+++ pkg/systemd/overview-cards/realmd-operation.js
@@ -611,7 +611,7 @@ export function setup() {
 
     function setup_realms_proxy() {
         // HACK: need to reinitialize after installing realmd (https://github.com/cockpit-project/cockpit/pull/9125)
-        realmd = cockpit.dbus("org.freedesktop.realmd");
+        realmd = cockpit.dbus("org.freedesktop.realmd", { superuser: "try" });
         realmd.watch(MANAGER);
 
         $(realmd).on("close", function(ev, options) {

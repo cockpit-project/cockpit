05c609b138564f00efc45476df71ca3157733e97
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2020-03-10 11:29:09 +0100

    all: stop using en_US.UTF-8 as a default locale
    We were mostly doing this for the sake of RHEL 7, which lacked C.UTF-8,
    which is what we actually want to be using.
    
    This should fix issues with some of the test machines not having
    en_US.UTF-8 installed, and allows removal of a few workarounds that
    tested for C.UTF-8 with a fallback to en_US.UTF-8.

--- pkg/systemd/overview-cards/realmd-operation.js
+++ pkg/systemd/overview-cards/realmd-operation.js
@@ -403,9 +403,8 @@ function instance(realmd, mode, realm, state) {
         script += 'kinit ' + user + '@' + kerberos.RealmName + ' || exit 0; ';
 
         // ensure this gets run with a non-C locale; ipa fails otherwise
-        // C.UTF-8 exists on most OSes now, except for RHEL 7
         script += "if [ $(sh -c 'eval `locale`; echo $LC_CTYPE') = 'C' ]; then " +
-                  "    locale -a | grep -iq ^'C.utf' && export LC_CTYPE=C.UTF-8 || export LC_CTYPE=en_US.UTF-8; " +
+                  "    export LC_CTYPE=C.UTF-8; " +
                   "fi; ";
 
         // create a kerberos Service Principal Name for cockpit-ws, unless already present

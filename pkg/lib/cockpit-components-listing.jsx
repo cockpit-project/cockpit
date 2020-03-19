5729356947728b6d687cb04d95c022865ee46d35
Simon Kobyda <skobyda@redhat.com>
2020-03-19 11:55:49 +0100

    lib: Implement SelectableListing
    PF4 compatible Listing with clickable rows to be used in dialogs.

--- pkg/lib/cockpit-components-listing.jsx
+++ pkg/lib/cockpit-components-listing.jsx
@@ -20,6 +20,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import './listing.scss';
+import '../../node_modules/@patternfly/patternfly/components/Select/select.scss';
+import '../../node_modules/@patternfly/patternfly/components/Check/check.scss';
 
 /* entry for an alert in the listing, can be expanded (with details) or standard
  * rowId optional: an identifier for the row which will be set as "data-row-id" attribute on the <tr>
@@ -402,4 +404,36 @@ Listing.propTypes = {
         ])),
     columnTitleClick: PropTypes.func,
     actions: PropTypes.node
+};
+
+/* Implements PF4-compatible listing with clickable rows
+ * Intended for use in modal dialogs
+ *
+ * - rows {object}: with properties: name
+ * - onRowToggle {function}: toggle handler which takes row name as an arguement
+ * - idPrefix {string}
+ */
+export const SelectableListing = ({ rows, onRowToggle, idPrefix }) => {
+    return (
+        <fieldset className="list-group dialog-list-ct pf-c-select" id={idPrefix + "-selectable-listing"}>
+            {rows.map(row => {
+                return (
+                    <label className="pf-c-check pf-c-select__menu-item" htmlFor={idPrefix + "-" + row.id} key={row.id}>
+                        <input className="pf-c-check__input"
+                            name="select-checkbox-expanded-active"
+                            onChange={() => onRowToggle(row.id)}
+                            type="checkbox"
+                            id={idPrefix + "-" + row.id + "-checkbox"}
+                            checked={row.selected} />
+                        <span className="pf-c-check__label">{row.name}</span>
+                    </label>);
+            })}
+        </fieldset>
+    );
+};
+
+SelectableListing.propTypes = {
+    rows: PropTypes.array,
+    idPrefix: PropTypes.string,
+    onRowToggle: PropTypes.func,
 };

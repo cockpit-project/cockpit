6fb8bd6a5f25a99fd810d2c3a7857970c16496ac
Marius Vollmer <mvollmer@redhat.com>
2020-03-24 09:09:46 +0200

    test: Clarify selenium-machines-non-root.py
    Closes #13772

--- test/selenium/selenium-machines-non-root.py
+++ test/selenium/selenium-machines-non-root.py
@@ -9,19 +9,35 @@ class MachinesNonrootTestSuite(MachinesLib):
 
     def testNonRootOperationWithVm(self):
         name = 'staticvm'
-        self.create_vm(name)
+        self.create_vm(name, wait=True)
 
-        self.non_root_user_operations()
+        # A regular user without any extra groups and without
+        # escalated privileges wont see any VM and can't do any
+        # operation on it.
+
+        self.non_root_user_operations(user_group=None, privilege=False,
+                                      operations=False)
 
     def testLibvirtUserOperationWithVm(self):
         name = 'staticvm'
         vm_args = self.create_vm(name, wait=True)
 
-        self.non_root_user_operations('libvirt', True, vm_args)
+        # A regular user in the libvirt group is allowed to see the VM
+        # and can do operations on it, even without escalated
+        # privileges.
+
+        self.non_root_user_operations(user_group='libvirt', privilege=False,
+                                      operations=True, vm_args=vm_args)
 
     def testWheelUserOperationWithVm(self):
         name = 'staticvm'
         vm_args = self.create_vm(name, wait=True)
 
-        self.non_root_user_operations('wheel')
-        self.non_root_user_operations('wheel', True, vm_args, True)
+        # A admin user in the wheel group is allowed to see the VM and
+        # can do operations on it, but only with escalated privileges.
+
+        self.non_root_user_operations(user_group='wheel', privilege=False,
+                                      operations=False)
+
+        self.non_root_user_operations(user_group='wheel', privilege=True,
+                                      operations=True, vm_args=vm_args)

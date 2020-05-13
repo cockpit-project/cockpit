b3adaa80a1eab99608ee53a8a9c7c3c6072de50a
Matej Marusak <mmarusak@redhat.com>
2020-05-13 12:39:08 +0200

    shell: Drop SimpleIndex
    It is not used anywhere and is not going to be compatible with the new
    navigation. Once we need it, we can just tweak the new navigation.

--- pkg/shell/indexes.js
+++ pkg/shell/indexes.js
@@ -868,119 +868,6 @@ function MachinesIndex(index_options, machines, loader, mdialogs) {
     });
 }
 
-function SimpleIndex(index_options) {
-    if (!index_options)
-        index_options = {};
-
-    index_options.navigate = function (state, sidebar) {
-        return navigate(state, sidebar);
-    };
-    var default_title = index_options.default_title || "Cockpit";
-    var index = base_index.new_index_from_proto(index_options);
-    var compiled = base_index.new_compiled();
-
-    compiled.load(cockpit.manifests, "dashboard");
-
-    /* Disconnection Dialog */
-    var watchdog_problem = null;
-    $(index).on("disconnect", function (ev, problem) {
-        watchdog_problem = problem;
-        show_disconnected();
-    });
-
-    /* Reconnect button */
-    $("#machine-reconnect").on("click", function(ev) {
-        cockpit.sessionStorage.clear();
-        window.location.reload(true);
-    });
-
-    function show_disconnected() {
-        var current_frame = index.current_frame();
-        if (current_frame)
-            $(current_frame).hide();
-
-        $(".curtains-ct .spinner").prop("hidden", true);
-        $("#machine-reconnect").toggle(true);
-        $(".curtains-ct i").toggle(true);
-        $(".curtains-ct h1").text(_("Disconnected"));
-        $(".curtains-ct p").text(cockpit.message(watchdog_problem));
-        $(".curtains-ct").prop("hidden", false);
-        $("#navbar-dropdown").addClass("disabled");
-    }
-
-    index.ready();
-
-    /* Handles navigation */
-    function navigate(state, reconnect) {
-        var dashboards = compiled.ordered("dashboard");
-
-        /* If this is a watchdog problem or we are troubleshooting
-         * let the dialog handle it */
-        if (watchdog_problem)
-            return;
-
-        /* phantomjs has a problem retrieving state, so we allow it to be passed in */
-        if (!state)
-            state = index.retrieve_state();
-
-        if (!state.component && dashboards.length > 0) {
-            state.component = dashboards[0].path;
-        }
-        update_navbar(state);
-        update_frame(state);
-
-        /* Just replace the state, and URL */
-        index.jump(state, true);
-    }
-
-    function update_navbar(state) {
-        $("#host-nav-item").toggleClass("dashboard-link", false);
-        $(".dashboard-link").each(function() {
-            var el = $(this);
-            el.toggleClass("active", el.attr("data-component") === state.component);
-        });
-
-        var item = compiled.items[state.component];
-        if (item && item.section == "dashboard")
-            delete state.sidebar;
-
-        $('.area-ct-body').toggleClass("single-nav", $(".dashboard-link").length < 2);
-    }
-
-    function update_title(label) {
-        if (label)
-            label += " - ";
-        else
-            label = "";
-        document.title = label + default_title;
-    }
-
-    function update_frame(state) {
-        var current_frame = index.current_frame();
-
-        var hash = state.hash;
-        var component = state.component;
-
-        var frame;
-        if (component)
-            frame = index.frames.lookup(null, component, hash);
-        if (frame != current_frame) {
-            $(current_frame).css('display', 'none');
-            index.current_frame(frame);
-        }
-
-        var label, item;
-        $(frame).css('display', 'block');
-        item = compiled.items[state.component];
-        label = item ? item.label : "";
-        update_title(label);
-    }
-
-    cockpit.transport.wait(function() {
-        index.start();
-    });
-}
-
 function message_queue(event) {
     window.messages.push(event);
 }
@@ -1008,10 +895,6 @@ if (document.documentElement.getAttribute("class") === "index-page") {
     };
 
     window.addEventListener("message", message_queue, false);
-}
-
-export function simple_index(options) {
-    return new SimpleIndex(options);
 }
 
 export function machines_index(options, machines_ins, loader, mdialogs) {

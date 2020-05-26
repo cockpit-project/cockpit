d60a19ad8341fff394f5b2bb16661d8450e325cd
sanne raymaekers <sanne.raymaekers@gmail.com>
2020-05-26 12:35:10 +0200

    test: Move run-tests to test/common
    This should fix TAP output for external projects using the cockpit test
    infrastructure.

---
+++ test/common/parent.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+TEST_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BOTS_DIR = os.path.join(os.path.dirname(TEST_DIR), "bots")
+sys.path.append(os.path.join(BOTS_DIR, "machine"))
+sys.path.append(os.path.join(TEST_DIR, "common"))
--- test/verify/parent.py
+++ test/verify/parent.py
@@ -1,7 +1,1 @@
-import os
-import sys
-
-TEST_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BOTS_DIR = os.path.join(os.path.dirname(TEST_DIR), "bots")
-sys.path.append(os.path.join(TEST_DIR, "common"))
-sys.path.append(os.path.join(BOTS_DIR, "machine"))
+../common/parent.py

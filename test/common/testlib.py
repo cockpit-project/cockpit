48c716de7ebda1457d627f4a3af04e979dfd3ab4
Lars Karlitski <lars@karlitski.net>
2020-06-08 09:53:44 +0200

    test: add --enable-network parameter
    This adds `--enable-network` to `test/common/run-tests` and test scripts
    (usually `test/check-*`). When passed, tests get access to the network.
    
    Closes #14208

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -1528,6 +1528,8 @@ def arg_parser(enable_sit=True):
                             help="Sit and wait after test failure")
     parser.add_argument('--nonet', dest="fetch", action="store_false",
                         help="Don't go online to download images or data")
+    parser.add_argument('--enable-network', dest='enable_network', action='store_true',
+                        help="Enable network access for tests")
     parser.add_argument('tests', nargs='*')
     parser.add_argument("-l", "--list", action="store_true", help="Print the list of tests that would be executed")
 

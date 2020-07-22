6bab37ebcb9ed19b17f7f895f30aceb444ed52fe
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2020-07-22 18:46:20 +0200

    all: remove "outfd" workaround
    GLib 2.68 is gaining g_log_writer_default_set_use_stderr() to allow us
    to direct all logging output toward stderr.  Use that when it's
    available.
    
    Because it's not widely available yet, move our dup2()-based "outfd"
    workarounds to a much simpler approach: assign the value of the stderr
    FILE* to stdout as well.  This is evil, but it will cause all logging to
    go to stderr, and is vastly simpler than what we have now.
    
    Closes #14391

--- src/common/cockpithacks-glib.h
+++ src/common/cockpithacks-glib.h
@@ -29,3 +29,27 @@
 
 #include "cockpithacks.h"
 #include <glib.h>
+
+/* g_debug() defaults to writing its output to stdout, which doesn't
+ * really work for us.  GLib 2.67.0 introduced an API to change this
+ * behaviour, and we want to use this API if it's available.
+ *
+ * Otherwise, we know that earlier versions of GLib use the `stdout`
+ * `FILE *` to do its write, while we write directly to fd 1, so we can
+ * replace the value of `stdout` to cause GLib to write elsewhere.  This
+ * is specifically not allowed by POSIX, but works on Linux.
+ */
+#include <stdio.h>
+
+static void
+cockpit_hacks_redirect_gdebug_to_stderr (void)
+{
+#if GLIB_CHECK_VERSION(2,67,0)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+  g_log_writer_default_set_use_stderr (TRUE);
+#pragma GCC diagnostic pop
+#else
+  stdout = stderr;
+#endif
+}

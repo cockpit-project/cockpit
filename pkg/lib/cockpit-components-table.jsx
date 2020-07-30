c61324110e094f2ed3f6f39c32adda60c39f4766
Katerina Koukiou <kkoukiou@redhat.com>
2020-07-30 12:55:33 +0200

    lib(table): Make sure that the expandable part of table rows has unique key
    Use '+' operator instead of the expensive cockpit.format when
    concatenating small strings.
    
    Relevant to https://bugzilla.redhat.com/show_bug.cgi?id=1865821
    Cherry-picked from master commit 7a04d0ea3

--- pkg/lib/cockpit-components-table.jsx
+++ pkg/lib/cockpit-components-table.jsx
@@ -17,7 +17,6 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-import cockpit from "cockpit";
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
@@ -170,7 +169,8 @@ export class ListingTable extends React.Component {
                     parent: rowIndex - 1,
                     cells: [{ title: currentValue.expandedContent }],
                     fullWidth: true, noPadding: true,
-                    rowId: currentValue.rowId ? cockpit.format("$0-expanded", currentValue.rowId) : undefined
+                    rowId: currentValue.rowId ? (currentValue.rowId + "-expanded") : undefined,
+                    props: { key: currentValue.props.key + "-expanded" },
                 });
                 rowIndex++;
             }

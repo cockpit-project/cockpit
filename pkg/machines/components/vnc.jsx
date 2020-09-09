2fa43704db357bed2247ab38b869cf54d6c31515
Katerina Koukiou <kkoukiou@redhat.com>
2020-09-09 23:31:37 +0200

    machines: Fix shared proptype introduced in commit f5b0630
    Closes #14594

--- pkg/machines/components/vnc.jsx
+++ pkg/machines/components/vnc.jsx
@@ -159,7 +159,7 @@ class Vnc extends React.Component {
                         port={window.location.port || (encrypt ? '443' : '80')}
                         path={path}
                         encrypt={encrypt}
-                        shared='true'
+                        shared
                         credentials={credentials}
                         vncLogging='warn'
                         onDisconnected={this.onDisconnected}

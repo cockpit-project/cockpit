1dea9da4c29c3191ac024fb2df4c31c59111a05e
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2020-09-14 16:49:18 +0200

    memfdread: add a couple more public accessors
    These will be helpful from testing, and eventually used in cockpit-ws.

--- src/common/cockpitmemfdread.h
+++ src/common/cockpitmemfdread.h
@@ -20,9 +20,18 @@
 
 #pragma once
 
+#include <json-glib/json-glib.h>
 #include <glib.h>
+
+gchar *
+cockpit_memfd_read (gint fd,
+                    GError **error);
 
 gboolean
 cockpit_memfd_read_from_envvar (gchar **result,
                                 const char *envvar,
                                 GError **error);
+
+JsonObject *
+cockpit_memfd_read_json (gint fd,
+                         GError **error);

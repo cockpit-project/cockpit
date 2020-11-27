01f9e997cf5ed16f67b7f38bbdacc8e87fa30953
Katerina Koukiou <kkoukiou@redhat.com>
2020-11-27 09:09:53 +0100

    machines: clean up create_machine.sh script from user-password-file option
    Commit 00263f56 removed the option from the UI but not the script. The
    script ended up with passing to virt-install the user-password-file with
    the content 'undefined' as string. This is obviously broken, so let's
    just remove the option from here as well.

--- pkg/machines/libvirt-common.js
+++ pkg/machines/libvirt-common.js
@@ -1257,7 +1257,7 @@ export function CONSOLE_VM({
     };
 }
 
-export function CREATE_VM({ connectionName, vmName, source, sourceType, os, memorySize, storageSize, startVm, storagePool, storageVolume, unattended, userPassword, rootPassword, profile }) {
+export function CREATE_VM({ connectionName, vmName, source, sourceType, os, memorySize, storageSize, startVm, storagePool, storageVolume, unattended, rootPassword, profile }) {
     logDebug(`${this.name}.CREATE_VM(${vmName}):`);
     return dispatch => {
         // shows dummy vm  until we get vm from virsh (cleans up inProgress)
@@ -1283,7 +1283,6 @@ export function CREATE_VM({ connectionName, vmName, source, sourceType, os, memo
             storagePool,
             storageVolume,
             unattended,
-            userPassword,
             rootPassword,
             profile,
         ], opts)

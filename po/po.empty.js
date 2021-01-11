3ecf55d5888d7c56cb844fbfcbfb10070986ec43
Martin Pitt <mpitt@redhat.com>
2021-01-11 08:45:31 +0100

    po: Simplify translation template
    `this.po` is not being used anywhere, so drop the whole `!loaded` code
    path. There is no defer mechanism, so `po.js` *must* be loaded after
    cockpit.js. Let's trigger a crash instead of silently dropping
    translations, to make such a bug obvious in manual and automatic tests.
    
    Ported from starter-kit commit 88fe99361be4.

--- po/po.empty.js
+++ po/po.empty.js
@@ -1,20 +1,3 @@
-(function (root, data) {
-    var loaded, module;
+/* The syntax of this line is important for po2json */
+cockpit.locale({"":{"language":"en"}});
 
-    /* Load into AMD if desired */
-    if (typeof define === 'function' && define.amd) {
-        define(data);
-        loaded = true;
-    }
-
-    /* Load into Cockpit locale */
-    if (typeof cockpit === 'object') {
-        cockpit.locale(data)
-        loaded = true;
-    }
-
-    if (!loaded)
-        root.po = data;
-
-/* The syntax of this line is important  by po2json */
-}(this, {"":{"language":"en"}}));

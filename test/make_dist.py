3b8024c2eedc2a7f50b9e80365d9f7eaa2238dae
Martin Pitt <mpitt@redhat.com>
2021-02-10 10:00:47 +0100

    test: Let make_dist.py download GitHub artifact tarball if available
    This happens if
    
     - there is a GitHub token (GitHub artifacts download needs to be
       authenticated)
     - the current git HEAD's SHA has an already built dist artifact, i.e.
       you checked out a revision from master or a fork/branch from a PR
     - you are requesting a production (not development) build.
    
    That replaces a 10 minute npm install+webpack run with a few seconds
    download, which will make it much nicer to locally test a PR or current
    master version: You can just check out what you want to test and run
    `test/image-prepare -q` to get a test VM in 3 minutes.

--- test/make_dist.py
+++ test/make_dist.py
@@ -16,13 +16,17 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
-
+import glob
+import io
 import multiprocessing
 import os
+import urllib
 import subprocess
+import sys
+import zipfile
 
 
-def make_dist():
+def build_dist():
     '''Build a dist tarball for CI testing
 
     This supports completely clean git trees, unpacked release tarballs, and already configured trees.
@@ -40,6 +44,105 @@ def make_dist():
     subprocess.check_call(["make", "--silent", "-j%i" % multiprocessing.cpu_count(),
                            "NO_DIST_CACHE=1", "XZ_COMPRESS_FLAGS=-0", "dist"])
     return subprocess.check_output(["make", "dump-dist"], universal_newlines=True).strip()
+
+
+def download_dist():
+    '''Download dists tarball for current git SHA from GitHub
+
+    These are produced by .github/workflows/build-dist.yml for every PR and push.
+    This is a lot faster than having to npm install and run webpack.
+
+    Returns path to downloaded tarball, or None if it isn't available.
+    This can happen because the current directory is not a git checkout, or it is
+    a SHA which is not pushed/PRed, or there is no ~/.config/github-token available.
+    '''
+    try:
+        sha = subprocess.check_output(["git", "rev-parse", "HEAD"], stderr=subprocess.DEVNULL).decode().strip()
+    except subprocess.CalledProcessError:
+        print("make_dist: not a git repository")
+        return None
+
+    dists = glob.glob(f"cockpit-*{sha[:8]}*.tar.xz")
+    if dists:
+        print("make_dist: already downloaded " + dists[0])
+        return os.path.abspath(dists[0])
+
+    sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "bots"))
+    import task
+
+    # task.api defaults to the current checkout's origin, but the artifacts are on cockpit-project
+    # except if a developer wants to change the artifact building workflow on their fork, support that with $GITHUB_BASE
+    api = task.github.GitHub(repo=os.getenv("GITHUB_BASE", "cockpit-project/cockpit"))
+
+    # downloading GitHub artifacts requires a token
+    if not api.token:
+        print("make_dist: no GitHub API token available")
+        return None
+
+    # iterate artifacts and search our SHA
+    page = 1
+    batch_size = 100
+    download_url = None
+    while not download_url and batch_size == 100:
+        batch = api.get(f"actions/artifacts?per_page={batch_size}&page={page}")["artifacts"]
+        for artifact in batch:
+            if artifact["name"] == "dist-" + sha:
+                download_url = artifact["archive_download_url"]
+                break
+        # if the current batch is < 100, we have the last page and can stop
+        batch_size = len(batch)
+        page += 1
+
+    if not download_url:
+        print(f"make_dist: no download available for commit {sha}")
+        return None
+
+    print(f"make_dist: Downloading dist tarball from {download_url} ...")
+    request = urllib.request.Request(download_url, headers={"Authorization": "token " + api.token})
+    zipio = io.BytesIO()
+    try:
+        with urllib.request.urlopen(request) as response:
+            if os.isatty(sys.stdout.fileno()):
+                total_size = 0
+            else:
+                total_size = None
+            MB = 10**6
+            # read zip into a stringio, as the stream is not seekable and zip requires that
+            while True:
+                block = response.read(MB)
+                if len(block) == 0:
+                    break
+                if total_size is not None:
+                    total_size += len(block)
+                    sys.stdout.write(f"\r{ total_size // MB } MB")
+
+                zipio.write(block)
+
+            # clear the download progress in tty mode
+            if total_size is not None:
+                sys.stdout.write("\r                             \r")
+
+    except urllib.error.HTTPError as e:
+        print("make_dist: Download failed:", e)
+        return None
+
+    with zipfile.ZipFile(zipio) as fzip:
+        names = fzip.namelist()
+        if len(names) != 1 or not names[0].endswith(".tar.xz"):
+            print("make_dist: expected zip artifact with exactly one tar.xz member")
+            return None
+        return fzip.extract(names[0])
+
+
+def make_dist():
+    # first try to download a pre-generated dist tarball; this is a lot faster
+    # but these tarballs are built for production NPM mode
+    source = None
+    if os.getenv("NODE_ENV") != "development":
+        source = download_dist()
+    if not source:
+        source = build_dist()
+    return source
 
 
 if __name__ == '__main__':

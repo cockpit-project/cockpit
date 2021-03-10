7d890cba407eacea892b2c449db36e93cd63221b
Martin Pitt <mpitt@redhat.com>
2021-03-10 06:25:02 +0100

    test: Add bots root directory to parent.py
    https://github.com/cockpit-project/bots/commit/b9d9b67e3c5 introduced a
    new `lib` Python module in bots. Adjust our common test parent.py to add
    the top-level bots dir to Python's search path, so that tests can find
    it.

--- test/common/parent.py
+++ test/common/parent.py
@@ -3,5 +3,6 @@ import sys
 
 TEST_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BOTS_DIR = os.path.join(os.path.dirname(TEST_DIR), "bots")
+sys.path.append(os.path.join(BOTS_DIR))  # for lib
 sys.path.append(os.path.join(BOTS_DIR, "machine"))
 sys.path.append(os.path.join(TEST_DIR, "common"))

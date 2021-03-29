69e3c1d99c49a858525002df7a179b9416b23ae8
Marius Vollmer <mvollmer@redhat.com>
2021-03-29 14:02:47 +0300

    test: Ignore more hostkey messages

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -1082,6 +1082,8 @@ class MachineCase(unittest.TestCase):
     def allow_hostkey_messages(self):
         self.allow_journal_messages('.*: .* host key for server is not known: .*',
                                     '.*: refusing to connect to unknown host: .*',
+                                    '.*: .* host key for server has changed to: .*',
+                                    '.*: host key for this server changed key type: .*',
                                     '.*: failed to retrieve resource: hostkey-unknown')
 
     def allow_restart_journal_messages(self):

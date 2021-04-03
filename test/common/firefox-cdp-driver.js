cockpit-project/cockpit

commit 90b39a51b104b82aaf444c0ec06ba0f663f87583

test: Firefox now can wait for promise
This was the main hack in the whole firefox driver so I am very happy to
see it go away.

test/common/firefox-cdp-driver.js
@@ -336,34 +336,6 @@ function addScriptToEvaluateOnNewDocument(script) {
     });
 }
 
-function evaluate(cmd) {
-    return new Promise((resolve, reject) => {
-        const match_exp = cmd.expression.match(/ph_wait_cond[^=]*=>\s*([\s\S]*),\s*(\d*)/);
-        let stepTimer = null;
-        let tm = setTimeout( () => {
-                if (stepTimer)
-                    clearTimeout(stepTimer);
-                resolve({exceptionDetails: {
-                    exception: {
-                        type: "string",
-                        value: "timeout",
-                    }
-                }});
-            }, parseInt(match_exp[2]));
-        function step() {
-            the_client.Runtime.evaluate({expression: match_exp[1], contextId: cmd.contextId}).then(r => {
-                if (r && r.result && r.result.value === true) {
-                    clearTimeout(tm);
-                    resolve(r);
-                } else {
-                    stepTimer = setTimeout(step, 100);
-                }
-            });
-        }
-        step();
-    });
-}
-
 // This should work on different targets (meaning tabs)
 // CDP takes {target:target} so we can pick target
 // Problem is that CDP.New() which creates new target works only for chrome/ium
@@ -405,8 +377,6 @@ CDP(options)
                     // HACKS: See description of related functions
                     if (command.startsWith("client.Page.addScriptToEvaluateOnNewDocument"))
                         command = command.substring(12);
-                    if (command.startsWith("client.Runtime.evaluate") && command.indexOf("ph_wait_cond") !== -1)
-                        command = command.substring(15);
 
                     // run the command
                     eval(command).then(reply => {

commit d25b51e0ef074d2bc9c37e8c2a8fd24e9592d038

test: Work around Firefox CDP mis-handling promise rejections
When Runtime.evaluate() encounters a rejected promise, it fails with
```
    "response": {
        "message": "Debugger: expected Debugger.Object, got Proxy",
        "data": "_returnError@chrome://remote/content/cdp/domains/content/runtime/ExecutionContext.jsm:193:22\nevaluate@chrome://remote/content/cdp/domains/content/runtime/ExecutionContext.jsm:173:23\nsetTimeout handler*ph_wait_cond/<@debugger eval code:242:25\nph_wait_cond@debugger eval code:238:12\n@debugger eval code:1:13\nevaluate@chrome://remote/content/cdp/domains/content/runtime/ExecutionContext.jsm:147:29\nevaluate@chrome://remote/content/cdp/domains/content/Runtime.jsm:279:20\nexecute@chrome://remote/content/cdp/domains/DomainCache.jsm:101:25\nreceiveMessage@chrome://remote/content/cdp/sessions/ContentProcessSession.jsm:84:45\n"
    }
```

instead of returning a "subtype: error" result like Chromium does. This
causes wait timeouts to fail with such a generic `RuntimeError` instead
of the expected `testlib.Error: timeout`, breaking naughty patterns.

This was fallout from commit 90b39a51b104b82aaf -- previously, the
firefox CDP driver emulated Runtime.evaluate() for promises. The actual
waiting part does work fine, but translate the useless error from above
into a synthetic "timeout" exception.

Firefox issue: https://bugzilla.mozilla.org/show_bug.cgi?id=1702860

Fixes #15646

test/common/firefox-cdp-driver.js
@@ -383,7 +383,19 @@ CDP(options)
                             fail(message.split("\n")[0]);
                             clearExceptions();
                         }
-                    }, fail);
+                    }, err => {
+                        // HACK: Runtime.evaluate() fails with "Debugger: expected Debugger.Object, got Proxy"
+                        // translate that into a proper timeout exception
+                        if (err.response && err.response.data && err.response.data.indexOf("setTimeout handler*ph_wait_cond") > 0) {
+                            success({exceptionDetails: {
+                                exception: {
+                                    type: "string",
+                                    value: "timeout",
+                                }
+                            }});
+                        } else
+                            fail(err);
+                    });
 
                     input_buf = input_buf.slice(i+1);
                 }
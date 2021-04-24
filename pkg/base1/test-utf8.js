a1737a1ad5b2231f21c79a87bcaed95581797655
Martin Pitt <mpitt@redhat.com>
2021-04-24 09:12:57 +0200

    Update link to Apache license
    Spotted by our link checker.

--- pkg/base1/test-utf8.js
+++ pkg/base1/test-utf8.js
@@ -19,7 +19,7 @@ QUnit.test("utf8 basic", function (assert) {
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// https://www.apache.org/licenses/LICENSE-2.0.html
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,

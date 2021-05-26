e4b583a12b9988ae6fc245841f0abb8432a42f6c
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2021-05-26 10:27:38 +0200

    containers: adjust environment for build changes
     - stop using NO_DIST_CACHE=1 (we never build the cache anymore)
    
     - XZ_OPT is the new XZ_COMPRESS_FLAGS

--- containers/unit-tests/scenario-dist.sh
+++ containers/unit-tests/scenario-dist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-make NO_DIST_CACHE=1 XZ_COMPRESS_FLAGS=-0 dist
+make XZ_OPT=-0 dist
 
 # container has a writable /results/ in the "dist" scenario, but not in others; copy dist tarball there
 if [ -w /results ]; then

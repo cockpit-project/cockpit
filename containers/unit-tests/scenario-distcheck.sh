a66f2019301a518d4d85dfc6fff33213b3ca87a1
Martin Pitt <mpitt@redhat.com>
2021-08-05 09:33:12 +0200

    containers: Properly run test-static-code during unit tests
    Since commit 4077dee3d2f65b our unit test runs did not run pycodestyle
    any more, as that started to skip the pycodestyle check in a non-git
    tree (such as the unpacked tree that `distcheck` runs in). As
    test-static-code does not run in `check-memory` either, none of the four
    scenarios ended up running it.
    
    So explicitly call it in the distcheck scenario, and relatively early so
    that failures (which happen quite often) are easier to spot in the log.

--- containers/unit-tests/scenario-distcheck.sh
+++ containers/unit-tests/scenario-distcheck.sh
@@ -17,6 +17,9 @@ grep -q 'src/bridge/cockpitpackages.c' po/cockpit.pot
 # Check that we are lint clean
 npm run eslint
 
+# run full test-static-code with git tree (several tests get skipped during distcheck)
+tools/test-static-code
+
 # validate that "distclean" does not remove too much
 mkdir _distcleancheck
 tar -C _distcleancheck -xf cockpit-[0-9]*.tar.xz

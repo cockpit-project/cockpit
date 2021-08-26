d3d76b9e7a813de2f29302008cfb8c3d6b78191f
Martin Pitt <mpitt@redhat.com>
2021-08-26 09:17:32 +0200

    test: Re-generalize missing kernel.all.cpu.nice metrics message
    This particular message can hit other tests, as pretty much all of them
    start on the Overview page, which displays the CPU/memory metrics.
    
    So go back to ignoring this particular message globally. We had that
    until commit 8bd081d8c1, but with a much wider pattern.

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -1196,6 +1196,9 @@ class MachineCase(unittest.TestCase):
         r"#1\) Respect the privacy of others.",
         r"#2\) Think before you type.",
         r"#3\) With great power comes great responsibility.",
+
+        # starting out with empty PCP logs and pmlogger not running causes this metrics channel message
+        "pcp-archive: no such metric: kernel.all.cpu.nice: Unknown metric name",
     ]
 
     default_allowed_messages += os.environ.get("TEST_ALLOW_JOURNAL_MESSAGES", "").split(",")

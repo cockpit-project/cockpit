8829080ee2791b1ad55c4e56b0243294ecfdbef9
Marius Vollmer <mvollmer@redhat.com>
2021-09-17 10:42:12 +0300

    storage: Add "offset", "total", and "small" props to the usage bar
    This requires us to implement it in HTML.

--- pkg/storaged/storage-controls.jsx
+++ pkg/storaged/storage-controls.jsx
@@ -22,7 +22,6 @@ import React, { useState } from 'react';
 import {
     Button, Dropdown, DropdownItem, DropdownToggle, KebabToggle,
     Tooltip, TooltipPosition,
-    Progress, ProgressMeasureLocation, ProgressVariant,
     Switch,
 } from '@patternfly/react-core';
 import { BarsIcon } from '@patternfly/react-icons';
@@ -33,6 +32,8 @@ import client from "./client.js";
 
 import { dialog_open } from "./dialog.jsx";
 import { fmt_to_fragments } from "utils.jsx";
+
+import '@patternfly/react-styles/css/components/Progress/progress.css';
 
 const _ = cockpit.gettext;
 
@@ -192,21 +193,28 @@ export class StorageOnOff extends React.Component {
  * in a dangerous color.
  */
 
-export const StorageUsageBar = ({ stats, critical, block }) => {
+export const StorageUsageBar = ({ stats, critical, block, offset, total, small }) => {
     if (!stats)
         return null;
 
     const fraction = stats[0] / stats[1];
-    const labelText = utils.format_fsys_usage(stats[0], stats[1]);
+    const off_fraction = offset / stats[1];
+    const total_fraction = total / stats[1];
+    const labelText = small ? cockpit.format_bytes(stats[0], 1024) : utils.format_fsys_usage(stats[0], stats[1]);
 
     return (
-        <Progress value={stats[0]} max={stats[1]}
-            valueText={labelText}
-            label={labelText}
-            aria-label={cockpit.format(_("Usage of $0"), block)}
-            variant={fraction > critical ? ProgressVariant.danger : ProgressVariant.info}
-            measureLocation={ProgressMeasureLocation.outside} />
-    );
+        <div className={"pf-c-progress pf-m-outside pf-m-singleline" + (fraction > critical ? " pf-m-danger" : "") + (small ? " pf-m-sm" : "")}>
+            <div className="pf-c-progress__status" aria-hidden="true">
+                <span className="pf-c-progress__measure">{labelText}</span>
+            </div>
+            <div className="pf-c-progress__bar" role="progressbar"
+                 aria-valuemin="0" aria-valuemax={stats[1]} aria-valuenow={stats[0]}
+                 aria-label={cockpit.format(_("Usage of $0"), block)}
+                 aria-valuetext={labelText}>
+                <div className="pf-c-progress__indicator ct-progress-other" aria-hidden="true" style={{ width: total_fraction * 100 + "%" }} />
+                <div className="pf-c-progress__indicator" style={{ left: off_fraction * 100 + "%", width: fraction * 100 + "%" }} />
+            </div>
+        </div>);
 };
 
 export const StorageMenuItem = ({ onClick, children }) => (

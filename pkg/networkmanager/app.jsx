5bab913d0fa7500496fc6a36e2b896bc41543618
Katerina Koukiou <kkoukiou@redhat.com>
2021-10-08 19:56:45 +0200

    networkmanager: stop including PF3 - this page is brand new

--- pkg/networkmanager/app.jsx
+++ pkg/networkmanager/app.jsx
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
-import '../lib/patternfly/patternfly-cockpit.scss';
+import '../lib/patternfly/patternfly-4-cockpit.scss';
 import cockpit from "cockpit";
 import ReactDOM from "react-dom";
 import React, { useRef } from 'react';

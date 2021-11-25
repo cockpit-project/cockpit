eb0c5b47df236351e642e2149919b796b07ba05c
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2021-11-25 13:18:34 +0100

    bastion: use cockpit-tls
    Have cockpit-ws listen on the unix socket where cockpit-tls would expect
    to find it if it had been systemd-activated.

--- containers/bastion/run.sh
+++ containers/bastion/run.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
-/usr/libexec/cockpit-certificate-ensure
+wssockdir=/run/cockpit/wsinstance
+sha256nil=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+wssock=$wssockdir/${sha256nil}
 
-exec /usr/libexec/cockpit-ws
+mkdir -p "$wssockdir"
+/usr/libexec/cockpit-ws -U ${wssock} &
+
+/usr/libexec/cockpit-certificate-ensure --for-cockpit-tls
+exec /usr/libexec/cockpit-tls

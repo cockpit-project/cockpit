47a4cc4523aa819a16ffe3edf367902e74209f28
Martin Pitt <mpitt@redhat.com>
2022-02-28 14:33:47 +0100

    test: Fix JS code style in firefox/chromium CDP driver
    One round of `eslint --fix` to fix indentation, unnecessary dict key
    quoting, avoid `var`, and similar.
    
    Just keep `var ssl_bad_certificate_action`, as that is currently written
    to in testlib.py. This will be cleaned up in a separate commit.

--- test/common/chromium-cdp-driver.js
+++ test/common/chromium-cdp-driver.js
@@ -30,7 +30,7 @@
 
 const CDP = require('chrome-remote-interface');
 
-var enable_debug = false;
+let enable_debug = false;
 
 function debug(msg) {
     if (enable_debug)
@@ -49,24 +49,24 @@ function fatal() {
 function fail(err) {
     if (typeof err === 'undefined')
         err = null;
-    process.stdout.write(JSON.stringify({"error": err}) + '\n');
+    process.stdout.write(JSON.stringify({ error: err }) + '\n');
 }
 
 function success(result) {
     if (typeof result === 'undefined')
         result = null;
-    process.stdout.write(JSON.stringify({"result": result}) + '\n');
+    process.stdout.write(JSON.stringify({ result: result }) + '\n');
 }
 
 /**
  * Record console.*() calls and Log messages so that we can forward them to
  * stderr and dump them on test failure
  */
-var messages = [];
-var logPromiseResolver;
-var nReportedLogMessages = 0;
-var unhandledExceptions = [];
-var shownMessages = []; // Show every message just once, keep here seen messages
+const messages = [];
+let logPromiseResolver;
+let nReportedLogMessages = 0;
+const unhandledExceptions = [];
+const shownMessages = []; // Show every message just once, keep here seen messages
 
 function clearExceptions() {
     unhandledExceptions.length = 0;
@@ -77,27 +77,27 @@ function setupLogging(client) {
     client.Runtime.enable();
 
     client.Runtime.consoleAPICalled(info => {
-        let msg = info.args.map(v => (v.value || "").toString()).join(" ");
+        const msg = info.args.map(v => (v.value || "").toString()).join(" ");
 
-        messages.push([ info.type, msg ]);
+        messages.push([info.type, msg]);
         if (shownMessages.indexOf(msg) == -1) {
             if (!enable_debug) // disable message de-duplication in --trace mode
                 shownMessages.push(msg);
-            process.stderr.write("> " + info.type + ": " + msg + "\n")
+            process.stderr.write("> " + info.type + ": " + msg + "\n");
         }
 
         resolveLogPromise();
     });
 
     client.Runtime.exceptionThrown(info => {
-        let details = info.exceptionDetails;
+        const details = info.exceptionDetails;
         // don't log test timeouts, they already get handled
         if (details.exception && details.exception.className === "PhWaitCondTimeout")
             return;
 
         // HACK: https://github.com/cockpit-project/cockpit/issues/14871
         if (details.description && details.description.indexOf("Rendering components directly into document.body is discouraged") > -1)
-            return
+            return;
 
         process.stderr.write(details.description || JSON.stringify(details) + "\n");
 
@@ -109,7 +109,7 @@ function setupLogging(client) {
 
     client.Log.enable();
     client.Log.entryAdded(entry => {
-        let msg = entry["entry"];
+        const msg = entry.entry;
         /* Ignore unsafe-inline messages from PatternFly's usage of Emotion
          * (https://github.com/patternfly/patternfly-react/issues/2919) */
         if ((msg.text || "").indexOf("Refused to apply inline style") >= 0) {
@@ -121,10 +121,10 @@ function setupLogging(client) {
                 msg.stackTrace = "(minified)";
         }
 
-        messages.push([ "cdp", msg ]);
+        messages.push(["cdp", msg]);
         /* Ignore authentication failure log lines that don't denote failures */
         if (!(msg.url || "").endsWith("/login") || (msg.text || "").indexOf("401") === -1) {
-            const orig = {...msg};
+            const orig = { ...msg };
             delete msg.timestamp;
             delete msg.args;
             const msgstr = JSON.stringify(msg);
@@ -168,7 +168,6 @@ function waitLog() {
     });
 }
 
-
 /**
  * Frame tracking
  *
@@ -180,10 +179,10 @@ function waitLog() {
  * to load. This is very laborious, see this issue for discussing improvements:
  * https://github.com/ChromeDevTools/devtools-protocol/issues/72
  */
-var frameIdToContextId = {};
-var frameNameToFrameId = {};
+const frameIdToContextId = {};
+const frameNameToFrameId = {};
 
-var pageLoadHandler = null;
+let pageLoadHandler = null;
 
 function setupFrameTracking(client) {
     client.Page.enable();
@@ -207,7 +206,7 @@ function setupFrameTracking(client) {
 
     client.Runtime.executionContextDestroyed(info => {
         debug("executionContextDestroyed " + info.executionContextId);
-        for (let frameId in frameIdToContextId) {
+        for (const frameId in frameIdToContextId) {
             if (frameIdToContextId[frameId] == info.executionContextId) {
                 delete frameIdToContextId[frameId];
                 break;
@@ -229,10 +228,10 @@ function setupLocalFunctions(client) {
 function getFrameExecId(frame) {
     if (frame === null)
         frame = "cockpit1";
-    var frameId = frameNameToFrameId[frame];
+    const frameId = frameNameToFrameId[frame];
     if (!frameId)
         return -1;
-    var execId = frameIdToContextId[frameId];
+    const execId = frameIdToContextId[frameId];
     if (!execId)
         return -1;
     return execId;
@@ -249,17 +248,17 @@ var ssl_bad_certificate_action = "cancel";
 function setupSSLCertHandling(client) {
     client.Security.enable();
 
-    client.Security.setOverrideCertificateErrors({override: true});
+    client.Security.setOverrideCertificateErrors({ override: true });
     client.Security.certificateError(info => {
         process.stderr.write(`CDP: Security.certificateError ${JSON.stringify(info)}; action: ${ssl_bad_certificate_action}\n`);
         client.Security.handleCertificateError({ eventId: info.eventId, action: ssl_bad_certificate_action })
-            .catch(ex => {
+                .catch(ex => {
                 // some race condition in Chromium, ok if the event is already gone
-                if (ex.response && ex.response.message && ex.response.message.indexOf("Unknown event id") >= 0)
-                    debug(`setupSSLCertHandling for event ${info.eventId} failed, ignoring: ${JSON.stringify(ex.response)}`);
-                else
-                    throw ex;
-            });
+                    if (ex.response && ex.response.message && ex.response.message.indexOf("Unknown event id") >= 0)
+                        debug(`setupSSLCertHandling for event ${info.eventId} failed, ignoring: ${JSON.stringify(ex.response)}`);
+                    else
+                        throw ex;
+                });
     });
 }
 
@@ -273,7 +272,7 @@ function setupSSLCertHandling(client) {
  */
 process.stdin.setEncoding('utf8');
 
-if (process.env["TEST_CDP_DEBUG"])
+if (process.env.TEST_CDP_DEBUG)
     enable_debug = true;
 
 options = { };
@@ -286,46 +285,45 @@ if (process.argv.length >= 3) {
 }
 
 CDP.New(options)
-    .then(target => {
-        target.port = options.port;
-        CDP({target: target})
-            .then(client => {
-                setupLogging(client);
-                setupFrameTracking(client);
-                setupSSLCertHandling(client);
-                setupLocalFunctions(client);
+        .then(target => {
+            target.port = options.port;
+            CDP({ target: target })
+                    .then(client => {
+                        setupLogging(client);
+                        setupFrameTracking(client);
+                        setupSSLCertHandling(client);
+                        setupLocalFunctions(client);
 
-                let input_buf = '';
-                process.stdin
-                    .on('data', chunk => {
-                        input_buf += chunk;
-                        while (true) {
-                            let i = input_buf.indexOf('\n');
-                            if (i < 0)
-                                break;
-                            let command = input_buf.slice(0, i);
+                        let input_buf = '';
+                        process.stdin
+                                .on('data', chunk => {
+                                    input_buf += chunk;
+                                    while (true) {
+                                        const i = input_buf.indexOf('\n');
+                                        if (i < 0)
+                                            break;
+                                        const command = input_buf.slice(0, i);
 
-                            // run the command
-                            eval(command).then(reply => {
-                                if (unhandledExceptions.length === 0) {
-                                    success(reply);
-                                } else {
-                                    let message = unhandledExceptions[0];
-                                    fail(message.split("\n")[0]);
-                                    clearExceptions();
-                                }
-                            }, fail);
+                                        // run the command
+                                        eval(command).then(reply => {
+                                            if (unhandledExceptions.length === 0) {
+                                                success(reply);
+                                            } else {
+                                                const message = unhandledExceptions[0];
+                                                fail(message.split("\n")[0]);
+                                                clearExceptions();
+                                            }
+                                        }, fail);
 
-                            input_buf = input_buf.slice(i+1);
-                        }
-
+                                        input_buf = input_buf.slice(i + 1);
+                                    }
+                                })
+                                .on('end', () => {
+                                    CDP.Close(target)
+                                            .then(() => process.exit(0))
+                                            .catch(fatal);
+                                });
                     })
-                   .on('end', () => {
-                       CDP.Close(target)
-                           .then(() => process.exit(0))
-                           .catch(fatal);
-                   });
-            })
-            .catch(fatal);
-    })
-    .catch(fatal);
+                    .catch(fatal);
+        })
+        .catch(fatal);

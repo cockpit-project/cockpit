4ff8b2a317e8131fd64e75baeb1c9e8aca2e0474
Marius Vollmer <mvollmer@redhat.com>
2022-05-02 16:59:36 +0300

    shell: Rewrite superuser dialogs
    Now they use function components and the UnlockDialog maintains its
    own state.
    
    Most importantly, the new SuperuserButton is no longer tied to the
    life-cycle of the dialogs and the SuperuserAlert component and its
    parents do not need to do anything special to keep it alive.
    
    The "Authenticate" button got wider because it now always has a
    "isLoading" attribute.  This causes PF to reserve room for the spinner
    also with "isLoading={false}".

--- pkg/systemd/superuser-alert.jsx
+++ pkg/systemd/superuser-alert.jsx
@@ -20,44 +20,24 @@
 import cockpit from "cockpit";
 import React from 'react';
 
-import {
-    Alert, Button
-} from '@patternfly/react-core';
+import { Alert } from '@patternfly/react-core';
 
 import { LockIcon } from '@patternfly/react-icons';
 
-import { SuperuserDialogs } from "../shell/superuser.jsx";
+import { SuperuserButton } from "../shell/superuser.jsx";
+import { superuser } from "superuser.js";
 
 const _ = cockpit.gettext;
 
 export class SuperuserAlert extends React.Component {
-    constructor () {
-        super();
-        this.superuser = cockpit.dbus(null, { bus: "internal" }).proxy("cockpit.Superuser", "/superuser");
-        this.superuser.addEventListener("changed", () => { this.setState({}) });
-    }
-
     render () {
-        const actions =
-            <SuperuserDialogs create_trigger={(unlocked, onclick) =>
-                <Button onClick={onclick}>{_("Turn on administrative access")}</Button>}
-                proxy={this.superuser}
-            />;
+        if (superuser.allowed)
+            return null;
 
-        // The SuperuserDialogs element above needs to be in the DOM
-        // regardless of the superuser level so that the dialogs are
-        // not closed unexpectedly.  Thus, we merely hide the Alert
-        // when it does not apply, instead of fully removing it.
-
-        return (
-            <>
-                <Alert className="ct-limited-access-alert"
-                       hidden={this.superuser.Current != "none"}
-                       variant="warning" isInline
-                       customIcon={<LockIcon />}
-                       actionClose={actions}
-                       title={_("Web console is running in limited access mode.")} />
-            </>
-        );
+        return <Alert className="ct-limited-access-alert"
+                      variant="warning" isInline
+                      customIcon={<LockIcon />}
+                      actionClose={<SuperuserButton />}
+        title={_("Web console is running in limited access mode.")} />;
     }
 }

a14a2d52c057e519b55598be160f2aee78001a6b
Marius Vollmer <mvollmer@redhat.com>
2022-05-10 11:40:55 +0300

    lib: More dialog documentation

--- pkg/lib/dialogs.jsx
+++ pkg/lib/dialogs.jsx
@@ -49,6 +49,51 @@
  * is open, and it does it high up in the DOM, in a stable place.
  * Even if ExampleButton is no longer part of the DOM, the dialog will
  * stay open and remain useable.
+ *
+ * The "WithDialogs" component enables all its children to show
+ * dialogs.  Such a dialog will stay open as long as the WithDialogs
+ * component itself is mounted.  Thus, you should put the WithDialogs
+ * component somewhere high up in your component tree, maybe even as
+ * the very top-most component.
+ *
+ * If your Cockpit application has multiple pages and navigation
+ * between these pages is controlled by the browser URL, then each of
+ * these pages should have its own WithDialogs wrapper. This way, a
+ * dialog opened on one page closes when the user navigates away from
+ * that page.
+ *
+ * A component that wants to show a dialogs needs to get hold of the
+ * current "Dialogs" context and then call it's "show" method.  For a
+ * function component the Dialogs context is returned by
+ * "useDialogs()", as shown above in the example.
+ *
+ * A class component can declare a static context type and then use
+ * "this.context" to find the Dialogs object:
+ *
+ * import { DialogsContext } from "dialogs.jsx";
+ *
+ * class ExampleButton extends React.Component {
+ *   static contextType = DialogsContext;
+ *
+ *   function render() {
+ *     const Dialogs = this.context;
+ *     return <Button onClick={() => Dialogs.show(<MyDialog />)}>Open dialog</Button>;
+ *   }
+ * }
+ *
+ *
+ * - Dialogs.show(component)
+ *
+ * Calling "Dialogs.show" will render the given component as a direct
+ * child of the inner-most enclosing "WithDialogs" component.  The
+ * component is of course intended to be a dialog, such as
+ * Patternfly's "Modal".  There is only ever one of these; a second
+ * call to "show" will remove the previously rendered component.
+ * Passing "null" will remove the currently rendered componenet, if any.
+ *
+ * - Dialogs.close()
+ *
+ * Same as "Dialogs.show(null)".
  */
 
 import React, { useState, useContext } from "react";

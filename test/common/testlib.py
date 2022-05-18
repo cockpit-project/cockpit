765828ba2acd46020e9991f12480d8a66262b4ca
Jelle van der Waa <jvanderwaa@redhat.com>
2022-05-18 19:35:55 +0200

    test: one-line docstring should fit on one line with quotes
    Solves pydocstyle D200.

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -15,9 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
-"""
-Tools for writing Cockpit test cases.
-"""
+"""Tools for writing Cockpit test cases."""
 
 from time import sleep
 

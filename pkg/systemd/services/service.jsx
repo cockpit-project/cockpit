841b302a223f45a09ed3009db5f056518cda9043
Jelle van der Waa <jvanderwaa@redhat.com>
2022-06-22 11:58:29 +0200

    systemd: drop unused error state in Service

--- pkg/systemd/services/service.jsx
+++ pkg/systemd/services/service.jsx
@@ -38,7 +38,6 @@ export class Service extends React.Component {
         super(props);
 
         this.state = {
-            error: undefined,
             /* The initial load of the Services page will not call GetAll for units Properties
              * since ListUnits API call already has provided us with a subset of the Properties.
              * As a result, properties like the 'Requires' are not present in the state at this point.

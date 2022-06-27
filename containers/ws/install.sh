447f03bb95081aa0eef0053acdf62b7a2434d31e
Martin Pitt <martin@piware.de>
2022-06-27 07:55:04 +0200

    containers/ws: Factorize install command in install.sh
    Drop `$INSTALLER` -- we don't use this from anywhere any more.

--- containers/ws/install.sh
+++ containers/ws/install.sh
@@ -13,26 +13,24 @@ package_name()
 
 OSVER=$(. /etc/os-release && echo "$VERSION_ID")
 
-if [ -z "$INSTALLER" ]; then
-    INSTALLER="dnf"
-fi
+dnf -y update
 
-"$INSTALLER" -y update
-"$INSTALLER" install -y util-linux-core sed
+INSTALL="dnf install -y"
+$INSTALL util-linux-core sed
 
 arch=`uname -p`
 rpm=$(ls /container/rpms/cockpit-ws-*$OSVER.*$arch.rpm /container/rpms/cockpit-bridge-*$OSVER.*$arch.rpm || true)
 
 # If there are rpm files in the current directory we'll install those
 if [ -n "$rpm" ]; then
-    $INSTALLER -y install /container/rpms/cockpit-ws-*$OSVER.*$arch.rpm /container/rpms/cockpit-bridge-*$OSVER.*$arch.rpm
+    $INSTALL /container/rpms/cockpit-ws-*$OSVER.*$arch.rpm /container/rpms/cockpit-bridge-*$OSVER.*$arch.rpm
 else
     # pull packages from https://copr.fedorainfracloud.org/coprs/g/cockpit/cockpit-preview/
     echo -e '[group_cockpit-cockpit-preview]\nname=Copr repo for cockpit-preview owned by @cockpit\nbaseurl=https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/fedora-$releasever-$basearch/\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/pubkey.gpg\nrepo_gpgcheck=0\nenabled=1\nenabled_metadata=1' > /etc/yum.repos.d/cockpit.repo
     ws=$(package_name "cockpit-ws")
     bridge=$(package_name "cockpit-bridge")
-    "$INSTALLER" -y install "$ws" "$bridge"
+    $INSTALL "$ws" "$bridge"
 fi
 
-"$INSTALLER" clean all
+dnf clean all
 rm -rf /container/rpms || true

dc7c3d6b718821d2927fcf7815f0a533c2a7f9b7
Martin Pitt <martin@piware.de>
2022-10-30 08:12:59 +0100

    containers/ws: Change tagging schema from date to cockpit version
    The upstream version number is a more useful description. Tags already
    have an associated creation date anyway.

--- containers/ws/release.sh
+++ containers/ws/release.sh
@@ -8,18 +8,23 @@ if [ -z "${RUNC:-}" ]; then
     }
 fi
 
-TAG=$(date +%Y-%m-%d)
-$RUNC build -t quay.io/cockpit/ws:$TAG containers/ws
+$RUNC build -t quay.io/cockpit/ws:release containers/ws
 
 # smoke test
 name=ws-release
-$RUNC run --name $name -p 19999:9090 -d quay.io/cockpit/ws:$TAG
+$RUNC run --name $name -p 19999:9090 -d quay.io/cockpit/ws:release
 until curl --fail --show-error -k --head https://localhost:19999; do
     sleep 1
 done
+
+# determine cockpit version
+TAG=$($RUNC exec $name cockpit-bridge --version | sed -n '/^Version:/ { s/^.*: //p }')
+
 $RUNC rm -f $name
 
-$RUNC tag quay.io/cockpit/ws:$TAG quay.io/cockpit/ws:latest
+$RUNC tag quay.io/cockpit/ws:release quay.io/cockpit/ws:$TAG
+$RUNC tag quay.io/cockpit/ws:release quay.io/cockpit/ws:latest
+$RUNC rmi quay.io/cockpit/ws:release
 
 # push both tags
 $RUNC push quay.io/cockpit/ws:$TAG

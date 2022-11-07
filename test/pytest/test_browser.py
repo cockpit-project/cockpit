79f378dfe020cd6fa823042dfbae3ba0be2ad893
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-11-07 09:28:06 +0100

    python: merge `make pycheck` into pytest
    Add a new testcase in test/pytest/test_server.py which knows how to run
    the QUnit tests, including collection of coverage information.
    
    Make sure we build the `test-server` and the webpacks before running
    `pytest` from the Makefile.  For running `pytest` directly, the html
    tests will be skipped if the test-server is missing or if there are no
    `dist/*/test-*.html` files.
    
    Remove the `make pycheck` target, and drop the extra scenario from the
    unit-tests workflow.
    
    `pytest` now takes significantly longer to run.  The pytest-xdist
    plugin can help with this, and it works in combination with our other
    plugins, pytest-timeout and pytest-cov.
    
    For manual invocation of pytest, use something like the following:
    
      pytest -n auto --cov --timeout 30
    
    We run the tests non-parallel in the unit-tests container because
    there's no real advantage to speeding this up in that case, and running
    in parallel makes the log output more difficult to read.

---
+++ test/pytest/test_browser.py
@@ -0,0 +1,38 @@
+import glob
+import os
+import subprocess
+import sys
+
+import pytest
+
+SRCDIR = os.path.realpath(f'{__file__}/../../..')
+BUILDDIR = os.environ.get('abs_builddir', SRCDIR)
+
+SKIP = {
+    'base1/test-dbus.html',
+    'base1/test-dbus-address.html',
+}
+
+XFAIL = {
+    'base1/test-websocket.html',
+}
+
+
+@pytest.mark.parametrize('html', glob.glob('*/test-*.html', root_dir=f'{SRCDIR}/dist'))
+def test_browser(html):
+    if not os.path.exists(f'{BUILDDIR}/test-server'):
+        pytest.skip('no test-server')
+    if html in SKIP:
+        pytest.skip()
+    elif html in XFAIL:
+        pytest.xfail()
+
+    if 'COVERAGE_RCFILE' in os.environ:
+        coverage = ['coverage', 'run', '--parallel-mode', '--module']
+    else:
+        coverage = []
+
+    # Merge 2>&1 so that pytest displays an interleaved log
+    subprocess.run(['test/common/tap-cdp', f'{BUILDDIR}/test-server',
+                    sys.executable, '-m', *coverage, 'cockpit.bridge',
+                    f'./dist/{html}'], check=True, stderr=subprocess.STDOUT)

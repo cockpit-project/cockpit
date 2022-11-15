ce6734980365417956fd31ca1f90113796178720
Martin Pitt <mpitt@redhat.com>
2022-11-15 13:41:39 +0100

    apps: Fix exception handling in watch-appstream.py
    `KeyboardInterrupt` and `SystemExit` are `BaseException`s as well, but
    should not be intercepted.
    
    Fixes CodeQL "Except block handles 'BaseException'" note.

--- pkg/apps/watch-appstream.py
+++ pkg/apps/watch-appstream.py
@@ -341,7 +341,7 @@ def watch_db():
                 callback(path, ET.parse(path).getroot())
             elif path.endswith('.xml.gz'):
                 callback(path, ET.parse(gzip.open(path)).getroot())
-        except BaseException:
+        except Exception:
             # If we hit an exception during handling a file, pretend
             # that it doesn't exist instead of keeping old data.  This
             # makes the behavior consistent across restarts of this

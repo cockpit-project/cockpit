49e534a8efde3a766baf637a4d75431dc7292b56
Matej Marusak <mmarusak@redhat.com>
2022-12-09 10:03:14 +0100

    users: Use standard components for 'Authorized public SSH keys'
    This also fixes highlighting of newly added keys.

--- pkg/users/authorized-keys-panel.js
+++ pkg/users/authorized-keys-panel.js
@@ -18,11 +18,18 @@
  */
 
 import cockpit from 'cockpit';
-import React from 'react';
+import React, { useState } from 'react';
 import { useObject, useEvent } from 'hooks.js';
 
-import { Button, TextArea } from '@patternfly/react-core';
+import {
+    Button,
+    Card, CardHeader, CardTitle, CardActions,
+    Dropdown, KebabToggle,
+    OverflowMenu, OverflowMenuContent, OverflowMenuGroup, OverflowMenuItem, OverflowMenuControl, OverflowMenuDropdownItem,
+    Text, TextArea, TextVariants
+} from '@patternfly/react-core';
 import { show_modal_dialog } from "cockpit-components-dialog.jsx";
+import { ListingTable } from 'cockpit-components-table.jsx';
 import { show_unexpected_error } from "./dialog-utils.js";
 import * as authorized_keys from './authorized-keys.js';
 
@@ -85,79 +92,94 @@ export function AuthorizedKeys({ name, home, allow_mods }) {
                               manager => manager.close(),
                               [name, home]);
     useEvent(manager, "changed");
+    const [openedMenu, setOpenedMenu] = useState([]);
 
     function remove_key(raw) {
         manager.remove_key(raw).catch(show_unexpected_error);
     }
 
     const { state, keys } = manager;
-    let key_items;
+    let error = "";
 
-    if (state == "access-denied") {
-        key_items = [
-            <li key={state} className="pf-c-data-list__item">
-                <div key={state} className="pf-c-data-list__item-row fingerprint">
-                    <span>{_("You do not have permission to view the authorized public keys for this account.")}</span>
-                </div>
-            </li>
-        ];
-    } else if (state == "failed") {
-        key_items = [
-            <li key={state} className="pf-c-data-list__item">
-                <div key={state} className="pf-c-data-list__item-row fingerprint">
-                    <span>{_("Failed to load authorized keys.")}</span>
-                </div>
-            </li>
-        ];
-    } else if (state == "ready") {
-        if (keys.length === 0) {
-            key_items = [
-                <li key={state} className="pf-c-data-list__item">
-                    <div key="empty" className="pf-c-data-list__item-row no-keys">
-                        {_("There are no authorized public keys for this account.")}
-                    </div>
-                </li>
-            ];
-        } else {
-            key_items = keys.map(k =>
-                <li key={k.raw} className="pf-c-data-list__item">
-                    <div className="pf-c-data-list__item-row">
-                        <div className="pf-c-data-list__item-content">
-                            <div className="pf-c-data-list__cell comment">
-                                { k.comment || <em>{_("Unnamed")}</em> }
-                            </div>
-                            <div className="pf-c-data-list__cell fingerprint">
-                                { k.fp || <span>{_("Invalid key")}</span> }
-                            </div>
-                        </div>
-                        { allow_mods &&
-                        <div className="pf-c-data-list__item-action">
-                            <Button variant="secondary" onClick={() => remove_key(k.raw)}
-                                    className="account-remove-key">
-                                {_("Remove")}
-                            </Button>
-                        </div> }
-                    </div>
-                </li>);
-        }
-    } else
+    if (state == "access-denied")
+        error = _("You do not have permission to view the authorized public keys for this account.");
+    else if (state == "failed")
+        error = _("Failed to load authorized keys.");
+    else if (state == "ready")
+        error = _("There are no authorized public keys for this account.");
+    else
         return null;
 
     return (
-        <div className="pf-c-card" id="account-authorized-keys">
-            <div className="pf-c-card__header">
-                <div className="pf-c-card__title"><h2>{_("Authorized public SSH keys")}</h2></div>
+        <Card id="account-authorized-keys">
+            <CardHeader>
+                <CardTitle><Text component={TextVariants.h2}>{_("Authorized public SSH keys")}</Text></CardTitle>
                 { allow_mods &&
-                <Button onClick={() => add_authorized_key_dialog(manager)}
-                        id="authorized-key-add">
-                    {_("Add key")}
-                </Button>}
-            </div>
-            <div className="pf-c-card__body contains-list">
-                <ul className="pf-c-data-list pf-m-compact" id="account-authorized-keys-list">
-                    { key_items }
-                </ul>
-            </div>
-        </div>
+                <CardActions>
+                    <Button variant="secondary" id="authorized-key-add" onClick={() => add_authorized_key_dialog(manager)}>
+                        {_("Add key")}
+                    </Button>
+                </CardActions>
+                }
+            </CardHeader>
+            <ListingTable
+                aria-label={ _("Authorized public SSH keys") }
+                id="account-authorized-keys-list"
+                showHeader={false}
+                emptyCaption={error}
+                columns={ [
+                    { title: _("Name"), header: true },
+                    { title: _("Fingerprint") },
+                    { title: "" },
+                ] }
+                rows={keys.map(k => {
+                    return {
+                        columns: [
+                            { title: k.comment || _("Unnamed"), props: { width: 20 } },
+                            { title: k.fp || _("Invalid key"), props: { width: 80 } },
+                            {
+                                title: <OverflowMenu breakpoint="lg">
+                                    <OverflowMenuContent>
+                                        <OverflowMenuGroup groupType="button">
+                                            <OverflowMenuItem>
+                                                <Button key={k.fp} variant="secondary" onClick={() => remove_key(k.raw)}>
+                                                    {_("Remove")}
+                                                </Button>
+                                            </OverflowMenuItem>
+                                        </OverflowMenuGroup>
+                                    </OverflowMenuContent>
+                                    <OverflowMenuControl>
+                                        <Dropdown position="right"
+                                                  onSelect={() => {
+                                                      if (openedMenu.indexOf(k.fp) >= 0)
+                                                          setOpenedMenu(openedMenu.filter(m => m !== k.fp));
+                                                      else
+                                                          setOpenedMenu([...openedMenu, k.fp]);
+                                                  }}
+                                                  toggle={
+                                                      <KebabToggle
+                                                      onToggle={open => {
+                                                          if (open)
+                                                              setOpenedMenu([...openedMenu, k.fp]);
+                                                          else
+                                                              setOpenedMenu(openedMenu.filter(m => m !== k.fp));
+                                                      }}
+                                                      />
+                                                  }
+                                                  isOpen={openedMenu.indexOf(k.fp) >= 0}
+                                                  isPlain
+                                                  dropdownItems={[<OverflowMenuDropdownItem key="delete" isShared onClick={() => remove_key(k.raw)}>
+                                                      {_("Remove")}
+                                                  </OverflowMenuDropdownItem>]}
+                                        />
+                                    </OverflowMenuControl>
+                                </OverflowMenu>,
+                                props: { className: "pf-c-table__action" }
+                            }
+                        ],
+                        props: { key: k.fp }
+                    };
+                })} />
+        </Card>
     );
 }

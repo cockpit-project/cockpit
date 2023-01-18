cockpit-project/cockpit

commit e100e4d8b0b4bd53f28fa746003f7439246d4662

python: Clean up children of spawned processes
Set `prctl(SET_PDEATHSIG, SIGHUP)` for cockpit.spawn() and friends, to
clean up subprocesses of the spawned process. The C bridge does that
also (in cockpitpipe.c).

src/cockpit/channels/stream.py
@@ -16,8 +16,10 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
+import ctypes
 import logging
 import os
+import signal
 import socket
 import subprocess
 
@@ -28,6 +30,16 @@ from ..channel import ProtocolChannel, ChannelError
 from ..transports import SocketTransport, SubprocessTransport, SubprocessProtocol
 
 logger = logging.getLogger(__name__)
+
+libc6 = ctypes.cdll.LoadLibrary('libc.so.6')
+
+
+def prctl(*args):
+    if libc6.prctl(*args) != 0:
+        raise OSError('prctl() failed')
+
+
+SET_PDEATHSIG = 1
 
 
 class UnixStreamChannel(ProtocolChannel):
@@ -84,7 +96,8 @@ class SubprocessStreamChannel(ProtocolChannel, SubprocessProtocol):
 
         try:
             logger.debug('Spawning process args=%s', args)
-            return SubprocessTransport(loop, self, args, pty, window, env=env, cwd=cwd, stderr=stderr)
+            return SubprocessTransport(loop, self, args, pty, window, env=env, cwd=cwd, stderr=stderr,
+                                       preexec_fn=lambda: prctl(SET_PDEATHSIG, signal.SIGHUP))
         except FileNotFoundError as error:
             raise ChannelError('not-found') from error
         except PermissionError as error:

test/verify/check-reauthorize
@@ -18,14 +18,13 @@
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
 import parent  # noqa: F401
-from testlib import MachineCase, nondestructive, skipDistroPackage, skipImage, todoPybridge, test_main
+from testlib import MachineCase, nondestructive, skipDistroPackage, skipImage, test_main
 
 
 @skipDistroPackage()
 @nondestructive
 class TestReauthorize(MachineCase):
 
-    @todoPybridge()
     def testBasic(self):
         self.allow_journal_messages('.*dropping message while waiting for child to exit.*')
         b = self.browser

test/verify/check-storage-luks
@@ -20,7 +20,7 @@
 import parent  # noqa: F401
 from storagelib import StorageCase
 from packagelib import PackageCase
-from testlib import skipImage, test_main, todoPybridge, wait, timeout, attach
+from testlib import skipImage, test_main, wait, timeout, attach
 import subprocess
 
 
@@ -38,7 +38,6 @@ class TestStorageLuks(StorageCase):
         "0": {"memory_mb": 1536}
     }
 
-    @todoPybridge()
     def testLuks(self):
         self.allow_journal_messages("Device is not initialized.*", ".*could not be opened.")
         m = self.machine

commit c6ef37c4f17a8e40456af28df65fa079699ad198

python: Avoid waiting for the child in SubprocessStreamChannel.do_close()
Commit e100e4d8b introduced a race condition: asyncio installs a "child
watcher" (via pidfd) for all launched subprocesses, and expects it to be
able to read the processes' exit code. But subprocess'es terminate()
method *also* does that. If the process is still running, this is
harmless (as it will just return None), but if the process terminates
around the same time as cancelling it, it can happen that the process
stopped in terminate(), it waits() the child and collects the exit code,
and asyncio's child watcher then does not get it any more. This causes
warnings like

    asyncio:child process pid <PID> exit status already read:  will report returncode 255

We actually want to retain that; terminate()'s wait call is just for
avoiding PID reuse races, but that race is negligible for us.
cockpit.spawn() already ensures that do_close() is only actually called
for valid (running) processes, and the kernel usually makes sure that a
PID does not get re-used immediately.

src/cockpit/channels/stream.py
@@ -74,8 +74,16 @@ class SubprocessStreamChannel(ProtocolChannel, SubprocessProtocol):
 
     def do_close(self):
         assert isinstance(self._transport, SubprocessTransport)
-        logger.debug('Received close signal from peer, terminating process %i', self._transport.get_pid())
-        self._transport.terminate()
+        pid = self._transport.get_pid()
+        # avoid calling .terminate(), as that will try to read the process' exit code and race with
+        # asyncio's ChildWatcher (which also wants to wait() the process); the cockpit.spawn() API
+        # already ensures that the process is valid before even calling do_close.
+        try:
+            os.kill(pid, signal.SIGTERM)
+            logger.debug('Received close signal from peer, SIGTERMed process %i', pid)
+        except ProcessLookupError:
+            # already gone? fine!
+            logger.debug('Received close signal from peer, but process %i is already gone', pid)
 
     def create_transport(self, loop: asyncio.AbstractEventLoop, options: Dict[str, Any]) -> SubprocessTransport:
         args: list[str] = options['spawn']
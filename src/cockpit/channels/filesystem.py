0fbd6f1d54ffd00448c4a16360df9cfb473f4050
Jelle van der Waa <jvanderwaa@redhat.com>
2023-01-24 10:46:20 +0100

    python: correctly log possible None value

--- src/cockpit/channels/filesystem.py
+++ src/cockpit/channels/filesystem.py
@@ -232,7 +232,7 @@ class FsWatchChannel(Channel):
             self.send_message(event=event, path=self._path, tag=self._tag, type=type_)
 
     def do_identity_changed(self, fd, err):
-        logger.debug("do_identity_changed(%s): fd %i, err %s", self._path, fd, err)
+        logger.debug("do_identity_changed(%s): fd %s, err %s", self._path, str(fd), err)
         self._tag = tag_from_fd(fd) if fd else '-'
         if self._active:
             self.send_message(event='created' if fd else 'deleted', path=self._path, tag=self._tag)

cab47a799d06980fd9ceaa931c6ff225c6edd45e
Jelle van der Waa <jvanderwaa@redhat.com>
2023-02-15 12:20:07 +0100

    try out script

---
+++ tryout.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import logging
+import json
+
+from cockpit.packages import Manifest
+
+logging.basicConfig(format='%(name)s-%(levelname)s: %(message)s')
+logging.getLogger().setLevel(level=logging.DEBUG)
+
+
+manifest = Manifest.from_json(**json.load(open('pkg/systemd/manifest.json', 'r')))
+print(manifest.name)
+valid = manifest.validate()
+if not valid:
+    print('invalid')

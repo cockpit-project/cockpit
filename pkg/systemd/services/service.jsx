cf2111ad7e3bf2be10cc015755b86e0efcdfe1e2
Martin Pitt <mpitt@redhat.com>
2023-03-09 10:50:57 +0100

    systemd/services: Rewrite Service into a functional component
    This makes the code follow standard React patterns, and nicely
    encapsulates subscriptions in useObject(). Turn `this.path` into a ref,
    so that it stays a local piece of information that isn't React state (as
    that would cause unnecessary renders and useObject reinitializations).
    
    Also inline the ServiceDetails component instantiation -- it's nicer to
    have the whole HTML structure in one piece, and the instantiation is not
    magic or overly complex in any way.

--- pkg/systemd/services/service.jsx
+++ pkg/systemd/services/service.jsx
@@ -17,7 +17,7 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Breadcrumb, BreadcrumbItem } from "@patternfly/react-core/dist/esm/components/Breadcrumb/index.js";
 import { Page, PageSection } from "@patternfly/react-core/dist/esm/components/Page/index.js";
 import { Gallery, GalleryItem } from "@patternfly/react-core/dist/esm/layouts/Gallery/index.js";
@@ -29,9 +29,10 @@ import { LogsPanel } from "cockpit-components-logs-panel.jsx";
 import { superuser } from 'superuser';
 import { WithDialogs } from "dialogs.jsx";
 
-import s_bus from "./busnames.js";
-
 import cockpit from "cockpit";
+import { useObject } from "hooks";
+
+import s_bus from "./busnames.js";
 
 const _ = cockpit.gettext;
 
@@ -40,147 +41,135 @@ function debug() {
         console.debug.apply(console, arguments); // not-covered: debugging
 }
 
-export class Service extends React.Component {
-    constructor(props) {
-        super(props);
+export const Service = ({ dbusClient, owner, unitId, unitIsValid, addTimerProperties, pinnedUnits }) => {
+    const _path = useRef(null);
+    const [error, setError] = useState(null);
+    const [reloading, setReloading] = useState(false);
+    const [unitProps, setUnitProps] = useState(null);
 
-        this.state = {
-            unit_id: null,
-            unit_props: null,
-            error: null,
-            reloading: false,
-        };
-
-        this.path = null;
-        this.subscriptions = [];
-    }
-
-    async componentDidMount() {
-        const dbus = this.props.dbusClient;
+    const updateProperties = async () => {
+        const path = _path.current?.path;
         try {
-            const [path] = await dbus.call(s_bus.O_MANAGER, s_bus.I_MANAGER, "LoadUnit", [this.props.unitId]);
-            this.path = path;
-        } catch (ex) {
-            this.setState({ error: ex.toString() });
-            return;
-        }
-
-        this.subscriptions.push(dbus.subscribe(
-            { interface: s_bus.I_MANAGER, member: "Reloading" },
-            async (_path, _iface, _signal, [reloading]) => {
-                debug("Service detail", this.props.unitId, "Reloading", reloading);
-                this.setState({ reloading });
-                if (!reloading)
-                    await this.updateProperties();
-            }));
-
-        this.subscriptions.push(dbus.subscribe(
-            { path: this.path, interface: s_bus.I_PROPS, member: "PropertiesChanged" }, async () => {
-                debug("Service detail", this.props.unitId, "PropertiesChanged");
-                await this.updateProperties();
-            }));
-
-        /* We need this *only* to pick up failed Conditions after attempting to start a service, as the unit immediately gets
-         * unloaded again and we don't get a PropertiesChanged signal. */
-        this.subscriptions.push(dbus.subscribe(
-            { interface: s_bus.I_MANAGER, member: "JobRemoved" }, async (_p, _i, _s, [_job_id, _job_path, unit, result]) => {
-                if (unit === this.props.unitId && result === "done") {
-                    debug("Service detail", this.props.unitId, "JobRemoved");
-                    await this.updateProperties();
-                }
-            }));
-
-        debug("Service detail", this.props.unitId, "componentDidMount, loading initial properties");
-        await this.updateProperties();
-    }
-
-    componentWillUnmount() {
-        this.subscriptions.forEach(s => s.remove());
-        this.subscriptions = [];
-        debug("Service detail", this.props.unitId, "componentWillUnmount");
-    }
-
-    async updateProperties() {
-        const dbus = this.props.dbusClient;
-        const unit_id = this.props.unitId;
-
-        try {
-            const [unit_props] = await dbus.call(this.path, s_bus.I_PROPS, "GetAll", [s_bus.I_UNIT]);
+            const [unit_props] = await dbusClient.call(path, s_bus.I_PROPS, "GetAll", [s_bus.I_UNIT]);
             // unwrap variants
             for (const key in unit_props)
                 unit_props[key] = unit_props[key].v;
 
-            if (unit_id.endsWith(".timer")) {
-                const [timer_props] = await dbus.call(this.path, s_bus.I_PROPS, "GetAll", [s_bus.I_TIMER]);
-                this.props.addTimerProperties(timer_props, unit_props);
+            if (unitId.endsWith(".timer")) {
+                const [timer_props] = await dbusClient.call(path, s_bus.I_PROPS, "GetAll", [s_bus.I_TIMER]);
+                addTimerProperties(timer_props, unit_props);
             }
 
-            if (unit_id.endsWith(".socket")) {
-                const [socket_props] = await dbus.call(this.path, s_bus.I_PROPS, "GetAll", [s_bus.I_SOCKET]);
+            if (unitId.endsWith(".socket")) {
+                const [socket_props] = await dbusClient.call(path, s_bus.I_PROPS, "GetAll", [s_bus.I_SOCKET]);
                 unit_props.Listen = socket_props.Listen.v;
             }
 
-            unit_props.path = this.path;
+            unit_props.path = path;
 
-            debug("Service detail", unit_id, "updated properties:", JSON.stringify(unit_props));
-            this.setState({ unit_id, unit_props, error: null });
+            debug("Service detail", unitId, "updated properties:", JSON.stringify(unit_props));
+            setUnitProps(unit_props);
+            setError(null);
         } catch (ex) {
-            this.setState({ error: ex.toString() }); // not-covered: unexpected OS error
+            setError(ex.toString()); // not-covered: unexpected OS error
         }
-    }
+    };
 
-    render() {
-        if (this.state.error)
-            return <EmptyStatePanel title={_("Loading unit failed")} icon={ExclamationCircleIcon} paragraph={this.state.error} />;
+    // load object path and set up PropertiesChanged subscription whenever unitId changes
+    useObject(
+        () => {
+            dbusClient.call(s_bus.O_MANAGER, s_bus.I_MANAGER, "LoadUnit", [unitId])
+                    .then(([path]) => {
+                        debug("Service detail", unitId, "loaded path", path);
 
-        if (this.state.unit_props === null)
-            return <EmptyStatePanel loading title={_("Loading...")} paragraph={this.props.unitId} />;
+                        _path.current?.propSub.remove();
+                        const propSub = dbusClient.subscribe(
+                            { path, interface: s_bus.I_PROPS, member: "PropertiesChanged" }, () => {
+                                debug("Service detail", unitId, "PropertiesChanged; path", path);
+                                updateProperties();
+                            });
 
-        const serviceDetails = <ServiceDetails unit={this.state.unit_props}
-                                owner={this.props.owner}
-                                permitted={superuser.allowed}
-                                loadingUnits={this.state.reloading}
-                                isValid={this.props.unitIsValid}
-                                pinnedUnits={this.props.pinnedUnits}
-        />;
+                        _path.current = { unitId, path, propSub };
 
-        // resolve Alias name to primary ID
-        const cur_unit_id = this.state.unit_props.Id;
+                        // initial load
+                        updateProperties();
+                    })
+                    .catch(ex => setError(ex.toString())); // not-covered: unexpected OS error
+        },
+        null, [unitId]);
 
-        const unit_type = this.props.owner == "system" ? "UNIT" : "USER_UNIT";
-        const match = [
-            "_SYSTEMD_" + unit_type + "=" + cur_unit_id, "+",
-            "COREDUMP_" + unit_type + "=" + cur_unit_id, "+",
-            unit_type + "=" + cur_unit_id,
-        ];
-        const service_type = this.props.owner == "system" ? "service" : "user-service";
-        const url = "/system/logs/#/?prio=debug&" + service_type + "=" + cur_unit_id;
+    useObject(
+        () => dbusClient.subscribe(
+            { interface: s_bus.I_MANAGER, member: "Reloading" },
+            async (_, _i, _s, [is_reloading]) => {
+                debug("Service detail", unitId, "Reloading", is_reloading, "current unit", _path.current?.path);
+                setReloading(is_reloading);
+                if (!is_reloading && _path.current)
+                    await updateProperties();
+            }),
+        sub => sub.remove(), []);
 
-        const load_state = this.state.unit_props.LoadState;
+    /* We need this *only* to pick up failed Conditions after attempting to start a service, as the unit immediately gets
+    * unloaded again and we don't get a PropertiesChanged signal. */
+    useObject(
+        () => dbusClient.subscribe(
+            { interface: s_bus.I_MANAGER, member: "JobRemoved" }, async (_p, _i, _s, [_job_id, _job_path, unit, result]) => {
+                if (result === "done" && unitId === _path.current?.unitId) {
+                    debug("Service detail", unitId, "JobRemoved", _path.current?.path);
+                    await updateProperties();
+                }
+            }),
+        sub => sub.remove(), []);
 
-        return (
-            <WithDialogs>
-                <Page groupProps={{ sticky: 'top' }}
-                      isBreadcrumbGrouped
-                      id="service-details"
-                      breadcrumb={
-                          <Breadcrumb>
-                              <BreadcrumbItem to={"#" + cockpit.location.href.replace(/\/[^?]*/, '')}>{_("Services")}</BreadcrumbItem>
-                              <BreadcrumbItem isActive>
-                                  {cur_unit_id}
-                              </BreadcrumbItem>
-                          </Breadcrumb>}>
-                    <PageSection>
-                        <Gallery hasGutter>
-                            <GalleryItem id="service-details-unit">{serviceDetails}</GalleryItem>
-                            {(load_state === "loaded" || load_state === "masked") &&
-                            <GalleryItem id="service-details-logs">
-                                <LogsPanel title={_("Service logs")} match={match} emptyMessage={_("No log entries")} max={10} goto_url={url} search_options={{ prio: "debug", [service_type]: cur_unit_id }} />
-                            </GalleryItem>}
-                        </Gallery>
-                    </PageSection>
-                </Page>
-            </WithDialogs>
-        );
-    }
-}
+    // render
+    if (error)
+        return <EmptyStatePanel title={_("Loading unit failed")} icon={ExclamationCircleIcon} paragraph={error} />; // not-covered: unexpected OS error
+
+    if (unitProps === null)
+        return <EmptyStatePanel loading title={_("Loading...")} paragraph={unitId} />;
+
+    // resolve Alias name to primary ID
+    const cur_unit_id = unitProps.Id;
+
+    const unit_type = owner == "system" ? "UNIT" : "USER_UNIT";
+    const match = [
+        "_SYSTEMD_" + unit_type + "=" + cur_unit_id, "+",
+        "COREDUMP_" + unit_type + "=" + cur_unit_id, "+",
+        unit_type + "=" + cur_unit_id,
+    ];
+    const service_type = owner == "system" ? "service" : "user-service";
+    const url = "/system/logs/#/?prio=debug&" + service_type + "=" + cur_unit_id;
+    const load_state = unitProps.LoadState;
+
+    return (
+        <WithDialogs>
+            <Page groupProps={{ sticky: 'top' }}
+                    isBreadcrumbGrouped
+                    id="service-details"
+                    breadcrumb={
+                        <Breadcrumb>
+                            <BreadcrumbItem to={"#" + cockpit.location.href.replace(/\/[^?]*/, '')}>{_("Services")}</BreadcrumbItem>
+                            <BreadcrumbItem isActive>
+                                {cur_unit_id}
+                            </BreadcrumbItem>
+                        </Breadcrumb>}>
+                <PageSection>
+                    <Gallery hasGutter>
+                        <GalleryItem id="service-details-unit">
+                            <ServiceDetails unit={unitProps}
+                                            owner={owner}
+                                            permitted={superuser.allowed}
+                                            loadingUnits={reloading}
+                                            isValid={unitIsValid}
+                                            pinnedUnits={pinnedUnits} />
+                        </GalleryItem>
+                        {(load_state === "loaded" || load_state === "masked") &&
+                        <GalleryItem id="service-details-logs">
+                            <LogsPanel title={_("Service logs")} match={match} emptyMessage={_("No log entries")} max={10} goto_url={url} search_options={{ prio: "debug", [service_type]: cur_unit_id }} />
+                        </GalleryItem>}
+                    </Gallery>
+                </PageSection>
+            </Page>
+        </WithDialogs>
+    );
+};

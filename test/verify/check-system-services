cockpit-project/cockpit

commit 0a19a39e9523f4627e1ea8ed6b357ac40b9c2a4d

systemd/services: Avoid excessive D-Bus calls and simplify state management
The services list does way too much: It loads all units, even the ones which
are completely unused, and thus systemd intentionally ignores them. On top of
that, it was getting all properties of all interfaces of all units plus
installing proxies, so loading the page required an avalanche of (literally)
thousands of D-Bus messages; and it was not even using most of the properties
in the summary view.

For the list view, we basically only need two calls: ListUnits() for enough
information about all units which are in systemd's brain; and ListUnitFiles()
to add the inert ones (disabled, stopped, not a dependency of anything). The
only exception are timers, where we have to get the properties of the Timers
interface to show their last and next run.

This causes two feature regressions:

 - We can't show the description of unloaded units any more. As they are
   completely irrelevant for the currently running system, that's a
   concession we want to make, as loading them all is just too
   expensive. Adjust the test case and pixel reference accordingly.

 - We can't show the runtime status of aliased units in the list view.
   Again it's too expensive to figure this out. The details view has the
   status of course.

Change the data model to match what systemd gives us, and track three maps:

 - `units`: loaded units, indexed by object path so that we can update
   it with PropertiesChanged

 - `timers`: Timer interface for loaded units, also indexed by object path

 - `unit_files`: from ListUnitFiles(), indexed by unit ID as unloaded
   units do not have an object path.

These are not React state: During rendering we never show all units, but
only the ones selected by type and filters. This fixes lagging
notifications for failed units, and avoids needless re-renders due to
changing units which are not shown. This also avoids having to copy the
entire map in each signal handler invocation.

test/verify/check-system-services
@@ -45,9 +45,6 @@ class TestServices(MachineCase):
         self.restore_dir("/etc/xdg/systemd/user", user_post_restore, True)
         self.restore_dir("/home/admin/.config", user_post_restore, True)
 
-        # FIXME: this page is too slow
-        self.browser.wait_timeout(60)
-
     def run_systemctl(self, user, cmd):
         if user:
             self.machine.execute(f"su - admin -c 'export XDG_RUNTIME_DIR=/run/user/$(id -u admin); systemctl --user {cmd}'")
@@ -301,7 +298,7 @@ WantedBy=default.target
         if not user:
             # Make sure that symlinked services also appear in the list
             self.wait_service_state("reboot.target", "inactive")
-            self.wait_service_state("ctrl-alt-del.target", "inactive")
+            self.wait_service_present("ctrl-alt-del.target")
             self.goto_service("ctrl-alt-del.target")
             b.wait_in_text(".service-name", "Reboot")
             b.click("#services-page .pf-c-breadcrumb__link")
@@ -500,7 +497,7 @@ WantedBy=default.target
         # Filter by description capital letters included
         init_filter_state()
         b.set_input_text("#services-text-filter input", "Test Service")
-        self.wait_service_present("test.service")
+        # test.service is not loaded, thus description search does not find it
         self.wait_service_present("test-fail.service")
         if not user:
             b.assert_pixels("#services-page", "text-filter-test", skip_layouts=["mobile"])
@@ -1204,8 +1201,7 @@ WantedBy=multi-user.target
 
         # overview: primary unit
         self.wait_service_present("flower-rose.service")
-        # FIXME: This ought to be "Enabled", but is "Alias" right now
-        # self.wait_service_in_panel("flower-rose.service", "Enabled")
+        self.wait_service_in_panel("flower-rose.service", "Enabled")
         self.wait_service_state("flower-rose.service", "inactive")
 
         # overview: alias
@@ -1215,18 +1211,18 @@ WantedBy=multi-user.target
             self.wait_service_in_panel("flower-byanyothername.service", "Enabled")
         else:
             self.wait_service_in_panel("flower-byanyothername.service", "Alias")
-        self.wait_service_state("flower-byanyothername.service", "inactive")
+        # runtime status of aliases is unknown in list view
+        self.wait_service_state("flower-byanyothername.service", "")
 
         # both react to state changes
         m.execute("systemctl start flower-byanyothername.service")
         self.wait_service_state("flower-rose.service", "active")
-        self.wait_service_state("flower-byanyothername.service", "active")
+        self.wait_service_state("flower-byanyothername.service", "")
 
         # details: primary unit
         self.goto_service("flower-rose.service")
         b.wait_in_text("#statuses", "Running")
-        # FIXME: This is broken right now and shows "Static"
-        # b.wait_in_text("#statuses", "Automatically starts")
+        b.wait_in_text("#statuses", "Automatically starts")
         b.wait_in_text("#service-details-unit", "/etc/systemd/system/flower-rose.service")
         b.wait_in_text('.cockpit-log-panel .pf-c-card__body', "Perfume")
 
@@ -1234,8 +1230,7 @@ WantedBy=multi-user.target
         b.click(".pf-c-breadcrumb a:contains('Services')")
         self.goto_service("flower-byanyothername.service")
         b.wait_in_text("#statuses", "Running")
-        # FIXME: This ought to be "Alias" or "Automatically starts"; shows "Static" right now
-        # b.wait_in_text("#statuses", "Alias")
+        b.wait_in_text("#statuses", "Automatically starts")
         b.wait_in_text("#service-details-unit", "/etc/systemd/system/flower-rose.service")
         b.wait_in_text('.cockpit-log-panel .pf-c-card__body', "Perfume")
 
@@ -1270,9 +1265,6 @@ class TestTimers(MachineCase):
             "busctl get-property org.freedesktop.timedate1 /org/freedesktop/timedate1 org.freedesktop.timedate1 NTP"))
         m.execute("timedatectl set-time '2036-01-01 12:00:00'")
         m.reboot()
-
-        # FIXME: this page is too slow
-        b.wait_timeout(30)
 
         m.execute("timedatectl set-time '2036-01-01 15:30:00'")
         self.login_and_go("/system/services")

pkg/systemd/services/service.jsx
@@ -112,9 +112,6 @@ export class Service extends React.Component {
 
             if (unit_id.endsWith(".timer")) {
                 const [timer_props] = await dbus.call(this.path, s_bus.I_PROPS, "GetAll", [s_bus.I_TIMER]);
-                // unwrap variants
-                for (const key in timer_props)
-                    timer_props[key] = timer_props[key].v;
                 this.props.addTimerProperties(timer_props, unit_props);
             }
 
@@ -136,10 +133,8 @@ export class Service extends React.Component {
         if (this.state.error)
             return <EmptyStatePanel title={_("Loading unit failed")} icon={ExclamationCircleIcon} paragraph={this.state.error} />;
 
-        const cur_unit_id = this.props.unitId;
-
         if (this.state.unit_props === null)
-            return <EmptyStatePanel loading title={_("Loading...")} paragraph={cur_unit_id} />;
+            return <EmptyStatePanel loading title={_("Loading...")} paragraph={this.props.unitId} />;
 
         const serviceDetails = <ServiceDetails unit={this.state.unit_props}
                                 owner={this.props.owner}
@@ -148,6 +143,9 @@ export class Service extends React.Component {
                                 isValid={this.props.unitIsValid}
                                 pinnedUnits={this.props.pinnedUnits}
         />;
+
+        // resolve Alias name to primary ID
+        const cur_unit_id = this.state.unit_props.Id;
 
         const unit_type = this.props.owner == "system" ? "UNIT" : "USER_UNIT";
         const match = [

test/verify/check-metrics
@@ -528,10 +528,8 @@ class TestHistoryMetrics(MachineCase):
 
         # Troubleshoot
         b.click(".pf-c-empty-state button.pf-m-link")
-        # FIXME: Services page is too slow
-        with b.wait_timeout(30):
-            b.enter_page("/system/services")
-            b.wait_in_text("#service-details", "pmlogger.service")
+        b.enter_page("/system/services")
+        b.wait_in_text("#service-details", "pmlogger.service")
 
     @nondestructive
     @skipOstree("no PCP support")
@@ -1019,9 +1017,7 @@ BEGIN {{
         # table entries are links to Services page
         b.click("table[aria-label='Top 5 memory services'] tbody tr:nth-of-type(1) td[data-label='Service'] a span")
         b.enter_page("/system/services")
-        # FIXME: Services page is too slow
-        with b.wait_timeout(30):
-            b.wait_in_text("#path", "/mem-hog.service")
+        b.wait_in_text("#path", "/mem-hog.service")
         b.wait_in_text(".service-name", "memhog.sh")
 
         b.go("/metrics")

test/verify/check-networkmanager-basic
@@ -234,9 +234,7 @@ class TestNetworkingBasic(NetworkCase):
         assert_stopped(True)
         b.click("#networking-nm-crashed a")
         b.enter_page("/system/services")
-        # FIXME: Services page is too slow
-        with b.wait_timeout(30):
-            b.wait_text(".service-name", "Network Manager")
+        b.wait_text(".service-name", "Network Manager")
         b.click(".service-top-panel .pf-c-dropdown button")
         b.click(".service-top-panel .pf-c-dropdown__menu a:contains('Start')")
         b.wait_in_text("#statuses", "Running")

test/verify/check-pages
@@ -112,9 +112,7 @@ OnCalendar=daily
         b.enter_page("/system/services")
         b.reload()
         b.enter_page("/system/services")
-        # FIXME: Services page is too slow
-        with b.wait_timeout(30):
-            b.wait_text(".service-name", "Test Service")
+        b.wait_text(".service-name", "Test Service")
         b.switch_to_top()
         self.checkDocs(["Managing services"])
         b.click("#toggle-docs")
@@ -130,9 +128,7 @@ OnCalendar=daily
 
         m.restart_cockpit()
         b.relogin("/system/services")
-        # FIXME: Services page is too slow
-        with b.wait_timeout(30):
-            b.wait_text(".service-name", "Test Service")
+        b.wait_text(".service-name", "Test Service")
 
         # check that navigating away and back preserves place
         b.click_system_menu("/system")

test/verify/check-system-realms
@@ -161,9 +161,7 @@ class CommonTests:
 
         # log in as domain admin and check that we can do privileged operations
         b.login_and_go('/system/services#/systemd-tmpfiles-clean.timer', user=f'{self.admin_user}@cockpit.lan', password=self.admin_password)
-        # FIXME: Services page is too slow
-        with b.wait_timeout(60):
-            b.wait_in_text("#statuses", "Running")
+        b.wait_in_text("#statuses", "Running")
         b.click(".service-top-panel .pf-c-dropdown button")
         b.click(".service-top-panel .pf-c-dropdown__menu a:contains('Stop')")
         b.wait_in_text("#statuses", "Not running")
@@ -176,9 +174,7 @@ class CommonTests:
         b.login_and_go('/system', user=f'{self.admin_user.lower()}@COCKPIT.LAN', password=self.admin_password)
         b.go('/system/services#/systemd-tmpfiles-clean.timer')
         b.enter_page('/system/services')
-        # FIXME: Services page is too slow
-        with b.wait_timeout(60):
-            b.wait_in_text("#statuses", "Not running")
+        b.wait_in_text("#statuses", "Not running")
         b.click(".service-top-panel .pf-c-dropdown button")
         b.click(".service-top-panel .pf-c-dropdown__menu a:contains('Start')")
         b.wait_in_text("#statuses", "Running")
@@ -324,9 +320,7 @@ class CommonTests:
         b.go('/system/services#/systemd-tmpfiles-clean.timer')
         b.enter_page('/system/services')
 
-        # FIXME: Services page is too slow
-        with b.wait_timeout(30):
-            b.wait_in_text("#statuses", "Running")
+        b.wait_in_text("#statuses", "Running")
         b.click(".service-top-panel .pf-c-dropdown button")
         b.click(".service-top-panel .pf-c-dropdown__menu a:contains('Stop')")
         b.wait_in_text("#statuses", "Not running")

commit 75d7bcd04bc74d6265dea18eabad343fc805195c

systemd/services: Fix initial loading of timers
`this.timers` is indexed by object path, not unit ID. Fix the initialization in
listUnits(). This regression was introduced in commit 0a19a39e9523
during the many rewrites.

This didn't get caught by integration tests as they only covered the
`PropertiesChanged` update path, not the initial load. Add such a test, using
the existing `deleteme.timer`.

pkg/systemd/services/services.jsx
@@ -485,7 +485,7 @@ class ServicesPageBody extends React.Component {
                         if (Id.endsWith(".timer")) {
                             timerPromises.push(dbus.call(ObjectPath, s_bus.I_PROPS, "GetAll",
                                                          [s_bus.I_TIMER]));
-                            timerPaths.push(Id);
+                            timerPaths.push(ObjectPath);
                         }
                     });
 

test/verify/check-system-services
@@ -241,6 +241,10 @@ Description=Delete Me Timer
 
 [Timer]
 OnCalendar=*:1/2
+
+# just for user mode
+[Install]
+WantedBy=default.target
 """)
         self.write_file(f"{path}/special@:-characters.service",
                         """
@@ -255,6 +259,10 @@ WantedBy=default.target
 """)
 
         self.make_test_service(path)
+        # ensure we have one running timer; in user mode we do not have
+        # a running logind session at this point yet, so enable instead
+        self.run_systemctl(False, ("enable --global --runtime" if user else "start") + " deleteme.timer")
+        self.addCleanup(self.run_systemctl, user, "stop deleteme.timer || true")
 
         url = "/system/services#/?owner=user" if user else "/system/services"
         self.login_and_go(url, user="admin")
@@ -317,6 +325,9 @@ WantedBy=default.target
         b.wait_visible(self.svc_sel('test.timer'))
         b.wait_text(self.svc_sel('test.timer') + ' .service-unit-triggers', '')
         today = b.eval_js("Intl.DateTimeFormat('en', { dateStyle: 'medium' }).format()")
+        # timer from initial page/units load
+        b.wait_in_text(self.svc_sel('deleteme.timer') + ' .service-unit-next-trigger', today)
+        # timer gets updated on PropertiesChanged event
         self.run_systemctl(user, "start test.timer")
         b.wait_in_text(self.svc_sel('test.timer') + ' .service-unit-next-trigger', today)  # next run
         b.wait_in_text(self.svc_sel('test.timer') + ' .service-unit-last-trigger', "unknown")  # last trigger
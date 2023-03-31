31362dba66c9d177614cb2615f8a3a3d2f74a7f4
Jelle van der Waa <jvanderwaa@redhat.com>
2023-03-31 15:58:03 +0200

    metrics: drop unused cpuTemperature state property

--- pkg/metrics/metrics.jsx
+++ pkg/metrics/metrics.jsx
@@ -242,7 +242,6 @@ class CurrentMetrics extends React.Component {
             swapUsed: null, // bytes
             cpuUsed: 0, // percentage
             cpuCoresUsed: [], // [ percentage ]
-            cpuTemperature: NaN, // degree Celsius
             loadAvg: null, // [ 1min, 5min, 15min ]
             disksRead: 0, // B/s
             disksWritten: 0, // B/s

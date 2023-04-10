6547000ff7626b1f0638b5627d0f5be871429568
Mahmoud Hamdy <mahmoud-mahgoub1@hotmail.com>
2023-04-10 22:56:05 +0200

    shell: Trim ssh:// from connection address
    This fixes adding a remote host with address `ssh://hostname` and
    a non-empty "User name" field.
    
    Fixes #17635
    
    Co-Authored-By: Martin Pitt <mpitt@redhat.com>

--- pkg/shell/machines/machines.js
+++ pkg/shell/machines/machines.js
@@ -379,6 +379,8 @@ function Machines() {
         let port_spot = -1;
 
         if (conn_to) {
+            if (conn_to.substring(0, 6) === "ssh://")
+                conn_to = conn_to.substring(6);
             user_spot = conn_to.lastIndexOf('@');
             port_spot = conn_to.lastIndexOf(':');
         }

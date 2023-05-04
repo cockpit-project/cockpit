31cfbc33434862431e64473fd12f1910a1e1d89d
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2023-05-04 08:49:37 +0200

    python: use pytest fixture style in test_packages
    In addition to being less code, and easier to understand, it also
    handles environment variables more consistently (always setting them
    back to their old value, for subsequent tests) and properly cleans up
    its temporary directories.

--- test/pytest/test_packages.py
+++ test/pytest/test_packages.py
@@ -16,10 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
-import os
-import tempfile
-import unittest
-from pathlib import Path
 
 import pytest
 
@@ -35,125 +31,129 @@ def test_parse_accept_language(test_input, expected):
     assert parse_accept_language({'Accept-Language': test_input}) == expected
 
 
-class TestPackages(unittest.TestCase):
-    def setUp(self):
-        self.workdir = Path(tempfile.mkdtemp(prefix='cockpit-packages-test-'))
-        self.package_dir = self.workdir / 'cockpit'
-        self.package_dir.mkdir()
-        os.environ['XDG_DATA_DIRS'] = str(self.workdir)
-        os.environ['XDG_DATA_HOME'] = '/nonexisting'
+@pytest.fixture
+def pkgdir(tmp_path, monkeypatch):
+    monkeypatch.setenv('XDG_DATA_DIRS', str(tmp_path))
+    monkeypatch.setenv('XDG_DATA_HOME', '/nonexisting')
 
-        self.make_package('basic', description="standard package", requires={"cockpit": "42"})
+    self = tmp_path / 'cockpit'
+    self.mkdir()
+    make_package(self, 'basic', description="standard package", requires={"cockpit": "42"})
+    return self
 
-    def make_package(self, dirname: str, **kwargs: object) -> None:
-        (self.package_dir / dirname).mkdir()
-        with (self.package_dir / dirname / 'manifest.json').open('w') as file:
-            json.dump(kwargs, file, indent=2)
 
-    def test_basic(self):
-        packages = Packages()
-        assert len(packages.packages) == 1
-        assert packages.packages['basic'].name == 'basic'
-        assert packages.packages['basic'].manifest['description'] == 'standard package'
-        assert packages.packages['basic'].manifest['requires'] == {'cockpit': "42"}
-        assert packages.packages['basic'].priority == 1
+@pytest.fixture
+def confdir(tmp_path, monkeypatch):
+    monkeypatch.setattr(cockpit.config, 'ETC_COCKPIT', tmp_path)
+    return tmp_path
 
-        assert packages.manifests == '{"basic": {"description": "standard package", "requires": {"cockpit": "42"}}}'
 
-    def test_override_etc(self):
-        config_dir = self.workdir / 'config'
-        config_dir.mkdir()
-        orig_etc_config = cockpit.config.ETC_COCKPIT
-        cockpit.config.ETC_COCKPIT = config_dir
+def make_package(pkgdir, dirname: str, **kwargs: object) -> None:
+    (pkgdir / dirname).mkdir()
+    with (pkgdir / dirname / 'manifest.json').open('w') as file:
+        json.dump(kwargs, file, indent=2)
 
-        def cleanUp():
-            cockpit.config.ETC_COCKPIT = orig_etc_config
-        self.addCleanup(cleanUp)
 
-        (config_dir / 'basic.override.json').write_text('{"description": "overridden package", "priority": 5}')
+def test_basic(pkgdir):
+    packages = Packages()
+    assert len(packages.packages) == 1
+    assert packages.packages['basic'].name == 'basic'
+    assert packages.packages['basic'].manifest['description'] == 'standard package'
+    assert packages.packages['basic'].manifest['requires'] == {'cockpit': "42"}
+    assert packages.packages['basic'].priority == 1
 
-        packages = Packages()
-        assert len(packages.packages) == 1
-        # original attributes
-        assert packages.packages['basic'].name == 'basic'
-        assert packages.packages['basic'].manifest['requires'] == {'cockpit': '42'}
-        # overridden attributes
-        assert packages.packages['basic'].manifest['description'] == 'overridden package'
-        assert packages.packages['basic'].priority == 5
+    assert packages.manifests == '{"basic": {"description": "standard package", "requires": {"cockpit": "42"}}}'
 
-        assert json.loads(packages.manifests) == {
-            'basic': {
-                'description': 'overridden package',
-                'requires': {'cockpit': '42'},
-                'priority': 5,
-            }
+
+def test_override_etc(pkgdir, confdir):
+    (confdir / 'basic.override.json').write_text('{"description": "overridden package", "priority": 5}')
+
+    packages = Packages()
+    assert len(packages.packages) == 1
+    # original attributes
+    assert packages.packages['basic'].name == 'basic'
+    assert packages.packages['basic'].manifest['requires'] == {'cockpit': '42'}
+    # overridden attributes
+    assert packages.packages['basic'].manifest['description'] == 'overridden package'
+    assert packages.packages['basic'].priority == 5
+
+    assert json.loads(packages.manifests) == {
+        'basic': {
+            'description': 'overridden package',
+            'requires': {'cockpit': '42'},
+            'priority': 5,
         }
+    }
 
-    def test_priority(self):
-        self.make_package('vip', name='basic', description='VIP', priority=100)
-        self.make_package('guest', description='Guest')
 
-        packages = Packages()
-        assert len(packages.packages) == 2
-        assert packages.packages['basic'].name == 'basic'
-        assert packages.packages['basic'].priority == 100
-        assert packages.packages['basic'].manifest['description'] == 'VIP'
-        assert packages.packages['guest'].name == 'guest'
-        assert packages.packages['guest'].priority == 1
+def test_priority(pkgdir):
+    make_package(pkgdir, 'vip', name='basic', description='VIP', priority=100)
+    make_package(pkgdir, 'guest', description='Guest')
 
-        parsed = json.loads(packages.manifests)
-        assert parsed['basic'] == {'name': 'basic', 'description': 'VIP', 'priority': 100}
-        assert parsed['guest'] == {'description': 'Guest'}
+    packages = Packages()
+    assert len(packages.packages) == 2
+    assert packages.packages['basic'].name == 'basic'
+    assert packages.packages['basic'].priority == 100
+    assert packages.packages['basic'].manifest['description'] == 'VIP'
+    assert packages.packages['guest'].name == 'guest'
+    assert packages.packages['guest'].priority == 1
 
-    def test_conditions(self):
-        self.make_package('empty', conditions=[])
+    parsed = json.loads(packages.manifests)
+    assert parsed['basic'] == {'name': 'basic', 'description': 'VIP', 'priority': 100}
+    assert parsed['guest'] == {'description': 'Guest'}
 
-        # path-exists only
-        self.make_package('exists-1-yes', conditions=[{'path-exists': '/usr'}])
-        self.make_package('exists-1-no', conditions=[{'path-exists': '/nonexisting'}])
-        self.make_package('exists-2-yes', conditions=[{"path-exists": "/usr"},
-                                                      {"path-exists": "/bin/sh"}])
-        self.make_package('exists-2-no', conditions=[{"path-exists": "/usr"},
-                                                     {"path-exists": "/nonexisting"}])
 
-        # path-not-exists only
-        self.make_package('notexists-1-yes', conditions=[{"path-not-exists": "/nonexisting"}])
-        self.make_package('notexists-1-no', conditions=[{"path-not-exists": "/usr"}])
-        self.make_package('notexists-2-yes', conditions=[{"path-not-exists": "/nonexisting"},
-                                                         {"path-not-exists": "/obscure"}])
-        self.make_package('notexists-2-no', conditions=[{"path-not-exists": "/nonexisting"},
-                                                        {"path-not-exists": "/usr"}])
+def test_conditions(pkgdir):
+    make_package(pkgdir, 'empty', conditions=[])
 
-        # mixed
-        self.make_package('mixed-yes', conditions=[{"path-exists": "/usr"},
-                                                   {"path-not-exists": "/nonexisting"}])
-        self.make_package('mixed-no', conditions=[{"path-exists": "/nonexisting"},
-                                                  {"path-not-exists": "/obscure"}])
+    # path-exists only
+    make_package(pkgdir, 'exists-1-yes', conditions=[{'path-exists': '/usr'}])
+    make_package(pkgdir, 'exists-1-no', conditions=[{'path-exists': '/nonexisting'}])
+    make_package(pkgdir, 'exists-2-yes', conditions=[{"path-exists": "/usr"},
+                                                     {"path-exists": "/bin/sh"}])
+    make_package(pkgdir, 'exists-2-no', conditions=[{"path-exists": "/usr"},
+                                                    {"path-exists": "/nonexisting"}])
 
-        packages = Packages()
-        assert set(packages.packages.keys()) == {
-            'basic', 'empty', 'exists-1-yes', 'exists-2-yes', 'notexists-1-yes', 'notexists-2-yes', 'mixed-yes'
-        }
+    # path-not-exists only
+    make_package(pkgdir, 'notexists-1-yes', conditions=[{"path-not-exists": "/nonexisting"}])
+    make_package(pkgdir, 'notexists-1-no', conditions=[{"path-not-exists": "/usr"}])
+    make_package(pkgdir, 'notexists-2-yes', conditions=[{"path-not-exists": "/nonexisting"},
+                                                        {"path-not-exists": "/obscure"}])
+    make_package(pkgdir, 'notexists-2-no', conditions=[{"path-not-exists": "/nonexisting"},
+                                                       {"path-not-exists": "/usr"}])
 
-    def test_conditions_errors(self):
-        self.make_package('broken-syntax-1', conditions=[1])
-        self.make_package('broken-syntax-2', conditions=[["path-exists"]])
-        self.make_package('broken-syntax-3', conditions=[{"path-exists": "/foo", "path-not-exists": "/bar"}])
+    # mixed
+    make_package(pkgdir, 'mixed-yes', conditions=[{"path-exists": "/usr"},
+                                                  {"path-not-exists": "/nonexisting"}])
+    make_package(pkgdir, 'mixed-no', conditions=[{"path-exists": "/nonexisting"},
+                                                 {"path-not-exists": "/obscure"}])
 
-        self.make_package('unknown-predicate-good', conditions=[{"path-exists": "/usr"},
-                                                                {"frobnicated": True}])
-        self.make_package('unknown-predicate-bad', conditions=[{"path-exists": "/nonexisting"},
+    packages = Packages()
+    assert set(packages.packages.keys()) == {
+        'basic', 'empty', 'exists-1-yes', 'exists-2-yes', 'notexists-1-yes', 'notexists-2-yes', 'mixed-yes'
+    }
+
+
+def test_conditions_errors(pkgdir):
+    make_package(pkgdir, 'broken-syntax-1', conditions=[1])
+    make_package(pkgdir, 'broken-syntax-2', conditions=[["path-exists"]])
+    make_package(pkgdir, 'broken-syntax-3', conditions=[{"path-exists": "/foo", "path-not-exists": "/bar"}])
+
+    make_package(pkgdir, 'unknown-predicate-good', conditions=[{"path-exists": "/usr"},
                                                                {"frobnicated": True}])
+    make_package(pkgdir, 'unknown-predicate-bad', conditions=[{"path-exists": "/nonexisting"},
+                                                              {"frobnicated": True}])
 
-        packages = Packages()
-        assert set(packages.packages.keys()) == {'basic', 'unknown-predicate-good'}
+    packages = Packages()
+    assert set(packages.packages.keys()) == {'basic', 'unknown-predicate-good'}
 
-    def test_condition_hides_priority(self):
-        self.make_package('vip', name="basic", description="VIP", priority=100,
-                          conditions=[{"path-exists": "/nonexisting"}])
 
-        packages = Packages()
-        assert packages.packages['basic'].name == 'basic'
-        assert packages.packages['basic'].manifest['description'] == 'standard package'
-        assert packages.packages['basic'].manifest['requires'] == {'cockpit': "42"}
-        assert packages.packages['basic'].priority == 1
+def test_condition_hides_priority(pkgdir):
+    make_package(pkgdir, 'vip', name="basic", description="VIP", priority=100,
+                 conditions=[{"path-exists": "/nonexisting"}])
+
+    packages = Packages()
+    assert packages.packages['basic'].name == 'basic'
+    assert packages.packages['basic'].manifest['description'] == 'standard package'
+    assert packages.packages['basic'].manifest['requires'] == {'cockpit': "42"}
+    assert packages.packages['basic'].priority == 1

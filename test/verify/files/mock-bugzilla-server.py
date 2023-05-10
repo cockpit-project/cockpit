42f58076d44103fa0f50aea73c3825ef5a5b2367
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2023-05-10 12:09:34 +0200

    test: drop code from mock-bugzilla-server.py
    This was flagged by vulture as being unused, and removing it doesn't
    seem to break the test that it was designed to be used as part of.
    
    It remains as an open question if this file is used for anything at all
    anymore, but let's remove the part that vulture was upset about, as a
    first step.

--- test/verify/files/mock-bugzilla-server.py
+++ test/verify/files/mock-bugzilla-server.py
@@ -8,36 +8,10 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 
 
 with SimpleXMLRPCServer(('', 8080), requestHandler=RequestHandler) as server:
-    class Bug:
-        @server.register_function(name='Bug.add_attachment')
-        def add_attachment(self):
-            return {'ids': [42]}
-
-        @server.register_function(name='Bug.create')
-        def create(self):
-            return {'id': 42}
-
-        @server.register_function(name='Bug.search')
-        def search(self):
-            return {'bugs': []}
-
-        @server.register_function(name='Bug.update')
-        def update(self):
-            return {'bugs': []}
-
     class Bugzilla:
         @server.register_function(name='Bugzilla.version')
         def version(self):
             return {'version': '42'}
-
-    class User:
-        @server.register_function(name='User.login')
-        def login(self):
-            return {'id': 0, 'token': '70k3n'}
-
-        @server.register_function(name='User.logout')
-        def logout(self):
-            return {}
 
     server.register_instance(Bugzilla())
 

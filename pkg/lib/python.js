06aba4d08e6017abed05a3cc3e3bbb49276bc0d1
Martin Pitt <mpitt@redhat.com>
2023-06-01 07:43:26 +0200

    lib: Avoid `which` in python.js
    This isn't installed everywhere any more. Use `command -v`.
    
    Also drop the long-obsolete check for `python`, all our images have
    Python 3 now.

--- pkg/lib/python.js
+++ pkg/lib/python.js
@@ -19,8 +19,7 @@
 
 import cockpit from "cockpit";
 
-// FIXME: eventually convert all images to python 3
-const pyinvoke = ["sh", "-ec", "exec $(which /usr/libexec/platform-python 2>/dev/null || which python3 2>/dev/null || which python) -c \"$@\"", "--"];
+const pyinvoke = ["sh", "-ec", "exec $(command -v /usr/libexec/platform-python || command -v python3) -c \"$@\"", "--"];
 
 export function spawn (script_pieces, args, options) {
     const script = (typeof script_pieces == "string")

eca8bfff9a105fac3946bcbe6c8bfc3905e09ccf
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2023-06-09 10:48:50 +0200

    test/verify: add sys.path hack for check-* tests
    Before attempting to load a check-* test file, make sure test/common and
    the relevant bots/ paths are in sys.path.
    
    We remove our own toplevel import of testlib to avoid needing the path
    set before pytest is invoked.  We can reimplement the decorator search
    for ourselves, trivially.
    
    This means that it's now possible to just write:
    
      pytest test/verify -n4

--- test/verify/conftest.py
+++ test/verify/conftest.py
@@ -1,15 +1,28 @@
 import importlib.machinery
 import importlib.util
+import os
+import sys
 from pathlib import Path
 from typing import List, Optional
 
 import pytest
-import testlib
+
+TOPDIR = os.path.realpath(__file__ + '../../../..')
+COMMON_PATH = [
+    f'{TOPDIR}/bots',
+    f'{TOPDIR}/bots/machine',
+    f'{TOPDIR}/test/common',
+]
 
 
 @pytest.hookimpl
 def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> Optional[pytest.Collector]:
-    """Support for loading our check-* scripts as if they were modules"""
+    """Support for loading our check-* scripts as if they were modules
+
+    Also adds our testlib paths to sys.path.  Doesn't check out the bots
+    because it's not possible to run tests without first running
+    test/image-prepare, and that will check out the bots automatically.
+    """
     if file_path.name.startswith('check-'):
         # Pretend that test/verify/check-name is called like test.verify.check_name
         modname = 'test.verify.' + file_path.name.replace('-', '_')
@@ -17,7 +30,13 @@ def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> Optional[p
         spec = importlib.util.spec_from_loader(loader.name, loader)
         assert spec is not None
         module = importlib.util.module_from_spec(spec)
-        loader.exec_module(module)
+
+        old_path = sys.path
+        try:
+            sys.path = [*COMMON_PATH, *sys.path]
+            loader.exec_module(module)
+        finally:
+            sys.path = old_path
 
         # Return the tree node with our module pre-loaded inside of it
         collector = pytest.Module.from_parent(parent, path=file_path)
@@ -33,9 +52,10 @@ def pytest_collection_modifyitems(session: pytest.Session, items: List[pytest.It
     assert isinstance(items, list)
 
     def is_nondestructive(item: pytest.Item) -> bool:
+        attr = '_testlib__nondestructive'
         assert isinstance(item, pytest.Function)
         assert isinstance(item.parent, pytest.Class | pytest.Module)
-        return testlib.get_decorator(item.obj, item.parent.obj, "nondestructive", False)
+        return getattr(item.obj, attr, getattr(item.parent.obj, attr, False))
 
     # put the destructive tests last under the assumption that they're slower
     items.sort(key=is_nondestructive, reverse=True)

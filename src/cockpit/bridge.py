cockpit-project/cockpit

commit b7895b8e8e0508af298c47939eadf6a50629afe3

pybridge: Fix ssh-agent cleanup for setgid
Some distributions install ssh-agent as setgid, to protect against
ptracing and malicious environment variables. Unfortunately this also
clears PDEATHSIG, causing a leak of the agent. (This does not affect the
one from cockpit-session, as that is launched via PAM).

Replace this with running ssh-agent for a shell process which will exit
on EOF on stdin when the parent bridge process dies.

This fixes cleaning up ssh-agent in Cockpit client and
testCockpitDesktop. However, bump the timeout, as it usually takes
around 8 seconds for the process to actually go away (it apparently
regularly polls its parent).

src/cockpit/bridge.py
@@ -17,13 +17,11 @@
 
 import argparse
 import asyncio
-import ctypes
 import json
 import logging
 import os
 import pwd
 import shlex
-import signal
 import socket
 import subprocess
 from typing import Dict, Iterable, List, Optional, Tuple, Type
@@ -209,17 +207,11 @@ def setup_logging(*, debug: bool):
 
 
 def start_ssh_agent() -> None:
-    prctl = ctypes.CDLL(None).prctl
-    PR_SET_PDEATHSIG = 1
-
-    # NB: We prefer SIGTERM to SIGKILL here since the agent needs to have a
-    # chance to clean up its listener socket.
+    # Launch the agent so that it goes down with us on EOF; PDEATHSIG would be more robust,
+    # but it gets cleared on setgid ssh-agent, which some distros still do
     try:
-        proc = subprocess.Popen(['ssh-agent',
-                                 '-D',  # no-daemon
-                                 '-s',  # shell-style output
-                                 ], stdout=subprocess.PIPE, universal_newlines=True,
-                                preexec_fn=lambda: prctl(PR_SET_PDEATHSIG, signal.SIGTERM))
+        proc = subprocess.Popen(['ssh-agent', 'sh', '-ec', 'echo $SSH_AUTH_SOCK; read a'],
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
         assert proc.stdout is not None
 
         # Wait for the agent to write at least one line and look for the

test/verify/check-client
@@ -61,9 +61,8 @@ Command = {ssh_command}
         self.machine.execute('''
             while [ -n "$(pgrep -au admin | grep -Ev 'cockpit-ws|ssh.*cockpit-bridge' >&2)" ]; do sleep 1; done
         ''', timeout=10)
-        # FIXME: ssh-agent leaks both with and without remote bridge; fixed in PR#18917
-        self.machines["target"].execute("while pgrep -af '([c]ockpit|[s]sh-agentFIXME)' >&2; do sleep 1; done",
-                                        timeout=10)
+        self.machines["target"].execute("while pgrep -af '([c]ockpit|[s]sh-agent)' >&2; do sleep 1; done",
+                                        timeout=30)
 
     # deprecated: c-client-ssh will disappear once c-beiboot goes from beta to release
     def testClientSSH(self):

test/verify/check-connection
@@ -52,7 +52,7 @@ class TestConnection(testlib.MachineCase):
         m = self.machine
         try:
             # there may still be user-wide ones like dbus-broker
-            m.execute("while pgrep -au admin '(cockpit|ssh-agent)'; do sleep 0.1; done", timeout=10)
+            m.execute("while pgrep -au admin '(cockpit|ssh-agent)'; do sleep 0.1; done", timeout=30)
         except RuntimeError:
             # show the leaked processes in the assertion
             self.fail(m.execute("pgrep -au admin '(cockpit|ssh-agent)'"))

commit 2f7510af49402e62f9065c13e55c9f813803c699

pybridge: Fix ssh-agent launching
Commit b7895b8e8e broke the agent spawned by the bridge. This is only
exercised on our CoreOS images, for which we don't yet run pybridge
tests; the other integration tests just make sure that it gets cleaned
up after logout, which was still the case.

The parsing code relies on the format `SSH_AUTH_SOCK=/path`, which the
shell command did not previously provide; fix that. We could also
simplify the parsing instead to just read the sole path from stdout, but
the `NAME=` format provides an additional assertion that we are not just
reading some unrelated gibberish from the command.

On failure, clean up the terminated process to avoid the zombie sticking
around.

src/cockpit/bridge.py
@@ -210,7 +210,7 @@ def start_ssh_agent() -> None:
     # Launch the agent so that it goes down with us on EOF; PDEATHSIG would be more robust,
     # but it gets cleared on setgid ssh-agent, which some distros still do
     try:
-        proc = subprocess.Popen(['ssh-agent', 'sh', '-ec', 'echo $SSH_AUTH_SOCK; read a'],
+        proc = subprocess.Popen(['ssh-agent', 'sh', '-ec', 'echo SSH_AUTH_SOCK=$SSH_AUTH_SOCK; read a'],
                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
         assert proc.stdout is not None
 
@@ -223,6 +223,7 @@ def start_ssh_agent() -> None:
                 break
         else:
             proc.terminate()
+            proc.wait()
 
     except FileNotFoundError:
         logger.debug("Couldn't start ssh-agent (FileNotFoundError)")
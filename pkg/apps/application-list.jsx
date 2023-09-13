cockpit-project/cockpit

commit 7bbf9168ac330fc3cb1d886cac4dd8409cfc0ace

apps: Consider ID_LIKE for mapping AppStream data packages
With only looking at aos-release's `ID` field, we are missing out a lot
of derivatives, such as CentOS Stream, Rocky, or Debian-likes. Consider
ID_LIKE as well to fix that.

E.g. on Ubuntu, `ID_LIKE` is "debian", on CentOS it's "rhel fedora", on
Rocky Linux it's "rhel centos fedora".

Use that to clean up the manifest map, as Ubuntu and RHEL are now
redundant.

testBasic covers the "direct package name" case in the manifest. Add
a new testOsMap test to check the distroname → packagename map that we
use in real life.

pkg/apps/application-list.jsx
@@ -98,11 +98,21 @@ export const ApplicationList = ({ metainfo_db, appProgress, appProgressTitle, ac
         comps.push(metainfo_db.components[id]);
     comps.sort((a, b) => a.name.localeCompare(b.name));
 
-    function get_config(name, distro_id, def) {
+    function get_config(name, os_release, def) {
+        // ID is a single value, ID_LIKE is a list
+        const os_list = [os_release.ID || "", ...(os_release.ID_LIKE || "").split(/\s+/)];
+
         if (cockpit.manifests.apps && cockpit.manifests.apps.config) {
             let val = cockpit.manifests.apps.config[name];
-            if (typeof val === 'object' && val !== null && !Array.isArray(val))
-                val = val[distro_id];
+            if (typeof val === 'object' && val !== null && !Array.isArray(val)) {
+                os_list.find(c => {
+                    if (val[c]) {
+                        val = val[c];
+                        return true;
+                    }
+                    return false;
+                });
+            }
             return val !== undefined ? val : def;
         } else {
             return def;
@@ -112,8 +122,8 @@ export const ApplicationList = ({ metainfo_db, appProgress, appProgressTitle, ac
     function refresh() {
         read_os_release().then(os_release =>
             PackageKit.refresh(metainfo_db.origin_files,
-                               get_config('appstream_config_packages', os_release.ID, []),
-                               get_config('appstream_data_packages', os_release.ID, []),
+                               get_config('appstream_config_packages', os_release, []),
+                               get_config('appstream_data_packages', os_release, []),
                                setProgress))
                 .finally(() => setProgress(false))
                 .catch(show_error);
@@ -121,7 +131,7 @@ export const ApplicationList = ({ metainfo_db, appProgress, appProgressTitle, ac
 
     let refresh_progress, refresh_button, tbody;
     if (progress) {
-        refresh_progress = <ProgressBar size="sm" title={_("Checking for new applications")} data={progress} />;
+        refresh_progress = <ProgressBar id="refresh-progress" size="sm" title={_("Checking for new applications")} data={progress} />;
         refresh_button = <CancelButton data={progress} />;
     } else {
         refresh_progress = null;

test/verify/check-apps
@@ -16,6 +16,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+import time
 
 import packagelib
 import testlib
@@ -128,6 +129,50 @@ class TestApps(packagelib.PackageCase):
 
     def testWithUrlRoot(self):
         self.testBasic(urlroot="/webcon")
+
+    def testOsMap(self):
+        b = self.browser
+        m = self.machine
+
+        self.allow_journal_messages("can't remove watch: Invalid argument")
+
+        self.restore_dir("/usr/share/metainfo", reboot_safe=True)
+        self.restore_dir("/usr/share/app-info", reboot_safe=True)
+        self.restore_dir("/var/cache/app-info", reboot_safe=True)
+
+        # Make sure none of the appstream directories exist.  They
+        # will be created later and we need to cope with that.
+        m.execute("rm -rf /usr/share/metainfo /usr/share/app-info /var/cache/app-info")
+
+        # use a fake distro map
+        self.write_file("/etc/cockpit/apps.override.json",
+                        '{ "config": { "appstream_data_packages":'
+                        '    {"testy": ["appstream-data-test"], "otheros": ["nosuchpackage"]}'
+                        '  }}')
+
+        self.createAppStreamPackage("app-1", "1.0", "1")
+        self.createAppStreamRepoPackage()
+
+        self.login_and_go("/apps")
+        b.wait_visible(".pf-v5-c-empty-state")
+
+        # os-release is a symlink target, don't clobber that
+        self.restore_file("/etc/os-release")
+        m.execute("rm /etc/os-release")
+
+        # unknown OS: nothing gets installed
+        m.write("/etc/os-release", 'ID="unmapped"\nID_LIKE="mysterious"\nVERSION_ID="1"\n')
+        b.click("#refresh")
+        # the progress bar is too fast to reliably catch it
+        time.sleep(1)
+        b.wait_not_present("#refresh-progress")
+        b.wait_visible(".pf-v5-c-empty-state")
+
+        # known OS: appstream-data-tst gets installed from the map
+        m.write("/etc/os-release", 'ID="derivative"\nID_LIKE="spicy testy"\nVERSION_ID="1"\n')
+        b.click("#refresh")
+        with b.wait_timeout(30):
+            b.wait_visible(".app-list #app-1")
 
     def testL10N(self):
         b = self.browser

commit 700392fd5a657d5446a0a0168689f47f74a0224d

apps: Fix reading AppStream metadata config
Commit 7bbf9168ac33 introduced a major bug: If the OS in os-release was
not found in the iteration, get_config() would return the whole map
instead of the default value. That crashed the page with
`TypeError: names.forEach is not a function`.

This is awkward to do correctly with .find(), especially due to
modifying `val` as a side effect; iterate over `os_list` the good old
`for` way instead.

This was hidden by the test's manifest override leaving
`appstream_config_packages` intact, so it was always taken from the real
underlying OS. Override it as well.

pkg/apps/application-list.jsx
@@ -103,15 +103,13 @@ export const ApplicationList = ({ metainfo_db, appProgress, appProgressTitle, ac
         const os_list = [os_release.ID || "", ...(os_release.ID_LIKE || "").split(/\s+/)];
 
         if (cockpit.manifests.apps && cockpit.manifests.apps.config) {
-            let val = cockpit.manifests.apps.config[name];
+            const val = cockpit.manifests.apps.config[name];
             if (typeof val === 'object' && val !== null && !Array.isArray(val)) {
-                os_list.find(c => {
-                    if (val[c]) {
-                        val = val[c];
-                        return true;
-                    }
-                    return false;
-                });
+                for (const os of os_list) {
+                    if (val[os])
+                        return val[os];
+                }
+                return def;
             }
             return val !== undefined ? val : def;
         } else {

test/verify/check-apps
@@ -147,7 +147,8 @@ class TestApps(packagelib.PackageCase):
         # use a fake distro map
         self.write_file("/etc/cockpit/apps.override.json",
                         '{ "config": { "appstream_data_packages":'
-                        '    {"testy": ["appstream-data-test"], "otheros": ["nosuchpackage"]}'
+                        '    {"testy": ["appstream-data-test"], "otheros": ["nosuchpackage"]},'
+                        '              "appstream_config_packages": []'
                         '  }}')
 
         self.createAppStreamPackage("app-1", "1.0", "1")
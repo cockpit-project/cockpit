7ab33831a42ee22b72f8bbe356f222f12ae24d2a
Martin Pitt <mpitt@redhat.com>
2023-09-19 12:49:59 +0200

    networkmanager: Close old D-Bus connection
    initFirewalldDbus() gets called on every unit change, clean up the old
    one properly.

--- pkg/networkmanager/firewall-client.js
+++ pkg/networkmanager/firewall-client.js
@@ -75,6 +75,9 @@ firewall.debouncedGetServices = debounce(300, () => {
 
 function initFirewalldDbus() {
     debug("initializing D-Bus connection");
+    if (firewalld_dbus)
+        firewalld_dbus.close();
+
     firewalld_dbus = cockpit.dbus('org.fedoraproject.FirewallD1', { superuser: "try" });
 
     firewalld_dbus.addEventListener('owner', (event, owner) => {

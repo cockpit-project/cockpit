cockpit-project/cockpit

commit ae5ce0a331f0e5f07450f0d6e6bad228cdd11f10

Improve JSON deconstruction static typing
Add a family of typed dictionary accessor functions to our jsonutil
file, with names like get_bool(), get_int(), get_strv() and so on.

Those will throw JsonError if the typing is wrong or a value is missing.

At the core of these functions is a new typechecked() function which
performs a checked cast to a given value type, plus a _get() function
which accepts a cast function as a argument.  Everything is correctly
statically typed through liberal use of type variables.

Use these throughout the code, allowing us to remove many assertions and
type:ignore comments.  This is a big chunk of the cases present, but
there are still some corners that need taking care of.

Catch the JsonError in various places for appropriate error reporting.

Having _get() take a cast function as an argument allows for another
interesting possibility: converting certain JSON types to other object
types.

Building on that, this patch also changes our approach to parsing
manifest files: we now do a lot more of the parsing up-front, storing
the results into properly-typed Python objects (BridgeConfig, Condition,
etc) so that we can detect errors and throw them at the time we parse
the file.  We also do requirements version checking up-front, allowing
for better error reporting.

We also do a similar (but much more trivial) change for parsing the
rows/cols fields of window size requests for pty-enabled stream
channels.

src/cockpit/peer.py
@@ -21,6 +21,7 @@ import os
 from typing import Callable, List, Optional, Sequence
 
 from .jsonutil import JsonObject
+from .packages import BridgeConfig
 from .protocol import CockpitProblem, CockpitProtocol, CockpitProtocolError
 from .router import Endpoint, Router, RoutingRule
 from .transports import SubprocessProtocol, SubprocessTransport
@@ -221,9 +222,9 @@ class ConfiguredPeer(Peer):
     args: Sequence[str]
     env: Sequence[str]
 
-    def __init__(self, router: Router, config: JsonObject):
-        self.args = config['spawn']  # type: ignore[assignment]
-        self.env = config.get('environ', [])  # type: ignore[assignment]
+    def __init__(self, router: Router, config: BridgeConfig):
+        self.args = config.spawn
+        self.env = config.environ
         super().__init__(router)
 
     async def do_connect_transport(self) -> None:
@@ -231,17 +232,20 @@ class ConfiguredPeer(Peer):
 
 
 class PeerRoutingRule(RoutingRule):
-    config: JsonObject
+    config: BridgeConfig
+    match: JsonObject
     peer: Optional[Peer]
 
-    def __init__(self, router: Router, config: JsonObject):
+    def __init__(self, router: Router, config: BridgeConfig):
         super().__init__(router)
         self.config = config
+        self.match = config.match
         self.peer = None
 
     def apply_rule(self, options: JsonObject) -> Optional[Peer]:
         # Check that we match
-        for key, value in self.config['match'].items():  # type: ignore[attr-defined,union-attr]
+
+        for key, value in self.match.items():
             if key not in options:
                 logger.debug('        rejecting because key %s is missing', key)
                 return None
@@ -272,7 +276,7 @@ class PeersRoutingRule(RoutingRule):
     def apply_rule(self, options: JsonObject) -> Optional[Endpoint]:
         logger.debug('    considering %d rules', len(self.rules))
         for rule in self.rules:
-            logger.debug('      considering %s', rule.config.get('spawn'))
+            logger.debug('      considering %s', rule.config.name)
             endpoint = rule.apply_rule(options)
             if endpoint is not None:
                 logger.debug('        selected')
@@ -280,17 +284,17 @@ class PeersRoutingRule(RoutingRule):
         logger.debug('      no peer rules matched')
         return None
 
-    def set_configs(self, bridge_configs: List[JsonObject]) -> None:
+    def set_configs(self, bridge_configs: Sequence[BridgeConfig]) -> None:
         old_rules = self.rules
         self.rules = []
 
         for config in bridge_configs:
             # Those are handled elsewhere...
-            if config.get('privileged') or 'host' in config['match']:  # type: ignore[operator]
+            if config.privileged or 'host' in config.match:
                 continue
 
             # Try to reuse an existing rule, if one exists...
-            for rule in old_rules:
+            for rule in list(old_rules):
                 if rule.config == config:
                     old_rules.remove(rule)
                     break

src/cockpit/superuser.py
@@ -23,14 +23,15 @@ import logging
 import os
 import socket
 from tempfile import TemporaryDirectory
-from typing import Dict, List, Optional, Sequence, Tuple
+from typing import List, Optional, Sequence, Tuple
 
 from cockpit._vendor import ferny
 from cockpit._vendor.bei.bootloader import make_bootloader
-from cockpit._vendor.systemd_ctypes import bus
+from cockpit._vendor.systemd_ctypes import Variant, bus
 
 from .beipack import BridgeBeibootHelper
-from .jsonutil import JsonObject
+from .jsonutil import JsonObject, get_str
+from .packages import BridgeConfig
 from .peer import ConfiguredPeer, Peer, PeerError
 from .polkit import PolkitAgent
 from .router import Router, RoutingError, RoutingRule
@@ -42,7 +43,7 @@ class SuperuserPeer(ConfiguredPeer):
     name: str
     responder: ferny.InteractionResponder
 
-    def __init__(self, router: Router, name: str, config: JsonObject, responder: ferny.InteractionResponder):
+    def __init__(self, router: Router, name: str, config: BridgeConfig, responder: ferny.InteractionResponder):
         super().__init__(router, config)
         self.name = name
         self.responder = responder
@@ -99,7 +100,7 @@ class AuthorizeResponder(CockpitResponder):
 
 
 class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface='cockpit.Superuser'):
-    superuser_configs: Dict[str, JsonObject]
+    superuser_configs: Sequence[BridgeConfig] = ()
     pending_prompt: Optional[asyncio.Future]
     peer: Optional[SuperuserPeer]
 
@@ -109,7 +110,7 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
     # D-Bus properties
     bridges = bus.Interface.Property('as', value=[])
     current = bus.Interface.Property('s', value='none')
-    methods = bus.Interface.Property('a{sv}')
+    methods = bus.Interface.Property('a{sv}', value={})
 
     # RoutingRule
     def apply_rule(self, options: JsonObject) -> Optional[Peer]:
@@ -143,9 +144,7 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
     def __init__(self, router: Router, *, privileged: bool = False):
         super().__init__(router)
 
-        self.superuser_configs = {}
         self.pending_prompt = None
-        self.bridges = []
         self.peer = None
         self.startup = None
 
@@ -160,16 +159,14 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
         if self.current != 'none':
             raise bus.BusError('cockpit.Superuser.Error', 'Superuser bridge already running')
 
-        if name == 'any' and self.bridges:
-            name = self.bridges[0]
-
         assert self.peer is None
         assert self.startup is None
 
-        try:
-            config = self.superuser_configs[name]
-        except KeyError as exc:
-            raise bus.BusError('cockpit.Superuser.Error', f'Unknown superuser bridge type "{name}"') from exc
+        for config in self.superuser_configs:
+            if name in (config.name, 'any'):
+                break
+        else:
+            raise bus.BusError('cockpit.Superuser.Error', f'Unknown superuser bridge type "{name}"')
 
         self.current = 'init'
         self.peer = SuperuserPeer(self.router, name, config, responder)
@@ -184,31 +181,28 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
 
         self.current = name
 
-    def set_configs(self, configs: List[JsonObject]):
+    def set_configs(self, configs: Sequence[BridgeConfig]):
         logger.debug("set_configs() with %d items", len(configs))
-        self.superuser_configs = {}
-        for config in configs:
-            if config.get('privileged', False):
-                spawn = config['spawn']
-                assert isinstance(spawn, list)
-                assert isinstance(spawn[0], str)
-                label = config.get('label')
-                if label is not None:
-                    assert isinstance(label, str)
-                    name = label
-                else:
-                    name = os.path.basename(spawn[0])
-                self.superuser_configs[name] = config
-        self.bridges = list(self.superuser_configs)
+        configs = [config for config in configs if config.privileged]
+        self.superuser_configs = tuple(configs)
+        self.bridges = [config.name for config in self.superuser_configs]
+        self.methods = {c.label: Variant({'label': Variant(c.label)}, 'a{sv}') for c in configs if c.label}
 
         logger.debug("  bridges are now %s", self.bridges)
 
-        # If the currently-active bridge got removed, stop it
-        if self.peer is not None and self.peer.name not in self.superuser_configs:
-            logger.debug("  stopping current superuser bridge '%s' (peer name %s), as it disappeared from configs",
-                         self.current, self.peer.name)
+        # If the currently active bridge got removed, stop it
+        if self.peer is not None:
+            if self.current == 'any':
+                removed = len(self.superuser_configs) == 0
+            else:
+                removed = any(c.name == self.peer.name for c in self.superuser_configs)
 
-            self.stop()
+            if removed:
+                logger.debug(
+                    "  stopping current superuser bridge '%s' (peer name %s), as it disappeared from configs",
+                    self.current, self.peer.name)
+
+                self.stop()
 
     def cancel_prompt(self):
         if self.pending_prompt is not None:
@@ -226,8 +220,7 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
 
     # Connect-on-startup functionality
     def init(self, params: JsonObject) -> None:
-        name = params.get('id', 'any')
-        assert isinstance(name, str)
+        name = get_str(params, 'id', 'any')
         responder = AuthorizeResponder(self.router)
         self._init_task = asyncio.create_task(self.go(name, responder))
         self._init_task.add_done_callback(self._init_done)
@@ -253,12 +246,3 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
             self.pending_prompt.set_result(reply)
         else:
             logger.debug('got Answer, but no prompt pending')
-
-    @methods.getter
-    def get_methods(self):
-        methods = {}
-        for name, config in self.superuser_configs.items():
-            label = config.get('label')
-            if label:
-                methods[name] = {'t': 'a{sv}', 'v': {'label': {'t': 's', 'v': label}}}
-        return methods

src/client/cockpit-beiboot
@@ -74,18 +74,10 @@ def ensure_ferny_askpass() -> Path:
 
 
 def get_interesting_files() -> Iterable[str]:
-    for candidate in PackagesLoader.load_candidates():
-        try:
-            conditions = candidate.manifest['conditions']
-        except KeyError:
-            continue
-
-        assert isinstance(conditions, list)
-        for condition in conditions:
-            assert isinstance(condition, dict)
-            for key, value in condition.items():
-                if key in ['path-exists', 'path-not-exists']:
-                    yield value
+    for manifest in PackagesLoader.load_manifests():
+        for condition in manifest.conditions:
+            if condition.name in ('path-exists', 'path-not-exists') and isinstance(condition.value, str):
+                yield condition.value
 
 
 class ProxyPackagesLoader(PackagesLoader):

src/cockpit/bridge.py
@@ -17,6 +17,7 @@
 
 import argparse
 import asyncio
+import contextlib
 import json
 import logging
 import os
@@ -34,8 +35,8 @@ from .channel import ChannelRoutingRule
 from .channels import CHANNEL_TYPES
 from .config import Config, Environment
 from .internal_endpoints import EXPORTS
-from .jsonutil import JsonObject
-from .packages import Packages, PackagesListener
+from .jsonutil import JsonError, JsonObject, get_dict
+from .packages import BridgeConfig, Packages, PackagesListener
 from .peer import PeersRoutingRule
 from .remote import HostRoutingRule
 from .router import Router
@@ -79,13 +80,13 @@ class Bridge(Router, PackagesListener):
 
         if args.beipack:
             # Some special stuff for beipack
-            self.superuser_rule.set_configs([
-                {
+            self.superuser_rule.set_configs((
+                BridgeConfig({
                     "privileged": True,
                     "spawn": ["sudo", "-k", "-A", "python3", "-ic", "# cockpit-bridge", "--privileged"],
                     "environ": ["SUDO_ASKPASS=ferny-askpass"],
-                }
-            ])
+                }),
+            ))
             self.packages = None
         elif args.privileged:
             self.packages = None
@@ -127,8 +128,10 @@ class Bridge(Router, PackagesListener):
         return os_release
 
     def do_init(self, message: JsonObject) -> None:
-        superuser = message.get('superuser')
-        if isinstance(superuser, dict):
+        # we're only interested in the case where this is a dict, but
+        # 'superuser' may well be `False` and that's not an error
+        with contextlib.suppress(JsonError):
+            superuser = get_dict(message, 'superuser')
             self.superuser_rule.init(superuser)
 
     def do_send_init(self) -> None:

src/cockpit/channel.py
@@ -19,7 +19,7 @@ import asyncio
 import logging
 from typing import ClassVar, Dict, Generator, List, Optional, Sequence, Set, Tuple, Type
 
-from .jsonutil import JsonObject
+from .jsonutil import JsonError, JsonObject, get_bool, get_str
 from .router import Endpoint, Router, RoutingRule
 
 logger = logging.getLogger(__name__)
@@ -112,9 +112,9 @@ class Channel(Endpoint):
         if command == 'open':
             self._tasks = set()
             self.channel = message['channel']
-            if message.get('flow-control'):
+            if get_bool(message, 'flow-control', default=False):
                 self._send_pings = True
-            self.group = message.get('group', 'default')
+            self.group = get_str(message, 'group', 'default')
             self.freeze_endpoint()
             self.do_open(message)
         elif command == 'ready':
@@ -140,6 +140,9 @@ class Channel(Endpoint):
             self.do_control(command, message)
         except ChannelError as exc:
             self.close(**exc.kwargs)
+        except JsonError as exc:
+            logger.warning("%s %s %s: %s", self, channel, command, exc)
+            self.close(problem='protocol-error', message=str(exc))
 
     def do_kill(self, host: Optional[str], group: Optional[str]) -> None:
         # Already closing?  Ignore.

src/cockpit/channels/filesystem.py
@@ -23,7 +23,7 @@ from cockpit._vendor.systemd_ctypes import PathWatch
 from cockpit._vendor.systemd_ctypes.inotify import Event as InotifyEvent
 
 from ..channel import Channel, ChannelError, GeneratorChannel
-from ..jsonutil import JsonObject
+from ..jsonutil import JsonObject, get_int, get_str
 
 logger = logging.getLogger(__name__)
 
@@ -94,18 +94,14 @@ class FsReadChannel(GeneratorChannel):
     payload = 'fsread1'
 
     def do_yield_data(self, options: JsonObject) -> GeneratorChannel.DataGenerator:
-        binary = options.get('binary', False)
-        max_read_size = options.get('max_read_size')
-        # TODO: generic JSON validation
-        if max_read_size is not None and not isinstance(max_read_size, int):
-            raise ChannelError('protocol-error', message='max_read_size must be an integer')
-        if not isinstance(options['path'], str):
-            raise ChannelError('protocol-error', message='path is not a string')
+        path = get_str(options, 'path')
+        binary = get_str(options, 'binary', None)
+        max_read_size = get_int(options, 'max_read_size', None)
 
-        logger.debug('Opening file "%s" for reading', options['path'])
+        logger.debug('Opening file "%s" for reading', path)
 
         try:
-            with open(options['path'], 'rb') as filep:
+            with open(path, 'rb') as filep:
                 buf = os.stat(filep.fileno())
                 if max_read_size is not None and buf.st_size > max_read_size:
                     raise ChannelError('too-large')

src/cockpit/channels/packages.py
@@ -22,7 +22,7 @@ from typing import Optional
 
 from ..channel import Channel
 from ..data import read_cockpit_data_file
-from ..jsonutil import JsonObject
+from ..jsonutil import JsonObject, get_dict, get_str
 from ..packages import Packages
 
 logger = logging.getLogger(__name__)
@@ -53,18 +53,12 @@ class PackagesChannel(Channel):
         options = self.options
 
         try:
-            if options.get('method') != 'GET':
+            if get_str(options, 'method') != 'GET':
                 raise ValueError(f'Unsupported HTTP method {options["method"]}')
 
-            path = options.get('path')
-            if not isinstance(path, str) or not path.startswith('/'):
-                raise ValueError(f'Unsupported HTTP method {options["method"]}')
-
-            headers = options.get('headers')
-            if not isinstance(headers, dict) or not all(isinstance(value, str) for value in headers.values()):
-                raise ValueError(f'Unsupported HTTP method {options["method"]}')
-
-            document = packages.load_path(path, headers)  # type: ignore[arg-type]
+            path = get_str(options, 'path')
+            headers = get_dict(options, 'headers')
+            document = packages.load_path(path, headers)
 
             # Note: we can't cache documents right now.  See
             # https://github.com/cockpit-project/cockpit/issues/19071

src/cockpit/channels/stream.py
@@ -22,8 +22,8 @@ import subprocess
 from typing import Dict
 
 from ..channel import ChannelError, ProtocolChannel
-from ..jsonutil import JsonObject
-from ..transports import SubprocessProtocol, SubprocessTransport
+from ..jsonutil import JsonObject, get_bool, get_int, get_object, get_str, get_strv
+from ..transports import SubprocessProtocol, SubprocessTransport, WindowSize
 
 logger = logging.getLogger(__name__)
 
@@ -38,24 +38,14 @@ class SocketStreamChannel(ProtocolChannel):
         try:
             # Unix
             if 'unix' in options:
-                path = options['unix']
-                # TODO: generic JSON validation
-                if not isinstance(path, str):
-                    raise ChannelError('protocol-error', message='unix option must be a string')
+                path = get_str(options, 'unix')
                 label = f'Unix socket {path}'
                 transport, _ = await loop.create_unix_connection(lambda: self, path)
 
             # TCP
             elif 'port' in options:
-                try:
-                    port: int = int(options['port'])  # type: ignore[call-overload,arg-type]
-                except ValueError as exc:
-                    raise ChannelError('protocol-error', message='invalid "port" option for stream channel') from exc
-                host = options.get('address', 'localhost')
-                # TODO: generic JSON validation
-                if not isinstance(host, str):
-                    raise ChannelError('protocol-error',
-                                       message='"address" option for stream channel must be a string')
+                port = get_int(options, 'port')
+                host = get_str(options, 'address', 'localhost')
                 label = f'TCP socket {host}:{port}'
 
                 transport, _ = await loop.create_connection(lambda: self, host, port)
@@ -92,36 +82,17 @@ class SubprocessStreamChannel(ProtocolChannel, SubprocessProtocol):
         return args
 
     def do_options(self, options):
-        window = options.get('window')
+        window = get_object(options, 'window', WindowSize, None)
         if window is not None:
-            self._transport.set_window_size(**window)
+            self._transport.set_window_size(window)
 
     async def create_transport(self, loop: asyncio.AbstractEventLoop, options: JsonObject) -> SubprocessTransport:
-        args = options['spawn']
-
-        # TODO: generic JSON validation
-        if not isinstance(args, list) or not all(isinstance(a, str) for a in args):
-            raise ChannelError('protocol-error', message='invalid "args" option for stream channel')
-
-        err = options.get('err')
-        if err is not None and not isinstance(err, str):
-            raise ChannelError('protocol-error', message='invalid "err" option for stream channel')
-
-        cwd = options.get('directory')
-        if cwd is not None and not isinstance(cwd, str):
-            raise ChannelError('protocol-error', message='invalid "cwd" option for stream channel')
-
-        pty = options.get('pty', False)
-        if not isinstance(pty, bool):
-            raise ChannelError('protocol-error', message='invalid "pty" option for stream channel')
-
-        window: Dict[str, int] = options.get('window')  # type: ignore[assignment]
-        if window is not None and (
-                not isinstance(window, dict) or
-                not all(isinstance(k, str) and isinstance(v, int) for k, v in window.items())):
-            raise ChannelError('protocol-error', message='invalid "window" option for stream channel')
-
-        environ: Dict[str, str] = options.get('environ')  # type: ignore[assignment]
+        args = get_strv(options, 'spawn')
+        err = get_str(options, 'err', 'msg')
+        cwd = get_str(options, 'directory', '.')
+        pty = get_bool(options, 'pty', default=False)
+        window = get_object(options, 'window', WindowSize, None)
+        environ = get_strv(options, 'environ', [])
 
         if err == 'out':
             stderr = subprocess.STDOUT
@@ -131,11 +102,10 @@ class SubprocessStreamChannel(ProtocolChannel, SubprocessProtocol):
             stderr = subprocess.PIPE
 
         env: Dict[str, str] = dict(os.environ)
-        if environ is not None:
-            if not isinstance(environ, list) or not all(isinstance(e, str) and '=' in e for e in environ):
-                raise ChannelError('protocol-error', message='invalid "environ" option for stream channel')
-
+        try:
             env.update(dict(e.split('=', 1) for e in environ))
+        except ValueError:
+            raise ChannelError('protocol-error', message='invalid "environ" option for stream channel') from None
 
         try:
             transport = SubprocessTransport(loop, self, args, pty=pty, window=window, env=env, cwd=cwd, stderr=stderr)

commit 15ca41d000ee2bda0a6c175e217b9666e40bf92f

cockpit.superuser: prevent 'any' from being set
ae5ce0a331f0 introduced a bug in the code that handled the case where
the `any` superuser bridge was requested: previously, the code would set
`self.current` to the name of the selected peer, but after the change,
it would get set to `any` instead.  Fix that up by ignoring the value of
`name` after we use it for finding the peer.

This allows us to simplify the condition for checking if the running
bridge ought to be shut down when reloading packages.  The current logic
reads that we should shutdown the current bridge "if there's any bridge
which has our name" (which is very likely, in case nothing changed).  We
need to change that to `not any`, but we can also simplify things by
storing the original config object on the peer and checking if it
appears in the new list (in its entirety).

Thanks to Jelle for the testcase.

Fixes #19327

Co-authored-by: Jelle van der Waa <jvanderwaa@redhat.com>

src/cockpit/peer.py
@@ -219,10 +219,12 @@ class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
 
 
 class ConfiguredPeer(Peer):
+    config: BridgeConfig
     args: Sequence[str]
     env: Sequence[str]
 
     def __init__(self, router: Router, config: BridgeConfig):
+        self.config = config
         self.args = config.spawn
         self.env = config.environ
         super().__init__(router)

src/cockpit/superuser.py
@@ -40,12 +40,10 @@ logger = logging.getLogger(__name__)
 
 
 class SuperuserPeer(ConfiguredPeer):
-    name: str
     responder: ferny.InteractionResponder
 
-    def __init__(self, router: Router, name: str, config: BridgeConfig, responder: ferny.InteractionResponder):
+    def __init__(self, router: Router, config: BridgeConfig, responder: ferny.InteractionResponder):
         super().__init__(router, config)
-        self.name = name
         self.responder = responder
 
     async def do_connect_transport(self) -> None:
@@ -169,7 +167,7 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
             raise bus.BusError('cockpit.Superuser.Error', f'Unknown superuser bridge type "{name}"')
 
         self.current = 'init'
-        self.peer = SuperuserPeer(self.router, name, config, responder)
+        self.peer = SuperuserPeer(self.router, config, responder)
         self.peer.add_done_callback(self.peer_done)
 
         try:
@@ -179,7 +177,7 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
         except (OSError, PeerError) as exc:
             raise bus.BusError('cockpit.Superuser.Error', str(exc)) from exc
 
-        self.current = name
+        self.current = self.peer.config.name
 
     def set_configs(self, configs: Sequence[BridgeConfig]):
         logger.debug("set_configs() with %d items", len(configs))
@@ -190,18 +188,10 @@ class SuperuserRoutingRule(RoutingRule, CockpitResponder, bus.Object, interface=
 
         logger.debug("  bridges are now %s", self.bridges)
 
-        # If the currently active bridge got removed, stop it
+        # If the currently active bridge config is not in the new set of configs, stop it
         if self.peer is not None:
-            if self.current == 'any':
-                removed = len(self.superuser_configs) == 0
-            else:
-                removed = any(c.name == self.peer.name for c in self.superuser_configs)
-
-            if removed:
-                logger.debug(
-                    "  stopping current superuser bridge '%s' (peer name %s), as it disappeared from configs",
-                    self.current, self.peer.name)
-
+            if self.peer.config not in self.superuser_configs:
+                logger.debug("  stopping superuser bridge '%s': it disappeared from configs", self.peer.config.name)
                 self.stop()
 
     def cancel_prompt(self):

test/verify/check-superuser
@@ -56,6 +56,14 @@ class TestSuperuser(testlib.MachineCase):
         b.leave_page()
         b.check_superuser_indicator("Limited access")
 
+        # A reload should not lose privileges
+        b.become_superuser()
+        b.reload()
+        b.check_superuser_indicator("Administrative access")
+
+        # Drop privileges
+        b.drop_superuser()
+
         # We want to be lectured again
         self.restore_file("/var/db/sudo/lectured/admin")
         self.restore_file("/var/lib/sudo/lectured/admin")
cockpit-project/cockpit

commit 89781bacb1027817b8e0cf077c8105f088351f2f

shell: python: load translations via manifest.js
Split the po files into po.$lang.js and po.manifest.$lang.js so the
shell can only serve the translated manifests for the C bridge we now
need to also load globbed manifest translations. The Python bridge does
not support globbing so we need to explicitly load the po.js file as
well.

Closes #18224

Co-authored-by: Allison Karlitskaya <allison.karlitskaya@redhat.com>

pkg/lib/cockpit-po-plugin.js
@@ -44,10 +44,7 @@ function get_plural_expr(statement) {
     return expr;
 }
 
-function buildFile(po_file, subdir, webpack_module, webpack_compilation) {
-    if (webpack_compilation)
-        webpack_compilation.fileDependencies.add(po_file);
-
+function buildFile(po_file, subdir, webpack_module, webpack_compilation, out_path, filter) {
     return new Promise((resolve, reject) => {
         // Read the PO file, remove fuzzy/disabled lines to avoid tripping up the validator
         const po_data = fs.readFileSync(po_file, 'utf8')
@@ -81,6 +78,9 @@ function buildFile(po_file, subdir, webpack_module, webpack_compilation) {
                 if (translation.comments.flag?.match(/\bfuzzy\b/))
                     continue;
 
+                if (!references.some(filter))
+                    continue;
+
                 const key = JSON.stringify(context_prefix + msgid);
                 // cockpit.js always ignores the first item
                 chunks.push(`,\n ${key}: [\n  null`);
@@ -95,8 +95,6 @@ function buildFile(po_file, subdir, webpack_module, webpack_compilation) {
         const wrapper = config.wrapper?.(subdir) || DEFAULT_WRAPPER;
         const output = wrapper.replace('PO_DATA', chunks.join('')) + '\n';
 
-        const lang = path.basename(po_file).slice(0, -3);
-        const out_path = (subdir ? (subdir + '/') : '') + 'po.' + lang + '.js';
         if (webpack_compilation)
             webpack_compilation.emitAsset(out_path, new webpack_module.sources.RawSource(output));
         else
@@ -115,9 +113,20 @@ function init(options) {
 
 function run(webpack_module, webpack_compilation) {
     const promises = [];
-    config.subdirs.map(subdir =>
-        promises.push(...get_po_files().map(po_file =>
-            buildFile(po_file, subdir, webpack_module, webpack_compilation))));
+    for (const subdir of config.subdirs) {
+        for (const po_file of get_po_files()) {
+            if (webpack_compilation)
+                webpack_compilation.fileDependencies.add(po_file);
+            const lang = path.basename(po_file).slice(0, -3);
+            promises.push(Promise.all([
+                // Separate translations for the manifest.json file and normal pages
+                buildFile(po_file, subdir, webpack_module, webpack_compilation,
+                          `${subdir}/po.${lang}.js`, str => !str.includes('manifest.json')),
+                buildFile(po_file, subdir, webpack_module, webpack_compilation,
+                          `${subdir}/po.manifest.${lang}.js`, str => str.includes('manifest.json'))
+            ]));
+        }
+    }
     return Promise.all(promises);
 }
 

pkg/shell/index.html
@@ -8,7 +8,10 @@
     <link href="../../static/branding.css" rel="stylesheet" />
     <script src="../base1/cockpit.js"></script>
     <script src="../manifests.js"></script>
+    <!-- HACK: C bridge loads translations via glob and Python via manifest.js -->
+    <script src="../*/po.manifest.js"></script>
     <script src="../*/po.js"></script>
+    <script src="po.js"></script>
   </head>
   <body class="pf-v5-m-tabular-nums" hidden="true">
     <div id="main" class="page">

src/cockpit/packages.py
@@ -157,7 +157,8 @@ class Manifest(JsonObject):
 
 
 class Package:
-    PO_JS_RE: ClassVar[Pattern] = re.compile(r'po\.([^.]+)\.js(\.gz)?')
+    # For po{,.manifest}.js files, the interesting part is the locale name
+    PO_JS_RE: ClassVar[Pattern] = re.compile(r'(po|po\.manifest)\.([^.]+)\.js(\.gz)?')
 
     # immutable after __init__
     manifest: Manifest
@@ -166,7 +167,7 @@ class Package:
     priority: int
 
     # computed later
-    translations: Optional[Dict[str, str]] = None
+    translations: Optional[Dict[str, Dict[str, str]]] = None
     files: Optional[Dict[str, str]] = None
 
     def __init__(self, manifest: Manifest):
@@ -186,7 +187,7 @@ class Package:
             return
 
         self.files = {}
-        self.translations = {}
+        self.translations = {'po.js': {}, 'po.manifest.js': {}}
 
         for file in self.path.rglob('*'):
             name = str(file.relative_to(self.path))
@@ -195,13 +196,19 @@ class Package:
 
             po_match = Package.PO_JS_RE.fullmatch(name)
             if po_match:
-                locale = po_match.group(1)
+                basename = po_match.group(1)
+                locale = po_match.group(2)
                 # Accept-Language is case-insensitive and uses '-' to separate variants
                 lower_locale = locale.lower().replace('_', '-')
-                self.translations[lower_locale] = name
+
+                self.translations[f'{basename}.js'][lower_locale] = name
             else:
                 basename = name[:-3] if name.endswith('.gz') else name
                 self.files[basename] = name
+
+        # support old cockpit-po-plugin which didn't write po.manifest.??.js
+        if not self.translations['po.manifest.js']:
+            self.translations['po.manifest.js'] = self.translations['po.js']
 
     def get_content_security_policy(self) -> str:
         policy = {
@@ -236,21 +243,21 @@ class Package:
 
         return Document(path.open('rb'), content_type, content_encoding, content_security_policy)
 
-    def load_translation(self, locales: List[str]) -> Document:
+    def load_translation(self, path: str, locales: List[str]) -> Document:
         self.ensure_scanned()
         assert self.translations is not None
 
         # First check the locales that the user sent
         for locale in locales:
             with contextlib.suppress(KeyError):
-                return self.load_file(self.translations[locale])
+                return self.load_file(self.translations[path][locale])
 
         # Next, check the language-only versions of variant-specified locales
         for locale in locales:
             language, _, region = locale.partition('-')
             if region:
                 with contextlib.suppress(KeyError):
-                    return self.load_file(self.translations[language])
+                    return self.load_file(self.translations[path][language])
 
         # We prefer to return an empty document than 404 in order to avoid
         # errors in the console when a translation can't be found
@@ -261,9 +268,9 @@ class Package:
         assert self.files is not None
         assert self.translations is not None
 
-        if path == 'po.js':
+        if path in self.translations:
             locales = parse_accept_language(headers)
-            return self.load_translation(locales)
+            return self.load_translation(path, locales)
         else:
             return self.load_file(self.files[path])
 
@@ -496,13 +503,15 @@ class Packages(bus.Object, interface='cockpit.Packages'):
         for name, package in self.packages.items():
             if name in ['static', 'base1']:
                 continue
+
             # find_translation will always find at least 'en'
-            translation = package.load_translation(locales)
+            translation = package.load_translation('po.manifest.js', locales)
             with translation.data:
                 if translation.content_encoding == 'gzip':
                     data = gzip.decompress(translation.data.read())
                 else:
                     data = translation.data.read()
+
             chunks.append(data)
 
         chunks.append(b"""

test/pytest/test_packages.py
@@ -158,3 +158,33 @@ def test_condition_hides_priority(pkgdir):
     assert packages.packages['basic'].manifest['description'] == 'standard package'
     assert packages.packages['basic'].manifest['requires'] == {'cockpit': "42"}
     assert packages.packages['basic'].priority == 1
+
+
+def test_translation(pkgdir):
+    # old style: make sure po.de.js is served as fallback for manifest translations
+    make_package(pkgdir, 'one')
+    (pkgdir / 'one' / 'po.de.js').write_text('eins')
+
+    # new style: separated translations
+    make_package(pkgdir, 'two')
+    (pkgdir / 'two' / 'po.de.js').write_text('zwei')
+    (pkgdir / 'two' / 'po.manifest.de.js').write_text('zwo')
+
+    packages = Packages()
+
+    # make sure we can read a po.js file with language fallback
+    document = packages.load_path('/one/po.js', {'Accept-Language': 'es, de'})
+    assert '/javascript' in document.content_type
+    assert document.data.read() == b'eins'
+
+    # make sure we fall back cleanly to an empty file with correct mime
+    document = packages.load_path('/one/po.js', {'Accept-Language': 'es'})
+    assert '/javascript' in document.content_type
+    assert document.data.read() == b''
+
+    # make sure the manifest translations get sent along with manifests.js
+    document = packages.load_path('/manifests.js', {'Accept-Language': 'de'})
+    contents = document.data.read()
+    assert b'eins\n' in contents
+    assert b'zwo\n' in contents
+    assert b'zwei\n' not in contents

commit b3db13cf82835db6c5d17fb602b6196a51a24bca

lib: handle `subdir` being undefined for cockpit projects
Cockpit itself always sets subdir for playground, systemd etc. while
cockpit-podman and other projects don't. This regression was introduced
in 89781bacb1027817b8e0cf07.

pkg/lib/cockpit-po-plugin.js
@@ -44,7 +44,7 @@ function get_plural_expr(statement) {
     return expr;
 }
 
-function buildFile(po_file, subdir, webpack_module, webpack_compilation, out_path, filter) {
+function buildFile(po_file, subdir, webpack_module, webpack_compilation, filename, filter) {
     return new Promise((resolve, reject) => {
         // Read the PO file, remove fuzzy/disabled lines to avoid tripping up the validator
         const po_data = fs.readFileSync(po_file, 'utf8')
@@ -95,6 +95,7 @@ function buildFile(po_file, subdir, webpack_module, webpack_compilation, out_pat
         const wrapper = config.wrapper?.(subdir) || DEFAULT_WRAPPER;
         const output = wrapper.replace('PO_DATA', chunks.join('')) + '\n';
 
+        const out_path = path.join(subdir ? (subdir + '/') : '', filename);
         if (webpack_compilation)
             webpack_compilation.emitAsset(out_path, new webpack_module.sources.RawSource(output));
         else
@@ -121,9 +122,9 @@ function run(webpack_module, webpack_compilation) {
             promises.push(Promise.all([
                 // Separate translations for the manifest.json file and normal pages
                 buildFile(po_file, subdir, webpack_module, webpack_compilation,
-                          `${subdir}/po.${lang}.js`, str => !str.includes('manifest.json')),
+                          `po.${lang}.js`, str => !str.includes('manifest.json')),
                 buildFile(po_file, subdir, webpack_module, webpack_compilation,
-                          `${subdir}/po.manifest.${lang}.js`, str => str.includes('manifest.json'))
+                          `po.manifest.${lang}.js`, str => str.includes('manifest.json'))
             ]));
         }
     }
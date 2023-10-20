b7911ffbd3adb4c8a30efd2cdc619a004b06726a
Marvin Schmidt <marv@exherbo.org>
2023-10-20 16:07:01 +0200

    tls: Sort includes (NFC)

--- src/tls/cockpit-certificate-ensure.c
+++ src/tls/cockpit-certificate-ensure.c
@@ -28,9 +28,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/fcntl.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include <common/cockpitwebcertificate.h>

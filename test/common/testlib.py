6d0adf27b512776d066dee298bf3e9e28b6e74d4
Martin Pitt <mpitt@redhat.com>
2023-12-07 10:14:17 +0100

    test: Adjust method name in #18355 message ignoring
    Commit 747e7c23b713 renamed send_message() to send_json(). Relax the
    pattern to get along with either (so that this also works for older
    cockpit versions in e.g. cockpit-machines tests).

--- test/common/testlib.py
+++ test/common/testlib.py
@@ -1857,7 +1857,7 @@ class MachineCase(unittest.TestCase):
             "Traceback .*most recent call last.*",
             "File .*",
             "async with self.watch_processing_lock:",
-            "self.send_message.*",
+            "self.send_.*",
             "self.release.*",
             "self._wake_up_first.*",
             "fut.set_result.*",

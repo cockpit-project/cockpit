5dc552481d6f4dca3d92ec0d373296f9af52a3d7
Marius Vollmer <mvollmer@redhat.com>
2024-02-01 12:27:01 +0200

    WIP

--- pkg/storaged/anaconda.jsx
+++ pkg/storaged/anaconda.jsx
@@ -19,6 +19,9 @@
 
 import client from "./client.js";
 
+import { decode_filename } from "./utils.js";
+import { parse_subvol_from_options } from "./btrfs/utils.jsx";
+
 export const AnacondaAdvice = () => {
     if (!client.in_anaconda_mode())
         return null;
@@ -26,3 +29,74 @@ export const AnacondaAdvice = () => {
     // Nothing yet.
     return null;
 };
+
+export function export_mount_point_mapping() {
+    if (!client.in_anaconda_mode())
+        return;
+
+    function fstab_info(config) {
+        let dir;
+        let subvols;
+
+        for (const c of config) {
+            if (c[0] == "fstab") {
+                const d = client.strip_mount_point_prefix(decode_filename(c[1].dir.v));
+                if (d) {
+                    const sv = parse_subvol_from_options(decode_filename(c[1].opts.v));
+                    if (sv) {
+                        if (sv.pathname) {
+                            if (!subvols)
+                                subvols = { };
+                            subvols[sv.pathname] = d;
+                        }
+                    } else if (!dir) {
+                        dir = d;
+                    }
+                }
+            }
+        }
+
+        if (dir || subvols)
+            return {
+                type: "filesystem",
+                dir,
+                subvolumes: subvols
+            };
+    }
+
+    function block_info(block) {
+        if (block.IdUsage == "filesystem") {
+            return fstab_info(block.Configuration);
+        } else if (block.IdUsage == "swap") {
+            return {
+                type: "swap",
+            };
+        } else if (block.IdUsage == "crypto") {
+            const cleartext_block = client.blocks_cleartext[block.path];
+
+            let content_info;
+            if (cleartext_block)
+                content_info = block_info(cleartext_block);
+            else {
+                const block_crypto = client.blocks_crypto[block.path];
+                if (block_crypto)
+                    content_info = fstab_info(block_crypto.ChildConfiguration);
+            }
+
+            return {
+                type: "crypto",
+                cleartext_device: cleartext_block && decode_filename(cleartext_block.Device),
+                content: content_info,
+            };
+        } else
+            return undefined;
+    }
+
+    const mpm = { };
+    for (const p in client.blocks) {
+        const b = client.blocks[p];
+        mpm[decode_filename(b.Device)] = block_info(b);
+    }
+
+    window.localStorage.setItem("cockpit_mount_points", JSON.stringify(mpm));
+}

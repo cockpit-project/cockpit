cockpit-project/cockpit

commit 96335a440c15859e10aa3c0b9caff6a97ce0c590

common: don't send error body on HEAD requests
The code in cockpit_web_response_error() sends a body, regardless of if
the request was for GET or for HEAD.  Let's fix it to only send the body
on GET.

src/common/cockpitwebresponse.c
@@ -1168,15 +1168,18 @@ cockpit_web_response_error (CockpitWebResponse *self,
       cockpit_web_response_headers (self, code, message, -1, "Content-Type", "text/html; charset=utf8", NULL);
     }
 
-  extern const char *cockpit_webresponse_fail_html_text;
-  g_autoptr(GBytes) input = g_bytes_new_static (cockpit_webresponse_fail_html_text, strlen (cockpit_webresponse_fail_html_text));
-  g_autolist(GBytes) output = cockpit_template_expand (input, "@@", "@@", substitute_message, (gpointer) message);
-
-  for (GList *l = output; l != NULL; l = g_list_next (l))
+  if (g_str_equal (self->method, "GET"))
     {
-      if (!cockpit_web_response_queue (self, l->data))
-        /* error: early exit */
-        return;
+      extern const char *cockpit_webresponse_fail_html_text;
+      g_autoptr(GBytes) input = g_bytes_new_static (cockpit_webresponse_fail_html_text, strlen (cockpit_webresponse_fail_html_text));
+      g_autolist(GBytes) output = cockpit_template_expand (input, "@@", "@@", substitute_message, (gpointer) message);
+
+      for (GList *l = output; l != NULL; l = g_list_next (l))
+        {
+          if (!cockpit_web_response_queue (self, l->data))
+            /* error: early exit */
+            return;
+        }
     }
 
   cockpit_web_response_complete (self);

commit 41578d8a300308b5b64bd647cc85d9eae80b2bab

common: Fix crash with unsupported HTTP methods
Commit 96335a440c15859 introduced a crash: Requesting any HTTP method
other than "GET" or "HEAD" segfaulted cockpit_web_response_error() as
`self->method` is NULL. That could happen because
cockpit_web_response_set_method()` refused to set unsupported ones.

Improve the /web-response/head test to set the "HEAD" method right away
with `cockpit_web_response_new()`, instead of overwriting setup()'s
"GET" default.

That was the only consumer of `cockpit_web_response_set_method()`. This
method is rather dubious, as the method should only be set in the
constructor. So eliminate it and move the check into the constructor.

Keep the provided method value also for unknown ones.

Drop the critical, as it's not an internal program error.

Fixes #19997

src/common/cockpitwebresponse.c
@@ -234,7 +234,9 @@ cockpit_web_response_new (GIOStream *io,
   self->url_root = NULL;
   self->full_path = g_strdup (path);
   self->path = self->full_path;
-  cockpit_web_response_set_method (self, method);
+
+  g_assert (method != NULL);
+  self->method = g_strdup (method);
 
   if (path && original_path)
     {
@@ -262,15 +264,6 @@ cockpit_web_response_new (GIOStream *io,
     self->origin = g_strdup_printf ("%s://%s", self->protocol, host);
 
   return self;
-}
-
-void
-cockpit_web_response_set_method (CockpitWebResponse *response,
-                                 const gchar *method)
-{
-  g_return_if_fail (g_strcmp0 (method, "GET") == 0 || g_strcmp0 (method, "HEAD") == 0);
-  g_free (response->method);
-  response->method = g_strdup (method);
 }
 
 /**
@@ -586,7 +579,7 @@ cockpit_web_response_queue (CockpitWebResponse *self,
       return FALSE;
     }
 
-  if (g_strcmp0 (self->method, "HEAD") == 0)
+  if (g_str_equal (self->method, "HEAD"))
     {
       g_debug ("%s: ignoring queued block for method HEAD", self->logname);
       return TRUE;
@@ -1168,7 +1161,7 @@ cockpit_web_response_error (CockpitWebResponse *self,
       cockpit_web_response_headers (self, code, message, -1, "Content-Type", "text/html; charset=utf8", NULL);
     }
 
-  if (g_str_equal (self->method, "GET"))
+  if (!g_str_equal (self->method, "HEAD"))
     {
       extern const char *cockpit_webresponse_fail_html_text;
       g_autoptr(GBytes) input = g_bytes_new_static (cockpit_webresponse_fail_html_text, strlen (cockpit_webresponse_fail_html_text));

src/common/cockpitwebresponse.h
@@ -56,8 +56,6 @@ CockpitWebResponse *  cockpit_web_response_new           (GIOStream *io,
                                                           GHashTable *in_headers,
                                                           const gchar *method,
                                                           const gchar *protocol);
-void                  cockpit_web_response_set_method    (CockpitWebResponse *response,
-                                                          const gchar *method);
 
 
 const gchar *         cockpit_web_response_get_path      (CockpitWebResponse *self);

src/common/test-webresponse.c
@@ -48,6 +48,7 @@ typedef struct {
     const gchar *path;
     const gchar *header;
     const gchar *value;
+    const gchar *method;
     const gchar *expected_content_type;
     CockpitCacheType cache;
     gboolean for_tls_proxy;
@@ -88,7 +89,8 @@ setup (TestCase *tc,
       g_hash_table_insert (headers, g_strdup (fixture->header), g_strdup (fixture->value));
     }
 
-  tc->response = cockpit_web_response_new (io, path, path, headers, "GET",
+  tc->response = cockpit_web_response_new (io, path, path, headers,
+                                           (fixture && fixture->method) ? fixture->method : "GET",
                                            (fixture && fixture->for_tls_proxy) ? "https" : "http");
 
   if (headers)
@@ -339,6 +341,14 @@ static const TestFixture content_type_fixture_wasm = {
   .expected_content_type = "application/wasm",
 };
 
+static const TestFixture fixture_head = {
+  .method = "HEAD",
+};
+
+static const TestFixture fixture_unsupported_method = {
+  .method = "PATCH",
+};
+
 static void
 test_content_type (TestCase *tc,
                    gconstpointer user_data)
@@ -575,8 +585,6 @@ test_head (TestCase *tc,
   const gchar *resp;
   GBytes *content;
 
-  cockpit_web_response_set_method (tc->response, "HEAD");
-
   g_assert_cmpint (cockpit_web_response_get_state (tc->response), ==, COCKPIT_WEB_RESPONSE_READY);
 
   cockpit_web_response_headers (tc->response, 200, "OK", 19, NULL);
@@ -599,6 +607,18 @@ test_head (TestCase *tc,
   g_assert_cmpint (cockpit_web_response_get_state (tc->response), ==, COCKPIT_WEB_RESPONSE_SENT);
 
   g_assert_cmpstr (resp, ==, "HTTP/1.1 200 OK\r\nContent-Length: 19\r\n" STATIC_HEADERS);
+}
+
+static void
+test_unsupported_method (TestCase *tc,
+                         gconstpointer data)
+{
+  cockpit_web_response_error (tc->response, 405, NULL, "Unsupported method");
+
+  const gchar *resp = output_as_string (tc);
+  g_assert (g_str_has_prefix (resp, "HTTP/1.1 405 Unsupported method\r\n"));
+  /* not a HEAD request, thus has body */
+  g_assert (strstr (resp, "<body>"));
 }
 
 static void
@@ -1470,8 +1490,10 @@ main (int argc,
               setup, test_stream, teardown);
   g_test_add ("/web-response/pressure", TestCase, NULL,
               setup, test_pressure, teardown);
-  g_test_add ("/web-response/head", TestCase, NULL,
+  g_test_add ("/web-response/head", TestCase, &fixture_head,
               setup, test_head, teardown);
+  g_test_add ("/web-response/unsupported-method", TestCase, &fixture_unsupported_method,
+              setup, test_unsupported_method, teardown);
   g_test_add ("/web-response/chunked-transfer-encoding", TestCase, NULL,
               setup, test_chunked_transfer_encoding, teardown);
   g_test_add ("/web-response/chunked-zero-length", TestCase, NULL,

test/verify/check-connection
@@ -760,6 +760,15 @@ class TestConnection(testlib.MachineCase):
         # Large requests are processed correctly with TLS
         self.assertIn('A Custom Title', m.curl('-k', 'https://localhost:9000/', headers=large_headers))
 
+        # unsupported HTTP method
+        self.assertIn("HTTP/1.1 405 Method Not Allowed",
+                      m.execute('curl -k --verbose -X PATCH https://localhost:9000/ 2>&1'))
+
+        # no body with HEAD request
+        out = m.execute('curl -k --head https://localhost:9000/')
+        self.assertIn("HTTP/1.1 200", out)
+        self.assertNotIn("<html>", out)
+
     @testlib.nondestructive
     def testHeadRequest(self):
         m = self.machine
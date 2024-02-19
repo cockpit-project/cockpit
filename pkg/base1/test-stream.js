c0db636662458327ca3119da6045d923ff611328
Martin Pitt <mpitt@redhat.com>
2024-02-19 10:58:17 +0100

    base1: Hack test-stream failure due to glibc resolver bug
    Fedora glibc's resolver hardcodes `getent hosts localhost` to `::1`,
    even if there is no IPv6 configured whatsoever (interface addresses or
    /etc/hosts) [1][2].
    
    This causes the C bridge to fail the http channel request with
    
        localhost:44191: couldn't connect: Cannot assign requested address
    
    The Python bridge handles that better, due to not using that obsolete
    resolver, so this isn't a problem on main. But we aren't going to fix
    that in the C bridge at this point, so hack around it so that it can
    pass in GitHub workflow's Fedora (cockpit/tasks) container environment.
    
    [1] https://unix.stackexchange.com/questions/530976/why-does-localhost-resolve-to-1-but-not-127-0-0-1
    [2] https://lists.opensuse.org/archives/list/factory@lists.opensuse.org/thread/CITDFVWNRTPZIHVJLA6R5BZ47UIU5RD3/

--- pkg/base1/test-stream.js
+++ pkg/base1/test-stream.js
@@ -4,7 +4,9 @@ import QUnit from "qunit-tests";
 const QS_REQUEST = "HEAD /mock/qs HTTP/1.0\nHOST: localhost\n\n";
 
 const test_server = {
-    address: window.location.hostname,
+    // HACK: container resolves localhost to ::1, which isn't configured
+    // https://unix.stackexchange.com/questions/530976/why-does-localhost-resolve-to-1-but-not-127-0-0-1
+    address: window.location.hostname === "localhost" ? "127.0.0.1" : window.location.hostname,
     port: parseInt(window.location.port, 10)
 };
 

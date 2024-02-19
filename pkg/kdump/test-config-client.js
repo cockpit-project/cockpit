20f6c7024f0bfeb32aa3035158d122077df43979
Jelle van der Waa <jvanderwaa@redhat.com>
2024-02-19 11:42:35 +0100

    kdump: use a temporary file for QUnit test
    With the fsinfo channel this test fails because it uses an relative path
    which is unsupported in fsinfo. As using a relative path with a
    hardcoded filename means we always write to the same file if you
    repeatedly run the test which could lead to undesired
    side-effects/issues
    
    Use QUnit's before/after helper functions to always create a temporary
    file, in the future `base1/test-file.js` should also use this instead of
    the current hack.

--- pkg/kdump/test-config-client.js
+++ pkg/kdump/test-config-client.js
@@ -44,37 +44,51 @@ const changedConfig = [
     ""
 ].join("\n");
 
-QUnit.test("config_update", function (assert) {
-    const done = assert.async();
-    assert.expect(6);
-    const dataWasChanged = new Promise(resolve => { this.dataWasChangedResolve = resolve });
-    let config;
-    const configChanged = (event, settings) => {
-        assert.equal(settings._internal.foo.value, "moo", "value changed correctly");
-        assert.equal("key" in settings._internal, false, "setting with comment deleted correctly");
-        assert.equal("will" in settings._internal, false, "setting without comment deleted correctly");
-        assert.equal(settings._internal.hooray.value, "value", "value added correctly");
-        assert.equal(config._rawContent, changedConfig, "raw text for changed config is correct");
-        this.dataWasChangedResolve();
-    };
+QUnit.module("kdump", hooks => {
+    let filename = "";
 
-    const filename = "cockpit_config_read";
-    const configFile = cockpit.file(filename);
-    configFile
-            .replace(basicConfig)
-            .then(() => {
-                assert.equal(configFile.path, filename, "file has correct path");
-                config = new kdump.ConfigFile(filename);
-                config.wait().then(() => {
-                    config.settings._internal.foo.value = "moo";
-                    delete config.settings._internal.key;
-                    delete config.settings._internal.will;
-                    config.settings._internal.hooray = { value: "value" };
-                    config.addEventListener('kdumpConfigChanged', configChanged);
-                    config.write(config.settings)
-                            .then(() => dataWasChanged.then(done));
+    hooks.before(async () => {
+        filename = await cockpit.spawn(["/usr/bin/mktemp", "--suffix", "kdump-test"]);
+        filename = filename.trim();
+    });
+
+    hooks.after(() => cockpit.spawn(["rm", "-f", filename]));
+
+    QUnit.test("config_update", function(assert) {
+        const done = assert.async();
+        assert.expect(6);
+        const dataWasChanged = new Promise(resolve => { this.dataWasChangedResolve = resolve });
+        let config;
+        const configChanged = (event, settings) => {
+            assert.equal(settings._internal.foo.value, "moo", "value changed correctly");
+            assert.equal("key" in settings._internal, false, "setting with comment deleted correctly");
+            assert.equal("will" in settings._internal, false, "setting without comment deleted correctly");
+            assert.equal(settings._internal.hooray.value, "value", "value added correctly");
+            assert.equal(config._rawContent, changedConfig, "raw text for changed config is correct");
+            this.dataWasChangedResolve();
+        };
+
+        const configFile = cockpit.file(filename);
+        configFile
+                .replace(basicConfig)
+                .then(() => {
+                    assert.equal(configFile.path, filename, "file has correct path");
+                    config = new kdump.ConfigFile(filename);
+                    config.wait().then(() => {
+                        config.settings._internal.foo.value = "moo";
+                        delete config.settings._internal.key;
+                        delete config.settings._internal.will;
+                        config.settings._internal.hooray = { value: "value" };
+                        config.addEventListener('kdumpConfigChanged', configChanged);
+                        config.write(config.settings)
+                                .then(() => {
+                                    // Close watch channel
+                                    config.close();
+                                    dataWasChanged.then(done);
+                                });
+                    });
                 });
-            });
+    });
 });
 
 window.setTimeout(function() {

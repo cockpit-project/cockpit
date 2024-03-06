cockpit-project/cockpit

commit e5b41b6cf442c0124493001ed5657dec5d75f697

logs: Port overview to React and PF4
Also remove content-header-extra class, logs page was the last consumer.

Fixes #14163
Fixes #11663

test/verify/check-system-journal
@@ -62,34 +62,6 @@ class TestJournal(MachineCase):
         }
         """)
 
-    def select_priority(self, value):
-        prio_map = {
-            "Only emergency": "emerg",
-            "Alert and above": "alert",
-            "Critical and above": "crit",
-            "Error and above": "err",
-            "Warning and above": "warning",
-            "Notice and above": "notice",
-            "Info and above": "info",
-            "Debug and above": "debug",
-        }
-
-        self.browser.set_val("#journal-prio-menu", prio_map[value])
-
-    def select_from_dropdown(self, browser, selector, value, click=True):
-        button_text_selector = "{0} button span:nth-of-type(1)".format(selector)
-
-        browser.wait_visible(selector)
-        if not browser.text(button_text_selector) == value:
-            item_selector = "{0} ul li a:contains({1})".format(selector, value)
-            browser.click("{0} button".format(selector))
-            browser.wait_visible(item_selector)
-            if click:
-                browser.click(item_selector)
-                browser.wait_in_text(button_text_selector, value)
-            else:
-                browser.click(button_text_selector)
-
     def crash(self):
         self.allow_core_dumps = True
 
@@ -134,6 +106,7 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
         self.injectExtras()
 
         def wait_log_lines(expected):
+            b.wait_visible(".cockpit-log-panel")
             b.wait_js_func("""(function (expected) {
           var lines = ph_get_log_lines ();
 
@@ -150,12 +123,13 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
         })""", expected)
 
         def wait_journal_empty():
-            wait_log_lines(["No logs foundCan not find any logs using the current combination of filters."])
+            b.wait_in_text(".pf-c-empty-state__body", "Can not find any logs using the current combination of filters.")
 
         b.go("#/?priority=debug&service=log123.service")
         wait_journal_empty()
 
         def wait_log123():
+            b.wait_visible(".cockpit-log-panel")
             b.wait_js_func("""(function () {
           var lines = ph_get_log_lines();
 
@@ -239,30 +213,32 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
         wait_log_present("JUST ERROR")
 
         # We cannot open this select with tests but we need to wait until it gets loaded
-        b._wait_present("#journal-service-menu option[value='just-a-service']")
-        b.set_val("#journal-service-menu", "just-a-service")
+        b.select_PF4("#journal-identifier-menu .pf-c-select__toggle", "just-a-service")
         wait_log_not_present("JUST ERROR")
         wait_log_present("THE SERVICE")
-        b.set_val("#journal-service-menu", "*")
+        b.select_PF4("#journal-identifier-menu .pf-c-select__toggle", "All")
         wait_log_present("JUST ERROR")
         wait_log_present("THE SERVICE")
 
-        self.select_priority("Error and above")
-        wait_log_not_present("THE SERVICE")
-        wait_log_present("JUST ERROR")
-        b.set_val("#journal-service-menu", "check-journal")
-        b.wait_not_present("#journal-service-menu option[value='just-a-service']")
-
-        b.go("#/?tag=test-stream&priority=notice")
-        wait_journal_empty()
-
-        # Test inline help
         # Debian supports grep only from 19.10, see https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1751006
         if m.image != "debian-stable":
-            b.click("#logs-help-toggle")
-            b.wait_text("#journal-query", "journalctl --no-tail --priority=notice --reverse -- SYSLOG_IDENTIFIER=test-stream")
-            b.click("#logs-help-toggle")
-            b.wait_not_visible("#journal-query")
+            self.browser.select_PF4("#journal-prio-menu", "Error and above")
+            wait_log_not_present("THE SERVICE")
+            wait_log_present("JUST ERROR")
+            b.select_PF4("#journal-identifier-menu .pf-c-select__toggle", "check-journal")
+            b.wait_not_present("#journal-identifier-menu + ul button:contains('just-a-service')")
+
+            b.go("#/?tag=test-stream&priority=notice")
+            wait_journal_empty()
+
+        # Test inline help
+        if m.image != "debian-stable":
+            b.click("#journal-grep button[aria-label='Open advanced search']")
+            b.wait_val("#journal-cmd-copy input", "journalctl --no-tail --priority=notice --reverse -- SYSLOG_IDENTIFIER=test-stream")
+            b.click("#journal-grep button[aria-label='Open advanced search']")
+            b.wait_not_present("#journal-cmd-copy")
+        else:
+            b.wait_not_present("#journal-grep")
 
         # Test following
         m.execute("logger --tag test-stream Message 1")
@@ -288,18 +264,21 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
 
         # Debian supports grep only from 19.10, see https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1751006
         if m.image == "debian-stable":
-            b.wait_not_visible(".text-search")
+            b.wait_not_present(".text-search")
             return
 
-        b.focus("#journal-grep")
-        b.key_press("1\r")
+        b.focus("#journal-grep input")
+        b.key_press("1")
+        b.click("#journal-grep button[aria-label=Search]")
+
         b.wait_js_cond('window.location.hash === "#/?priority=notice&tag=test-stream&grep=1"')
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 2')")
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 3')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 1')")
 
-        b.focus("#journal-grep")
-        b.key_press("\bm.*[2-5]\r")
+        b.focus("#journal-grep input")
+        b.key_press("\bm.*[2-5]")
+        b.click("#journal-grep button[aria-label=Search]")
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 1')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 2')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 3')")
@@ -314,40 +293,39 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 5')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 6')")
 
-        b.focus("#journal-grep")
-        b.key_press("FOO=bar [5-6]\r")
-        b.wait_val("#journal-grep", "priority:err identifier:logger FOO=bar [5-6]")
+        b.focus("#journal-grep input")
+        b.key_press("FOO=bar [5-6]")
+        b.click("#journal-grep button[aria-label=Search]")
+        b.wait_val("#journal-grep input", "priority:err identifier:logger FOO=bar [5-6]")
 
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 4')")
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 6')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 5')")
 
-        b.focus("#journal-grep")
-        b.key_press("\b" * 13 + "[5-6]\r")
+        b.focus("#journal-grep input")
+        b.key_press("\b" * 13 + "[5-6]")
+        b.click("#journal-grep button[aria-label=Search]")
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 4')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 6')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 5')")
 
         # Test filters
-        b.click("#log-filters")
-        b.click("#logs-predefined-filters li:first-child a")
-        b.wait_val("#journal-grep", "priority:err identifier:logger boot:0 [5-6]")
+        b.select_PF4("#logs-predefined-filters", "Current boot")
+        b.wait_val("#journal-grep input", "priority:err identifier:logger boot:0 [5-6]")
 
-        b.click("#log-filters")
-        b.click("#logs-predefined-filters li:nth-child(2) a")
-        b.wait_val("#journal-grep", "priority:err identifier:logger boot:-1 [5-6]")
+        b.select_PF4("#logs-predefined-filters", "Previous boot")
+        b.wait_val("#journal-grep input", "priority:err identifier:logger boot:-1 [5-6]")
 
-        b.click("#log-filters")
-        b.click("#logs-predefined-filters li:nth-child(3) a")
-        b.wait_val("#journal-grep", "priority:err identifier:logger since:-24hours [5-6]")
+        b.select_PF4("#logs-predefined-filters", "Last 24 hours")
+        b.wait_val("#journal-grep input", "priority:err identifier:logger since:-24hours [5-6]")
 
-        b.click("#log-filters")
-        b.click("#logs-predefined-filters li:nth-child(4) a")
-        b.wait_val("#journal-grep", "priority:err identifier:logger since:-7days [5-6]")
+        b.select_PF4("#logs-predefined-filters", "Last 7 days")
+        b.wait_val("#journal-grep input", "priority:err identifier:logger since:-7days [5-6]")
 
         # Check that 'until' qualifier keeps streaming until given time
-        b.focus("#journal-grep")
-        b.key_press("\b\b\b\b6-9] until:+15s\r")
+        b.focus("#journal-grep input")
+        b.key_press("\b\b\b\b6-9] until:+15s")
+        b.click("#journal-grep button[aria-label=Search]")
         b.wait_not_present("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 5')")
         b.wait_visible("#journal-box .cockpit-logline .cockpit-log-message:contains('Message 6')")
         m.execute(r"printf 'MESSAGE=Message 7\nPRIORITY=3\nBAR=foo\n' | logger --journald")
@@ -379,8 +357,10 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
         m.execute("timedatectl set-time 2038-01-01")
 
         def wait_log_lines(expected):
+            b.wait_visible(".cockpit-log-panel")
             b.wait_js_func("""(function (expected) {
           var lines = ph_get_log_lines ();
+
           if (expected.length != lines.length)
             return false;
           for (i = 0; i < expected.length; i++)
@@ -469,7 +449,7 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
 
         # Debian supports grep only from release 10, see https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1751006
         if m.image == "debian-stable":
-            b.wait_not_visible(".text-search")
+            b.wait_not_present(".text-search")
             return
 
         # Check that 'until' qualifier shows only until some date
@@ -479,14 +459,16 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
                         ["", "Reboot", ""],
                         ["check-journal", "BEFORE BOOT", ""]
                         ])
-        b.focus("#journal-grep")
-        b.key_press("until:2038-01-01\\ 00:03\r")
+        b.focus("#journal-grep input")
+        b.key_press("until:2038-01-01\\ 00:03")
+        b.click("#journal-grep button[aria-label=Search]")
         wait_log_lines([expected_date,
                         ["check-journal", "BEFORE BOOT", ""],
                         "Load earlier entries"
                         ])
 
-        b._wait_present("#journal-service-menu option[value='check-journal']")
+        b.click("#journal-identifier-menu .pf-c-select__toggle")
+        b.wait_visible("#journal-identifier-menu .pf-c-select__toggle + ul button:contains('check-journal')")
 
         # New messages are not streamed when 'until' is in the past
         m.execute("logger -p user.err --tag check-journal RIGHT NOW")
@@ -540,17 +522,29 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
 
         journal_line_selector = "#journal-box .cockpit-logline:has(span:contains({0}))"
 
-        self.select_priority("Only emergency")
+        if m.image == "debian-stable":
+            b.wait_not_present(".text-search")  # Guard so we know when we can remove this special case
+            b.go("#/?tag=check-journal&priority=emerg&since=@" + self.test_start_time)
+        else:
+            self.browser.select_PF4("#journal-prio-menu", "Only emergency")
         b.wait_visible(journal_line_selector.format("EMERGENCY_MESSAGE"))
         b.wait_not_present(journal_line_selector.format("WARNING_MESSAGE"))
         b.wait_not_present(journal_line_selector.format("INFO_MESSAGE"))
 
-        self.select_priority("Warning and above")
+        if m.image == "debian-stable":
+            b.wait_not_present(".text-search")
+            b.go("#/?tag=check-journal&priority=warning&since=@" + self.test_start_time)
+        else:
+            self.browser.select_PF4("#journal-prio-menu", "Warning and above")
         b.wait_visible(journal_line_selector.format("EMERGENCY_MESSAGE"))
         b.wait_visible(journal_line_selector.format("WARNING_MESSAGE"))
         b.wait_not_present(journal_line_selector.format("INFO_MESSAGE"))
 
-        self.select_priority("Info and above")
+        if m.image == "debian-stable":
+            b.wait_not_present(".text-search")
+            b.go("#/?tag=check-journal&priority=info&since=@" + self.test_start_time)
+        else:
+            self.browser.select_PF4("#journal-prio-menu", "Info and above")
         b.wait_visible(journal_line_selector.format("EMERGENCY_MESSAGE"))
         b.wait_visible(journal_line_selector.format("WARNING_MESSAGE"))
         b.wait_visible(journal_line_selector.format("INFO_MESSAGE"))
@@ -566,13 +560,13 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
         self.crash()
 
         self.login_and_go("/system/logs#/?since=@" + self.test_start_time)
-        self.select_priority("Critical and above")
+        self.browser.select_PF4("#journal-prio-menu", "Critical and above")
         b.wait_visible(sleep_crash_list_sel)
 
-        self.select_priority("Alert and above")
+        self.browser.select_PF4("#journal-prio-menu", "Alert and above")
         b.wait_not_present(sleep_crash_list_sel)
 
-        self.select_priority("Critical and above")
+        self.browser.select_PF4("#journal-prio-menu", "Critical and above")
         b.click(sleep_crash_list_sel)
 
         def wait_field(name, value):

pkg/lib/patternfly/patternfly-overrides.scss
@@ -18,7 +18,6 @@ h1, h2, h3, h4, h5, h6 {
 // Restyle inputs & dropdowns
 .input-group-addon,
 .bootstrap-select.btn-group .btn,
-.content-header-extra .dropdown-toggle,
 .dropdown-toggle,
 .form-control:not(.ct-select):not(.rbt-input):not(select) {
   background-image: none !important;
@@ -26,7 +25,6 @@ h1, h2, h3, h4, h5, h6 {
 
 .input-group-addon,
 .bootstrap-select.btn-group .btn,
-.content-header-extra .dropdown-toggle,
 .dropdown-toggle,
 .form-control:not(.ct-select) {
   border-radius: 3px;
@@ -559,24 +557,6 @@ th > label {
 // Bump down graph text size
 .server-graph {
   font-size: var(--pf-global--FontSize--xs);
-}
-
-// Restyle nav tabs (in content header only, for services)
-// in Cockpit, nav-tabs are also used in lists
-// and Cockpit's lists use underlines, not overlines
-.content-header-extra > .nav-tabs > li {
-  position: relative;
-
-  &.active::after {
-    position: absolute;
-    background: var(--pf-global--primary-color--100);
-    content: "";
-    height: 2px;
-    left: 0;
-    top: 0;
-    width: 100%;
-    z-index: 1;
-  }
 }
 
 // Flatten the progress bar trough

pkg/systemd/logs.html
@@ -28,94 +28,8 @@ along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 </head>
 
 <body class="pf-m-redhat-font">
-  <div class="ct-page-fill" id="journal">
-    <div class="pf-c-page">
-      <main class="pf-c-page__main" tabindex="-1">
-        <section class="pf-c-page__main-section content-header-extra">
-          <div class="filters">
-            <div class="filter">
-                <button id="log-filters" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-                  <span translate>Time</span>
-                  <div class="caret"></div>
-                </button>
-                <ul class="dropdown-menu" id="logs-predefined-filters">
-                  <li><a tabindex="0" data-key="boot" data-value="0" translate>Current boot</a></li>
-                  <li><a tabindex="0" data-key="boot" data-value="-1" translate>Previous boot</a></li>
-                  <li><a tabindex="0" data-key="since" data-value="-24hours" translate>Last 24 hours</a></li>
-                  <li><a tabindex="0" data-key="since" data-value="-7days" translate>Last 7 days</a></li>
-                </ul>
-            </div>
-
-            <div class="filter">
-                <label for="journal-prio-menu" translate>Priority</label>
-                <select id="journal-prio-menu" class="pf-c-form-control">
-                  <option value="emerg" translate>Only emergency</option>
-                  <option value="alert" translate>Alert and above</option>
-                  <option value="crit" translate>Critical and above</option>
-                  <option value="err" translate>Error and above</option>
-                  <option value="warning" translate>Warning and above</option>
-                  <option value="notice" translate>Notice and above</option>
-                  <option value="info" translate>Info and above</option>
-                  <option value="debug" translate>Debug and above</option>
-                </select>
-            </div>
-
-            <div class="filter">
-                <label for="journal-service-menu" translate>Identifier</label>
-                <select id="journal-service-menu" class="pf-c-form-control">
-                    <!-- This is filled by from logs.js !-->
-                </select>
-            </div>
-
-            <div class="filter text-search">
-                <label for="journal-grep" translate>Text</label>
-                <span class="filter-group">
-                    <input id="journal-grep" class="form-control" type="text" translate="placeholder" placeholder="Type to filter">
-                    <div id="logs-help" class="dropdown">
-                      <a role="link" tabindex="0" id="logs-help-toggle" class="dropdown-toggle dropdown-form-control" data-toggle="dropdown">
-                        <span class="pficon pficon-help"></span>
-                      </a>
-                      <div id="logs-help-menu" class="dropdown-menu">
-                        <span translate>Search the logs with a combination of terms:</span>
-                        <ul>
-                            <li><span translate>qualifiers</span>, <span translate>e.g.</span> 'priority:', 'identifier:', 'service:'</li>
-                            <li><span translate>log fields</span>, <span translate>e.g.</span> '_EXE=/usr/bin/python'</li>
-                            <li translate>any free-form string as regular expression</li>
-                        </ul>
-                        <span class="help-links">
-                            <a href="https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/managing_systems_using_the_rhel_8_web_console/reviewing-logs_system-management-using-the-rhel-8-web-console#filtering-logs-in-the-web-console_reviewing-logs" target="blank" rel="noopener noreferrer"><span translate>Learn more</span><i class="fa fa-external-link fa-xs"></i></a>
-                            <a href="https://www.freedesktop.org/software/systemd/man/journalctl.html" target="blank" rel="noopener noreferrer"><span translate>journalctl manpage</span><i class="fa fa-external-link fa-xs"></i></a>
-                        </span>
-                        <span class="journal-cmd">
-                            <pre id="journal-query"></pre>
-                            <button class="copy-cmd" id="journal-cmd-copy"><i class="fa fa-clipboard fa-xs"></i></button>
-                        </span>
-                      </div>
-                    </div>
-                </span>
-            </div>
-          </div>
-
-          <div class="filter-separator"></div>
-
-          <div class="filter-actions">
-
-            <span class="filters-toggle" hidden>
-              <button class="pf-c-button pf-m-link" translate>Show filters</button>
-            </span>
-
-            <button id="journal-follow" class="pf-c-button pf-m-secondary" type="button" translate>Pause</button>
-          </div>
-        </section>
-        <section id="journal-box" class="pf-c-page__main-section pf-m-light">
-          <div class="panel panel-default cockpit-log-panel" id="journal-logs" role="table"></div>
-          <div class="journal-start" id="start-box" role="rowgroup"></div>
-        </section>
-      </main>
-    </div>
+  <div class="ct-page-fill" id="logs">
   </div>
-
-  <div class="ct-page-fill" id="journal-entry"></div>
 
   <script type="text/javascript" src="logs.js"></script>
 </body>

commit afe5a0889110d2808f836573b81bce23bb435ee8

logs: Reload identifiers when priority changed
The command for listing identifiers also includes priority filter therefore when priority changes list of identifiers differ.

This was broken in e5b41b6cf (notice https://github.com/cockpit-project/cockpit/commit/2f7d00f248e5e7a9b0b48e1de04792635c43e442#diff-36ded31cb7479a8e037644de728bc2f101aa79c772b6a328a90f74703623d64dR894 )

pkg/systemd/logs.jsx
@@ -145,7 +145,7 @@ export const LogsPage = () => {
     };
 
     const onJournalPrioChange = (value) => {
-        setUpdateIdentifiersList(false);
+        setUpdateIdentifiersList(true);
 
         updateUrl(Object.assign(options, { priority: value }));
     };

test/verify/check-system-journal
@@ -329,6 +329,39 @@ ExecStart=/bin/sh -c 'sleep 5; for s in $(seq 10); do echo SLOW; sleep 0.1; done
 
         self.allow_journal_messages(".*Data from the specified boot .* is not available: No such boot ID in journal.*")
 
+    def testIdentifierReloading(self):
+        """
+        Test verifies https://github.com/cockpit-project/cockpit/issues/20123
+        Updating Identifiers when changing priorities
+        """
+        b = self.browser
+        m = self.machine
+        tag_error = "check-error-inside"
+        tag_info = "check-info-inside"
+        m.execute(f"logger -p user.err --tag {tag_error} {tag_error.upper()}")
+        m.execute(f"logger -p 5 --tag {tag_info} {tag_info.upper()}")
+        self.login_and_go("/system/logs")
+
+        b.go("#/?priority=err")
+        b.wait_visible(f"#journal-box .cockpit-logline .cockpit-log-message:contains('{tag_error.upper()}')")
+        b.wait_not_present(f"#journal-box .cockpit-logline .cockpit-log-message:contains('{tag_info.upper()}')")
+        b.select_PF4("#journal-identifier-menu .pf-v5-c-select__toggle", tag_error)
+        b.wait_not_present(f"#journal-identifier-menu + ul button:contains('{tag_info}')")
+
+        self.browser.select_PF4("#journal-prio-menu", "Debug and above")
+        b.select_PF4("#journal-identifier-menu .pf-v5-c-select__toggle", "All")
+        b.wait_visible(f"#journal-box .cockpit-logline .cockpit-log-message:contains('{tag_error.upper()}')")
+        b.wait_visible(f"#journal-box .cockpit-logline .cockpit-log-message:contains('{tag_info.upper()}')")
+        b.select_PF4("#journal-identifier-menu .pf-v5-c-select__toggle", tag_error)
+        b.select_PF4("#journal-identifier-menu .pf-v5-c-select__toggle", tag_info)
+
+        self.browser.select_PF4("#journal-prio-menu", "Error and above")
+        b.select_PF4("#journal-identifier-menu .pf-v5-c-select__toggle", "All")
+        b.wait_visible(f"#journal-box .cockpit-logline .cockpit-log-message:contains('{tag_error.upper()}')")
+        b.wait_not_present(f"#journal-box .cockpit-logline .cockpit-log-message:contains('{tag_info.upper()}')")
+        b.select_PF4("#journal-identifier-menu .pf-v5-c-select__toggle", tag_error)
+        b.wait_not_present(f"#journal-identifier-menu + ul button:contains('{tag_info}')")
+
     def testRebootAndTime(self):
         b = self.browser
         m = self.machine
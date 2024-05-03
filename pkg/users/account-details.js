93d0a6d4dbe97937e69b126870b4bd4675c326d5
Luna <luna.dragon@suse.com>
2024-05-03 11:56:22 +0530

    users: Support for watching lastlog2

--- pkg/users/account-details.js
+++ pkg/users/account-details.js
@@ -98,16 +98,32 @@ function get_expire(name) {
 
 export function AccountDetails({ accounts, groups, current_user, user, shells }) {
     const [expiration, setExpiration] = useState(null);
-    useEffect(() => {
-        get_expire(user).then(setExpiration);
+    const [lastlogpath, setLastlogPath] = useState(null);
 
-        // Watch `/var/run/utmp` to register when user logs in or out
-        const handle = cockpit.file("/var/run/utmp", { superuser: "try", binary: true });
-        handle.watch(() => {
+    useEffect(() => {
+        cockpit.spawn(["test", "-e", "/var/run/utmp"], { err: "ignore" }).then(() => {
+            setLastlogPath("/var/run/utmp");
+        }).catch(() => {
+            cockpit.spawn(["test", "-e", "/var/lib/lastlog/lastlog2.db"], { err: "ignore" }).then(() => {
+                setLastlogPath("/var/lib/lastlog/lastlog2.db");
+            }).catch(() => {
+                setLastlogPath(null);
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        if (lastlogpath !== null) {
             get_expire(user).then(setExpiration);
-        }, { read: false });
-        return handle.close;
-    }, [user, accounts]);
+
+            // Watch lastlog log to register when user logs in or out
+            const handle = cockpit.file(lastlogpath, { superuser: "try", binary: true });
+            handle.watch(() => {
+                get_expire(user).then(setExpiration);
+            }, { read: false });
+            return handle.close;
+        }
+    }, [user, accounts, lastlogpath]);
 
     const [edited_real_name, set_edited_real_name] = useState(null);
     const [committing_real_name, set_committing_real_name] = useState(false);

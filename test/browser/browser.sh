89a30ba8ea09d3d1abff6d70b36da60b39bd56bb
Martin Pitt <mpitt@redhat.com>
2024-05-28 12:34:52 +0200

    fmf: Reorganize and rebalance tests
    The "basic" vs. "optional" package split was only a RHEL 8 thing, and
    was removed in commit 429b3c28. Also the test plans are very imbalanced:
    basic takes 14 mins, network only 4.5 mins, and optional about 30 mins.
    
    Reorganize them to split the storage tests in two plans
    (`storage-{basic,extra}`) and merge the others into a `main`.
    
    As we now have to enumerate test class names explicitly, this cleans up
    the `EXCLUDES` list quite a bit: We can drop the parts where we skipped
    all tests of a class, and instead just not mention the class in `TESTS`.

--- test/browser/browser.sh
+++ test/browser/browser.sh
@@ -4,7 +4,7 @@ set -eux
 
 cd "${0%/*}/../.."
 
-# like "basic", passed on to run-test.sh
+# like "storage-basic", passed on to run-test.sh
 PLAN="$1"
 
 # show some system info
@@ -23,7 +23,7 @@ if rpm -q setroubleshoot-server; then
     dnf remove -y --setopt=clean_requirements_on_remove=False setroubleshoot-server
 fi
 
-if grep -q 'ID=.*fedora' /etc/os-release && [ "$PLAN" = "basic" ]; then
+if grep -q 'ID=.*fedora' /etc/os-release && [ "$PLAN" = "main" ]; then
     # Fedora-only packages which are not available in CentOS/RHEL
     # required by TestLogin.testBasic
     dnf install -y tcsh
@@ -31,13 +31,13 @@ if grep -q 'ID=.*fedora' /etc/os-release && [ "$PLAN" = "basic" ]; then
     dnf install -y abrt abrt-addon-ccpp reportd libreport-plugin-bugzilla libreport-fedora
 fi
 
-if grep -q 'ID=.*fedora' /etc/os-release && [ "$PLAN" = "optional" ]; then
+if grep -q 'ID=.*fedora' /etc/os-release && [ "$PLAN" = "storage-basic" ]; then
     # required by TestStorageBtrfs*
     dnf install -y udisks2-btrfs
 fi
 
-# dnf installs "missing" weak dependencies, but we don't want them for plans other than "optional"
-if [ "$PLAN" != "optional" ] && rpm -q cockpit-packagekit; then
+# dnf installs "missing" weak dependencies, but we don't want them for plans other than "main"
+if [ "$PLAN" != "main" ] && rpm -q cockpit-packagekit; then
     dnf remove -y cockpit-packagekit
 fi
 
@@ -49,11 +49,8 @@ fi
 # HACK: RHEL has these bundled in cockpit-system, but Fedora doesn't; Provides: break in CentOS/RHEL 10
 # due to https://issues.redhat.com/browse/TFT-2564
 if ! grep -q platform:el /etc/os-release; then
-    if [ "$PLAN" = "basic" ]; then
+    if [ "$PLAN" = "main" ]; then
         dnf install -y cockpit-kdump cockpit-networkmanager cockpit-sosreport
-    fi
-    if [ "$PLAN" = "network" ]; then
-        dnf install -y cockpit-networkmanager
     fi
 fi
 
@@ -64,12 +61,6 @@ if [ -n "$main_builds_repo" ]; then
     echo 'priority=0' >> "$main_builds_repo"
     dnf distro-sync -y 'cockpit*'
 fi
-
-# RHEL 8 does not build cockpit-tests; when dropping RHEL 8 support, move to test/browser/main.fmf
-if [ "$PLAN" = basic ] && ! grep -q el8 /etc/os-release; then
-    dnf install -y cockpit-tests
-fi
-
 
 #HACK: unbreak RHEL 9's default choice of 999999999 rounds, see https://bugzilla.redhat.com/show_bug.cgi?id=1993919
 sed -ie 's/#SHA_CRYPT_MAX_ROUNDS 5000/SHA_CRYPT_MAX_ROUNDS 5000/' /etc/login.defs

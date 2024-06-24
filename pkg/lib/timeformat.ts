4f24e57681d0f079ea40f44841c67466f077c547
Martin Pitt <mpitt@redhat.com>
2024-06-24 22:35:30 +0200

    lib: Type-annotate timeformat
    Turn the optional `addSuffix` parameter of `distanceToNow()` into a
    proper boolean, to match `formatDistanceToNow()`'s signature.
    
    We have to apply an "any" crowbar for the "magic" locale import. But
    that's not exposed in the API, just an internal implementation detail.

--- pkg/lib/timeformat.ts
+++ pkg/lib/timeformat.ts
@@ -8,47 +8,56 @@ import { parse, formatDistanceToNow } from 'date-fns';
 import * as locales from 'date-fns/locale';
 
 // this needs to be dynamic, as some pages don't initialize cockpit.language right away
-export const dateFormatLang = () => cockpit.language.replace('_', '-');
+export const dateFormatLang = (): string => cockpit.language.replace('_', '-');
 
-const dateFormatLangDateFns = () => {
+const dateFormatLangDateFns = (): string => {
     if (cockpit.language == "en") return "enUS";
     else return cockpit.language.replace('_', '');
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const dateFnsLocale = (): any => (locales as any)[dateFormatLangDateFns()];
+
+type Time = Date | number;
+
 // general Intl.DateTimeFormat formatter object
-export const formatter = options => new Intl.DateTimeFormat(dateFormatLang(), options);
+export const formatter = (options?: Intl.DateTimeFormatOptions) => new Intl.DateTimeFormat(dateFormatLang(), options);
 
 // common formatters; try to use these as much as possible, for UI consistency
 // 07:41 AM
-export const time = t => formatter({ timeStyle: "short" }).format(t);
+export const time = (t: Time): string => formatter({ timeStyle: "short" }).format(t);
 // 7:41:26 AM
-export const timeSeconds = t => formatter({ timeStyle: "medium" }).format(t);
+export const timeSeconds = (t: Time): string => formatter({ timeStyle: "medium" }).format(t);
 // June 30, 2021
-export const date = t => formatter({ dateStyle: "long" }).format(t);
+export const date = (t: Time): string => formatter({ dateStyle: "long" }).format(t);
 // 06/30/2021
-export const dateShort = t => formatter().format(t);
+export const dateShort = (t: Time): string => formatter().format(t);
 // Jun 30, 2021, 7:41 AM
-export const dateTime = t => formatter({ dateStyle: "medium", timeStyle: "short" }).format(t);
+export const dateTime = (t: Time): string => formatter({ dateStyle: "medium", timeStyle: "short" }).format(t);
 // Jun 30, 2021, 7:41:23 AM
-export const dateTimeSeconds = t => formatter({ dateStyle: "medium", timeStyle: "medium" }).format(t);
+export const dateTimeSeconds = (t: Time): string => formatter({ dateStyle: "medium", timeStyle: "medium" }).format(t);
 // Jun 30, 7:41 AM
-export const dateTimeNoYear = t => formatter({ month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(t);
+export const dateTimeNoYear = (t: Time): string => formatter({ month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(t);
 // Wednesday, June 30, 2021
-export const weekdayDate = t => formatter({ dateStyle: "full" }).format(t);
+export const weekdayDate = (t: Time): string => formatter({ dateStyle: "full" }).format(t);
 
 // The following options are helpful for placeholders
 // yyyy/mm/dd
-export const dateShortFormat = () => locales[dateFormatLangDateFns()].formatLong.date({ width: 'short' });
+export const dateShortFormat = (): string => dateFnsLocale().formatLong.date({ width: 'short' });
 // about 1 hour [ago]
-export const distanceToNow = (t, addSuffix) => formatDistanceToNow(t, { locale: locales[dateFormatLangDateFns()], addSuffix });
+export const distanceToNow = (t: Time, addSuffix?: boolean): string => formatDistanceToNow(t, {
+    locale: dateFnsLocale(),
+    addSuffix: addSuffix ?? false
+});
 
 // Parse a string localized date like 30.06.21 to a Date Object
-export function parseShortDate(dateStr) {
+export function parseShortDate(dateStr: string): Date {
     const parsed = parse(dateStr, dateShortFormat(), new Date());
 
     // Strip time which may cause bugs in calendar
-    const timePortion = parsed.getTime() % (3600 * 1000 * 24);
-    return new Date(parsed - timePortion);
+    const time = parsed.getTime();
+    const timePortion = time % (3600 * 1000 * 24);
+    return new Date(time - timePortion);
 }
 
 /***
@@ -61,6 +70,6 @@ export function parseShortDate(dateStr) {
 
 const first_dow_sun = ['en', 'ja', 'ko', 'pt', 'pt_BR', 'sv', 'zh_CN', 'zh_TW'];
 
-export function firstDayOfWeek() {
+export function firstDayOfWeek(): number {
     return first_dow_sun.indexOf(cockpit.language) >= 0 ? 0 : 1;
 }

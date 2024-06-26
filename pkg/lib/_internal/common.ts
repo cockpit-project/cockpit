04aaba3a36a2e13e1bd6f43444253e29380b183f
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-06-26 16:06:28 +0200

    pkg/lib: rename common.js → common.ts

--- pkg/lib/_internal/common.ts
+++ pkg/lib/_internal/common.ts
@@ -1,3 +1,5 @@
+export type StrOrBytes = string | Uint8Array;
+
 /*
  * The debugging property is a global that is used
  * by various parts of the code to show/hide debug
@@ -17,7 +19,7 @@ if (typeof window.debugging === "undefined") {
     } catch (e) { }
 }
 
-export function in_array(array, val) {
+export function in_array(array: unknown[], val: unknown): boolean {
     const length = array.length;
     for (let i = 0; i < length; i++) {
         if (val === array[i])
@@ -26,19 +28,19 @@ export function in_array(array, val) {
     return false;
 }
 
-export function is_function(x) {
+export function is_function(x: unknown): x is (...args: unknown[]) => unknown {
     return typeof x === 'function';
 }
 
-export function is_object(x) {
+export function is_object(x: unknown): x is object {
     return x !== null && typeof x === 'object';
 }
 
-export function is_plain_object(x) {
+export function is_plain_object(x: unknown): boolean {
     return is_object(x) && Object.prototype.toString.call(x) === '[object Object]';
 }
 
-export function invoke_functions(functions, self, args) {
+export function invoke_functions<F extends (...args: never[]) => void>(functions: F[], self: ThisType<F>, args: Parameters<F>): void {
     const length = functions?.length ?? 0;
     for (let i = 0; i < length; i++) {
         if (functions[i])
@@ -46,7 +48,7 @@ export function invoke_functions(functions, self, args) {
     }
 }
 
-export function iterate_data(data, callback, batch = 64 * 1024) {
+export function iterate_data(data: StrOrBytes, callback: (chunk: StrOrBytes) => void, batch: number = 64 * 1024): void {
     if (typeof data === 'string') {
         for (let i = 0; i < data.length; i += batch) {
             callback(data.substring(i, i + batch));
@@ -59,7 +61,7 @@ export function iterate_data(data, callback, batch = 64 * 1024) {
     }
 }
 
-export function join_data(buffers, binary) {
+export function join_data(buffers: StrOrBytes[], binary: boolean): StrOrBytes {
     if (!binary)
         return buffers.join("");
 
@@ -70,7 +72,7 @@ export function join_data(buffers, binary) {
 
     const data = new Uint8Array(total);
     for (let j = 0, i = 0; i < length; i++) {
-        data.set(buffers[i], j);
+        data.set(buffers[i] as Uint8Array, j);
         j += buffers[i].length;
     }
 

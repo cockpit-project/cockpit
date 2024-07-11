e70f995a2345b4141248053680f47035161fdebf
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-07-11 12:19:35 -0400

    pkg/lib: move methods out of Transport.constructor
    These methods no longer depend on anything inside of the constructor()
    function and can be moved out.  This is safe because there are no call
    sites that use these functions as callbacks, so `this` will always be
    correctly bound.
    
    The methods formerly exported on the `self` object are turned into
    public methods and the others are turned into privates with `#`.
    
    The one tricky case here is `dispatch_data()` which is now a public
    method, but also used as the WebSocket `.onmessage` handler.  In that
    case we use an arrow to make sure `this` is properly provided:
    
        this.#ws.onmessage = event => this.dispatch_data(event);

--- pkg/lib/cockpit/_internal/transport.js
+++ pkg/lib/cockpit/_internal/transport.js
@@ -120,14 +120,6 @@ class Transport extends EventEmitter {
 
         this.ready = false;
 
-        /* Called when ready for channels to interact */
-        const ready_for_channels = () => {
-            if (!this.ready) {
-                this.ready = true;
-                this.emit("ready");
-            }
-        };
-
         this.#ws.onopen = () => {
             if (this.#ws) {
                 if (typeof this.#ws.binaryType !== "undefined")
@@ -146,200 +138,210 @@ class Transport extends EventEmitter {
             this.close();
         };
 
-        this.#ws.onmessage = this.dispatch_data = (arg) => {
-            this.#got_message = true;
+        this.#ws.onmessage = event => this.dispatch_data(event);
+    }
 
-            /* The first line of a message is the channel */
-            const message = arg.data;
+    /* Called when ready for channels to interact */
+    #ready_for_channels() {
+        if (!this.ready) {
+            this.ready = true;
+            this.emit("ready");
+        }
+    }
 
-            const channel = parse_channel(message);
-            if (channel === null)
-                return false;
+    #process_init(options) {
+        if (options.problem) {
+            this.close({ problem: options.problem });
+            return;
+        }
 
-            const payload = message instanceof window.ArrayBuffer
-                ? new window.Uint8Array(message, channel.length + 1)
-                : message.substring(channel.length + 1);
-            let control;
+        if (options.version !== 1) {
+            console.error("received unsupported version in init message: " + options.version);
+            this.close({ problem: "not-supported" });
+            return;
+        }
 
-            /* A control message, always string */
-            if (!channel) {
-                transport_debug("recv control:", payload);
-                control = JSON.parse(payload);
-            } else {
-                transport_debug("recv " + channel + ":", payload);
+        if (options["channel-seed"])
+            this.#channel_seed = String(options["channel-seed"]);
+        if (options.host)
+            transport_globals.default_host = options.host;
+
+        if (transport_globals.public_transport) {
+            transport_globals.public_transport.options = options;
+            transport_globals.public_transport.csrf_token = options["csrf-token"];
+            transport_globals.public_transport.host = transport_globals.default_host;
+        }
+
+        if (transport_globals.init_callback)
+            transport_globals.init_callback(options);
+
+        if (this.#waiting_for_init) {
+            this.#waiting_for_init = false;
+            this.#ready_for_channels();
+        }
+    }
+
+    #process_control(data) {
+        const channel = data.channel;
+
+        /* Init message received */
+        if (data.command == "init") {
+            this.#process_init(data);
+        } else if (this.#waiting_for_init) {
+            this.#waiting_for_init = false;
+            if (data.command != "close" || channel) {
+                console.error("received message before init: ", data.command);
+                data = { problem: "protocol-error" };
             }
+            this.close(data);
 
-            const length = transport_globals.incoming_filters ? transport_globals.incoming_filters.length : 0;
-            for (let i = 0; i < length; i++) {
-                if (transport_globals.incoming_filters[i](message, channel, control) === false)
-                    return false;
-            }
-
-            if (!channel)
-                process_control(control);
-
-            else
-                process_message(channel, payload);
-
-            return true;
-        };
-
-        this.close = (options) => {
-            if (!options)
-                options = { problem: "disconnected" };
-            options.command = "close";
-            window.clearInterval(this.#check_health_timer);
-            const ows = this.#ws;
-            this.#ws = null;
-            if (ows)
-                ows.close();
-            if (transport_globals.expect_disconnect)
-                return;
-            ready_for_channels(); /* ready to fail */
-
-            /* Broadcast to everyone */
-            for (const chan in this.#control_cbs)
-                this.#control_cbs[chan].apply(null, [options]);
-        };
-
-        this.next_channel = () => {
-            this.#last_channel++;
-            return this.#channel_seed + String(this.#last_channel);
-        };
-
-        const process_init = (options) => {
-            if (options.problem) {
-                this.close({ problem: options.problem });
-                return;
-            }
-
-            if (options.version !== 1) {
-                console.error("received unsupported version in init message: " + options.version);
-                this.close({ problem: "not-supported" });
-                return;
-            }
-
-            if (options["channel-seed"])
-                this.#channel_seed = String(options["channel-seed"]);
-            if (options.host)
-                transport_globals.default_host = options.host;
-
-            if (transport_globals.public_transport) {
-                transport_globals.public_transport.options = options;
-                transport_globals.public_transport.csrf_token = options["csrf-token"];
-                transport_globals.public_transport.host = transport_globals.default_host;
-            }
-
-            if (transport_globals.init_callback)
-                transport_globals.init_callback(options);
-
-            if (this.#waiting_for_init) {
-                this.#waiting_for_init = false;
-                ready_for_channels();
-            }
-        };
-
-        const process_control = (data) => {
-            const channel = data.channel;
-
-            /* Init message received */
-            if (data.command == "init") {
-                process_init(data);
-            } else if (this.#waiting_for_init) {
-                this.#waiting_for_init = false;
-                if (data.command != "close" || channel) {
-                    console.error("received message before init: ", data.command);
-                    data = { problem: "protocol-error" };
-                }
-                this.close(data);
-
-                /* Any pings get sent back as pongs */
-            } else if (data.command == "ping") {
-                data.command = "pong";
-                this.send_control(data);
-            } else if (data.command == "pong") {
-                /* Any pong commands are ignored */
-            } else if (data.command == "hint") {
-                if (transport_globals.process_hints)
-                    transport_globals.process_hints(data);
-            } else if (channel !== undefined) {
-                const func = this.#control_cbs[channel];
-                if (func)
-                    func(data);
-            }
-        };
-
-        const process_message = (channel, payload) => {
-            const func = this.#message_cbs[channel];
+            /* Any pings get sent back as pongs */
+        } else if (data.command == "ping") {
+            data.command = "pong";
+            this.send_control(data);
+        } else if (data.command == "pong") {
+            /* Any pong commands are ignored */
+        } else if (data.command == "hint") {
+            if (transport_globals.process_hints)
+                transport_globals.process_hints(data);
+        } else if (channel !== undefined) {
+            const func = this.#control_cbs[channel];
             if (func)
-                func(payload);
-        };
+                func(data);
+        }
+    }
 
-        /* The channel/control arguments is used by filters, and auto-populated if necessary */
-        this.send_data = (data, channel, control) => {
-            if (!this.#ws) {
+    #process_message(channel, payload) {
+        const func = this.#message_cbs[channel];
+        if (func)
+            func(payload);
+    }
+
+    dispatch_data(arg) {
+        this.#got_message = true;
+
+        /* The first line of a message is the channel */
+        const message = arg.data;
+
+        const channel = parse_channel(message);
+        if (channel === null)
+            return false;
+
+        const payload = message instanceof window.ArrayBuffer
+            ? new window.Uint8Array(message, channel.length + 1)
+            : message.substring(channel.length + 1);
+        let control;
+
+        /* A control message, always string */
+        if (!channel) {
+            transport_debug("recv control:", payload);
+            control = JSON.parse(payload);
+        } else {
+            transport_debug("recv " + channel + ":", payload);
+        }
+
+        const length = transport_globals.incoming_filters ? transport_globals.incoming_filters.length : 0;
+        for (let i = 0; i < length; i++) {
+            if (transport_globals.incoming_filters[i](message, channel, control) === false)
                 return false;
-            }
+        }
 
-            const length = transport_globals.outgoing_filters ? transport_globals.outgoing_filters.length : 0;
-            for (let i = 0; i < length; i++) {
-                if (channel === undefined)
-                    channel = parse_channel(data);
-                if (!channel && control === undefined)
-                    control = JSON.parse(data);
-                if (transport_globals.outgoing_filters[i](data, channel, control) === false)
-                    return false;
-            }
+        if (!channel)
+            this.#process_control(control);
 
-            this.#ws.send(data);
-            return true;
-        };
+        else
+            this.#process_message(channel, payload);
 
-        /* The control arguments is used by filters, and auto populated if necessary */
-        this.send_message = (payload, channel, control) => {
-            if (channel)
-                transport_debug("send " + channel, payload);
+        return true;
+    }
 
-            else
-                transport_debug("send control:", payload);
+    close(options) {
+        if (!options)
+            options = { problem: "disconnected" };
+        options.command = "close";
+        window.clearInterval(this.#check_health_timer);
+        const ows = this.#ws;
+        this.#ws = null;
+        if (ows)
+            ows.close();
+        if (transport_globals.expect_disconnect)
+            return;
+        this.#ready_for_channels(); /* ready to fail */
 
-            if (typeof payload !== 'string') {
-                /* A binary message */
-                const body = payload instanceof ArrayBuffer ? new Uint8Array(payload) : payload;
+        /* Broadcast to everyone */
+        for (const chan in this.#control_cbs)
+            this.#control_cbs[chan].apply(null, [options]);
+    }
 
-                // We want to create channel + '\n' + body in binary
-                const header = new TextEncoder().encode(`${channel}\n`);
-                const output = new Uint8Array(header.length + body.length);
-                output.set(header);
-                output.set(body, header.length);
-                return this.send_data(output.buffer, channel, control);
-            } else {
-                /* A string message */
-                return this.send_data(channel.toString() + "\n" + payload, channel, control);
-            }
-        };
+    next_channel() {
+        this.#last_channel++;
+        return this.#channel_seed + String(this.#last_channel);
+    }
 
-        this.send_control = (data) => {
-            if (!this.#ws && (data.command == "close" || data.command == "kill"))
-                return; /* don't complain if closed and closing */
-            if (this.#check_health_timer &&
-                data.command == "hint" && data.hint == "ignore_transport_health_check") {
-                /* This is for us, process it directly. */
-                this.#ignore_health_check = data.data;
-                return;
-            }
-            return this.send_message(JSON.stringify(data), "", data);
-        };
+    /* The channel/control arguments is used by filters, and auto-populated if necessary */
+    send_data(data, channel, control) {
+        if (!this.#ws) {
+            return false;
+        }
 
-        this.register = (channel, control_cb, message_cb) => {
-            this.#control_cbs[channel] = control_cb;
-            this.#message_cbs[channel] = message_cb;
-        };
+        const length = transport_globals.outgoing_filters ? transport_globals.outgoing_filters.length : 0;
+        for (let i = 0; i < length; i++) {
+            if (channel === undefined)
+                channel = parse_channel(data);
+            if (!channel && control === undefined)
+                control = JSON.parse(data);
+            if (transport_globals.outgoing_filters[i](data, channel, control) === false)
+                return false;
+        }
 
-        this.unregister = (channel) => {
-            delete this.#control_cbs[channel];
-            delete this.#message_cbs[channel];
-        };
+        this.#ws.send(data);
+        return true;
+    }
+
+    /* The control arguments is used by filters, and auto populated if necessary */
+    send_message(payload, channel, control) {
+        if (channel)
+            transport_debug("send " + channel, payload);
+
+        else
+            transport_debug("send control:", payload);
+
+        if (typeof payload !== 'string') {
+            /* A binary message */
+            const body = payload instanceof ArrayBuffer ? new Uint8Array(payload) : payload;
+
+            // We want to create channel + '\n' + body in binary
+            const header = new TextEncoder().encode(`${channel}\n`);
+            const output = new Uint8Array(header.length + body.length);
+            output.set(header);
+            output.set(body, header.length);
+            return this.send_data(output.buffer, channel, control);
+        } else {
+            /* A string message */
+            return this.send_data(channel.toString() + "\n" + payload, channel, control);
+        }
+    }
+
+    send_control(data) {
+        if (!this.#ws && (data.command == "close" || data.command == "kill"))
+            return; /* don't complain if closed and closing */
+        if (this.#check_health_timer &&
+            data.command == "hint" && data.hint == "ignore_transport_health_check") {
+            /* This is for us, process it directly. */
+            this.#ignore_health_check = data.data;
+            return;
+        }
+        return this.send_message(JSON.stringify(data), "", data);
+    }
+
+    register(channel, control_cb, message_cb) {
+        this.#control_cbs[channel] = control_cb;
+        this.#message_cbs[channel] = message_cb;
+    }
+
+    unregister(channel) {
+        delete this.#control_cbs[channel];
+        delete this.#message_cbs[channel];
     }
 }
 

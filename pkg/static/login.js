d5b6ca17370d24d2d549806938de3ba67efffae0
Marius Vollmer <marius.vollmer@gmail.com>
2024-08-09 11:18:41 +0300

    login: Warn before logging into multiple hosts

--- pkg/static/login.js
+++ pkg/static/login.js
@@ -354,8 +354,16 @@ import "./login.scss";
                 login_reload("/=" + cur_machine);
         }
 
-        if (cur_machine && !environment.page.allow_multihost)
-            redirect_to_current_machine();
+        if (cur_machine) {
+            if (!environment.page.allow_multihost)
+                redirect_to_current_machine();
+            else {
+                id("multihost-message").textContent = format(_("You are already connected to '$0' in this browser session. Connecting to other hosts will allow them to execute arbitrary code on each other. Please be careful."),
+                                                             cur_machine == "." ? "localhost" : cur_machine);
+                id("multihost-get-me-there").addEventListener("click", redirect_to_current_machine);
+                show('#multihost-warning');
+            }
+        }
     }
 
     function boot() {

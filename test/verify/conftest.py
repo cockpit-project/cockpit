41dea51f1927e32c600fc394cf743e193499ea78
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-08-14 11:32:18 +0200

    pyproject.toml: teach pytest about testlib paths
    Doing this at the toplevel lets us drop some hacks for adding those
    paths for our verify/ test cases.  Plus: we'll soon be using test/common
    from outside of verify/, so the logic of wanting to isolate it to that
    subdirectory no longer exists.
    
    mypy already knows about it, but for the sake of LSP users, let pyright
    know about it too.

--- test/verify/conftest.py
+++ test/verify/conftest.py
@@ -1,28 +1,14 @@
 import importlib.machinery
 import importlib.util
-import os
-import sys
 from pathlib import Path
 from typing import List, Optional
 
 import pytest
 
-TOPDIR = os.path.realpath(__file__ + '../../../..')
-COMMON_PATH = [
-    f'{TOPDIR}/bots',
-    f'{TOPDIR}/bots/machine',
-    f'{TOPDIR}/test/common',
-]
-
 
 @pytest.hookimpl
 def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> Optional[pytest.Collector]:
-    """Support for loading our check-* scripts as if they were modules
-
-    Also adds our testlib paths to sys.path.  Doesn't check out the bots
-    because it's not possible to run tests without first running
-    test/image-prepare, and that will check out the bots automatically.
-    """
+    """Support for loading our check-* scripts as if they were modules."""
     if file_path.name.startswith('check-'):
         # Pretend that test/verify/check-name is called like test.verify.check_name
         modname = 'test.verify.' + file_path.name.replace('-', '_')
@@ -31,12 +17,7 @@ def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> Optional[p
         assert spec is not None
         module = importlib.util.module_from_spec(spec)
 
-        old_path = sys.path
-        try:
-            sys.path = [*COMMON_PATH, *sys.path]
-            loader.exec_module(module)
-        finally:
-            sys.path = old_path
+        loader.exec_module(module)
 
         # Return the tree node with our module pre-loaded inside of it
         collector = pytest.Module.from_parent(parent, path=file_path)

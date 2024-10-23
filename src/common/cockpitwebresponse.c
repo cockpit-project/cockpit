cockpit-project/cockpit

commit 7c22f854d11c9821f7d9dacad2394d5d91681f28

common: add cockpit_web_response_error_with_body()
Add a variant of this function which accepts a body to return to the
client instead of the usual error page.  We take this approach instead
of adding a parameter to the existing function in order to avoid needing
to modify the (very) many call sites.

src/common/cockpitwebresponse.c
@@ -1084,19 +1084,46 @@ cockpit_web_response_error (CockpitWebResponse *self,
                             const gchar *format,
                             ...)
 {
-  va_list var_args;
   g_autofree gchar *reason = NULL;
+  va_list ap;
+
+  if (format) {
+    va_start (ap, format);
+    reason = g_strdup_vprintf (format, ap);
+    va_end (ap);
+  }
+
+  cockpit_web_response_error_with_body (self, code, reason, headers, NULL);
+}
+
+
+/**
+ * cockpit_web_response_error_with_Body:
+ * @self: the response
+ * @status: the HTTP status code
+ * @reason: the HTTP status next (or NULL to choose one by @code)
+ * @headers: headers to include or NULL
+ *
+ * Send an error response with an optional body.  If no body is
+ * provided, sends basic HTML page containing the error.
+ */
+void
+cockpit_web_response_error_with_body (CockpitWebResponse *self,
+                                      guint code,
+                                      const gchar *reason,
+                                      GHashTable *headers,
+                                      GBytes *body)
+{
   g_autofree gchar *escaped = NULL;
   const gchar *message;
 
   g_return_if_fail (COCKPIT_IS_WEB_RESPONSE (self));
 
-  if (format)
+  if (reason)
     {
-      va_start (var_args, format);
-      reason = g_strdup_vprintf (format, var_args);
-      va_end (var_args);
-      message = reason;
+      /* If sending arbitrary messages, make sure they're escaped */
+      escaped = g_uri_escape_string (reason, " :", FALSE);
+      message = escaped;
     }
   else
     {
@@ -1142,14 +1169,6 @@ cockpit_web_response_error (CockpitWebResponse *self,
 
   g_debug ("%s: returning error: %u %s", self->logname, code, message);
 
-  /* If sending arbitrary messages, make sure they're escaped */
-  if (reason)
-    {
-      g_strstrip (reason);
-      escaped = g_uri_escape_string (reason, " :", FALSE);
-      message = escaped;
-    }
-
   if (headers)
     {
       if (!g_hash_table_lookup (headers, "Content-Type"))
@@ -1163,15 +1182,24 @@ cockpit_web_response_error (CockpitWebResponse *self,
 
   if (!g_str_equal (self->method, "HEAD"))
     {
-      extern const char *cockpit_webresponse_fail_html_text;
-      g_autoptr(GBytes) input = g_bytes_new_static (cockpit_webresponse_fail_html_text, strlen (cockpit_webresponse_fail_html_text));
-      g_autolist(GBytes) output = cockpit_template_expand (input, "@@", "@@", substitute_message, (gpointer) message);
-
-      for (GList *l = output; l != NULL; l = g_list_next (l))
+      if (body != NULL)
         {
-          if (!cockpit_web_response_queue (self, l->data))
-            /* error: early exit */
-            return;
+          // If the caller had content, send it...
+          cockpit_web_response_queue (self, body);
+        }
+      else
+        {
+          // Otherwise, show our fail.html page...
+          extern const char *cockpit_webresponse_fail_html_text;
+          g_autoptr(GBytes) input = g_bytes_new_static (cockpit_webresponse_fail_html_text, strlen (cockpit_webresponse_fail_html_text));
+          g_autolist(GBytes) output = cockpit_template_expand (input, "@@", "@@", substitute_message, (gpointer) message);
+
+          for (GList *l = output; l != NULL; l = g_list_next (l))
+            {
+              if (!cockpit_web_response_queue (self, l->data))
+                /* error: early exit */
+                return;
+            }
         }
     }
 

src/common/cockpitwebresponse.h
@@ -95,6 +95,11 @@ void                  cockpit_web_response_content       (CockpitWebResponse *se
                                                           GBytes *block,
                                                           ...) G_GNUC_NULL_TERMINATED;
 
+void                  cockpit_web_response_error_with_body (CockpitWebResponse *self,
+                                                            guint code,
+                                                            const gchar *reason,
+                                                            GHashTable *headers,
+                                                            GBytes *body);
 void                  cockpit_web_response_error         (CockpitWebResponse *self,
                                                           guint status,
                                                           GHashTable *headers,

commit 1f6b22226a60f87073abe282527a6cef27fd9f0b

common: Fix memory leak in cockpit_web_response_error_with_body()
Commit 7c22f854d11c9821f7d introduced a resource leak:
cockpit_web_response_error_with_body() now receives a constant "reason"
as argument, but in some cases shadows that with a locally allocated
string. The latter never got freed.

Rename the variable and ensure it gets cleaned up at the end of the
function.

Spotted by Coverity.

src/common/cockpitwebresponse.c
@@ -1115,6 +1115,7 @@ cockpit_web_response_error_with_body (CockpitWebResponse *self,
                                       GBytes *body)
 {
   g_autofree gchar *escaped = NULL;
+  g_autofree gchar *reason_local = NULL;
   const gchar *message;
 
   g_return_if_fail (COCKPIT_IS_WEB_RESPONSE (self));
@@ -1155,14 +1156,14 @@ cockpit_web_response_error_with_body (CockpitWebResponse *self,
           break;
         default:
           if (code < 100)
-            reason = g_strdup_printf ("%u Continue", code);
+            reason_local = g_strdup_printf ("%u Continue", code);
           else if (code < 200)
-            reason = g_strdup_printf ("%u OK", code);
+            reason_local = g_strdup_printf ("%u OK", code);
           else if (code < 300)
-            reason = g_strdup_printf ("%u Moved", code);
+            reason_local = g_strdup_printf ("%u Moved", code);
           else
-            reason = g_strdup_printf ("%u Failed", code);
-          message = reason;
+            reason_local = g_strdup_printf ("%u Failed", code);
+          message = reason_local;
           break;
         }
     }
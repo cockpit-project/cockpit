cdf376ff6a02120d8213378a27ed343cabd2ac77
Marius Vollmer <marius.vollmer@gmail.com>
2024-10-30 17:03:27 +0200

    DBG - play with React keys

--- pkg/playground/react-demo-cards.jsx
+++ pkg/playground/react-demo-cards.jsx
@@ -17,7 +17,7 @@
  * along with Cockpit; If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from 'react-dom/client';
 
 import { Button } from "@patternfly/react-core/dist/esm/components/Button/index.js";
@@ -25,39 +25,31 @@ import { Card, CardBody, CardFooter, CardHeader, CardTitle } from '@patternfly/r
 import { Gallery, GalleryItem } from "@patternfly/react-core/dist/esm/layouts/Gallery/index.js";
 
 const CardsDemo = () => {
-    const cards = [
-        <Card isCompact key="card1">
-            <CardBody>I'm a card in a gallery</CardBody>
-        </Card>,
-        <Card isCompact key="card2">
-            <CardBody>I'm a card in a gallery</CardBody>
-            <CardFooter>I have a footer</CardFooter>
-        </Card>,
-        <Card isCompact key="card3">
-            <CardBody>I'm a card in a gallery</CardBody>
-        </Card>,
-        <Card isCompact key="card4">
-            <CardTitle>I have a header too</CardTitle>
-            <CardBody>I'm a card in a gallery</CardBody>
-        </Card>,
-        <Card key="card5">
-            <CardHeader actions={{
-                actions: <><input type="checkbox" />
-                    <Button className="btn">click</Button></>,
-            }} />
-            <CardTitle>This is a card header</CardTitle>
-            <CardBody>I'm a card in a gallery</CardBody>
-        </Card>,
-        <GalleryItem key="card6">
-            I'm not a card, but I'm in the gallery too, as a generic GalleryItem.
-        </GalleryItem>,
+    const [toggle, setToggle ] = useState(true);
+
+    const array_1 = [
+        <div key="1">
+            {"I'm just a div."}
+        </div>,
+        <p key="2">
+            {"I'm just a paragraph."}
+        </p>,
     ];
+
+    const array_2 = [
+        <p key="1">
+            {"I'm just a paragraph."}
+        </p>,
+        <div key="2">
+            {"I'm just a div."}
+        </div>,
+    ];
+
     return (
         <>
             <h4>PF4 cards arranged using a PF4 Gallery</h4>
-            <Gallery hasGutter>
-                { cards }
-            </Gallery>
+            <Button onClick={() => setToggle(!toggle)}>Toggle</Button>
+            { toggle ? array_1 : array_2  }
         </>
     );
 };

8dae54e0b8eb4a5616d67eaed6e1c6d88a496fdf
Martin Pitt <mpitt@redhat.com>
2024-10-31 11:28:55 +0100

    lib: Initial typing of python.js
    This is incomplete, but as we don't currently use binary python.spawn()
    anywhere, it's better than nothing.

--- pkg/lib/python.ts
+++ pkg/lib/python.ts
@@ -21,10 +21,15 @@ import cockpit from "cockpit";
 
 const pyinvoke = ["sh", "-ec", "exec $(command -v /usr/libexec/platform-python || command -v python3) -c \"$@\"", "--"];
 
-export function spawn (script_pieces, args, options) {
+// only declare the string variant for the time being; we don't use the binary variant
+export function spawn (
+    script_pieces: string | string[],
+    args?: string[],
+    options?: cockpit.SpawnOptions & { binary?: false; }
+): cockpit.Spawn<string> {
     const script = (typeof script_pieces == "string")
         ? script_pieces
         : script_pieces.join("\n");
 
-    return cockpit.spawn(pyinvoke.concat([script]).concat(args), options);
+    return cockpit.spawn(pyinvoke.concat([script]).concat(args ?? []), options);
 }

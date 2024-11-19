ecae4c2d49ae5d370d95d7aae4410a3699834a90
Martin Pitt <mpitt@redhat.com>
2024-11-19 15:00:21 +0100

    tls: Fix code reference

--- src/tls/client-certificate.c
+++ src/tls/client-certificate.c
@@ -304,7 +304,7 @@ client_certificate_create_tmpfile (int  dirfd,
  * referenced by @dirfd.  This file will contain the expected cgroup of
  * the cockpit-ws instance in question, plus the client certificate.
  * That data is interpreted by the counterpart to this code, living in
- * src/ws/cockpit-session-client-certificate.c
+ * src/session/client-certificate.c.
  *
  * In any case, %true will be returned in case of success, and %false
  * will be returned in case of an error.  In case of success, any values

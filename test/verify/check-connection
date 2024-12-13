cockpit-project/cockpit

commit 5229d60a844d28bc120d28a382f3e85463fe79de

tls: force write certificates to disk
On embedded devices such as a RPI storage is more fragile and power loss
can happen more frequently.

Co-Authored-By: Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>

src/tls/cockpit-certificate-helper.in
@@ -14,11 +14,17 @@ install_cert() {
 
     # The certificate should be world-readable
     chmod a+r "${destination}"
+
+    # Force flush to disk for embedded devices
+    sync "${destination}"
 }
 
 install_key() {
     local destination="${COCKPIT_WS_CERTS_D}/$1"
     mv -Z "$1" "${destination}"
+
+    # Force flush to disk for embedded devices
+    sync "${destination}"
 }
 
 selfsign_sscg() {

commit f61391fc469ae0cb5f9af7da01273d1489ec22e2

test: Check error reporting with missing or broken TLS key
Since commit 5229d60a844 we expect our certificate generation to be
reasonably atomic. If the certificate is broken, we want to know, so
let's make sure that we get a proper error message.

test/verify/check-connection
@@ -403,6 +403,23 @@ class TestConnection(testlib.MachineCase):
         self.assertRegex(
             output, r"no cipher match|no ciphers available|ssl handshake failure|Cipher is \(NONE\)")
 
+        # fails with useful error message with broken certs/keys
+        if not m.ostree_image:  # not using systemd unit
+            def check_fail(expected: list[str]):
+                m.start_cockpit(tls=True)
+                m.execute('! openssl s_client -connect 172.27.0.15:9090 2>&1')
+                out = m.execute("! systemctl status cockpit.service")
+                self.assertIn("failed", out)
+                for s in expected:
+                    self.assertIn(s, out)
+
+            # missing key
+            m.execute("rm /etc/cockpit/ws-certs.d/0-self-signed.key")
+            check_fail(["0-self-signed.key", "No such file or directory"])
+            # broken key
+            m.write("/etc/cockpit/ws-certs.d/0-self-signed.key", "-----BEGIN PRIVATE KEY-----\nnope")
+            check_fail(["/etc/cockpit/ws-certs.d/0-self-signed.cert/.key", "decoding error"])
+
         # gets along with missing directory
         m.execute("rm -r /etc/cockpit/ws-certs.d")
         m.start_cockpit(tls=True)
7ebd5992f4bb7aa68f2ef3877360d0a3f4a253a2
Martin Pitt <mpitt@redhat.com>
2024-12-18 12:05:10 +0100

    containers/ws: Fix build on Fedora 41
    dnf5 changed `--installroot` behaviour that it now only considers the
    repositories in the root instead of the host. This broke the container
    build.
    
    Unfortunately my fix for that [1] got lost in a git pilot failure, mea
    culpa!
    
    [1] https://github.com/cockpit-project/cockpit/pull/21360#issuecomment-2511242349

--- containers/ws/install.sh
+++ containers/ws/install.sh
@@ -4,7 +4,7 @@ set -ex
 
 OSVER=$(. /etc/os-release && echo "$VERSION_ID")
 INSTALLROOT=/build
-INSTALL="dnf install -y --installroot=$INSTALLROOT --releasever=$OSVER --setopt=install_weak_deps=False"
+INSTALL="dnf install -y --installroot=$INSTALLROOT --releasever=$OSVER --setopt=install_weak_deps=False --use-host-config"
 
 # keep in sync with test/ws-container.install
 PACKAGES="

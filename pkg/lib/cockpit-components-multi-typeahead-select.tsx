cockpit-project/cockpit

commit 8d9f97b2c9ea71716535d681ebda02f41db701b4

lib: Support headers with TypeaheadSelect
This also changes the way dividers are encoded, so that TypeScript can
better understand the option variants.

pkg/lib/cockpit-components-typeahead-select.tsx
@@ -57,9 +57,24 @@ SOFTWARE.
 
        [
          ...
-         { divider: true },
+         { decorator: "divider", key: "..." },
          ...
        ]
+
+   - Allow headers.
+
+       [
+         ...
+         { decorator: "header", content: _("Nice things"), key: "..." }
+         { value: "icecream", content: _("Icecream") },
+         ...
+       ]
+
+     Note that PatternFly uses SelectGroup and MenuGroup instead of
+     headers, but their recursive nature makes them harder to
+     implement here, mostly because of how keyboard navigation is
+     done. And there is no visual nesting going on anyway. Keeping the
+     options a flat list is just all around easier.
 
 */
 
@@ -83,19 +98,37 @@ import {
   SelectProps
 } from '@patternfly/react-core';
 import TimesIcon from '@patternfly/react-icons/dist/esm/icons/times-icon';
+import "cockpit-components-typeahead-select.scss";
 
 const _ = cockpit.gettext;
 
-export interface TypeaheadSelectOption extends Omit<SelectOptionProps, 'content' | 'isSelected'> {
+export interface TypeaheadSelectDividerOption {
+  decorator: "divider";
+
+  key: string | number;
+};
+
+export interface TypeaheadSelectHeaderOption {
+  decorator: "header";
+
+  content: string | number;
+  key: string | number;
+};
+
+export interface TypeaheadSelectMenuOption extends Omit<SelectOptionProps, 'content' | 'isSelected'> {
+  decorator?: undefined;
+
   /** Content of the select option. */
   content: string | number;
   /** Value of the select option. */
   value: string | number;
   /** Indicator for option being selected */
   isSelected?: boolean;
-  /** Is this just a divider */
-  divider?: boolean;
 }
+
+export type TypeaheadSelectOption = TypeaheadSelectMenuOption |
+                                    TypeaheadSelectDividerOption |
+                                    TypeaheadSelectHeaderOption;
 
 export interface TypeaheadSelectProps extends Omit<SelectProps, 'toggle' | 'onSelect'> {
   /** @hide Forwarded ref */
@@ -137,8 +170,27 @@ export interface TypeaheadSelectProps extends Omit<SelectProps, 'toggle' | 'onSe
   toggleProps?: MenuToggleProps;
 }
 
-const defaultFilterFunction = (filterValue: string, options: TypeaheadSelectOption[]) =>
-  options.filter((o) => String(o.content).toLowerCase().includes(filterValue.toLowerCase()));
+const defaultFilterFunction = (filterValue: string, options: TypeaheadSelectOption[]) => {
+    // Filter by search term, keep headers and dividers
+    const filtered = options.filter((o) => {
+        return o.decorator || String(o.content).toLowerCase().includes(filterValue.toLowerCase());
+    });
+
+    //  Remove headers that have nothing following them, and dividers that have nothing in front of them.
+    const filtered2 = filtered.filter((o, i) => {
+        if (o.decorator == "header" && (i >= filtered.length - 1 || filtered[i + 1].decorator))
+            return false;
+        if (o.decorator == "divider" && (i <= 0 || filtered[i - 1].decorator))
+            return false;
+        return true;
+    });
+
+    // If the last item is now a divider, remove it as well.
+    if (filtered2.length > 0 && filtered2[filtered2.length-1].decorator == "divider")
+        filtered2.pop();
+
+    return filtered2;
+};
 
 export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> = ({
   innerRef,
@@ -175,9 +227,14 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
   if (isCreatable)
     selectedIsTrusted = true;
 
+  const isMenu = (o: TypeaheadSelectOption): o is TypeaheadSelectMenuOption => !o.decorator;
+  const isEnabledMenu = (o: TypeaheadSelectOption): o is TypeaheadSelectMenuOption => !(o.decorator || o.isDisabled);
+
   const selected = React.useMemo(
     () => {
-       let res = selectOptions?.find((option) => option.value === props.selected || option.isSelected);
+       let res = selectOptions?.find((o): o is TypeaheadSelectMenuOption =>
+                                     (isEnabledMenu(o) &&
+                                      (o.value === props.selected || !!o.isSelected)));
        if (!res && props.selected && selectedIsTrusted)
          res = { value: props.selected, content: props.selected };
        return res;
@@ -195,7 +252,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
       if (
         isCreatable &&
         filterValue.trim() &&
-        !newSelectOptions.find((o) => String(o.content).toLowerCase() === filterValue.toLowerCase())
+        !newSelectOptions.find((o) => isMenu(o) && String(o.content).toLowerCase() === filterValue.toLowerCase())
       ) {
         const createOption = {
           content: typeof createOptionMessage === 'string' ? createOptionMessage : createOptionMessage(filterValue),
@@ -253,7 +310,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
 
   const setActiveAndFocusedItem = (itemIndex: number) => {
     setFocusedItemIndex(itemIndex);
-    const focusedItem = selectOptions[itemIndex];
+    const focusedItem = selectOptions[itemIndex] as TypeaheadSelectMenuOption;
     setActiveItemId(String(focusedItem.value));
   };
 
@@ -290,7 +347,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
 
   const selectOption = (
     _event: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<HTMLInputElement> | undefined,
-    option: TypeaheadSelectOption
+    option: TypeaheadSelectMenuOption
   ) => {
     onSelect && onSelect(_event, option.value);
     closeMenu();
@@ -298,7 +355,8 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
 
   const _onSelect = (_event: React.MouseEvent<Element, MouseEvent> | undefined, value: string | number | undefined) => {
     if (value && value !== NO_RESULTS) {
-      const optionToSelect = selectOptions.find((option) => option.value === value);
+        const optionToSelect = selectOptions.find(
+            (option): option is TypeaheadSelectMenuOption => isMenu(option) && option.value === value);
       if (optionToSelect) {
         selectOption(_event, optionToSelect);
       } else if (isCreatable) {
@@ -320,9 +378,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
 
     openMenu();
 
-    const ignore = o => o.isDisabled || o.divider;
-
-    if (filteredSelections.every(ignore)) {
+    if (filteredSelections.every(o => !isMenu(o))) {
       return;
     }
 
@@ -335,7 +391,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
       }
 
       // Skip non-items
-      while (ignore(filteredSelections[indexToFocus])) {
+      while (!isEnabledMenu(filteredSelections[indexToFocus])) {
         indexToFocus--;
         if (indexToFocus === -1) {
           indexToFocus = filteredSelections.length - 1;
@@ -352,7 +408,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
       }
 
       // Skip non-items
-      while (ignore(filteredSelections[indexToFocus])) {
+      while (!isEnabledMenu(filteredSelections[indexToFocus])) {
         indexToFocus++;
         if (indexToFocus === filteredSelections.length) {
           indexToFocus = 0;
@@ -364,7 +420,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
   };
 
   const onInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const focusedItem = focusedItemIndex !== null ? filteredSelections[focusedItemIndex] : null;
+    const focusedItem = (focusedItemIndex !== null ? filteredSelections[focusedItemIndex] : null) as TypeaheadSelectMenuOption | null;
 
     switch (event.key) {
       case 'Enter':
@@ -458,8 +514,18 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
     >
       <SelectList>
         {filteredSelections.map((option, index) => {
-          if (option.divider)
-              return <Divider key={option.key || index} component="li" />;
+          if (option.decorator == "divider")
+              return <Divider key={option.key} component="li" />;
+
+          if (option.decorator == "header") {
+              return (
+                  <SelectOption key={option.key}
+                                isDisabled
+                                className="ct-typeahead-header">
+                      {option.content}
+                  </SelectOption>
+              );
+          }
 
           const { content, value, ...props } = option;
           return (

pkg/lib/cockpit-components-typeahead-select.scss
@@ -1,0 +1,4 @@
+.ct-typeahead-header .pf-v5-c-menu__item-main {
+    color: var(--pf-v5-c-menu__group-title--Color);
+    font-size: var(--pf-v5-c-menu__group-title--FontSize);
+}

pkg/playground/react-demo-typeahead.tsx
@@ -1,0 +1,147 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import cockpit from "cockpit";
+
+import React, { useState } from "react";
+import { createRoot, Container } from 'react-dom/client';
+
+import { Checkbox } from '@patternfly/react-core';
+import { TypeaheadSelect, TypeaheadSelectOption } from "cockpit-components-typeahead-select";
+
+const TypeaheadDemo = ({ options } : { options: TypeaheadSelectOption[] }) => {
+    const [isCreatable, setIsCreatable] = useState(false);
+    const [notFoundIsString, setNotFoundIsString] = useState(false);
+    const [value, setValue] = useState<string | number | null>();
+    const [toggles, setToggles] = useState(0);
+    const [changes, setChanges] = useState(0);
+
+    return (
+        <div>
+            <TypeaheadSelect
+                id='typeahead-widget'
+                placeholder="Select a state"
+                isScrollable
+                noOptionsFoundMessage={notFoundIsString ? "Not found" : val => cockpit.format("'$0' not found", val) }
+                isCreatable={isCreatable}
+                createOptionMessage={val => cockpit.format("Create $0", val)}
+                onClearSelection={() => setValue(null)}
+                selectOptions={options}
+                selected={value}
+                onSelect={(_, value) => setValue(value) }
+                onToggle={() => setToggles(val => val + 1)}
+                onInputChange={() => setChanges(val => val + 1)}
+            />
+            <div>Selected: <span id="value">{value || "-"}</span></div>
+            <div>Toggles: <span id="toggles">{toggles}</span></div>
+            <div>Changes: <span id="changes">{changes}</span></div>
+            <Checkbox
+                id="isCreatable"
+                label="isCreatable"
+                isChecked={isCreatable}
+                onChange={(_event, checked) => setIsCreatable(checked)}
+            />
+            <Checkbox
+                id="notFoundIsString"
+                label="notFoundIsString"
+                isChecked={notFoundIsString}
+                onChange={(_event, checked) => setNotFoundIsString(checked)}
+            />
+        </div>
+    );
+};
+
+export function showTypeaheadDemo(rootElement: Container) {
+    const states: Record<string, string> = {
+        AL: "Alabama",
+        AK: "Alaska",
+        AZ: "Arizona",
+        AR: "Arkansas",
+        CA: "California",
+        CO: "Colorado",
+        CT: "Connecticut",
+        DE: "Delaware",
+        FL: "Florida",
+        GA: "Georgia",
+        HI: "Hawaii",
+        ID: "Idaho",
+        IL: "Illinois",
+        IN: "Indiana",
+        IA: "Iowa",
+        KS: "Kansas",
+        KY: "Kentucky",
+        LA: "Louisiana",
+        ME: "Maine",
+        MD: "Maryland",
+        MA: "Massachusetts",
+        MI: "Michigan",
+        MN: "Minnesota",
+        MS: "Mississippi",
+        MO: "Missouri",
+        MT: "Montana",
+        NE: "Nebraska",
+        NV: "Nevada",
+        NH: "New Hampshire",
+        NJ: "New Jersey",
+        NM: "New Mexico",
+        NY: "New York",
+        NC: "North Carolina",
+        ND: "North Dakota",
+        OH: "Ohio",
+        OK: "Oklahoma",
+        OR: "Oregon",
+        PA: "Pennsylvania",
+        RI: "Rhode Island",
+        SC: "South Carolina",
+        SD: "South Dakota",
+        TN: "Tennessee",
+        TX: "Texas",
+        UT: "Utah",
+        VT: "Vermont",
+        VA: "Virginia",
+        WA: "Washington",
+        WV: "West Virginia",
+        WI: "Wisconsin",
+        WY: "Wyoming",
+    };
+
+    const options: TypeaheadSelectOption[] = [];
+    let last = "";
+
+    options.push({ value: "start", content: "The Start" });
+
+    for (const st of Object.keys(states).sort()) {
+        if (st[0] != last) {
+            options.push({ decorator: "divider", key: "_divider-" + st });
+            options.push({
+                decorator: "header",
+                key: "_header-" + st,
+                content: "Starting with " + st[0]
+            });
+            last = st[0];
+        }
+        options.push({ value: st, content: states[st] });
+    }
+
+    options.push({ decorator: "divider", key: "_divider-end" });
+    options.push({ value: "end", content: "The End" });
+
+    const root = createRoot(rootElement);
+    root.render(<TypeaheadDemo options={options} />);
+}

pkg/playground/react-patterns.html
@@ -19,6 +19,11 @@
             </section>
 
             <section class="pf-v5-c-page__main-section pf-m-light">
+                <h3>Typeahead</h3>
+                <div id="demo-typeahead"></div>
+            </section>
+
+            <section class="pf-v5-c-page__main-section pf-m-light">
                 <h3>Dialogs</h3>
                 <button id="demo-show-dialog" class="pf-v5-c-button pf-m-secondary">Show Dialog</button>
                 <button id="demo-show-error-dialog" class="pf-v5-c-button pf-m-secondary">Show Error-Dialog</button>

pkg/playground/react-patterns.js
@@ -30,6 +30,7 @@ import { PatternDialogBody } from "./react-demo-dialog.jsx";
 import { showCardsDemo } from "./react-demo-cards.jsx";
 import { showUploadDemo } from "./react-demo-file-upload.jsx";
 import { showFileAcDemo, showFileAcDemoPreselected } from "./react-demo-file-autocomplete.jsx";
+import { showTypeaheadDemo } from "./react-demo-typeahead.jsx";
 
 /* -----------------------------------------------------------------------------
   Modal Dialog
@@ -125,6 +126,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // File autocomplete
     showFileAcDemo(document.getElementById('demo-file-ac'));
     showFileAcDemoPreselected(document.getElementById('demo-file-ac-preselected'));
+
+    // Plain typeahead select with headers and dividers
+    showTypeaheadDemo(document.getElementById('demo-typeahead'));
 
     // Cards
     showCardsDemo(document.getElementById('demo-cards'));

pkg/systemd/logs.jsx
@@ -293,7 +293,7 @@ const IdentifiersFilter = ({ identifiersFilter, onIdentifiersFilterChange, curre
             { value: "all", content: _("All") }
         ];
         if (currentIdentifiers.length > 0) {
-            identifiersArray.push({ divider: true, key: "divider" });
+            identifiersArray.push({ decorator: "divider", key: "divider" });
         }
         identifiersArray = identifiersArray.concat(
             currentIdentifiers

test/verify/check-lib
@@ -1,0 +1,136 @@
+#!/usr/bin/python3 -cimport os, sys; os.execv(os.path.dirname(sys.argv[1]) + "/../common/pywrap", sys.argv)
+
+# This file is part of Cockpit.
+#
+# Copyright (C) 2024 Red Hat, Inc.
+#
+# Cockpit is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Cockpit is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Cockpit; If not, see <https://www.gnu.org/licenses/>.
+
+import testlib
+
+
+@testlib.nondestructive
+class TestLib(testlib.MachineCase):
+
+    def testTypeaheadSelect(self):
+        b = self.browser
+
+        # Login
+
+        self.login_and_go("/playground/react-patterns")
+
+        # No clear button
+
+        b.wait_not_visible("#demo-typeahead .pf-v5-c-text-input-group__utilities button")
+
+        # Open menu, pixel test
+
+        b.click("#demo-typeahead .pf-v5-c-menu-toggle__button")
+        b.assert_pixels("#typeahead-widget", "menu")
+        b.wait_text("#toggles", "1")
+        b.wait_text("#changes", "0")
+
+        # Select from menu (with mouse)
+
+        b.click("#typeahead-widget .pf-v5-c-menu__item:contains(Hawaii)")
+        b.wait_not_present("#typeahead-widget")
+        b.assert_pixels("#demo-typeahead .pf-v5-c-menu-toggle", "input")
+        b.wait_text("#value", "HI")
+        b.wait_text("#toggles", "2")
+        b.wait_text("#changes", "0")
+
+        # Clear
+
+        b.click("#demo-typeahead .pf-v5-c-text-input-group__utilities button")
+        b.wait_not_visible("#demo-typeahead .pf-v5-c-text-input-group__utilities button")
+        b.wait_text("#value", "-")
+        b.wait_text("#toggles", "2")
+        b.wait_text("#changes", "1")
+
+        # Open by clicking into input, close with ESC
+
+        b.click("#demo-typeahead .pf-v5-c-text-input-group__text-input")
+        b.wait_visible("#typeahead-widget")
+        b.key("Escape")
+        b.wait_not_present("#typeahead-widget")
+        b.wait_text("#toggles", "4")
+        b.wait_text("#changes", "1")
+
+        # Select with keys, verify that dividers and headers are skipped
+
+        b.click("#demo-typeahead .pf-v5-c-menu-toggle__button")
+        b.wait_visible("#typeahead-widget")
+        b.key("ArrowDown")
+        b.key("ArrowUp")     # wraps around
+        b.key("ArrowUp")     # skips over divider
+        b.key("ArrowDown")   # skips over divider
+        b.key("ArrowDown")   # wraps around
+        b.key("ArrowDown")   # skips over divider and header
+        b.key("ArrowDown")
+        b.key("Enter")
+        b.wait_not_present("#typeahead-widget")
+        b.wait_text("#value", "AL")
+        b.wait_val("#demo-typeahead .pf-v5-c-text-input-group__text-input", "Alabama")
+        b.wait_text("#toggles", "6")
+        b.wait_text("#changes", "1")
+
+        b.click("#demo-typeahead .pf-v5-c-text-input-group__utilities button")
+        b.wait_text("#value", "-")
+        b.wait_text("#toggles", "6")
+        b.wait_text("#changes", "2")
+
+        # Search for non-existent
+
+        b.set_input_text("#demo-typeahead .pf-v5-c-text-input-group__text-input", "Olabama")
+        b.wait_text("#typeahead-widget .pf-v5-c-menu__item", "'Olabama' not found")
+        b.click("#demo-typeahead .pf-v5-c-menu-toggle__button")
+        b.wait_not_present("#typeahead-widget")
+        b.wait_text("#toggles", "8")
+        b.wait_text("#changes", "9")
+
+        # Again with formatted "not found" message
+
+        b.set_checked("#notFoundIsString", val=True)
+        b.set_input_text("#demo-typeahead .pf-v5-c-text-input-group__text-input", "Olabama")
+        b.wait_text("#typeahead-widget .pf-v5-c-menu__item", "Not found")
+        b.click("#demo-typeahead .pf-v5-c-menu-toggle__button")
+        b.wait_not_present("#typeahead-widget")
+        b.wait_text("#toggles", "10")
+        b.wait_text("#changes", "16")
+
+        # Search for existing, pixel test, select
+
+        b.set_input_text("#demo-typeahead .pf-v5-c-text-input-group__text-input", "Flori")
+        b.wait_visible("#typeahead-widget")
+        b.assert_pixels("#typeahead-widget", "search")
+        b.click("#typeahead-widget .pf-v5-c-menu__item:contains(Florida)")
+        b.wait_not_present("#typeahead-widget")
+        b.wait_text("#value", "FL")
+        b.wait_text("#toggles", "12")
+        b.wait_text("#changes", "21")
+
+        # Enable creation, create new state
+
+        b.set_checked("#isCreatable", val=True)
+        b.set_input_text("#demo-typeahead .pf-v5-c-text-input-group__text-input", "Flori")
+        b.click("#typeahead-widget .pf-v5-c-menu__item:contains(Create Flori)")
+        b.wait_not_present("#typeahead-widget")
+        b.wait_text("#value", "Flori")
+        b.wait_val("#demo-typeahead .pf-v5-c-text-input-group__text-input", "Flori")
+        b.wait_text("#toggles", "14")
+        b.wait_text("#changes", "26")
+
+
+if __name__ == '__main__':
+    testlib.test_main()

commit 09d94a2c621b13d04cb791479a1bccf94b734be4

lib: Improve key navigation in TypeaheadSelect and MultiTypeaheadSelect
In the TypeaheadSelect component, the focusedItemIndex is for the
filteredSelections array, not for the selectOptions one. Thus without
this fix, we would get the aria stuff wrong. We might even end up
accessing non-existing items, when focusedItemIndex refers to a "No
results found" fake entry in filteredSelections with index 0 while
selectOptions is empty.

The fake entries are now properly disabled and will be skipped by key
navigation in both TypeaheadSelect and MultiTypeaheadSelect.

Also, key navigation relies on there being at least one option that
will not be skipped. Thus, we need to us isEnabledMenu instead of
isMenu when deciding whether to return early. This got broken in
8d9f97b2c9ea71716535d681ebda02f41db701b4.

pkg/lib/cockpit-components-multi-typeahead-select.tsx
@@ -148,6 +148,7 @@ export const MultiTypeaheadSelectBase: React.FunctionComponent<MultiTypeaheadSel
         newSelectOptions = [
           {
             isAriaDisabled: true,
+            isDisabled: true,
             content:
               typeof noOptionsFoundMessage === 'string' ? noOptionsFoundMessage : noOptionsFoundMessage(inputValue),
             value: NO_RESULTS

pkg/lib/cockpit-components-typeahead-select.tsx
@@ -274,6 +274,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
         newSelectOptions = [
           {
             isAriaDisabled: true,
+            isDisabled: true,
             content:
               typeof noOptionsFoundMessage === 'string' ? noOptionsFoundMessage : noOptionsFoundMessage(filterValue),
             value: NO_RESULTS
@@ -287,6 +288,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
       newSelectOptions = [
         {
           isAriaDisabled: true,
+          isDisabled: true,
           content: noOptionsAvailableMessage,
           value: NO_RESULTS
         }
@@ -316,7 +318,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
 
   const setActiveAndFocusedItem = (itemIndex: number) => {
     setFocusedItemIndex(itemIndex);
-    const focusedItem = selectOptions[itemIndex] as TypeaheadSelectMenuOption;
+    const focusedItem = filteredSelections[itemIndex] as TypeaheadSelectMenuOption;
     setActiveItemId(String(focusedItem.value));
   };
 
@@ -384,7 +386,7 @@ export const TypeaheadSelectBase: React.FunctionComponent<TypeaheadSelectProps> 
 
     openMenu();
 
-    if (filteredSelections.every(o => !isMenu(o))) {
+    if (filteredSelections.every(o => !isEnabledMenu(o))) {
       return;
     }
 
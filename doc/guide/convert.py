76eaf450bd9019f3e4710fb4366fc787abf6ece3
Aleksandra Fedorova <afedorova@redhat.com>
2025-01-11 11:26:27 +0100

    Skip cockpit-guide from conversion
    cockpit-guide is an index page and pandoc can not process it correctly.
    Thus any changes in it need to be reviewed and transferred to
    cockpit-guide.adoc manually

--- doc/guide/convert.py
+++ doc/guide/convert.py
@@ -9,6 +9,9 @@ file_names = [ os.path.splitext(item)[0] for item in files if item.endswith('.xm
 
 for name in file_names:
     print(f"Processing {name}")
+    if name == "cockpit-guide":
+        print(f"Skipping {name}")
+        continue
     # pandoc --from docbook --to asciidoc -o cockpit-cache.adoc cockpit-cache.xml
     subprocess.run(
         [

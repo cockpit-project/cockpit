d410391e5638c777386bcdd4163a923648a31f89
Aleksandra Fedorova <afedorova@redhat.com>
2025-01-13 15:38:02 +0100

    Add man pages and Makefile
    - adjust convert.py to include man pages
    - add Makefile to produce single page html and man pages
    - include man pages to the html guide index page (cockpit-guide.adoc)

--- doc/guide/convert.py
+++ doc/guide/convert.py
@@ -3,11 +3,26 @@
 import os
 import subprocess
 
-files = os.listdir()
+man_files = os.listdir('../man/')
+man_file_names = [ os.path.splitext(item)[0] for item in man_files if item.endswith('.xml') ]
 
-file_names = [ os.path.splitext(item)[0] for item in files if item.endswith('.xml') ]
+for name in man_file_names:
+    print(f"Processing {name}")
+    # pandoc --from docbook --to asciidoc -o cockpit-cache.adoc cockpit-cache.xml
+    subprocess.run(
+        [
+            "pandoc",
+            "--from", "docbook",
+            "--to", "asciidoc",
+            "-o", f"./asciidoc/man/{name}.adoc",
+            f"../man/{name}.xml",
+        ]
+    )
 
-for name in file_names:
+guide_files = os.listdir()
+guide_file_names = [ os.path.splitext(item)[0] for item in guide_files if item.endswith('.xml') ]
+
+for name in guide_file_names:
     print(f"Processing {name}")
     if name == "cockpit-guide":
         print(f"Skipping {name}")

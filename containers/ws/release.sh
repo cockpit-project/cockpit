202d22b415f3689495393a80e02f7a669e21f8f7
Marvin A. Ruder <signed@mruder.dev>
2025-05-03 13:14:27 +0200

    Support both Podman and Docker
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- containers/ws/release.sh
+++ containers/ws/release.sh
@@ -2,8 +2,8 @@
 set -eux
 
 if [ -z "${RUNC:-}" ]; then
-    RUNC=$(command -v docker) || {
-        echo "ERROR: docker required" >&2
+    RUNC=$(command -v podman || command -v docker) || {
+        echo "ERROR: podman or docker required" >&2
         exit 1
     }
 fi
@@ -15,7 +15,21 @@ if [ -z "${PLATFORMS:-}" ]; then
      esac
 fi
 
-$RUNC buildx build --load --platform $PLATFORMS -t quay.io/cockpit/ws:release containers/ws
+if [[ "$RUNC" == *podman ]]; then
+    PUSH_CMD="$RUNC manifest push"
+    REMOVE_IMAGE_CMD="$RUNC manifest rm"
+
+    $RUNC manifest create quay.io/cockpit/ws:release
+    $RUNC build --platform $PLATFORMS --manifest quay.io/cockpit/ws:release containers/ws
+elif [[ "$RUNC" == *docker ]]; then
+    PUSH_CMD="$RUNC push"
+    REMOVE_IMAGE_CMD="$RUNC rmi"
+
+    $RUNC buildx build --load --platform $PLATFORMS -t quay.io/cockpit/ws:release containers/ws
+else
+    echo "Unknown runtime: $RUNC" >&2
+    exit 1
+fi
 
 for platform in ${PLATFORMS//,/ }
 do
@@ -35,8 +49,8 @@ done
 
 $RUNC tag quay.io/cockpit/ws:release quay.io/cockpit/ws:$TAG
 $RUNC tag quay.io/cockpit/ws:release quay.io/cockpit/ws:latest
-$RUNC rmi quay.io/cockpit/ws:release
+$REMOVE_IMAGE_CMD quay.io/cockpit/ws:release
 
 # push both tags
-$RUNC push quay.io/cockpit/ws:$TAG
-$RUNC push quay.io/cockpit/ws:latest
+$PUSH_CMD quay.io/cockpit/ws:$TAG
+$PUSH_CMD quay.io/cockpit/ws:latest

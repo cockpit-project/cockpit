5c89380b0cfb180e90287e59ac0c545f26d5a2f6
Miguel Ribeiro <miguelbribeiro@tecnico.ulisboa.pt>
2025-06-02 15:20:12 +0100

    systemd: Allow editing timers created by Cockpit
    Add a marker in the unit files of timers (and their services) created
    with Cockpit. Those timers can be edited by reading the current settings
    from the systemd's dbus API and reusing the CreateTimerDialog
    
    Related: #9467

--- pkg/systemd/timer-dialog-helpers.js
+++ pkg/systemd/timer-dialog-helpers.js
@@ -18,8 +18,142 @@
  */
 
 import cockpit from "cockpit";
+import * as python from "python";
 import s_bus from "./busnames.js";
 import { systemd_client, clock_realtime_now } from "./services.jsx";
+import { CockpitManagedMarker } from "./service-details.jsx";
+
+export async function join_command_arguments(command, args) {
+    const exec = [command].concat(args.slice(1));
+    const shlex = python.spawn(
+        "from json import loads; from shlex import join; from sys import stdin;" +
+        "out = join(loads(stdin.read())); print(out, end='');"
+    );
+
+    shlex.input(JSON.stringify(exec));
+    shlex.input();
+    const output = await shlex;
+
+    return output;
+}
+
+export function from_boot_usec(value) {
+    const result = { delay: "system-boot" };
+    const seconds = Math.floor(value / 1e6); // Convert from microseconds
+
+    if (seconds % 604800 === 0) {
+        result.delayNumber = seconds / 604800;
+        result.delayUnit = "weeks";
+    } else if (seconds % 3600 === 0) {
+        result.delayNumber = seconds / 3600;
+        result.delayUnit = "hours";
+    } else if (seconds % 60 === 0) {
+        result.delayNumber = seconds / 60;
+        result.delayUnit = "minutes";
+    } else {
+        result.delayNumber = seconds;
+        result.delayUnit = "seconds";
+    }
+
+    return result;
+}
+
+export function from_on_calendar(patterns) {
+    const joined = patterns.join('\n').trim();
+    let result = null;
+
+    const minutely = /^\*-\*-\* \*:\*:(\d{1,2}(?:,\d{1,2})*)$/;
+    const hourly = /^\*-\*-\* \*:(\d{1,2}(?:,\d{1,2})*)(?::00)?$/;
+    const daily = /^\*-\*-\* (\d{2}:\d{2})(?::00)?$/;
+    const weekly = /^([A-Za-z]{3}) \*-\*-\* (\d{2}:\d{2})(?::00)?$/;
+    const monthly = /^\*-\*-(\d{1,2}) (\d{2}:\d{2})(?::00)?$/;
+    const yearly = /^\*-(\d{2}-\d{2}) (\d{2}:\d{2})(?::00)?$/;
+    const specific = /^\d{4}-\d{2}-\d{2} (\d{2}:\d{2})(?::00)?$/;
+
+    if (minutely.test(joined)) {
+        const match = joined.match(minutely);
+        const seconds = match[1];
+        result = {
+            repeat: "minutely",
+            repeatPatterns: seconds.split(",").map(second => ({ second }))
+        };
+    }
+
+    if (hourly.test(joined)) {
+        const match = joined.match(hourly);
+        const minutes = match[1];
+        result = {
+            repeat: "hourly",
+            repeatPatterns: minutes.split(",").map(minute => ({ minute }))
+        };
+    }
+
+    if (patterns.every(line => daily.test(line))) {
+        result = {
+            repeat: "daily",
+            repeatPatterns: patterns.map(line => {
+                const match = line.match(daily);
+                const time = match[1];
+                return { time };
+            })
+        };
+    }
+
+    if (patterns.every(line => weekly.test(line))) {
+        result = {
+            repeat: "weekly",
+            repeatPatterns: patterns.map(line => {
+                const match = line.match(weekly);
+                const weekDay = match[1].toLowerCase();
+                const time = match[2];
+                return { day: weekDay, time };
+            })
+        };
+    }
+
+    if (patterns.every(line => monthly.test(line))) {
+        result = {
+            repeat: "monthly",
+            repeatPatterns: patterns.map(line => {
+                const match = line.match(monthly);
+                const day = Number(match[1]);
+                const time = match[2];
+                return { day, time };
+            })
+        };
+    }
+
+    if (patterns.every(line => yearly.test(line))) {
+        result = {
+            repeat: "yearly",
+            repeatPatterns: patterns.map((line) => {
+                const match = line.match(yearly);
+                const monthAndDay = match[1];
+                const time = match[2];
+                return { date: `${(new Date(clock_realtime_now)).getFullYear()}-${monthAndDay}`, time }; // specific year isn't important
+            })
+        };
+    }
+
+    if (specific.test(joined)) {
+        result = {
+            repeat: "no",
+            repeatPatterns: [],
+            specificTime: joined.match(specific)[1]
+        };
+    }
+
+    if (result) {
+        result.delay = "specific-time";
+        result.repeatPatterns = result.repeatPatterns.map((item, index) => {
+            return { key: index, ...item };
+        });
+
+        return result;
+    } else {
+        return null;
+    }
+}
 
 export function create_timer({ name, description, command, delay, delayUnit, delayNumber, repeat, repeatPatterns, specificTime, owner }) {
     const timer_unit = {};
@@ -64,14 +198,14 @@ function create_timer_file({ timer_unit, delay, owner }) {
     const service = "\n[Service]\nExecStart=";
     const timer = "\n[Timer]\n";
     const install = "[Install]\nWantedBy=timers.target\n";
-    const service_file = unit + timer_unit.Description + service + timer_unit.Command + "\n";
-    let timer_file = " ";
+    const service_file = CockpitManagedMarker + unit + timer_unit.Description + service + timer_unit.Command + "\n";
+    let timer_file = CockpitManagedMarker;
     if (delay == "system-boot") {
         const boottimer = timer + "OnBootSec=" + timer_unit.boot_time + timer_unit.boot_time_unit + "\n";
-        timer_file = unit + timer_unit.Description + boottimer;
+        timer_file += unit + timer_unit.Description + boottimer;
     } else if (timer_unit.OnCalendar) {
         const calendartimer = timer + timer_unit.OnCalendar + "\n";
-        timer_file = unit + timer_unit.Description + calendartimer;
+        timer_file += unit + timer_unit.Description + calendartimer;
     }
     timer_file += install;
     // writing to file

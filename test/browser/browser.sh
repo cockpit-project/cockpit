22acd7121a04177690e7e8b354bd9453b6e57a20
Jelle van der Waa <jvanderwaa@redhat.com>
2025-06-26 12:05:23 +0200

    test: generalise stratisd-tools install for RHEL
    Install stratisd-tools on CentOS / RHEL and Fedora.

--- test/browser/browser.sh
+++ test/browser/browser.sh
@@ -42,13 +42,6 @@ fi
 if grep -q 'ID=.*fedora' /etc/os-release && [ "$PLAN" = "storage-basic" ]; then
     # required by TestStorageBtrfs*
     dnf install -y udisks2-btrfs
-    # required by TestStorageStratis
-    dnf install -y stratisd-tools
-fi
-
-if grep -q 'ID=.*centos' /etc/os-release && grep -q 'VERSION_ID=.*10' /etc/os-release && [ "$PLAN" = "storage-basic" ]; then
-    # required by TestStorageStratis
-    dnf install -y stratisd-tools
 fi
 
 # dnf installs "missing" weak dependencies, but we don't want them for plans other than "main"

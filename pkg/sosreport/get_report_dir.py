46c6273d4111d0f7579617320c215edec86bd9c6
Martin Pitt <mpitt@redhat.com>
2025-07-09 07:11:47 +0200

    sosreport: Query sos policy for default tmp dir
    sos 4.6.2 changed the tmp dir policy [1] from a static /var/tmp to a
    dynamic per-distro policy, which happens to be /tmp on Debian/Ubuntu
    right now. This is wrong, and there is no official way to query it [2].
    
    To cope, query the sos policy for the tmp-dir, and only fall back to
    static /var/tmp if that fails.
    
    [1] https://github.com/sosreport/sos/pull/4062
    [2] https://github.com/sosreport/sos/issues/4074

--- pkg/sosreport/get_report_dir.py
+++ pkg/sosreport/get_report_dir.py
@@ -1,8 +1,17 @@
 import configparser
+import sys
 
 c = configparser.ConfigParser()
 c.read("/etc/sos/sos.conf")
 try:
     print(c["global"]["tmp-dir"])
 except KeyError:
-    print("/var/tmp")
+    # very expensive and internal API, but there is no official way to get the tmp-dir
+    # https://github.com/sosreport/sos/issues/4074
+    try:
+        import sos.policies
+        print(sos.policies.load().get_tmp_dir(None))
+    except Exception as e:
+        # in case the API changes, sane fallback
+        print("Failed to get policy tmp-dir:", e, file=sys.stderr)
+        print("/var/tmp")

819a282cb107f1e88ee1f464dabf871471a2e1c9
Jelle van der Waa <jvanderwaa@redhat.com>
2025-07-10 17:58:58 +0200

    playground: dnf5daemon demo

---
+++ pkg/lib/dnf5daemon.js
@@ -0,0 +1,137 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import cockpit from "cockpit";
+import { superuser } from 'superuser';
+
+const _ = cockpit.gettext;
+
+let _dbus_client = null;
+
+/**
+ * Get dnf5daemon D-Bus client
+ *
+ * This will get lazily initialized and re-initialized after dnf5daemon
+ * disconnects (due to a crash or idle timeout).
+ */
+function dbus_client() {
+    if (_dbus_client === null) {
+        _dbus_client = cockpit.dbus("org.rpm.dnf.v0", { superuser: "try", track: true });
+        _dbus_client.addEventListener("close", () => {
+            console.log("dnf5daemon went away from D-Bus");
+            _dbus_client = null;
+        });
+    }
+
+    return _dbus_client;
+}
+
+// Reconnect when privileges change
+superuser.addEventListener("changed", () => { _dbus_client = null });
+
+/**
+ * Call a dnf5daemon method
+ */
+export function call(objectPath, iface, method, args, opts) {
+    return dbus_client().call(objectPath, iface, method, args, opts);
+}
+
+/**
+ * Figure out whether dnf5daemon is available and usable
+ */
+export function detect() {
+    function dbus_detect() {
+        return call("/org/rpm/dnf/v0", "org.freedesktop.DBus.Peer",
+                    "Ping", [])
+                .then(() => true,
+                      () => false);
+    }
+
+    return cockpit.spawn(["findmnt", "-T", "/usr", "-n", "-o", "VFS-OPTIONS"])
+            .then(options => {
+                if (options.split(",").indexOf("ro") >= 0)
+                    return false;
+                else
+                    return dbus_detect();
+            })
+            .catch(dbus_detect);
+}
+
+// TODO: close_session needs to be handled
+// handle
+// Cannot open new session - maximal number of simultaneously opened sessions achieved
+export async function check_missing_packages(names, progress_cb) {
+    const data = {
+        missing_ids: [],
+        missing_names: [],
+        unavailable_names: [],
+    };
+
+    if (names.length === 0)
+        return data;
+
+    function open_session() {
+        return call("/org/rpm/dnf/v0", "org.rpm.dnf.v0.SessionManager",
+                    "open_session", [{}]);
+    }
+
+    function close_session(session) {
+        return call("/org/rpm/dnf/v0", "org.rpm.dnf.v0.SessionManager",
+                    "close_session", [session]);
+    }
+
+    function refresh() {
+        // refresh dnf5daemon state
+    }
+
+    function list() {
+        // const package_attrs = ["name", "version", "release", "arch"];
+        // await call(session, "org.rpm.dnf.v0.rpm.Rpm", "list", [{ package_attrs, scope: "installed", patterns: ["bash"] }]);
+    }
+
+    function signal_emitted(path, iface, signal, args) {
+        if (progress_cb)
+            progress_cb(signal);
+        console.log("signal_emitted", path, iface, signal, args);
+    }
+
+    // TODO: decorator / helper for opening a session?
+    let session;
+    dbus_client().addEventListener("notify", (event, data) => console.log("notify", event, data));
+    const subscription = _dbus_client.subscribe({ }, signal_emitted);
+    console.log(subscription);
+
+    try {
+        [session] = await open_session();
+        console.log(session);
+
+        await call(session, "org.rpm.dnf.v0.Base", "read_all_repos", []);
+        const resolve_results = await call(session, "org.rpm.dnf.v0.Goal", "resolve", [{}]);
+        console.log(resolve_results);
+        const transaction_results = await call(session, "org.rpm.dnf.v0.Goal", "do_transaction", [{}]);
+        console.log(transaction_results);
+        await close_session(session);
+    } catch (err) {
+        if (session)
+            await close_session(session);
+        console.warn(err);
+    }
+
+    return data;
+}
---
+++ pkg/playground/dnf5daemon.js
@@ -0,0 +1,79 @@
+import cockpit from "cockpit";
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import 'cockpit-dark-theme'; // once per page
+
+import { Button } from "@patternfly/react-core/dist/esm/components/Button/index.js";
+import { Card, CardBody, CardTitle } from '@patternfly/react-core/dist/esm/components/Card/index.js';
+import { Content } from "@patternfly/react-core/dist/esm/components/Content/index.js";
+import { List, ListItem } from "@patternfly/react-core/dist/esm/components/List/index.js";
+import { Page, PageSection } from "@patternfly/react-core/dist/esm/components/Page/index.js";
+import {
+    CheckIcon,
+    ExclamationCircleIcon,
+} from '@patternfly/react-icons';
+
+import * as PK from "../lib/dnf5daemon.js";
+
+import '../lib/patternfly/patternfly-6-cockpit.scss';
+import "../../node_modules/@patternfly/patternfly/components/Page/page.css";
+
+const DnfPage = ({ exists }) => {
+    const [isRefreshing, setRefreshing] = React.useState(false);
+    const [events, setEvents] = React.useState([]);
+
+    const progressCallback = (signal_text) => {
+        setEvents(prevState => [...prevState, signal_text]);
+    };
+
+    const refreshDatabase = async () => {
+        setEvents([]);
+        setRefreshing(true);
+        const data = await PK.check_missing_packages("bash", progressCallback);
+        console.log(data);
+        setRefreshing(false);
+    };
+
+    const cancelRefreshDatabase = () => {
+        setRefreshing(false);
+    };
+    console.log(events);
+
+    return (
+        <Page id="accounts" className='no-masthead-sidebar'>
+            <PageSection hasBodyWrapper={false}>
+                <Content>
+                    <h1>dnf5daemon example</h1>
+                    <p>daemon available?: { exists ? <CheckIcon /> : <ExclamationCircleIcon /> }</p>
+                    <Card>
+                        <CardTitle>Refresh database</CardTitle>
+                        <CardBody>
+                            <Button variant="primary" onClick={() => refreshDatabase()} isLoading={isRefreshing}>Refresh</Button>
+                            {isRefreshing && <Button variant="secondary" isDanger onClick={() => cancelRefreshDatabase()}>Cancel refresh</Button>}
+
+                        </CardBody>
+                        {events.length !== 0 &&
+                            <CardBody>
+                                <h4>Events</h4>
+                                <List isBordered>
+                                    {events.map((evt, idx) => {
+                                        return <ListItem key={idx} icon={<CheckIcon />}>{evt}</ListItem>;
+                                    })}
+                                </List>
+                            </CardBody>
+                        }
+                    </Card>
+                </Content>
+            </PageSection>
+        </Page>
+
+    );
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const dnf5daemon_exists = await PK.detect();
+    console.log("dnf5daemon", dnf5daemon_exists);
+
+    const root = createRoot(document.getElementById("dnf5daemon"));
+    root.render(<DnfPage exists={dnf5daemon_exists} />);
+});

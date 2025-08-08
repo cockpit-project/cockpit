f449eed4c622c11f3762b9169772aa5f168c3939
Martin Pitt <mpitt@redhat.com>
2025-08-08 10:44:42 +0200

    build: Drop obsolete CSS hacking
    This got fixed properly with the move to PF5 by changing the variables
    in pkg/lib/_global-variables.scss.
    
    This also gets rid of the `esbuild-plugin-replace` npm dependency.

--- pkg/lib/esbuild-common.js
+++ pkg/lib/esbuild-common.js
@@ -1,23 +1,9 @@
-import { replace } from 'esbuild-plugin-replace';
 import { sassPlugin } from 'esbuild-sass-plugin';
 
 // List of directories to use when resolving import statements
 const nodePaths = ['pkg/lib'];
 
 export const esbuildStylesPlugins = [
-    // Redefine grid breakpoints to count with our shell
-    // See https://github.com/patternfly/patternfly-react/issues/3815 and
-    // [Redefine grid breakpoints] section in pkg/lib/_global-variables.scss for explanation
-    replace({
-        include: /\.css$/,
-        values: {
-            // Do not override the sm breakpoint as for width < 768px the left nav is hidden
-            '768px': '428px',
-            '992px': '652px',
-            '1200px': '876px',
-            '1450px': '1100px',
-        }
-    }),
     sassPlugin({
         loadPaths: [...nodePaths, 'node_modules'],
         filter: /\.scss/,

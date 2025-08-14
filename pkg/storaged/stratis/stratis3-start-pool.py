7efac793b707fb51350e25c86d624037207e1544
Marius Vollmer <marius.vollmer@gmail.com>
2025-08-14 16:51:33 +0300

    FIXUP - "fd" -> "file_descriptor"

--- pkg/storaged/stratis/stratis3-start-pool.py
+++ pkg/storaged/stratis/stratis3-start-pool.py
@@ -1,10 +1,9 @@
 import sys
-
 import dbus
 
 uuid = sys.argv[1]
 slot = sys.argv[2]
-fd = sys.argv[3]
+file_descriptor = sys.argv[3]
 
 if slot == "-":
     slot_arg = [False, [False, 0]]
@@ -13,15 +12,15 @@ elif slot == "any":
 else:
     slot_arg = [True, [True, int(slot)]]
 
-if fd == "-":
-    fd_arg = [False, 0]
+if file_descriptor == "-":
+    file_descriptor_arg = [False, 0]
 else:
-    fd_arg = [True, int(fd)]
+    file_descriptor_arg = [True, int(fd)]
 
 reply = dbus.SystemBus().call_blocking('org.storage.stratis3', '/org/storage/stratis3',
                                        'org.storage.stratis3.Manager.r8', 'StartPool',
                                        'ss(b(bu))(bh)',
-                                       [uuid, 'uuid', slot_arg, fd_arg])
+                                       [uuid, 'uuid', slot_arg, file_descriptor_arg])
 if reply[1] != 0:
     sys.stderr.write(reply[2] + '\n')
     sys.exit(1)

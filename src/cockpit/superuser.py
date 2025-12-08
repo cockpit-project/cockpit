15630a5ac4c629a48d376acce2521341ee734d37
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2025-12-08 10:40:25 +0100

    superuser: filter out sudo-rs
    If we detect a version of sudo which lacks support for the -A (askpass)
    flag then we exclude it from the list of available authentication
    methods.
    
    This prevents us from trying to use sudo-rs, which lacks askpass support
    (which we depend on).
    
    See https://github.com/trifectatechfoundation/sudo-rs/issues/1249

--- src/cockpit/superuser.py
+++ src/cockpit/superuser.py
@@ -23,6 +23,8 @@ import logging
 import os
 import shutil
 import socket
+import subprocess
+from os.path import basename
 from tempfile import TemporaryDirectory
 from typing import List, Optional, Sequence, Tuple
 
@@ -40,10 +42,27 @@ from .router import Router, RoutingError, RoutingRule
 logger = logging.getLogger(__name__)
 
 
+def sudo_supports_askpass(sudo_path: str) -> bool:
+    try:
+        # Returns 0 if -A is supported, non-zero if it's not
+        subprocess.run(
+            [sudo_path, '-A', '--help'],
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True
+        )
+        return True
+    except (subprocess.CalledProcessError, OSError):
+        return False
+
+
 def is_valid_superuser_config(config: BridgeConfig) -> bool:
     if not config.privileged:
         return False
-    if not shutil.which(config.spawn[0]):
+    if not (spawn_path := shutil.which(config.spawn[0])):
+        return False
+    if basename(spawn_path) == 'sudo' and not sudo_supports_askpass(spawn_path):
         return False
     return True
 

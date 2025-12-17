7be5bec9048b2bdf10262583ffca771324d676bc
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2025-12-17 16:38:08 +0100

    peer, superuser: add/modernize some typing
    Enable strict mode and add the places where complaints popped up.
    
    One not-entirely-mechanical thing: we replace the call to .stop() with a
    call to .shutdown().  .stop() is decorated as a D-Bus method (which
    unfortunately currently erases type information), which causes
    complaints at the call site.  But it just calls .shutdown() anyway, so
    we may as well just do that directly ourselves.

--- src/cockpit/peer.py
+++ src/cockpit/peer.py
@@ -18,7 +18,7 @@
 import asyncio
 import logging
 import os
-from typing import Callable, List, Optional, Sequence
+from typing import Any, Callable, Sequence
 
 from .jsonutil import JsonObject, JsonValue
 from .packages import BridgeConfig
@@ -39,8 +39,8 @@ class PeerExited(Exception):
 
 
 class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
-    done_callbacks: List[Callable[[], None]]
-    init_future: Optional[asyncio.Future]
+    done_callbacks: 'list[Callable[[], None]]'
+    init_future: 'asyncio.Future[JsonObject] | None'
 
     def __init__(self, router: Router):
         super().__init__(router)
@@ -55,13 +55,13 @@ class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
     async def do_connect_transport(self) -> None:
         raise NotImplementedError
 
-    async def spawn(self, argv: Sequence[str], env: Sequence[str], **kwargs) -> asyncio.Transport:
+    async def spawn(self, argv: Sequence[str], env: Sequence[str], **kwargs: Any) -> asyncio.Transport:
         # Not actually async...
         loop = asyncio.get_running_loop()
         user_env = dict(e.split('=', 1) for e in env)
         return SubprocessTransport(loop, self, argv, env=dict(os.environ, **user_env), **kwargs)
 
-    async def start(self, init_host: Optional[str] = None, **kwargs: JsonValue) -> JsonObject:
+    async def start(self, init_host: 'str | None' = None, **kwargs: JsonValue) -> JsonObject:
         """Request that the Peer is started and connected to the router.
 
         Creates the transport, connects it to the protocol, and participates in
@@ -78,7 +78,7 @@ class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
         """
         assert self.init_future is not None
 
-        def _connect_task_done(task: asyncio.Task) -> None:
+        def _connect_task_done(task: 'asyncio.Task[None]') -> None:
             assert task is connect_task
             try:
                 task.result()
@@ -126,8 +126,8 @@ class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
         return init_message
 
     # Background initialization
-    def start_in_background(self, init_host: Optional[str] = None, **kwargs: JsonValue) -> None:
-        def _start_task_done(task: asyncio.Task) -> None:
+    def start_in_background(self, init_host: 'str | None' = None, **kwargs: JsonValue) -> None:
+        def _start_task_done(task: 'asyncio.Task[JsonObject]') -> None:
             assert task is start_task
 
             try:
@@ -170,7 +170,7 @@ class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
         logger.debug('Peer %s received unexpected EOF', self.__class__.__name__)
         return True
 
-    def do_closed(self, exc: Optional[Exception]) -> None:
+    def do_closed(self, exc: 'Exception | None') -> None:
         logger.debug('Peer %s connection lost %s %s', self.__class__.__name__, type(exc), exc)
 
         if exc is None:
@@ -235,6 +235,7 @@ class Peer(CockpitProtocol, SubprocessProtocol, Endpoint):
 
 class ConfiguredPeer(Peer):
     config: BridgeConfig
+    method: str
     args: Sequence[str]
     env: Sequence[str]
 
@@ -245,13 +246,14 @@ class ConfiguredPeer(Peer):
         super().__init__(router)
 
     async def do_connect_transport(self) -> None:
+        assert self.args is not None
         await self.spawn(self.args, self.env)
 
 
 class PeerRoutingRule(RoutingRule):
     config: BridgeConfig
     match: JsonObject
-    peer: Optional[Peer]
+    peer: 'Peer | None'
 
     def __init__(self, router: Router, config: BridgeConfig):
         super().__init__(router)
@@ -259,7 +261,7 @@ class PeerRoutingRule(RoutingRule):
         self.match = config.match
         self.peer = None
 
-    def apply_rule(self, options: JsonObject) -> Optional[Peer]:
+    def apply_rule(self, options: JsonObject) -> 'Peer | None':
         # Check that we match
 
         for key, value in self.match.items():
@@ -288,9 +290,9 @@ class PeerRoutingRule(RoutingRule):
 
 
 class PeersRoutingRule(RoutingRule):
-    rules: List[PeerRoutingRule] = []
+    rules: 'list[PeerRoutingRule]' = []
 
-    def apply_rule(self, options: JsonObject) -> Optional[Endpoint]:
+    def apply_rule(self, options: JsonObject) -> 'Endpoint | None':
         logger.debug('    considering %d rules', len(self.rules))
         for rule in self.rules:
             logger.debug('      considering %s', rule.config.name)

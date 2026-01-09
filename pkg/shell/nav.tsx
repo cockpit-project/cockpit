78c635980537e8fa4432f4aa90e486a2b337307a
Martin Pitt <mpitt@redhat.com>
2026-01-09 14:53:04 +0100

    DNM: Touch the shell
    This should cause 3x affected retries.

--- pkg/shell/nav.tsx
+++ pkg/shell/nav.tsx
@@ -15,6 +15,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Cockpit; If not, see <https://www.gnu.org/licenses/>.
+ * PWNED
  */
 
 import cockpit from "cockpit";

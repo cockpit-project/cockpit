cockpit-project/cockpit

commit a30e928bdb9bd944083e1d75a07568d32caebdeb

packaging: Rebuild our bundle during Fedora package build
Also adjust the packit config. We have to stop using `create-archive:`
as that can only handle a single tarball [1]. It does two things for us
which we replace:

  * Set `Version:`; our `make-dist` already does that, so fish out
    the generated/fixed spec file from the release tarball instead of
    taking the template.
  * Replace SourceN URLs with local file names. Do that in the
    `fix-spec-file` rule manually, as suggested in [1].

[1] https://packit.dev/docs/configuration/actions#create-archive

tools/cockpit.spec
@@ -51,6 +51,11 @@
 %define pamdir %{_libdir}/security
 %endif
 
+# distributions which ship nodejs-esbuild can rebuild the bundle during package build
+%if 0%{?fedora} >= 42
+%define rebuild_bundle 1
+%endif
+
 Name:           cockpit
 Summary:        Web Console for Linux servers
 
@@ -60,6 +65,7 @@ URL:            https://cockpit-project.org/
 Version:        0
 Release:        1%{?dist}
 Source0:        https://github.com/cockpit-project/cockpit/releases/download/%{version}/cockpit-%{version}.tar.xz
+Source1:        https://github.com/cockpit-project/cockpit/releases/download/%{version}/cockpit-node-%{version}.tar.xz
 
 %if 0%{?fedora} >= 41 || 0%{?rhel}
 ExcludeArch: %{ix86}
@@ -107,6 +113,11 @@ BuildRequires: docbook-style-xsl
 BuildRequires: krb5-server
 BuildRequires: gdb
 
+%if %{defined rebuild_bundle}
+BuildRequires: nodejs
+BuildRequires: nodejs-esbuild
+%endif
+
 # For documentation
 BuildRequires: xmlto
 
@@ -149,8 +160,20 @@ BuildRequires:  python3-pytest-timeout
 
 %prep
 %setup -q -n cockpit-%{version}
+%if %{defined rebuild_bundle}
+%setup -q -D -T -a 1 -n cockpit-%{version}
+%endif
 
 %build
+%if %{defined rebuild_bundle}
+rm -rf dist
+# HACK: node module packaging is currently broken in Fedora, should be in
+# common location, not major version specific one
+NODE_ENV=production NODE_PATH=$(echo /usr/lib/node_modules_*) ./build.js
+%else
+# Use pre-built bundle on distributions without nodejs-esbuild
+%endif
+
 %configure \
     %{?selinux_configure_arg} \
 %if 0%{?suse_version}

packit.yaml
@@ -1,17 +1,21 @@
 upstream_project_url: https://github.com/cockpit-project/cockpit
 specfile_path: cockpit.spec
 actions:
+  # create-archive does not work with multiple sources, so do the spec mangling ourselves
   post-upstream-clone:
-    # build patched spec
-    - tools/node-modules make_package_lock_json
-    - env NODE_ENV=production ./build.js
-    - cp tools/cockpit.spec .
-    # packit will compute and set the version by itself
-    - tools/fix-spec ./cockpit.spec 0
+    # we already build a spec file with correct Version, so extract it
+    - |
+      bash -exc '
+      mapfile -t TARBALLS < <(tools/make-dist)
+      # Extract spec file from main tarball (first in array)
+      tar -xJf "${TARBALLS[0]}" --wildcards "*/tools/cockpit.spec" --strip-components=2
+      # Move all tarballs to current directory for packit
+      mv "${TARBALLS[@]}" .
+      '
 
-  create-archive:
-    # temporary hack: not using the -node tarball yet, that requires spec changes; that's the next step
-    - sh -c 'tools/make-dist | grep "cockpit-[1-9].*[.]tar[.]xz"'
+  fix-spec-file:
+    # packit needs local file names, not URLs
+    - sed -i 's|^\(Source[0-9]*:\s*\).*\/|\1|' cockpit.spec
 
 srpm_build_deps:
   - automake
@@ -89,11 +93,11 @@ jobs:
         - cp tools/cockpit.spec .
         # packit will compute and set the version by itself
         - tools/fix-spec ./cockpit.spec 0
-      # HACK: tarball for releases (copr_build, koji, etc.), copying spec's Source0; this
+      # HACK: tarball for releases (copr_build, koji, etc.), copying spec's Source0 and Source1; this
       # really should be the default, see https://github.com/packit/packit-service/issues/1505
-      create-archive:
-        - sh -exc "curl -L -O https://github.com/cockpit-project/cockpit/releases/download/${PACKIT_PROJECT_VERSION}/${PACKIT_PROJECT_NAME_VERSION}.tar.xz"
-        - sh -exc "ls ${PACKIT_PROJECT_NAME_VERSION}.tar.xz"
+      fix-spec-file:
+        - 'sh -exc "curl -L --fail -O https://github.com/cockpit-project/cockpit/releases/download/${PACKIT_PROJECT_VERSION}/${PACKIT_PROJECT_NAME_VERSION}.tar.xz"'
+        - 'sh -exc "curl -L --fail -O https://github.com/cockpit-project/cockpit/releases/download/${PACKIT_PROJECT_VERSION}/cockpit-node-${PACKIT_PROJECT_VERSION}.tar.xz"'
 
   - job: propose_downstream
     trigger: release

tools/make-rpms
@@ -56,7 +56,8 @@ if [ $# -gt 1 ]; then
 fi
 
 if [ -z "${1-}" ]; then
-    # only use the main release tarball for now
+    # we specify the main tarball that contains the spec file;
+    # the others (-node) have to be next to it in the same directory
     source="$("$base/tools/make-dist" | head -n1)"
 else
     source="$1"

commit c4882eaba40c25dfae5cdf822930feeab27f806b

test: Fix devel scenario
Commit a30e928bdb9bd94 broke the /devel scenario, as we always
rebuilt the bundle during RPM build. Change the spec to accept a
pre-defined `%{rebuild_bundle}`, and pass that from test/image-prepare.

tools/cockpit.spec
@@ -52,8 +52,9 @@
 %endif
 
 # distributions which ship nodejs-esbuild can rebuild the bundle during package build
+# allow override from command line (e.g. for development builds)
 %if 0%{?fedora} >= 42
-%define rebuild_bundle 1
+%{!?rebuild_bundle: %define rebuild_bundle 1}
 %endif
 
 # to avoid using asciidoc-py in RHEL and CentOS we use the prebuilt docs
@@ -116,7 +117,7 @@ BuildRequires: openssh-clients
 BuildRequires: krb5-server
 BuildRequires: gdb
 
-%if %{defined rebuild_bundle}
+%if 0%{?rebuild_bundle}
 BuildRequires: nodejs
 BuildRequires: nodejs-esbuild
 %endif
@@ -168,12 +169,12 @@ BuildRequires:  python3-pytest-timeout
 
 %prep
 %setup -q -n cockpit-%{version}
-%if %{defined rebuild_bundle}
+%if 0%{?rebuild_bundle}
 %setup -q -D -T -a 1 -n cockpit-%{version}
 %endif
 
 %build
-%if %{defined rebuild_bundle}
+%if 0%{?rebuild_bundle}
 rm -rf dist
 # HACK: node module packaging is currently broken in Fedora ≤ 43, should be in
 # common location, not major version specific one

test/image-prepare
@@ -278,6 +278,10 @@ def main() -> int:
 
     dist_tars = subprocess.check_output([f'{BASE_DIR}/tools/make-dist'], text=True).strip().splitlines()
 
+    # Don't rebuild bundle if built with NODE_ENV=development
+    if os.environ.get('NODE_ENV') == 'development':
+        customize += ["--build-options", '--define "rebuild_bundle 0"']
+
     if args.image == "fedora-coreos":
         customize += build_install_ostree(dist_tars, args.image, verbose=args.verbose, quick=args.quick)
     elif args.image.endswith("-bootc"):
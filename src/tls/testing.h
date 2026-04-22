bb60eac93ff78f34e40db54d5d2ef9a57d269c42
Martin Pitt <mpitt@redhat.com>
2026-04-22 05:11:07 +0200

    tls: Update testing.h when regenerating test certificates
    This wasn't obvious nor documented.

--- src/tls/testing.h
+++ src/tls/testing.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+/* Fingerprints for src/tls/ca/. Updated by generate.sh. */
+
 #define CLIENT_CERT_FINGERPRINT "64b541b74b5c45cac5842677ff22619c0bed9b2bf8e91f65b1379141df0cba17"
 #define CLIENT_EXPIRED_FINGERPRINT "76f9961be798030c7f057601044194113ebd9df5496e2a97e4fea5bd51e7db88"
 #define ALTERNATE_FINGERPRINT "8206a4f89a4ed1e734190a13c0b6a403b01ace0599f4bcb291884d000a5c9ec7"

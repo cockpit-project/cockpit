1cdf8faea894a29152ead4d06857d5c08b37c0cf
Martin Pitt <mpitt@redhat.com>
2026-04-23 15:30:13 +0200

    tls: Robustify fd initialization in request_dynamic_wsinstance
    This makes it crystal clear that `fd` is never uninitialized.

--- src/tls/connection.c
+++ src/tls/connection.c
@@ -369,11 +369,10 @@ request_dynamic_wsinstance (const char *fingerprint)
 {
   bool status = false;
   char reply[20];
-  int fd;
 
   debug (CONNECTION, "requesting dynamic wsinstance for %s:\n", fingerprint);
 
-  fd = socket (AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
+  int fd = socket (AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
   if (fd == -1)
     {
       warn ("socket() failed");

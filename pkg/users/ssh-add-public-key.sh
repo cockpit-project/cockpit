c3eac4c08c28c25faa58c52ca2ffdb7b4a3cddf2
Jelle van der Waa <jvanderwaa@redhat.com>
2026-04-29 09:33:34 +0200

    users: assign arguments to proper variables
    This makes the script easier to read, its not very complicated to see
    what is going on but the renaming doesn't hurt.

--- pkg/users/ssh-add-public-key.sh
+++ pkg/users/ssh-add-public-key.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-set -euf
-mkdir -p "$2/.ssh"
-cd "$2/.ssh"
+user_name="$1"
+home_dir="$2"
 
-chown $1 .
+set -euf
+mkdir -p "$home_dir/.ssh"
+cd "$home_dir/.ssh"
+
+chown $user_name .
 
 touch authorized_keys 2> /dev/null || true
-chown $1 authorized_keys 2> /dev/null || true
+chown $user_name authorized_keys 2> /dev/null || true
 
 sed -i -e '$a\' authorized_keys
 cat >> authorized_keys
 
-chown $1 authorized_keys 2> /dev/null || true
+chown $user_name authorized_keys 2> /dev/null || true
 chmod 600 authorized_keys

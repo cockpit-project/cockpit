9f9cdbd08c0060b2262988db7bbe2527776cfbc1
Marius Vollmer <marius.vollmer@gmail.com>
2026-05-08 09:51:03 +0300

    storage: Fix call to stratis3.Manager SetKey to use correct signature
    We have been calling it with 'shb' since forever, while it really
    should have been 'sh' all along.  I think this mistake happened when
    creating stratis3-set-key as a copy of stratis2-set-key.
    
    Versions of stratisd before 3.9 would accept the wrong signature and
    ignore the extra 'b' argument, but 3.9 is now rejecting it.

--- pkg/storaged/stratis/stratis3-set-key.py
+++ pkg/storaged/stratis/stratis3-set-key.py
@@ -5,7 +5,7 @@ import dbus
 
 reply = dbus.SystemBus().call_blocking('org.storage.stratis3', '/org/storage/stratis3',
                                        'org.storage.stratis3.Manager.r0', 'SetKey',
-                                       'shb', [sys.argv[1], 0, False])
+                                       'sh', [sys.argv[1], 0])
 if reply[1] != 0:
     sys.stderr.write(reply[2] + '\n')
     sys.exit(1)

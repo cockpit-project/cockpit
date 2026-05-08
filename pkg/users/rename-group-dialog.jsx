b1b18b9255fc297e14a3fcbcc600a035f65f4b2f
Jelle van der Waa <jvanderwaa@redhat.com>
2026-05-08 11:06:58 +0200

    users: Validate group name when renaming
    We already validate the group name on creation but not when renaming.

--- pkg/users/rename-group-dialog.jsx
+++ pkg/users/rename-group-dialog.jsx
@@ -10,16 +10,21 @@ import { HelperText, HelperTextItem } from "@patternfly/react-core/dist/esm/comp
 import { TextInput } from "@patternfly/react-core/dist/esm/components/TextInput/index.js";
 
 import { apply_modal_dialog, show_modal_dialog } from "cockpit-components-dialog.jsx";
+import { FormHelper } from "cockpit-components-form-helper";
+import { has_errors, is_valid_char_name } from "./dialog-utils.js";
 
 const _ = cockpit.gettext;
 
-function RenameGroupDialogBody({ state, change }) {
+function RenameGroupDialogBody({ state, errors, change }) {
     const { name } = state;
 
     return (
         <Form isHorizontal onSubmit={apply_modal_dialog}>
             <FormGroup fieldId="group-name" label={_("New name")}>
-                <TextInput id="group-name" onChange={(_event, val) => change("name", val)} value={name} />
+                <TextInput id="group-name"
+                           validated={(errors?.name) ? "error" : "default"}
+                           onChange={(_event, val) => change("name", val)} value={name} />
+                <FormHelper fieldId="group-name" helperTextInvalid={errors?.name} />
             </FormGroup>
             <HelperText>
                 <HelperTextItem variant="warning">{_("Renaming a group may affect sudo and similar rules")}</HelperTextItem>
@@ -28,15 +33,29 @@ function RenameGroupDialogBody({ state, change }) {
     );
 }
 
+function validate_name(name) {
+    if (!name)
+        return _("Group name cannot be empty");
+
+    for (let i = 0; i < name.length; i++) {
+        if (!is_valid_char_name(name[i]))
+            return _("The group name can only consist of letters from a-z, digits, dots, dashes and underscores");
+    }
+
+    return null;
+}
+
 export function rename_group_dialog(group) {
     let dlg = null;
 
     const state = {
         name: group
     };
+    let errors = { };
 
     function change(field, value) {
         state[field] = value;
+        errors = { };
         update();
     }
 
@@ -44,7 +63,7 @@ export function rename_group_dialog(group) {
         const props = {
             id: "group-confirm-rename-dialog",
             title: cockpit.format(_("Rename group $0"), group),
-            body: <RenameGroupDialogBody state={state} change={change} />,
+            body: <RenameGroupDialogBody state={state} errors={errors} change={change} />,
             variant: 'small',
         };
 
@@ -53,7 +72,14 @@ export function rename_group_dialog(group) {
                 {
                     caption: _("Rename"),
                     style: "primary",
-                    clicked: () => cockpit.spawn(["groupmod", group, "--new-name", state.name], { superuser: "require", err: "message" }).then(dlg.close)
+                    clicked: () => {
+                        errors.name = validate_name(state.name);
+                        if (has_errors(errors)) {
+                            update();
+                            return Promise.reject();
+                        }
+                        return cockpit.spawn(["groupmod", group, "--new-name", state.name], { superuser: "require", err: "message" }).then(dlg.close);
+                    }
                 }
             ]
         };

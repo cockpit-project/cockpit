2e2d125b6d4c45344d6586fa1f28f2ea619efabd
Josephine Pfeiffer <hi@josie.lol>
2026-06-16 15:40:33 +0200

    playground: Demo notification board publish and subscribe
    Signed-off-by: Josephine Pfeiffer <hi@josie.lol>

--- pkg/playground/notifications-receiver.js
+++ pkg/playground/notifications-receiver.js
@@ -1,25 +1,30 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-import { page_status } from "notifications";
+import cockpit from "cockpit";
+import { board } from "_internal/notifications";
+
+const demo_board = board("playground:demo");
 
 function id(sel) {
     return document.getElementById(sel);
 }
 
 function update() {
-    const status = page_status.get("playground");
+    const status = demo_board.list()[0];
 
     if (status) {
-        id("received-type").innerText = status.type;
-        id("received-title").innerText = status.title;
-    } else if (status !== undefined) {
+        id("received-type").innerText = status.type ?? "-";
+        id("received-title").innerText = status.title ?? "-";
+    } else {
         id("received-type").innerText = "-";
         id("received-title").innerText = "-";
     }
 }
 
 function init () {
-    page_status.addEventListener("changed", update);
+    demo_board.addEventListener("changed", update);
     update();
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", () => {
+    cockpit.transport.wait(init);
+});

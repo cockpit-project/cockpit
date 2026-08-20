cockpit-project/cockpit

commit 5516e7af36f6926423bf724019494c9b08afd53f

ws: Translate login page
Reviewed-by: Dominik Perpeet <dperpeet@redhat.com>

src/ws/cockpithandlers.c
@@ -326,7 +326,8 @@ build_environment (GHashTable *os_release)
 
 static void
 send_login_html (CockpitWebResponse *response,
-                 CockpitHandlerData *ws)
+                 CockpitHandlerData *ws,
+                 GHashTable *headers)
 {
   static const gchar *marker = "<head>";
 
@@ -339,6 +340,11 @@ send_login_html (CockpitWebResponse *response,
   CockpitWebFilter *filter2 = NULL;
   const gchar *url_root = NULL;
   gchar *base;
+
+  gchar *language = NULL;
+  gchar **languages = NULL;
+  GBytes *po_bytes;
+  CockpitWebFilter *filter3 = NULL;
 
   environment = build_environment (ws->os_release);
   filter = cockpit_web_inject_new (marker, environment, 1);
@@ -359,6 +365,30 @@ send_login_html (CockpitWebResponse *response,
   g_object_unref (filter2);
 
   cockpit_web_response_set_cache_type (response, COCKPIT_WEB_RESPONSE_NO_CACHE);
+
+  if (ws->login_po_html)
+    {
+      language = cockpit_web_server_parse_cookie (headers, "CockpitLang");
+      if (!language)
+        {
+          languages = cockpit_web_server_parse_languages (headers, NULL);
+          language = languages[0];
+        }
+
+      po_bytes = cockpit_web_response_negotiation (ws->login_po_html, NULL, language, NULL, &error);
+      if (error)
+        {
+          g_message ("%s", error->message);
+          g_clear_error (&error);
+        }
+      else
+        {
+          filter3 = cockpit_web_inject_new (marker, po_bytes, 1);
+          g_bytes_unref (po_bytes);
+          cockpit_web_response_add_filter (response, filter3);
+          g_object_unref (filter3);
+        }
+    }
 
   bytes = cockpit_web_response_negotiation (ws->login_html, NULL, NULL, NULL, &error);
   if (error)
@@ -383,6 +413,8 @@ send_login_html (CockpitWebResponse *response,
 
       g_bytes_unref (bytes);
     }
+
+  g_strfreev (languages);
 }
 
 static void
@@ -495,7 +527,7 @@ handle_resource (CockpitHandlerData *data,
         }
       else if (g_str_has_suffix (path, ".html"))
         {
-          send_login_html (response, data);
+          send_login_html (response, data, headers);
         }
       else
         {
@@ -545,7 +577,7 @@ handle_shell (CockpitHandlerData *data,
     }
   else
     {
-      send_login_html (response, data);
+      send_login_html (response, data, headers);
     }
 }
 

src/ws/Makefile-ws.am
@@ -1,8 +1,10 @@
 
 COCKPIT_BUILD_INFO = "Built on $(shell date)"
 
+LOGIN_PO_FILES = $(patsubst %,dist/static/login.po.%.html,$(LINGUAS))
+
 staticdir = $(datadir)/cockpit/static
-nodist_static_DATA = dist/static/login.min.html
+nodist_static_DATA = dist/static/login.min.html dist/static/login.po.html $(LOGIN_PO_FILES)
 
 staticdebugdir = $(debugdir)$(staticdir)
 staticdebug_DATA = dist/static/login.html
@@ -27,6 +29,14 @@ dist/static/login.min.html: src/ws/login.html dist/static/login.min.js dist/stat
 	    -e '/<style>/ r dist/static/login.min.css' $< > $@.tmp && \
 	$(MV) $@.tmp $@
 
+dist/static/login.po.html: src/ws/po.empty.html
+	$(COPY_RULE)
+
+dist/static/login.po.%.html: src/ws/%.po src/ws/po.empty.html
+	$(AM_V_GEN) $(MKDIR_P) $(dir $@) && \
+	$(srcdir)/tools/missing $(srcdir)/po/po2json -m $(srcdir)/src/ws/po.empty.html -o $@.tmp src/ws/$*.po && \
+	$(MV) $@.tmp $@
+
 staticfontsdir = $(staticdir)/fonts
 staticfonts_DATA = $(OPENSANS_FONTS)
 
@@ -49,6 +59,8 @@ EXTRA_DIST += \
 	src/ws/login.html \
 	src/ws/login.js \
 	src/ws/login.css \
+	src/ws/po.empty.html \
+	$(LOGIN_PO_FILES) \
 	$(staticfonts_DATA) \
 	$(NULL)
 

src/ws/cockpithandlers.h
@@ -30,6 +30,7 @@ extern const gchar *cockpit_ws_shell_component;
 typedef struct {
   CockpitAuth *auth;
   const gchar *login_html;
+  const gchar *login_po_html;
   const gchar **branding_roots;
   GHashTable *os_release;
 } CockpitHandlerData;

src/ws/login.html
@@ -2,7 +2,6 @@
 <html>
   <head>
     <title>Loading...</title>
-    <base href="/">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>

src/ws/login.js
@@ -14,6 +14,31 @@ var phantom_checkpoint = phantom_checkpoint || function () { };
             oauth.ErrorParam = "error_description";
     }
 
+    var fmt_re = /\$\{([^}]+)\}|\$([a-zA-Z0-9_]+)/g;
+    function format(fmt /* ... */) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return fmt.replace(fmt_re, function(m, x, y) { return args[x || y] || ""; });
+    }
+
+    function gettext(key) {
+        if (window.cockpit_po) {
+            var translated = window.cockpit_po[key];
+            if (translated && translated[1])
+                return translated[1];
+        }
+        return key;
+    }
+
+    function translate() {
+        if (!document.querySelectorAll)
+            return;
+        var list = document.querySelectorAll("[translate]");
+        for (var i = 0; i < list.length; i++)
+            list[i].textContent = gettext(list[i].textContent);
+    }
+
+    var _ = gettext;
+
     var login_path, application, org_login_path, org_application;
     var qs_re = /[?&]?([^=]+)=([^&]*)/g;
     var oauth_redirect_to = null;
@@ -177,6 +202,8 @@ var phantom_checkpoint = phantom_checkpoint || function () { };
 
     function boot() {
         window.onload = null;
+
+        translate();
 
         setup_path_globals (window.location.pathname);
 

src/ws/main.c
@@ -108,6 +108,7 @@ main (int argc,
   gchar *cert_path = NULL;
   GMainLoop *loop = NULL;
   gchar *login_html = NULL;
+  gchar *login_po_html = NULL;
 
   signal (SIGPIPE, SIG_IGN);
   g_setenv ("GSETTINGS_BACKEND", "memory", TRUE);
@@ -163,6 +164,8 @@ main (int argc,
   data.branding_roots = (const gchar **)roots;
   login_html = g_strdup (DATADIR "/cockpit/static/login.html");
   data.login_html = (const gchar *)login_html;
+  login_po_html = g_strdup (DATADIR "/cockpit/static/login.po.html");
+  data.login_po_html = (const gchar *)login_po_html;
 
   server = cockpit_web_server_new (opt_address,
                                    opt_port,
@@ -234,6 +237,7 @@ out:
   g_clear_object (&certificate);
   g_free (cert_path);
   g_strfreev (roots);
+  g_free (login_po_html);
   g_free (login_html);
   g_free (opt_address);
   cockpit_conf_cleanup ();

src/ws/po.empty.html
@@ -1,0 +1,6 @@
+<script>
+(function (root, data) {
+    window.cockpit_po = data;
+/* The syntax of this line is important for po2json */
+}(this, {"":{"language":"en"}}));
+</script>

src/ws/test-handlers.c
@@ -107,6 +107,7 @@ base_setup (Test *test)
   test->data.auth = test->auth;
   test->data.branding_roots = (const gchar **)test->roots;
   test->data.login_html = (const gchar *)test->login_html;
+  test->data.login_po_html = NULL;
 
   test->headers = cockpit_web_server_new_table ();
 

commit 4dfa2a2ef69aa13e6b7ac9f48cd69e91c5618edd

ws: Fix memory leak of the CockpitLang cookie value in the login page
send_login_html() takes the language from the "CockpitLang" cookie, which
cockpit_web_server_parse_cookie() returns as a newly allocated string.  That
string was never freed: the function only releases "languages", the string
vector used for the "Accept-Language" fallback path.

Every unauthenticated request that carries a CockpitLang cookie (a plain
GET / on the login page is enough) therefore leaks one heap allocation whose
size is attacker controlled, up to the request header size limit.  A remote
unauthenticated attacker can repeat this to exhaust the memory of the host
and cause a denial of service.

Keep "language" a borrowed pointer and give the cookie value an owner:
"language_cookie" for the cookie path, "languages" for the Accept-Language
path, so both are released on return.

The leak was introduced in 5516e7af3 ("ws: Translate login page").

Fixes: CVE-2026-76235

src/ws/cockpithandlers.c
@@ -399,7 +399,10 @@ send_login_html (CockpitWebResponse *response,
   gchar *cookie_line = NULL;
   gchar *base;
 
-  gchar *language = NULL;
+  /* "language" is a borrowed pointer, owned by either "language_cookie"
+   * or "languages" below */
+  const gchar *language = NULL;
+  g_autofree gchar *language_cookie = NULL;
   gchar **languages = NULL;
   GBytes *po_bytes;
   CockpitWebFilter *filter3 = NULL;
@@ -426,8 +429,12 @@ send_login_html (CockpitWebResponse *response,
 
   if (ws->login_po_js)
     {
-      language = cockpit_web_server_parse_cookie (headers, "CockpitLang");
-      if (!language)
+      language_cookie = cockpit_web_server_parse_cookie (headers, "CockpitLang");
+      if (language_cookie)
+        {
+          language = language_cookie;
+        }
+      else
         {
           accept = g_hash_table_lookup (headers, "Accept-Language");
           languages = cockpit_web_server_parse_accept_list (accept, NULL);
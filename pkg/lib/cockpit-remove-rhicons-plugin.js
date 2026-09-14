c90d7cb4ecd565323a4bc9f836a85528aab75fa2
Jelle van der Waa <jvanderwaa@redhat.com>
2026-09-14 20:56:26 +0200

    lib: Use async I/O APIs in RemoveRHIconsPlugin
    This function is async so use asynchronous I/O functions.

--- pkg/lib/cockpit-remove-rhicons-plugin.js
+++ pkg/lib/cockpit-remove-rhicons-plugin.js
@@ -6,9 +6,8 @@ export const cockpitRemoveRHIconsPlugin = () => ({
     name: 'cockpitRemoveRHIconsPlugin',
     setup(build) {
         build.onLoad({ filter: /-icon\.js$/ }, async ({ path }) => {
-            let contents = fs.readFileSync(path, 'utf-8');
-            contents = contents.replace(/rhUiIcon: {.*}/g, "undefined");
-            return { contents };
+            const contents = await fs.promises.readFile(path, 'utf-8');
+            return { contents: contents.replace(/rhUiIcon: {.*}/g, "undefined") };
         });
     }
 });

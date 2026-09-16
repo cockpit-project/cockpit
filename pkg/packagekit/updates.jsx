68fd7bda0231c046fb238f35c0fedec81e0c7a07
Jelle van der Waa <jvanderwaa@redhat.com>
2026-09-16 15:33:43 +0200

    packagekit: Initialize bug/cve/vendor urls
    Simplify the code by creating new variables for urls and use them
    consistently through the code. In a follow up commit we will make use of
    this to filter bad/malicious urls.
    
    Backported from: 86f93e3393c588118f2a0ea3af368ab08a281a2f

--- pkg/packagekit/updates.jsx
+++ pkg/packagekit/updates.jsx
@@ -227,7 +227,7 @@ function getPageStatusSeverityIcon(severity) {
 }
 
 function getSeverityURL(urls) {
-    if (!urls)
+    if (urls.length === 0)
         return null;
 
     // in ascending severity
@@ -280,10 +280,14 @@ function customRemarkable() {
 }
 
 function updateItem(remarkable, info, pkgNames, key) {
+    const bug_urls = info.bug_urls || [];
+    const cve_urls = info.cve_urls || [];
+    const vendor_urls = info.vendor_urls || [];
+
     let bugs = null;
-    if (info.bug_urls && info.bug_urls.length) {
+    if (bug_urls.length > 0) {
         // we assume a bug URL ends with a number; if not, show the complete URL
-        bugs = insertCommas(info.bug_urls.map(url => (
+        bugs = insertCommas(bug_urls.map(url => (
             <a key={url} rel="noopener noreferrer" target="_blank" href={url}>
                 {url.match(/[0-9]+$/) || url}
             </a>)
@@ -291,8 +295,8 @@ function updateItem(remarkable, info, pkgNames, key) {
     }
 
     let cves = null;
-    if (info.cve_urls && info.cve_urls.length) {
-        cves = insertCommas(info.cve_urls.map(url => (
+    if (cve_urls.length > 0) {
+        cves = insertCommas(cve_urls.map(url => (
             <a key={url} href={url} rel="noopener noreferrer" target="_blank">
                 {url.match(/[^/=]+$/)}
             </a>)
@@ -300,8 +304,8 @@ function updateItem(remarkable, info, pkgNames, key) {
     }
 
     let errata = null;
-    if (info.vendor_urls) {
-        errata = insertCommas(info.vendor_urls.filter(url => url.indexOf("/errata/") > 0).map(url => (
+    if (vendor_urls.length > 0) {
+        errata = insertCommas(vendor_urls.filter(url => url.indexOf("/errata/") > 0).map(url => (
             <a key={url} href={url} rel="noopener noreferrer" target="_blank">
                 {url.match(/[^/=]+$/)}
             </a>)
@@ -310,7 +314,7 @@ function updateItem(remarkable, info, pkgNames, key) {
             errata = null; // simpler testing below
     }
 
-    let secSeverityURL = getSeverityURL(info.vendor_urls);
+    let secSeverityURL = getSeverityURL(vendor_urls);
     const secSeverity = secSeverityURL ? secSeverityURL.slice(secSeverityURL.indexOf("#") + 1) : null;
     const icon = getSeverityIcon(info.severity, secSeverity);
     let type;
@@ -322,7 +326,7 @@ function updateItem(remarkable, info, pkgNames, key) {
                 <Tooltip id="tip-severity" content={ secSeverity || _("security") }>
                     <span>
                         {icon}
-                        { (info.cve_urls && info.cve_urls.length > 0) ? info.cve_urls.length : "" }
+                        { cve_urls.length > 0 && cve_urls.length }
                     </span>
                 </Tooltip>
             </>);
@@ -332,7 +336,7 @@ function updateItem(remarkable, info, pkgNames, key) {
             <Tooltip id="tip-severity" content={tip}>
                 <span>
                     {icon}
-                    { bugs ? info.bug_urls.length : "" }
+                    { bug_urls.length > 0 && bug_urls.length }
                 </span>
             </Tooltip>
         );
